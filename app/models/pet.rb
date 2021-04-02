422e79f8f8bf6d3c5cef2faeac3d781d00e58d15
austinandrade <austinmandrade@gmail.com>
2021-04-02 17:31:07 -0400

    Add adopting applications association and destroy dependency

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,7 +3,7 @@ class Pet < ApplicationRecord
   validates :age, presence: true, numericality: true
   belongs_to :shelter
 
-  has_many :applications, dependent: :destroy
+  has_many :adopting_applications, dependent: :destroy
 
   def shelter_name
     shelter.name
