2d007f04c97c5742ccd31ea2a2a09e829e31c5e9
Wyattwicks <wyattwicks@gmail.com>
2021-04-02 16:35:44 -0600

    Add initial set up

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationsController < ApplicationController
+  def index
+    @applications = Applications.all
+  end
+
+  def show
+    @applications = Application.find(params[:id])
+  end
+end
