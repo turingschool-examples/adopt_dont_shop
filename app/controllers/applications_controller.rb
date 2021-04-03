4bbc47c2051493a20e641a881b1c4f3d8b011353
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-03 18:26:38 -0400

    Complete create spec for applications

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,6 +7,21 @@ class ApplicationsController < ApplicationController
   end
 
   def new
-    @application = Application.new 
+    @application = Application.new
+  end
+
+  def create
+    # binding.pry
+
+    application = Application.create!({
+      name: params[:name],
+      street_address: params[:street_address],
+      city: params[:city],
+      state: params[:state],
+      zip_code: params[:zip_code],
+      description: params[:description]
+      })
+
+    redirect_to "/applications/#{application.id}"
   end
 end
--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'application creation' do
   before(:each) do
-    applicant = Application.create!(name: "Fred",
+    @applicant = Application.create!(name: "Fred",
                               street_address: "123 way",
                               city: "Denver",
                               state: "Colorado",
@@ -24,8 +24,39 @@ RSpec.describe 'application creation' do
       expect(find('form')).to have_content('City')
       expect(find('form')).to have_content('State')
       expect(find('form')).to have_content('Zip code')
-      # expect(find('form')).to have_content('Description')
+      expect(find('form')).to have_content('Description')
       # expect(find('form')).to have_content('Status')
+    end
+
+    describe 'the application create' do
+      context 'given valid data' do
+        it 'creates the application and redirects to the application show page' do
+          @applicant1 = Application.create!(name: "Klaud",
+                                    street_address: "123 way",
+                                    city: "Denver",
+                                    state: "Colorado",
+                                    zip_code: 80204,
+                                    description: "I like dogs",
+                                    status: "In Progress",
+                                    created_at: Time.now,
+                                    updated_at: Time.now
+                                    )
+
+          visit "/applications/new"
+
+          fill_in 'Name', with: '#{@applicant1.name}'
+          fill_in 'Street address', with: '#{@applicant1.street_address}'
+          fill_in 'City', with: '#{@applicant1.city}'
+          fill_in 'State', with: '#{@applicant1.state}'
+          fill_in 'Zip code', with: '#{@applicant1.zip_code}'
+          fill_in 'Description', with: '#{@applicant1.description}'
+          click_button 'Save'
+          
+          expect(page).to have_current_path("/applications/#{@applicant1.id}")
+          expect(page).to have_content('#{@applicant1.name}')
+        end
+      end
+
     end
   end
 end
