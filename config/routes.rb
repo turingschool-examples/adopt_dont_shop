c9d94492f5cd06a8b328a0973d342f4b313ae09c
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-03 14:55:27 -0400

    Move around gets for applications

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,10 @@ Rails.application.routes.draw do
   get '/pets/:id/edit', to: 'pets#edit'
   patch '/pets/:id', to: 'pets#update'
   delete '/pets/:id', to: 'pets#destroy'
-  
+
   get '/applications', to: 'applications#index' #don't necessarily need a view for index
-  get '/applications/:id', to: 'applications#show'
   get '/applications/new', to: 'applications#new'
+  get '/applications/:id', to: 'applications#show'
   post '/applications', to: 'applications#create'
 
   get '/veterinary_offices', to: 'veterinary_offices#index'
