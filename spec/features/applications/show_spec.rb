87f9305c0cc96abe055eb61a5d91f81a5ed57b70
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-03 14:25:43 -0400

    Update applications show page feature tests

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,46 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "the Application show page" do
   it "should display applicant info" do
-    # pound = Shelter.create!(foster_program: true,
-    #                         name: "da pound",
-    #                         city: "Denver",
-    #                         rank: 1,
-    #                         created_at: Time.now,
-    #                         updated_at: Time.now
-    #                         )
-    #   dog = pound.pets.create!(name: "Bob",
-    #                           adoptable: true,
-    #                           age: 1,
-    #                           breed: "smol",
-    #                           created_at: Time.now,
-    #                           updated_at: Time.now
-    #                           )
-    # dog = Pet.create!(name: "Bob",
-    #                   adoptable: true,
-    #                   age: 1,
-    #                   breed: "smol",
-    #                   created_at: Time.now,
-    #                   updated_at: Time.now)
-    # applicant = dog.applications.create!(name: "Fred",
-    #                                 street_address: "123 way",
-    #                                 city: "Denver",
-    #                                 state: "Colorado",
-    #                                 zip_code: 80204,
-    #                                 description: "I like dogs",
-    #                                 created_at: Time.now,
-    #                                 updated_at: Time.now
-    #                                 )
     applicant = Application.create!(name: "Fred",
                               street_address: "123 way",
                               city: "Denver",
                               state: "Colorado",
                               zip_code: 80204,
                               description: "I like dogs",
+                              status: "In Progress",
                               created_at: Time.now,
                               updated_at: Time.now
                               )
-    # ApplicationPet.create!(application: applicant, pet: dog)
-
     visit "/applications/#{applicant.id}"
 
     expect(page).to have_content(applicant.name)
@@ -50,7 +20,24 @@ RSpec.describe "the Application show page" do
     expect(page).to have_content(applicant.state)
     expect(page).to have_content(applicant.zip_code)
     expect(page).to have_content(applicant.description)
-    expect(page).to have_content(applicant.created_at)
-    expect(page).to have_content(applicant.updated_at)
+  end
+
+  it "shows links that can be clicked on" do
+    applicant = Application.create!(name: "Fred",
+                              street_address: "123 way",
+                              city: "Denver",
+                              state: "Colorado",
+                              zip_code: 80204,
+                              description: "I like dogs",
+                              status: "In Progress",
+                              created_at: Time.now,
+                              updated_at: Time.now
+                              )
+    visit "/applications/#{applicant.id}"
+
+    click_link "Pets"
+    click_link "Shelters"
+    click_link "Veterinarians"
+    click_link "Veterinary Offices"
   end
 end
