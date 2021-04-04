dec684ecbdd83da8997d00f79ff327ae525220c3
austinandrade <austinmandrade@gmail.com>
2021-04-03 23:38:51 -0400

    Delete old adopting_applications routes

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,4 @@ Rails.application.routes.draw do
   get '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinary_offices#veterinarians'
   get '/veterinary_offices/:veterinary_office_id/veterinarians/new', to: 'veterinarians#new'
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
-
-  get '/adopting_applications/:id', to: 'adopting_applications#show'
-  get '/adopting_applications/new', to: 'adopting_applications#new'
 end
