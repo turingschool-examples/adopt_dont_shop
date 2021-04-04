1364d069defa24b7be157a9f7bb70e2e8b48369f
joemecha <jwmecha@gmail.com>
2021-04-03 22:01:20 -0500

    Add search pets to application show page in progress

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'the application show' do
   before :each do
     @foothills_shelter = Shelter.create!(name: 'Foothills Animal Shelter', city: 'Golden, CO', foster_program: true, rank: 4)
-    @ramone = @foothills_shelter.pets.create!(name: 'Ramone', breed: 'hound', age: 2, adoptable: true)
-    @lawson = @foothills_shelter.pets.create!(name: 'Lawson', breed: 'Australian shepherd', age: 6, adoptable: true)
+    @rumba = @foothills_shelter.pets.create!(name: 'Rumba', breed: 'hound', age: 2, adoptable: true)
+    @rug = @foothills_shelter.pets.create!(name: 'Rug', breed: 'Australian shepherd', age: 6, adoptable: true)
     @petunia = @foothills_shelter.pets.create!(name: 'Petunia', breed: 'chihuahua', age: 1, adoptable: true)
 
     @hayley_williams = Application.create!(applicant_name: 'Hayley Williams',
@@ -12,25 +12,25 @@ RSpec.describe 'the application show' do
       city: "Golden",
       state: "CO",
       zip_code: "80123",
-      statement: "I sing to my pets",
+      statement: " ",
       status: "In Progress"
       )
-    @hayley_williams.pets << @petunia
+    # @hayley_williams.pets << @petunia
 
     @jeff_rosenstock = Application.create!(applicant_name: 'Jeff Rosenstock',
       street: "456 Mississippi Ave.",
       city: "Denver",
       state: "CO",
       zip_code: "80012",
-      statement: "I have a fenced in yard",
-      status: "pending"
+      statement: " ",
+      status: "In Progress"
       )
-    @jeff_rosenstock.pets << @lawson
+    # @jeff_rosenstock.pets << @lawson
 
     visit "/applications/#{@hayley_williams.id}"
   end
-  it "shows the application and all of it's attributes" do
 
+  it "shows the application and all of it's attributes" do
     expect(page).to have_content(@hayley_williams.applicant_name)
     expect(page).to have_content(@hayley_williams.street)
     expect(page).to have_content(@hayley_williams.city)
@@ -41,11 +41,54 @@ RSpec.describe 'the application show' do
     expect(page).to_not have_content(@jeff_rosenstock.applicant_name)
   end
 
-  # Description of why the applicant says they'd be a good home for the pet(s)
-  # names of all pets that this application is for (all names of pets should be links to their show page)
-
   # The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
   it "has a status of 'In Progress', 'Pending', 'Accepted', or 'Rejected'" do
     expect(@hayley_williams.status).to eq("In Progress")
   end
-end
+
+  it "When I visit an application's show page and that application has not been\
+    submitted, then I see a section on the page to 'Add a Pet to this\
+    Application." do
+
+    expect(page).to have_content("Add a Pet to this Application")
+  end
+
+  it 'has a text box where I can search for pets by name' do
+    expect(page).to have_button("Search by Pet name")
+  end
+
+  it "Fill in search field with a Pet's name and click submit, then I am taken\
+   back to the application show page. And under the search bar I see any Pet\
+   whose name matches my search" do
+
+   fill_in "Search by Pet name", with: "Ru"
+   click_on("Search by Pet name")
+
+   expect(page).to have_content(@rug.name)
+   expect(page).to have_content(@rumba.name)
+  end
+
+  it "next to each Pet's name I see a button to 'Adopt this Pet', when I click\
+   one of these buttons I am taken back to the application show page and\
+   I see the Pet I want to adopt listed on this application" do
+
+   end
+
+  it "Has a section to submit my application after I have added one or more\
+    pets to the application. And in that section I see an input to enter why I\
+    would make a good owner for these pet(s)" do
+  
+  end
+
+  #
+  #   it "When I fill in that input and I click a button to submit this application\
+  #     then I am taken back to the application's show page" do
+  #
+  #   end
+  #
+  #   it "And I see an indicator that the application is 'Pending'.\
+  #     And I see all the pets that I want to adopt. And I do not see a section\
+  #     to add more pets to this application" do
+  #
+  #   end
+  end
