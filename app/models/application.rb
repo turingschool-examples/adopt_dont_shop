8113dc312a672d5dfed70db09c116e9d10d17f74
joemecha <jwmecha@gmail.com>
2021-04-03 19:41:23 -0500

    Update Application controller, model, new-show-index html

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,11 +1,9 @@
 class Application < ApplicationRecord
-  # validates :applicant_name, presence: true
-  # validates :street, presence: true
-  # validates :city, presence: true
-  # validates :state, presence: true
-  # validates :zip_code, presence: true #, numericality: true
-  # validates :pet_names, presence: true
-  # validates :status, presence: true
+  validates :applicant_name, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true, numericality: true
 
   has_many :application_pets
   has_many :pets, through: :application_pets
