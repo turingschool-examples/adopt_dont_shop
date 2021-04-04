478bcaa833724de8959dd4a46af38ef8f4c8fe38
koshea9 <keegan.oshea9@gmail.com>
2021-04-04 16:27:08 -0600

    Add application show view

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,10 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+
   has_many :application_pets
   has_many :pets, through: :application_pets
 end
