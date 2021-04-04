4503c101f21d03f913592629b221032bb3dc5f61
austinandrade <austinmandrade@gmail.com>
2021-04-03 23:35:45 -0400

    Update for name spacing and proper associations

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Pet, type: :model do
   describe 'relationships' do
     it { should belong_to(:shelter) }
-    it { should have_many(:adopting_applications) }
+    it { should have_many(:applicants_pets) }
+    it {should have_many(:applicants).through(:applicants_pets)}
   end
 
   describe 'validations' do
