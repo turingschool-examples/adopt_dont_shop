f2080ac0b2f0bd0a148d4f7cbe1b653b8f252c89
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-04-03 21:31:21 -0500

    Add show page for application that shows attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Pet.destroy_all
+# Shelter.destroy_all
+# Application.destroy_all
+#
+# shelter_1 = Shelter.create!(name: "Adopt-a-Pet", city: "Denver", rank: 1, foster_program: true)
+# shelter_2 = Shelter.create!(name: "Boulder Animal Shelter", city: "Boulder", rank: 2, foster_program: false)
+#
+# dog_1 = shelter_1.pets.create!(name: "Taz", breed: "Maltese", age: 3, adoptable: true)
+# dog_2 = shelter_1.pets.create!(name: "Dusty", breed: "Golden Retriever", age: 5, adoptable: true)
+# dog_3 = shelter_2.pets.create!(name: "Bear", breed: "German Shepherd", age: 7, adoptable: true)
+# cat_1 = shelter_2.pets.create!(name: "Lucy", breed: "Gray Tabby", age: 4, adoptable: true)
+#
+# john = Application.create!(name: "John Johnson", address: "123 Main St Denver, CO 80202", pets: "Taz, Dusty")
+# bob = Application.create!(name: "Bob Baker", address: "345 2nd St Denver, CO 80206", pets: "Dusty")
+# lisa = Application.create!(name: "Lisa Lincoln", address: "678 14th Ave Boulder, CO 80301", pets: "Bear, Lucy")
+# jane = Application.create!(name: "Jane James", address: "910 Lovely Ln. Boulder, CO 80203", pets: "Bear")
