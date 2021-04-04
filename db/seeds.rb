004cffb1ea1f2cbfe311e9a91a46d9beed7ad949
austinandrade <austinmandrade@gmail.com>
2021-04-03 23:36:59 -0400

    Update seed to Create applicants and assocation through joints

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 VeterinaryOffice.destroy_all
 Shelter.destroy_all
+Applicant.destroy_all
+ApplicantsPet.destroy_all
 # Shelter.reset_pk_sequence
 # Veterinarian.reset_pk_sequence
 # Pet.reset_pk_sequence
@@ -13,6 +15,9 @@ shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', 
 vet_office_1 = VeterinaryOffice.create!(name: 'Best Vets', boarding_services: true, max_patient_capacity: 20)
 vet_office_2 = VeterinaryOffice.create!(name: 'Vets R Us', boarding_services: true, max_patient_capacity: 50)
 vet_office_3 = VeterinaryOffice.create!(name: 'We Will Save Them', boarding_services: true, max_patient_capacity: 100)
+applicant_1 = Applicant.create!(full_name: 'Mike Piz', street_address: '13214 Yeet Rd.', city: 'Cleveland', state: 'OH', zipcode: 18907)
+applicant_2 = Applicant.create!(full_name: 'Yo Lo', street_address: '95837 Tomahawk Dr.', city: 'Cincinatti', state: 'OH', zipcode: 44567)
+applicant_3 = Applicant.create!(full_name: 'Sam Smith', street_address: '34573 Board Rd.', city: 'Austin', state: 'TX', zipcode: 67875 )
 
 
 pet_1 = shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
@@ -33,19 +38,22 @@ vet_office_2.veterinarians.create!(name: 'Sarah', review_rating: 7, on_call: fal
 vet_office_3.veterinarians.create!(name: 'Joe', review_rating: 3, on_call: true, veterinary_office_id: vet_office_3.id)
 vet_office_3.veterinarians.create!(name: 'Rebecca', review_rating: 9, on_call: true, veterinary_office_id: vet_office_3.id)
 
-pet_1.adopting_applications.create(full_name: 'Mike Piz', street_address: '13214 Yeet Rd.',
-                                   city: 'Cleveland', state: 'OH', zipcode: 18907,
-                                   description: 'I like this dog. It is very fluffy. Let me have it.',
-                                   in_progress: true, accepted: false, declined: false)
-pet_2.adopting_applications.create(full_name: 'Sam Smith', street_address: '95837 Tomahawk Dr.',
-                                   city: 'Cincinatti', state: 'OH', zipcode: 44567 ,
-                                   description: 'I have been a big fan of birds since I was a child. I would like to adopt this little guy so I can have a friend.',
-                                   in_progress: false, accepted: true, declined: false)
-pet_3.adopting_applications.create(full_name: 'Yo Lo', street_address: '34573 Board Rd.',
-                                   city: 'Austin', state: 'TX', zipcode: 67875,
-                                   description: 'I have 25 cats. Another one wouldnt hurt',
-                                   in_progress: true, accepted: false, declined: false)
-pet_4.adopting_applications.create(full_name: 'Kristy Teal', street_address: '47674 Bolas St.',
-                                   city: 'San Fransisco', state: 'CA', zipcode: 18907,
-                                   description: 'I am a lover of animals and I have been ever since I was little. I would love to give this bird a home.',
-                                   in_progress: true, accepted: false, declined: false)
+
+
+
+ApplicantsPet.create(description: 'blah', in_progress: true, pending: false, accepted: false, rejected: false, pet_id: pet_1.id, applicant_id: applicant_1.id )
+ApplicantsPet.create(description: 'aer', in_progress: true, pending: false, accepted: false, rejected: false, pet_id: pet_2.id, applicant_id: applicant_2.id )
+ApplicantsPet.create(description: 'TBD', in_progress: true, pending: false, accepted: false, rejected: false, pet_id: pet_3.id, applicant_id: applicant_3.id )
+
+# pet_2.applying_applications.create(full_name: 'Sam Smith', street_address: '95837 Tomahawk Dr.',
+#                                    city: 'Cincinatti', state: 'OH', zipcode: 44567 ,
+#                                    description: 'I have been a big fan of birds since I was a child. I would like to adopt this little guy so I can have a friend.',
+#                                    in_progress: false, accepted: true, declined: false)
+# pet_3.applying_applications.create(full_name: 'Yo Lo', street_address: '34573 Board Rd.',
+#                                    city: 'Austin', state: 'TX', zipcode: 67875,
+#                                    description: 'I have 25 cats. Another one wouldnt hurt',
+#                                    in_progress: true, accepted: false, declined: false)
+# pet_4.applying_applications.create(full_name: 'Kristy Teal', street_address: '47674 Bolas St.',
+#                                    city: 'San Fransisco', state: 'CA', zipcode: 18907,
+#                                    description: 'I am a lover of animals and I have been ever since I was little. I would love to give this bird a home.',
+#                                    in_progress: true, accepted: false, declined: false)
