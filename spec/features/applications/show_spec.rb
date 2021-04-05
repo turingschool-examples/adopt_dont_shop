2b6eecb7777fd9beb58e0540f4cf2fdb75d79707
ignored-comment <info.konham@gmail.com>
2021-04-05 18:35:35 -0400

    Assert tests for user story 1

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   describe 'When I visit an applications show page' do
-    
+    it 'can show an application with attributes' do
+      application = Application.create!(
+        name: "Bob Ham",
+        street_address: "5714 Carmel Rd",
+        city: "Charlotte",
+        state: "NC",
+        zipcode: 28226,
+        description: "eh breddy gool guy",
+        status: "In Progress"
+      )
+
+      visit "/applications/#{application.id}"
+
+      expect(page).to have_content("Bob Ham")
+      expect(page).to have_content("5714 Carmel Rd")
+      expect(page).to have_content("Charlotte")
+      expect(page).to have_content("NC")
+      expect(page).to have_content("28226")
+      expect(page).to have_content("eh breddy gool guy")
+      expect(page).to have_content("In Progress")
+    end
   end
 end
