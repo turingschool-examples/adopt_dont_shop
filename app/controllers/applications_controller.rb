0a876fd2dcb7f2ae29e0cd249036ef89eba31b3d
ignored-comment <info.konham@gmail.com>
2021-04-05 19:16:39 -0400

    Complete User Story 3; random assertions failing. This one passes

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,6 +4,19 @@ class ApplicationsController < ApplicationController
   end
 
   def new
+  end
 
+  def create
+    application = Application.new(application_params)
+    if application.save
+      redirect_to "/applications/#{application.id}/"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error: #{error_message(application.errors)}"
+    end
+  end
+
+  def application_params
+    params.permit(:id, :name, :street_address, :city, :state, :zipcode, :description, :status, :pet_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'application#welcome'
 
-  get '/applications/new', to: 'applications#new', as: 'new_application'
-  get '/applications/:id', to: 'applications#show'
-
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
@@ -39,4 +36,8 @@ Rails.application.routes.draw do
   get '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinary_offices#veterinarians'
   get '/veterinary_offices/:veterinary_office_id/veterinarians/new', to: 'veterinarians#new'
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
+
+  get '/applications/new', to: 'applications#new'
+  get '/applications/:id', to: 'applications#show'
+  post '/applications/new', to: 'applications#create'
 end
