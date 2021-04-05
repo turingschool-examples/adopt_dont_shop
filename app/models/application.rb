6a2d47b14aa67c32a42441c7516034876885bbee
ignored-comment <info.konham@gmail.com>
2021-04-05 18:27:00 -0400

    Complete User Story 1 and 2

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,10 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true, numericality: true
+  validates :description, presence: true
+
   has_many :pets
 end
