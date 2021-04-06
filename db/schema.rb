5d84a00606f668876ef0945dd3019babd36f99c6
Joe Ray <jray100.jr@gmail.com>
2021-04-05 18:21:18 -0600

    db and seeds setup

--- db/schema.rb
+++ db/schema.rb
@@ -1,19 +1,26 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2021_02_13_001717) do
+ActiveRecord::Schema.define(version: 2021_04_05_235619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.string "name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.string "description"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pet_applications", force: :cascade do |t|
+    t.bigint "application_id"
+    t.bigint "pet_id"
+    t.index ["application_id"], name: "index_pet_applications_on_application_id"
+    t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.boolean "adoptable"
@@ -53,6 +60,8 @@ ActiveRecord::Schema.define(version: 2021_02_13_001717) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pet_applications", "applications"
+  add_foreign_key "pet_applications", "pets"
   add_foreign_key "pets", "shelters"
   add_foreign_key "veterinarians", "veterinary_offices"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+denver_animal_shelter = Shelter.create!(name: "Denver Animal Shelter", city: "Denver", rank: 1, foster_program: true)
+
+baxter = Pet.create!(name: "baxter", breed: "bulldog", age: 7, adoptable: true)
+ace = Pet.create!(name: "ace", breed: "siamese", age: 3, adoptable: true)
+bella = Pet.create!(name: "bella", breed: "poodle", age: 2, adoptable: true)
+fallon = Pet.create!(name: "fallon", breed: "bengal", age: 8, adoptable: true)
+
+application1 = Application.create!(name: "application1", street: "82 baker st", city: "Denver", state: "CO", zip: 80123, description: "plenty of time for care", status: "in progress")
+application2 = Application.create!(name: "application2", street: "1327 beedle dr", city: "Austin", state: "TX", zip: 78613, description: "big backyard to run around", status: "pending")
