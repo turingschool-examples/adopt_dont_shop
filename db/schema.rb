ecb500f2d0f754052a7c3b0b8075a44e7d407832
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-06 13:12:55 -0700

    reorder seeeeeds

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_04_04_234837) do
     t.string "state"
     t.integer "zip_code"
     t.string "description"
-    t.string "status"
+    t.string "status" #status needs to be in pet applications table as well
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Shelter.destroy_all
-Pet.destroy_all
+PetApplication.destroy_all
 Application.destroy_all
+Pet.destroy_all
+Shelter.destroy_all
 VeterinaryOffice.destroy_all
 Veterinarian.destroy_all
 
