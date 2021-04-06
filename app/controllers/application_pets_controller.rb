8739f1297f38ae67a7c20d5fef7a552ca52f73ae
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-05 22:19:47 -0400

    Closeout user story two branch

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -3,7 +3,6 @@ class ApplicationPetsController < ApplicationController
     pet = Pet.find(params[:pet_id])
     app = Application.find(params[:id])
     app_pet = ApplicationPet.find_or_create_by!(pet: pet, application: app)
-    binding.pry 
     redirect_to "/applications/#{app.id}"
   end
 end
