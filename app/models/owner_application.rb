0d2374ccc84af5665c988b9afb4edb631b779c42
Wyattwicks <wyattwicks@gmail.com>
2021-04-06 20:08:04 -0600

    Fix owner app model validations

--- app/models/owner_application.rb
+++ app/models/owner_application.rb
@@ -1,9 +1,7 @@
 class OwnerApplication < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
-  # validates :name, presence: true,
-  # :street_address, presence: true,
-  # :city, presence: true,
-  # :state, presence: true,
-  # :zip_code, presence: true
+  validates :name, :street_address, :city, :state, :zip_code, presence: true
+
+   
 end
