ca85c97b423cb38e411488339564ee07405f788a
Richard <rdesilvey@gmail.com>
2021-05-20 15:34:38 -0600

    Add and Update existing migrations

--- db/migrate/20210520211546_create_applicants.rb
+++ db/migrate/20210520211546_create_applicants.rb
@@ -2,6 +2,7 @@ class CreateApplicants < ActiveRecord::Migration[5.2]
   def change
     create_table :applicants do |t|
       t.string :name
+      t.belongs_to :application
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_211546) do
+ActiveRecord::Schema.define(version: 2021_05_20_212400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2021_05_20_211546) do
 
   create_table "applicants", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "applications", force: :cascade do |t|
+    t.text "description"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
