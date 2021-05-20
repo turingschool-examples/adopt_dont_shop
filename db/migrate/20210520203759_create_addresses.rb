d1378652e9993bbd6db4938c6b3046c4f3076ea8
Richard <rdesilvey@gmail.com>
2021-05-20 14:57:07 -0600

    Add Address model

---
+++ db/migrate/20210520203759_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :city
+      t.string :state
+      t.numeric :zipcode
+
+      t.timestamps
+    end
+  end
+end
