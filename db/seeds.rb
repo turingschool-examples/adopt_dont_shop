16bae840a01d5e53040655fc47ef1e5e214bb987
NoahZinter <noah.zinter@gmail.com>
2021-05-20 16:20:58 -0600

    Create Office and Vet seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,19 @@ daisy = eagle.pets.create!(name: 'Daisy', breed: 'Maine Coon Cat', age: 2, adopt
 moon = eagle.pets.create!(name: 'Moon', breed: 'Dacshund', age: 3, adoptable: true)
 chalky = eagle.pets.create!(name: 'Chalky', breed: 'Pekingese', age: 4, adoptable: true)
 chomper = eagle.pets.create!(name: 'Chomper', breed: 'Pig', age: 5, adoptable: false)
+
+hospital = VeterinaryOffice.create!(name: 'Denver Pet Hospital', max_patient_capacity:12, boarding_services: false)
+vet = VeterinaryOffice.create!(name: 'Bettes Vets', max_patient_capacity:2, boarding_services: true)
+doctor = VeterinaryOffice.create!(name: 'Doctor Paws', max_patient_capacity:7, boarding_services: true)
+
+ramesh = hospital.veterinarians.create!(name: 'Ramesh Ranganathan', review_rating: 10, on_call: false)
+nish = hospital.veterinarians.create!(name: 'Nish Kumar', review_rating: 1, on_call: true)
+aislin = hospital.veterinarians.create!(name: 'Aislin Bea', review_rating: 6, on_call: true)
+
+greg = hospital.veterinarians.create!(name: 'Greg Davies', review_rating: 4, on_call: true)
+sarah = hospital.veterinarians.create!(name: 'Sarah Pascoe', review_rating: 8, on_call: true)
+
+victoria = hospital.veterinarians.create!(name: 'Victoria Coren Mitchell', review_rating: 10, on_call: true)
+david = hospital.veterinarians.create!(name: 'David Mitchell', review_rating: 7, on_call: false)
+lolly = hospital.veterinarians.create!(name: 'Lolly Adefope', review_rating: 9, on_call: true)
+mel = hospital.veterinarians.create!(name: 'Mel Gedroic', review_rating: 2, on_call: true)
