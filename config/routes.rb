364a479d10ae510de38e7ab9d34b4827b948484c
Suzie K <suziekim.dev@gmail.com>
2021-05-20 16:21:08 -0400

    Green test for links to the new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'application#welcome'
+  post '/new', to: 'application#new'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
