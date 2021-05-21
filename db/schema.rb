72561d12a7746853cc5c6e4d454a3f991c336625
Suzie K <suziekim.dev@gmail.com>
2021-05-20 23:11:05 -0400

    Update db and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_000007) do
+ActiveRecord::Schema.define(version: 2021_05_21_015807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_05_21_000007) do
     t.integer "zip_code"
     t.string "description"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pets", force: :cascade do |t|
