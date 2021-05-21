8353c114f11bd8d6b8c6b0504b70c46f48ce1053
Suzie K <suziekim.dev@gmail.com>
2021-05-21 09:14:56 -0400

    Red test for application validations

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,16 @@
+#spec/models/application_spec
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  # describe 'relationships' do
+  #   it { should belong_to(:shelter) }
+  # end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_numericality_of(:zip_code) }
+  end
+end
