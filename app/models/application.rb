dd710d56613f626a1a866dae46c6a4338db18ef4
suzkiee <70981102+suzkiee@users.noreply.github.com>
2021-05-21 09:33:44 -0400

    Update application.rb

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,6 @@ class Application < ApplicationRecord
   validates :name, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
-  validates :state, presence: true 
+  validates :state, presence: true
   validates :zip_code, presence: true, numericality: true
-  
 end
