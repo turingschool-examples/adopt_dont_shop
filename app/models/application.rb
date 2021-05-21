97bf103c22178c5d674c8eafb65197fae575eac3
Suzie K <suziekim.dev@gmail.com>
2021-05-21 09:18:07 -0400

    Green test for application validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,8 @@
 class Application < ApplicationRecord
-
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true 
+  validates :zip_code, presence: true, numericality: true
+  
 end
