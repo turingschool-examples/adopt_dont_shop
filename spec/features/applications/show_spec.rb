aa7171b9996f4b175d77bea4ac6ef455dd0cbcfc
Suzie K <suziekim.dev@gmail.com>
2021-05-20 20:11:08 -0400

    Red test for shows application  and attributes

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'the application show' do
+  it "shows the application and all it's attributes" do
+    app_1 = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "I'm ready to love again" )
+
+    visit "/applications/#{app_1.id}"
+
+    expect(page).to have_content(app_1.name)
+    expect(page).to have_content(app_1.street_address)
+    expect(page).to have_content(app_1.city)
+    expect(page).to have_content(app_1.state)
+    expect(page).to have_content(app_1.zip_code)
+    expect(page).to have_content(app_1.description)
+    expect(page).to have_content(app_1.status)
+  end
+
+  xit "allows the user to delete a pet" do
+    shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+
+    visit "/pets/#{pet.id}"
+
+    click_on("Delete #{pet.name}")
+
+    expect(page).to have_current_path('/pets')
+    expect(page).to_not have_content(pet.name)
+  end
+end
+
