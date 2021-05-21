0270a691d6d247bba56ba457e0c644b8de705c8b
Suzie K <suziekim.dev@gmail.com>
2021-05-21 12:39:42 -0400

    Red test for has search text box

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'the application show' do
+  before :each do 
+    @app_1 = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "I'm ready to love again" )
+  end
   it "shows the application and all its attributes" do
-    app_1 = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "I'm ready to love again" )
-    visit "/applications/#{app_1.id}"
+    visit "/applications/#{@app_1.id}"
 
-    expect(page).to have_content(app_1.name)
-    expect(page).to have_content(app_1.street_address)
-    expect(page).to have_content(app_1.city)
-    expect(page).to have_content(app_1.state)
-    expect(page).to have_content(app_1.zip_code)
-    expect(page).to have_content(app_1.description)
-    expect(page).to have_content(app_1.status)
+    expect(page).to have_content(@app_1.name)
+    expect(page).to have_content(@app_1.street_address)
+    expect(page).to have_content(@app_1.city)
+    expect(page).to have_content(@app_1.state)
+    expect(page).to have_content(@app_1.zip_code)
+    expect(page).to have_content(@app_1.description)
+    expect(page).to have_content(@app_1.status)
+  end
+
+  it 'has text box to search for pet by name' do
+    visit "/applications/#{@app_1.id}"
+
+    expect(page).to have_button("Search")
   end
 end
 
