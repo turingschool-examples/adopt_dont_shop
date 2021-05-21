16e3dafce17143324de28c2aaeec6f44fa17637b
Suzie K <suziekim.dev@gmail.com>
2021-05-21 09:54:35 -0400

    Green all many to many relationship tests

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,9 @@ class Pet < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true, numericality: true
   belongs_to :shelter
-
+  has_many :pet_applications
+  has_many :applications, through: :pet_applications
+  
   def shelter_name
     shelter.name
   end
