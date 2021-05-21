7fae165e4eef8183cdba5c79b398f1ae15df3616
NoahZinter <noah.zinter@gmail.com>
2021-05-20 18:10:50 -0600

    Test form elements for new applications

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'applications creation' do
 
-describe 'applications new' do
-  it 'renders the form for new applications' do
-    visit 'applications/new'
+  describe 'applications new' do
+    it 'renders the form for new applications' do
+      visit 'applications/new'
 
-    expect(page).to have_content('Apply to Adopt')
-    expect(find('form')).to have_content('Name')
-    expect(find('form')).to have_content('Street Address')
-    expect(find('form')).to have_content('City')
-    expect(find('form')).to have_content('State')
-    expect(find('form')).to have_content('Zipcode')
-    expect(find('form')).to have_content('Why I Would Provide a Good Home')
-
+      expect(page).to have_content('Apply to Adopt a Pet!')
+      expect(find('form')).to have_content('Name')
+      expect(find('form')).to have_content('Street Address')
+      expect(find('form')).to have_content('City')
+      expect(find('form')).to have_content('State')
+      expect(find('form')).to have_content('Zipcode')
+      expect(find('form')).to have_content('Why I Would Provide a Good Home')
+      expect(page).to have_button('Start Application')
+    end
   end
 end
