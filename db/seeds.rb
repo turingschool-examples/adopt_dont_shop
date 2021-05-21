c036440acdd23594287ea54ae83b1071a0f058bc
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-20 20:44:04 -0600

    Modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 aurora = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-lucille_bald = aurora.create(adoptable: false, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: aurora.id)
-lobster = aurora.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: aurora.id)
+lucille_bald = aurora.pets.create(adoptable: false, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: aurora.id)
+lobster = aurora.pets.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: aurora.id)
 
 hollywood = Shelter.create(name: 'Hollywood shelter', city: 'Irvine, CA', foster_program: false, rank: 7)
 george_hairlesson = hollywood.create(adoptable: true, age: 1, breed: 'sphynx', name: 'George Hairlesson', shelter_id: hollywood.id)
