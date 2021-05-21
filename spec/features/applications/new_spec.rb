dc1f8d93e27b93efab7f64c3470ae2dc8edfbebb
NoahZinter <noah.zinter@gmail.com>
2021-05-20 18:05:30 -0600

    Refactor text area for new applications form

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -12,7 +12,7 @@ describe 'applications new' do
     expect(find('form')).to have_content('City')
     expect(find('form')).to have_content('State')
     expect(find('form')).to have_content('Zipcode')
-    expect(find('form')).to have_content('Why I Would Be a Good Home')
+    expect(find('form')).to have_content('Why I Would Provide a Good Home')
 
   end
 end
