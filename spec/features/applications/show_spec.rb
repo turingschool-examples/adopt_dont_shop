ac5946cdbd27ce88ff42547c0526057ef8918c65
Suzie K <suziekim.dev@gmail.com>
2021-05-21 13:07:22 -0400

    Red test for partial input search

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'the application show' do
   before :each do 
+    @shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter.id)
+    @pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
+    @pet_3 = Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: @shelter.id)
     @app_1 = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "I'm ready to love again" )
   end
   it "shows the application and all its attributes" do
@@ -21,6 +25,17 @@ RSpec.describe 'the application show' do
 
     expect(page).to have_content("Add a Pet to this Application")
     expect(page).to have_button("Search")
+  end
+
+  it 'lists partial matches as search results' do
+    visit "/applications/#{@app_1.id}"
+
+    fill_in 'Search', with: "Ba"
+    click_on("Search")
+
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to_not have_content(@pet_2.name)
+    expect(page).to_not have_content(@pet_3.name)
   end
 end
 
