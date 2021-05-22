003005ff1dd9c981a52f0b8c7de4cba2ccae0e17
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-21 21:21:40 -0600

    Attempt start application story

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,8 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+    @application_pets = ApplicationPet.find(params[:application_id])
+  end
 end
