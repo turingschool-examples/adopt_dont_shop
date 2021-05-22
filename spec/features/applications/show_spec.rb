5e9396ce0b90edc33c3a69621d123143dfdf907c
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-05-22 10:31:21 -0600

    Add application#show feature test for default page appearance

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -10,14 +10,21 @@ RSpec.describe 'applications show page', type: :feature do
       zip_code: 80205,
       statement: nil
     )
+    shelter1 = Shelter.create!(foster_program: 'Cat Savers', name: 'Cats R Us', city: 'Denver', rank: 1)
+    @pet1 = shelter1.pets.create!(adoptable: true, age: 0, breed: 'Siamese Cat', name: 'Pear')
+    @pet2 = shelter1.pets.create!(adoptable: true, age: 1, breed: 'Blue Russian Cat', name: 'Oleg')
+    @pet3 = shelter1.pets.create!(adoptable: true, age: 2, breed: 'Domestic Short Hair Cat', name: 'Ralph')
   end
 
   describe '#show page appearance' do
-
-    it 'has certain fields' do
+    it 'has certain fields in the Applicant Details section by default' do
       visit "/applications/#{@application1.id}"
 
-      expect(page).to have_content('Application Status: In Progress')
+      expect(page).to have_content('Applicant Details:')
+      expect(page).to have_content('Application Status:')
+      expect(page).to have_content('In Progress')
+      expect(page).to have_content('Note - Your application has not been submitted yet!')
+      expect(page).to have_content("In order to submit, you must add both 'Pets To Adopt' and an 'Adoption Statement' below.")
       expect(page).to have_content('Name:')
       expect(page).to have_content('Big Bird')
       expect(page).to have_content('Street Address:')
@@ -28,7 +35,24 @@ RSpec.describe 'applications show page', type: :feature do
       expect(page).to have_content('CO')
       expect(page).to have_content('Zip Code:')
       expect(page).to have_content('80205')
-      expect(page).to have_content('Why you think you would be a good pet parent:')
+    end
+    it 'has certain text in the Pets To Be Adopted section by default' do
+      visit "/applications/#{@application1.id}"
+
+      expect(page).to have_content('Pets To Be Adopted:')
+      expect(page).to have_content('Please add pets to your application')
+      expect(page).to have_content('Search pets by name')
+      expect(page).to have_content('All Adoptable Pets:')
+      expect(page).to have_content('Pear')
+      expect(page).to have_content('Oleg')
+      expect(page).to have_content('Ralph')
+    end
+    it 'has certain text in the Adoption Statement section by default' do
+      visit "/applications/#{@application1.id}"
+
+      expect(page).to have_content('Adoption Statement')
+      expect(page).to have_content('Please add an adoption statement')
+      expect(page).to have_button('Add Statement')
     end
   end
 end
