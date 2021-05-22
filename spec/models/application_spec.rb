97ae2e8fc4d5d3c00436d03915c3d6ca70295ef6
Zach T <ztrokey@gmail.com>
2021-05-21 21:37:24 -0600

    Add test for full_address method

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Application, type: :model do
     it {should have_many :application_pets}
     it {should have_many(:pets).through(:application_pets)}
   end
+  describe '#full_address' do
+    it 'concatenates the address colums into a single string' do
+      application = Application.create!(name: 'Chris P. Bacon', street_address: '123 Main Street', city: 'Anytown', state: 'CO', zip_code: 12345, description: 'I like pets', status: 'Pending')
+
+      expected = "123 Main Street, Anytown, CO 12345"
+      expect(application.full_address).to eq(expected)
+    end
+  end
 end
