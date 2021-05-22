7dec1d226e011f63b7e276745dccc91f46a024f9
NoahZinter <noah.zinter@gmail.com>
2021-05-22 16:51:29 -0600

    test incomplete information

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class PetitionsController < ApplicationController
-  def new; end
+  def new
+  end
 
   def create
-    @petition = Petition.create(petition_params)
-    redirect_to "/petitions/#{@petition.id}"
-    flash[:status] = 'In Progress'
+    @petition = Petition.new(petition_params)
+    if @petition.save
+      redirect_to "/petitions/#{@petition.id}"
+      flash[:status] = 'In Progress'
+    else
+      flash[:incomplete] = ' Must Fill In All Fields'
+      redirect_to '/petitions/new'
+    end
   end
 
   def show
--- app/models/petition.rb
+++ app/models/petition.rb
@@ -2,4 +2,10 @@
 
 class Petition < ApplicationRecord
   has_many :pets
+
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
 end
