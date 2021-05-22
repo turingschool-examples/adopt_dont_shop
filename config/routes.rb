18bbad9eab5eb6004197be6cbc4d66c192580dc0
Regina Casias <reginaannvigil@gmail.com>
2021-05-21 19:06:49 -0600

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -36,4 +36,12 @@ Rails.application.routes.draw do
   get '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinary_offices#veterinarians'
   get '/veterinary_offices/:veterinary_office_id/veterinarians/new', to: 'veterinarians#new'
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
+
+  get '/appliactions', to: 'appliactions#index'
+  get '/appliactions/new', to: 'appliactions#new'
+  get '/appliactions/:id', to: 'appliactions#show'
+  post '/appliactions', to: 'appliactions#create'
+  get '/appliactions/:id/edit', to: 'appliactions#edit'
+  patch '/appliactions/:id', to: 'appliactions#update'
+  delete '/appliactions/:id', to: 'appliactions#destroy'
 end
