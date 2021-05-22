95c6f4cbaf263fea74538a18455d2a30dec65af1
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-22 17:54:33 -0600

    Implement new and create methods

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,4 +4,32 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @pets = @application.pets
   end
+
+  def new
+  end
+
+  def create
+    application = Application.new(application_params)
+    application.save
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+
+  def application_params
+    params.permit(
+      :first_name,
+      :middle_name,
+      :last_name,
+      :street_number,
+      :street_prefix,
+      :street_name,
+      :street_type,
+      :city,
+      :state,
+      :zip_code,
+      :description,
+      :status
+    )
+  end
 end
