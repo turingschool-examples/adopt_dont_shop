95a8eb5ec346e6c42b2e2dd5e0130dcb8db67bb0
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-05-22 10:42:19 -0600

    Add application#show feature tests for search function

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,21 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'applications show page', type: :feature do
   before (:each) do
-    @application1 = Application.create(
-      name: "Big Bird",
-      street_address: "1311 E 27TH AVE",
-      city: "DENVER",
-      state: "CO",
-      zip_code: 80205,
-      statement: nil
-    )
+    @application1 = Application.create!(name: "Big Bird", street_address: '1311 E 27TH AVE', city: 'DENVER', state: 'CO', zip_code: 80205, statement: nil)
     shelter1 = Shelter.create!(foster_program: 'Cat Savers', name: 'Cats R Us', city: 'Denver', rank: 1)
     @pet1 = shelter1.pets.create!(adoptable: true, age: 0, breed: 'Siamese Cat', name: 'Pear')
     @pet2 = shelter1.pets.create!(adoptable: true, age: 1, breed: 'Blue Russian Cat', name: 'Oleg')
     @pet3 = shelter1.pets.create!(adoptable: true, age: 2, breed: 'Domestic Short Hair Cat', name: 'Ralph')
   end
 
-  describe '#show page appearance' do
+  describe 'default show page appearance' do
     it 'has certain fields in the Applicant Details section by default' do
       visit "/applications/#{@application1.id}"
 
@@ -53,6 +46,32 @@ RSpec.describe 'applications show page', type: :feature do
       expect(page).to have_content('Adoption Statement')
       expect(page).to have_content('Please add an adoption statement')
       expect(page).to have_button('Add Statement')
+    end
+  end
+  describe 'Searching pets functionality' do
+    it 'allows you to search by an exact pet name' do
+      visit "/applications/#{@application1.id}"
+      fill_in 'search', with: 'Pear'
+      click_button('Search')
+
+      expect(page).to have_content('Pear')
+      expect(page).to_not have_content('Oleg')
+    end
+    it 'allows you to search by a partial pet name' do
+      visit "/applications/#{@application1.id}"
+      fill_in 'search', with: 'Pe'
+      click_button('Search')
+
+      expect(page).to have_content('Pear')
+      expect(page).to_not have_content('Oleg')
+    end
+    it 'allows for case insensitive searches' do
+      visit "/applications/#{@application1.id}"
+      fill_in 'search', with: 'PeAr'
+      click_button('Search')
+
+      expect(page).to have_content('Pear')
+      expect(page).to_not have_content('Oleg')
     end
   end
 end
