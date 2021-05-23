63860f1951d42a6dc709a6ee71106a8131586326
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-05-23 15:10:24 -0700

    Add applications migration

---
+++ db/migrate/20210523215609_create_applications.rb
@@ -0,0 +1,13 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.integer :zip_code
+
+      t.timestamps
+    end
+  end
+end
