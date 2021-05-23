7abc02c88c9a71e33c6b0c27673fca75280e1f61
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-05-23 16:22:42 -0700

    Create fill_in expect

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'application creation' do
       expect(page).to have_content('Fill out the form below')
     end
 
-    it 'has a form to fill out' do
+    it 'renders a new form' do
       visit '/applications/new'
 
       expect(find('form')).to have_content('Name')
@@ -21,6 +21,25 @@ RSpec.describe 'application creation' do
       expect(find('form')).to have_content('City')
       expect(find('form')).to have_content('State')
       expect(find('form')).to have_content('Zip code')
+      save_and_open_page
     end
+  end
+
+  context 'the application create' do
+    describe 'give vaild data' do
+      it 'creates the application and redirects to the applicaiton show page' do
+        visit '/applications/new'
+
+        fill_in 'Name', with: "#{@application_1.name}"
+        fill_in 'Name', with: "#{@application_1.street_address}"
+        fill_in 'Name', with: "#{@application_1.city}"
+        fill_in 'Name', with: "#{@application_1.state}"
+        fill_in 'Name', with: "#{@application_1.zip_code}"
+        click_button 'Save'
+      end
+    end
+
+    describe 'given invalid data'
+      it 're-renders the new form'
   end
 end
