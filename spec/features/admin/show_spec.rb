e2e9cd477e8b41d9edde646241a424d7a1104a71
Suzie K <suziekim.dev@gmail.com>
2021-05-22 22:31:54 -0400

    Red test for shows shelter and its attributes

---
+++ spec/features/admin/show_spec.rb
@@ -0,0 +1,16 @@
+# spec/features/admin/show_spec
+
+require 'rails_helper'
+
+RSpec.describe 'the admin show' do
+  before(:each) do
+    @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+  end
+
+  it 'shows shelter and its attritbutes' do
+    visit "/admin/shelters/#{@shelter.id}"
+
+    expect(page).to have_content(@shelter.name)
+    expect(page).to have_content(@shelter.address)
+  end
+end
