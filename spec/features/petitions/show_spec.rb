217bebaea90d03c89a6fcae930158e489b13e76a
NoahZinter <noah.zinter@gmail.com>
2021-05-23 13:43:12 -0600

    Create test for pet search display

--- spec/features/petitions/show_spec.rb
+++ spec/features/petitions/show_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe 'petition show' do
     fill_in 'State', with: 'CO'
     fill_in 'Zipcode', with: '80210'
     click_button 'Start Application'
+    @id = Petition.last.id
   end
   it 'displays applicant name and address' do
     expect(page).to have_content('Your name: Ted Leo')
@@ -20,5 +21,16 @@ RSpec.describe 'petition show' do
 
   it 'displays option to search for pets and add to petition' do
     expect(page).to have_content('Add a Pet to this Application')
+  end
+
+  it 'searching returns a list of pets on existing show page' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Fluffy', shelter_id: shelter.id)
+    fill_in 'Add a Pet to this Application', with: 'Fluffy'
+    click_button 'Search'
+
+    expect(current_path).to eq "/petitions/#{@id}"
+    expect(page).to have_content('Fluffy')
   end
 end
