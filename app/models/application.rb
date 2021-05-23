fe2bd361e661cb221e4d4c685a9586cb55dfc550
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-22 18:03:13 -0600

    Add default status value

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+  before_save :default_status
+
+  def default_status
+    self.status = 'In Progress'
+  end
 end
