91c2a188e1185b20503fb8a7e1325372dfb9c95a
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-23 13:41:46 -0600

    Create model test for application default value

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Application, type: :model do
     it {should have_many :application_pets}
     it {should have_many(:pets).through(:application_pets)}
   end
+
+  describe 'instance methods' do
+    it 'should default application_status to in progress' do
+      application = Application.new
+      application.save
+      application.application_status.should == 'In Progress'
+    end
+  end
 end
