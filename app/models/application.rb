0627ec8e58b28366afea682ec885ad349e5b4eb1
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-23 18:22:27 -0600

    Add validates certain fields

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,15 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+  validates :first_name, :last_name, presence: true
+  validates :street_number, presence: true, numericality: true
+  validates :street_name, :street_type, presence: true
+  validates :city, :state, presence: true
+  validates :zip_code, presence: true, numericality: true
+  validates :description, presence: true
   before_save :default_status
+
+
 
   def default_status
     self.status = 'In Progress'
