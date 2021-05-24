dab9eddefc73b77541fefdf807871a8a21d7eace
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-05-24 19:28:40 -0400

    Set up schema and many to many relationships

---
+++ app/models/pet_application.rb
@@ -0,0 +1,6 @@
+class PetApplication < ApplicationRecord
+
+  belongs_to :pets
+  belongs_to :applications
+
+end
---
+++ db/migrate/20210524232041_create_applications.rb
@@ -0,0 +1,12 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.integer :zip_code
+      t.text :description
+    end
+  end
+end
