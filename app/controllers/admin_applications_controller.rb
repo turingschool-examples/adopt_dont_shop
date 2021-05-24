4f770d5d6742dcfb9873216e5fa00949e18d1b63
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-05-24 16:58:38 -0600

    Add progress towards application approval capability

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -4,4 +4,16 @@ class AdminApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @pets_adopting = @application.pets
   end
+
+  def update
+    #should this all live here or in the application?
+    application = Application.find(params[:id])
+    if params[:outcome] == 'approve'
+      application.update(status: 'Approved')
+    else
+      application.update(status: 'Rejected')
+    end
+    application.save
+    redirect_to action: "show", id: application.id
+  end
 end
