e27b2bee7bff06e6c943d8c6a8d1c352efa94650
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-24 20:04:18 -0600

    Create test for pet search

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -37,7 +37,6 @@ RSpec.describe 'the applications show page' do
     expect(page).to have_content(@jt_application.city)
     expect(page).to have_content(@jt_application.state)
     expect(page).to have_content(@jt_application.zip_code)
-    expect(page).to have_content(@jt_application.description)
     expect(page).to have_content(@jt_application.status)
 
     within("#application-#{@jt_application.id}") do
@@ -47,5 +46,27 @@ RSpec.describe 'the applications show page' do
 
     click_on(@mr_pirate.name)
     expect(current_path).to eq("/pets/#{@mr_pirate.id}")
+  end
+
+  it 'has a search field' do
+    visit "/applications/#{@jt_application.id}"
+
+    expect(page).to have_button("Search")
+  end
+
+  it 'lists partial matches as search results' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+
+    visit "/applications/#{@jt_application.id}"
+
+    fill_in 'Search', with: "ba"
+    click_on("Search")
+
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_2.name)
+    expect(page).to_not have_content(pet_3.name)
   end
 end
