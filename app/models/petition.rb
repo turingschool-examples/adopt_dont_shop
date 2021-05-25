3138400892beab4363e9874f53e3acd07a8af507
NoahZinter <noah.zinter@gmail.com>
2021-05-25 13:45:22 -0600

    Hound refactor

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -3,7 +3,5 @@
 class Petition < ApplicationRecord
   has_many :pet_petitions
   has_many :pets, through: :pet_petitions
-
   validates :name, :street_address, :city, :state, :zipcode, presence: true
-
 end
