5b53f02c596b5650b9d6ac397529e54fb354eedf
Brett Jones <bhjones4545@gmail.com>
2021-07-15 16:02:54 -0600

    DB migrations for applications and pet applications

---
+++ db/migrate/20210715212612_create_applications.rb
@@ -0,0 +1,15 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :status, default: "In Progress"
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
