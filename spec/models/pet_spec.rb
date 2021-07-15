02db889e94a1c91a61fdb495181a6156ca6622d4
Matt Toensing <matthew.toensing@gmail.com>
2021-07-15 16:03:14 -0700

    Include tests for additional validations

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe Pet, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:age) }
     it { should validate_numericality_of(:age) }
+    it { should validate_presence_of(:breed) }
+    # it { should validate_presence_of(:adoptable) }
   end
 
   before(:each) do
