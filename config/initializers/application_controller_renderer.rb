da8c8bd64fe5478daf515a5ea7f1ffe34407edfc
Matt Toensing <matthew.toensing@gmail.com>
2021-07-15 16:20:30 -0700

    Shenanigans

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+# config.assets.precompile += %w( mailers.js )
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
