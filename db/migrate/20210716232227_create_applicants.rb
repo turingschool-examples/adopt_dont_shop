29307d2c63fe5f4b7fa541f255c7b522b090fe95
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-16 17:44:38 -0600

    Set up many to many relationships between pets, applicants, and pet_applications

---
+++ db/migrate/20210716232227_create_applicants.rb
@@ -0,0 +1,15 @@
+class CreateApplicants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applicants do |t|
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.integer :zip_code
+      t.string :description
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
