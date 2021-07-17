e98930793907526bf4dd1905dae540f356021d03
Caroline Tan <carolinectan3@gmail.com>
2021-07-16 21:42:47 -0400

    Add section comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+PetApplication.destroy_all
 Pet.destroy_all
 Shelter.destroy_all
-PetApplication.destroy_all
 Application.destroy_all
 
+# shelters
 brighter_days = Shelter.create!(name: 'Brighter Days Shelter', rank: 1, city: 'boulder', foster_program: true)
 underdogs = Shelter.create!(name: 'Underdogs Shelter', rank: 2, city: 'denver', foster_program: true)
 
+# pets
 bosco = brighter_days.pets.create!(name: 'Bosco', adoptable: true, age: 8, breed: 'Springer Spaniel')
 lily = brighter_days.pets.create!(name: 'Lily', adoptable: true, age: 5, breed: 'German Shepard Boxer Mix')
 leia = brighter_days.pets.create!(name: 'Leia', adoptable: true, age: 6, breed: 'Bernadoodle')
@@ -27,6 +29,7 @@ clyde = underdogs.pets.create!(name: 'Clyde', adoptable: true, age: 8, breed: 'B
 jasmine = underdogs.pets.create!(name: 'Jasmine', adoptable: true, age: 8, breed: 'Black Lab')
 finn = underdogs.pets.create!(name: 'Finn', adoptable: true, age: 8, breed: 'Australian Shepard')
 
+# applications
 app_1 = Application.create!(name: 'Elliot Olbright', street: '5743 Squirrel Circle', city: 'Aspen', state: 'CO', zip_code: 81611, application_status: 'pending')
 app_2 = Application.create!(name: 'Sami Peterson', street: '1123 Arbor Lane', city: 'Chicago', state: 'IL', zip_code: 60007, application_status: 'approved')
 app_3 = Application.create!(name: 'Amanda McGuire', street: '883 Teller Court', city: 'Wheat Ridge', state: 'CO', zip_code: 80033, application_status: 'pending')
@@ -34,6 +37,7 @@ app_4 = Application.create!(name: 'Brian Fletcher', street: '9090 Veterans Stree
 app_5 = Application.create!(name: 'Jacob Martinez', street: '8439 Felines Court', city: 'Westminster', state: 'CO', zip_code: 80021, application_status: 'rejected')
 app_6 = Application.create!(name: 'Jacob Piland', street: '953 Brewers Street', city: 'Austin', state: 'TX', zip_code: 78704, application_status: 'pending')
 
+# pet applications
 PetApplication.create!(pet: bosco, application: app_1)
 PetApplication.create!(pet: lily, application: app_1)
 PetApplication.create!(pet: leia, application: app_1)
