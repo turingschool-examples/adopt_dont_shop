96e8121a401519c522fea427b141a6d948259b2d
Caroline Tan <carolinectan3@gmail.com>
2021-07-17 12:52:44 -0400

    Add description expect and view

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe 'the applications show page' do
     @zephyr = @underdogs.pets.create!(name: 'Zephyr', adoptable: true, age: 8, breed: 'Malamute')
     @ruger = @underdogs.pets.create!(name: 'Ruger', adoptable: true, age: 8, breed: 'Husky Samoyed Mix')
 
-    @app_1 = Application.create!(name: 'Elliot O.', street: '5743 Squirrel Circle', city: 'Aspen', state: 'CO', zip_code: 81611, application_status: 'pending')
-    @app_2 = Application.create!(name: 'Sami P.', street: '1123 Arbor Lane', city: 'Chicago', state: 'IL', zip_code: 60007, application_status: 'approved')
-    @app_3 = Application.create!(name: 'Amanda M.', street: '883 Teller Court', city: 'Wheat Ridge', state: 'CO', zip_code: 80033, application_status: 'pending')
+    @app_1 = Application.create!(name: 'Elliot O.', street: '5743 Squirrel Circle', city: 'Aspen', state: 'CO', zip_code: 81611, application_status: 'pending', description: 'loves animals')
+    @app_2 = Application.create!(name: 'Sami P.', street: '1123 Arbor Lane', city: 'Chicago', state: 'IL', zip_code: 60007, application_status: 'approved', description: 'loving home')
+    @app_3 = Application.create!(name: 'Amanda M.', street: '883 Teller Court', city: 'Wheat Ridge', state: 'CO', zip_code: 80033, application_status: 'pending', description: 'active family')
 
     @pet_app_1 = PetApplication.create!(pet: @bosco, application: @app_1)
     @pet_app_2 = PetApplication.create!(pet: @lily, application: @app_1)
@@ -31,7 +31,7 @@ RSpec.describe 'the applications show page' do
     expect(page).to have_content("City: #{@app_1.city}")
     expect(page).to have_content("State: #{@app_1.state}")
     expect(page).to have_content("Zip Code: #{@app_1.zip_code}")
-    # Description of why the applicant says they'd be a good home for this pet(s)
+    expect(page).to have_content("Description: #{@app_1.description}")
     # Names of all pets that this application is for (all names of pets should be links to their show page)
     expect(page).to have_content("Application Status: #{@app_1.application_status}")
   end
