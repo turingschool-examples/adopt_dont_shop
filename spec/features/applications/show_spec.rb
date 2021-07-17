7b95605f3d03ab3df88f119cbe7209d87e799bff
Caroline Tan <carolinectan3@gmail.com>
2021-07-17 14:33:34 -0400

    Add link to pet show page from application show page test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe 'the applications show page' do
     @zephyr = @underdogs.pets.create!(name: 'Zephyr', adoptable: true, age: 8, breed: 'Malamute')
     @ruger = @underdogs.pets.create!(name: 'Ruger', adoptable: true, age: 8, breed: 'Husky Samoyed Mix')
 
-    @app_1 = Application.create!(name: 'Elliot O.', street: '5743 Squirrel Circle', city: 'Aspen', state: 'CO', zip_code: 81611, application_status: 'pending', description: 'loves animals')
-    @app_2 = Application.create!(name: 'Sami P.', street: '1123 Arbor Lane', city: 'Chicago', state: 'IL', zip_code: 60007, application_status: 'approved', description: 'loving home')
-    @app_3 = Application.create!(name: 'Amanda M.', street: '883 Teller Court', city: 'Wheat Ridge', state: 'CO', zip_code: 80033, application_status: 'pending', description: 'active family')
+    @app_1 = Application.create!(name: 'Elliot O.', street: '5743 Squirrel Circle', city: 'Aspen', state: 'CO', zip_code: 81611, application_status: 'Pending', description: 'loves animals')
+    @app_2 = Application.create!(name: 'Sami P.', street: '1123 Arbor Lane', city: 'Chicago', state: 'IL', zip_code: 60007, application_status: 'Approved', description: 'loving home')
+    @app_3 = Application.create!(name: 'Amanda M.', street: '883 Teller Court', city: 'Wheat Ridge', state: 'CO', zip_code: 80033, application_status: 'Pending', description: 'active family')
 
     @pet_app_1 = PetApplication.create!(pet: @bosco, application: @app_1)
     @pet_app_2 = PetApplication.create!(pet: @lily, application: @app_1)
@@ -23,7 +23,7 @@ RSpec.describe 'the applications show page' do
     @pet_app_5 = PetApplication.create!(pet: @ruger, application: @app_3)
   end
 
-  it 'can display the name, full address, description, pet(s) applied for, and status of the applicantion' do
+  it 'can display the name, full address, description, pet(s) applied for, and status of the application' do
     visit "/applications/#{@app_1.id}"
 
     expect(page).to have_content("#{@app_1.name}'s Application")
@@ -34,5 +34,19 @@ RSpec.describe 'the applications show page' do
     expect(page).to have_content("Description: #{@app_1.description}")
     expect(page).to have_content("Pet(s) Applied For: #{@pet_app_1.pet.name} , #{@pet_app_2.pet.name}")
     expect(page).to have_content("Application Status: #{@app_1.application_status}")
+  end
+
+  it 'can link to each pets show page in the pets applied for section' do
+    visit "/applications/#{@app_1.id}"
+
+    click_link("Bosco")
+
+    expect(current_path).to eq("/pets/#{@bosco.id}")
+
+    visit "/applications/#{@app_1.id}"
+
+    click_link("Lily")
+
+    expect(current_path).to eq("/pets/#{@lily.id}")
   end
 end
