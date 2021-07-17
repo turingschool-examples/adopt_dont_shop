511871804d9f175f10dc01e143eba688e97ce799
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-17 10:56:45 -0400

    Configure applications routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get '/', to: 'application#welcome'
+  get '/', to: 'welcome#index'
+
+  get '/applications', to: 'applications#index'
+  get '/applications/new', to: 'applications#new'
+  get '/applications/:id', to: 'applications#show'
+  post '/applications', to: 'applications#create'
+  get '/applications/:id/edit', to: 'applications#edit'
+  patch '/applications/:id', to: 'applications#update'
+  delete '/applications/:id', to: 'applications#destroy'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
