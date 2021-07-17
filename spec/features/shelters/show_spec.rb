8cdbf84030c08407f5e2debaa97c5dfb946e3f97
Caroline Tan <carolinectan3@gmail.com>
2021-07-17 01:49:35 -0400

    Refactor tests with before each

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -1,46 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe 'the shelter show' do
-  it "shows the shelter and all it's attributes" do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+  before :each do
+    @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
 
-    visit "/shelters/#{shelter.id}"
-
-    expect(page).to have_content(shelter.name)
-    expect(page).to have_content(shelter.rank)
-    expect(page).to have_content(shelter.city)
+    @shelter_1.pets.create(name: 'garfield', breed: 'shorthair', adoptable: true, age: 1)
   end
 
-  it "shows the number of pets associated with the shelter" do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    shelter.pets.create(name: 'garfield', breed: 'shorthair', adoptable: true, age: 1)
+  it 'shows the shelter and all its attributes' do
+    visit "/shelters/#{@shelter_1.id}"
 
-    visit "/shelters/#{shelter.id}"
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_1.rank)
+    expect(page).to have_content(@shelter_1.city)
+  end
 
-    within ".pet-count" do
-      expect(page).to have_content(shelter.pets.count)
+  it 'shows the number of pets associated with the shelter' do
+    visit "/shelters/#{@shelter_1.id}"
+
+    within '.pet-count' do
+      expect(page).to have_content(@shelter_1.pets.count)
     end
   end
 
-  it "allows the user to delete a shelter" do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+  it 'allows the user to delete a shelter' do
+    visit "/shelters/#{@shelter_1.id}"
 
-    visit "/shelters/#{shelter.id}"
-
-    click_on("Delete #{shelter.name}")
+    click_on("Delete #{@shelter_1.name}")
 
     expect(page).to have_current_path('/shelters')
-    expect(page).to_not have_content(shelter.name)
+    expect(page).to_not have_content(@shelter_1.name)
   end
 
   it 'displays a link to the shelters pets index' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: true, rank: 9)
+    shelter_2 = Shelter.create(name: 'Denver shelter', city: 'Denver, CO', foster_program: true, rank: 10)
 
-    visit "/shelters/#{shelter.id}"
+    visit "/shelters/#{shelter_2.id}"
 
-    expect(page).to have_link("All pets at #{shelter.name}")
-    click_link("All pets at #{shelter.name}")
+    expect(page).to have_link("All pets at #{shelter_2.name}")
+    click_link("All pets at #{shelter_2.name}")
 
-    expect(page).to have_current_path("/shelters/#{shelter.id}/pets")
+    expect(page).to have_current_path("/shelters/#{shelter_2.id}/pets")
+  end
+
+
+  xit "displays a link to the admin application show page next to each pet's name" do
+    visit "/admin/shelters" # admin_show.html.erb // admin_show_spec.rb
+
+    # And I look in the "Action Required" section
+
+    expect(page).to have_link("admin/application")
   end
 end
