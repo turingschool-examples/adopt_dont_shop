db883eef97e40c8131cc7b257c9ead4fc5b66b5c
Michael Abbott <michael@mjabbottdesign.com>
2021-07-18 12:08:23 -0700

    Add adoption_application_pets relationships test to model pet_spec

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Pet, type: :model do
   describe 'relationships' do
     it { should belong_to(:shelter) }
+    it { should have_many(:adoption_application_pets) }
+    it { should have_many(:adoption_applications).through(:adoption_application_pets) }
   end
 
   describe 'validations' do
