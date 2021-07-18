02c83a580747fd1d3a91ae6bd71b62adfbe25282
Michael Abbott <michael@mjabbottdesign.com>
2021-07-18 12:09:18 -0700

    Add adoption_application_pets associations to Pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,8 @@ class Pet < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true, numericality: true
   belongs_to :shelter
+  has_many :adoption_application_pets
+  has_many :adoption_applications, through: :adoption_application_pets
 
   def shelter_name
     shelter.name
