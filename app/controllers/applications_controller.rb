0af252772e3d46642ace4c74d1dcb27d60ffbf13
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-18 11:25:09 -0400

    Create applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,19 @@ class ApplicationsController < ApplicationController
   def index
     @applications = Application.all
   end
+
+  def new
+    # @application = Application.find(params[:application_id])
+  end
+
+  def create
+    application = Application.create!(application_params)
+  end
+
+private
+  def application_params
+    params.permit(:id, :name, :street, :city, :state, :zip_code, :application_status, :description)
+  end
+
+
 end
