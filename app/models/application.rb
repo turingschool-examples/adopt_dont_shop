9d503f9f5c065cc9c9dfa93227cc3bfcb3a768a8
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-18 14:12:05 -0600

    Add validations: name, street_address, city, state, zip_code

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
