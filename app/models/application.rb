cfd97a1d36f840b54bf0b778cd97fa1677968158
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-18 11:22:58 -0600

    Add associated pets method

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,12 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  def associated_pets(application_id)
+    PetApplication.where('application_id = ?', application_id).map do |join|
+      join.pet_id
+    end.map do |pet_id|
+      Pet.find(pet_id).name
+    end
+  end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Application do
     it {should have_many :pet_applications}
     it {should have_many(:pets).through(:pet_applications)}
   end
+  describe 'instance methods' do
+    it 'can list associated pet names' do
+      furry = Shelter.create!(name:'Furrry Shelter', foster_program: true, city: 'New Orleans', rank: 5)
+
+      lana = furry.pets.create!(name: 'Lana', age: 1, adoptable: true, breed: 'short-haired')
+      doc = furry.pets.create!(name: 'Doc', age: 8, adoptable: true, breed: 'schnauzer')
+
+      application1 = Application.create!(name: 'Carina', street_address: '455 Cool Street', city: 'Portland', state: 'OR', zip_code: 23392, home_description: 'I love my furry friends and have a great yard they can roam around in', status: 'pending')
+
+      application1.pets << [lana, doc]
+
+      expect(application1.associated_pets(application1.id)).to eq(['Lana', 'Doc'])
+    end
+  end
 end
