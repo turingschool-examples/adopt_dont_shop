e361ef20253a5ef00a2c70c1e5001e5d18f98545
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-18 13:28:20 -0400

    Fix association test for joins model

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Pet Application, type: :model do
+RSpec.describe PetApplication, type: :model do
   describe 'relationships' do
     it { should belong_to(:pet) }
-    it { should have_many(:application) }
+    it { should belong_to(:application) }
   end
+end
