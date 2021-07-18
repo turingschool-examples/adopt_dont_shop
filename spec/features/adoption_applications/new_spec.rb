001652f859ceb0c0b59d6bb9780415b588c285b1
Michael Abbott <michael@mjabbottdesign.com>
2021-07-18 15:12:26 -0700

    Create adoption applicaiton new test

---
+++ spec/features/adoption_applications/new_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'adoption application creation' do
+
+  describe 'new adoption applicaiton' do
+    it 'renders the new form' do
+      visit '/adoption_applications/new'
+
+      fill_in('Name', with: 'Michael Abbott')
+      fill_in('Street address', with: '66 Bell St.')
+      fill_in('City', with: 'Seattle')
+      fill_in('State', with: 'Wa')
+      fill_in('Zip code', with: '98121')
+      click_button('Submit')
+
+      new_app_id = AdoptionApplication.last.id
+      expect(current_path).to eq("/adoption_applications/#{new_app_id}")
+      # expect(page).to have_content('Michael Abbott')
+    end
+  end
+end
