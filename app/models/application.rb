98caf7601463f9efb2af8f6b8d5df7cc47b8684a
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-18 11:22:28 -0400

    Create application model associations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,21 +1,6 @@
 class Application < ApplicationRecord
-  def index
-    @applications = Application.all
-  end
+  has_many :pets
+  has_many :pets, through: :pet_applications
 
-  def new
-    # @application = Application.find(params[:application_id])
-  end
-
-  def create
-    # application = Application.find(params[:application_id])
-    # park = state.parks.create!(state_park_params)
-    # redirect_to new_state_park_path(state)
-  end
-
-private
-  # def state_park_params
-  #   params.permit(:id, :state_id, :name, :camping_allowed, :kayaking_available, :park_rating)
-  # end
 
 end
