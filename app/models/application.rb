2d9a11eb74cbae01fdaaeed2262595f0f91af7b8
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-19 00:47:56 -0700

    Update applications model

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,7 @@ class Application < ApplicationRecord
   validates :name, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
-  validates :zip_code, presence: true
+  validates :zip_code, presence: true, numericality: true
   enum status: ["In Progress", "Pending", "Accepted", "Rejected"]
 
 end
