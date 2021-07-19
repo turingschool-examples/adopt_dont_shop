bfa5d10beeb90bb0e990cff703e070967f99a21e
sami-p <sami.peterson14@gmail.com>
2021-07-19 18:39:51 -0500

    Validate presence of attributes on application

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Application, type: :model do
     it { should have_many(:pet_applications) }
     it { should have_many(:pets).through(:pet_applications) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_presence_of(:description) }
+  end
 end
