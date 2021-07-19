a872701d804561d30b00282f69a891a80dfa23fa
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-18 23:49:33 -0400

    Update validations for description situation

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,8 +4,7 @@ class Application < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true, numericality: true
-  validates :description, presence: true
-  # validates :application_status, presence: true
+  validates :application_status, presence: true
 
   has_many :pet_applications
   has_many :pets, through: :pet_applications
