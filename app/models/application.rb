3bdbd31da01ba338753f761433a1be1eb24a5a0a
Brett Jones <bhjones4545@gmail.com>
2021-07-19 17:40:16 -0600

    Add application validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,5 +2,9 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
-  
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true, numericality: true
 end
