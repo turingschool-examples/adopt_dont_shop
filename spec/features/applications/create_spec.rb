da925ff3e9062be955a195a719f5320a409dd328
Brett Jones <bhjones4545@gmail.com>
2021-07-19 17:40:03 -0600

    Add flash error

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -15,5 +15,33 @@ RSpec.describe 'create application' do
 
       expect(page).to have_content("Taylor Hammond")
     end
+
+    it 'can display flash errors when left blank' do
+      visit '/applications/new'
+
+      fill_in 'Name', with: 'Taylor Hammond'
+      fill_in 'Street address', with: '123 Street avenue'
+      fill_in 'City', with: 'Dallas'
+      fill_in 'State', with: 'Texas'
+      fill_in 'Zip code', with: ''
+
+      click_button 'Submit Application'
+
+      expect(page).to have_content("Zip code can't be blank")
+    end
+
+    it 'can display flash errors for numericality' do
+      visit '/applications/new'
+
+      fill_in 'Name', with: 'Taylor Hammond'
+      fill_in 'Street address', with: '123 Street avenue'
+      fill_in 'City', with: 'Dallas'
+      fill_in 'State', with: 'Texas'
+      fill_in 'Zip code', with: 'dog'
+
+      click_button 'Submit Application'
+
+      expect(page).to have_content("Zip code is not a number")
+    end
   end
 end
