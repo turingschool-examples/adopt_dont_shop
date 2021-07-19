3566a8aeac9b81c9f516e9c827abdb4e6738b65c
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-19 00:47:24 -0700

    Add spec test for invalid Applicant data

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -37,25 +37,52 @@ RSpec.describe 'applications' do
     click_button "Submit"
   end
 
-  it 'can create a new applicant' do
-    visit "/applications/new"
+  describe 'the applicant create' do
+    context 'given valid data' do
+      it 'can create a new applicant' do
+        visit "/applications/new"
 
-    fill_in "Name", with: "Megan"
-    fill_in "Street Address", with: "5678 W. second St."
-    fill_in "City", with: "Dallas"
-    fill_in "State", with: "TX"
-    fill_in "Zip Code", with: "88888"
+        fill_in "Name", with: "Megan"
+        fill_in "Street Address", with: "5678 W. second St."
+        fill_in "City", with: "Dallas"
+        fill_in "State", with: "TX"
+        fill_in "Zip Code", with: "88888"
 
-    click_button "Submit"
-    application = Application.last
-    save_and_open_page
+        click_button "Submit"
+        application = Application.last
 
-    expect(current_path).to eq("/applications/#{application.id}")
-    expect(page).to have_content("Megan")
-    expect(page).to have_content("5678 W. second St.")
-    expect(page).to have_content("Dallas")
-    expect(page).to have_content("TX")
-    expect(page).to have_content("88888")
-    expect(page).to have_content("In Progress")
+        expect(current_path).to eq("/applications/#{application.id}")
+        expect(page).to have_content("Megan")
+        expect(page).to have_content("5678 W. second St.")
+        expect(page).to have_content("Dallas")
+        expect(page).to have_content("TX")
+        expect(page).to have_content("88888")
+        expect(page).to have_content("In Progress")
+      end
+    end
+
+    context 'given invalid data' do
+      it 'must have all fields correctly filled' do
+        # Starting an Application, Form not Completed
+        #
+        # As a visitor
+        # When I visit the new application page
+        # And I fail to fill in any of the form fields
+        # And I click submit
+        # Then I am taken back to the new applications page
+        # And I see a message that I must fill in those fields.
+
+        visit "/applications/new"
+
+        fill_in "Name", with: "Megan"
+        fill_in "Street Address", with: "5678 W. second St."
+
+        click_button "Submit"
+        save_and_open_page
+
+        expect(current_path).to eq("/applications/new")
+        expect(page).to have_content("Error: City can't be blank, Zip code can't be blank, Zip code is not a number")
+      end
+    end
   end
 end
