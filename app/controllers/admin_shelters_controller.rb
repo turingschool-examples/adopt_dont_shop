121c2b10881d84bcd583584f9797c413534a9682
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-20 15:56:02 -0400

    Created method for index and show for admin shelters with proper sort

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,5 +1,10 @@
 class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.all
+    @reversed = @shelters.reverse_alphabetical_order
+  end
+
+  def show
+    @shelter = Shelter.find(params[:id])
   end
 end
