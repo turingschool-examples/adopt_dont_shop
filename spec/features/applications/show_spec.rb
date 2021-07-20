4c6ecb7450a55486d2fb3b868fcde75d301051eb
Brett Jones <bhjones4545@gmail.com>
2021-07-19 21:29:00 -0600

    WIP

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -48,7 +48,8 @@ RSpec.describe 'application show page' do
     expect(page).to_not have_content(pet_2.name)
   end
 
-  it ' can add pet to application' do
+  it 'can add pet to application' do
+    Pet.destroy_all
     shelter = Shelter.create!(name:'Boise Shelter', city: 'Boise', foster_program: false, rank: 7)
     pet_1 = Pet.create!(adoptable: true, age: 2, breed: 'Bulldog', name: 'Butch', shelter_id: shelter.id)
     pet_2 = Pet.create!(adoptable: true, age: 1, breed: 'German Shpherd', name: 'Sandy', shelter_id: shelter.id)
@@ -66,5 +67,24 @@ RSpec.describe 'application show page' do
     expect(page).to have_content(pet_1.name)
   end
 
-  it 'can submit application when pet is added'
+  it 'can submit application when pet is added' do
+    Pet.destroy_all
+    shelter = Shelter.create!(name:'Boise Shelter', city: 'Boise', foster_program: false, rank: 7)
+    pet_1 = Pet.create!(adoptable: true, age: 2, breed: 'Bulldog', name: 'Butch', shelter_id: shelter.id)
+    pet_2 = Pet.create!(adoptable: true, age: 1, breed: 'German Shpherd', name: 'Sandy', shelter_id: shelter.id)
+    pet_3 = Pet.create!(adoptable: false, age: 4, breed: 'Poodle', name: 'Zinnia', shelter_id: shelter.id)
+    application = Application.create!(name: "Taylor Hammond", street_address: "5234 Broadway Avenue", city: "Dallas", state: "Texas", zip_code: '86754', description: "I am very good person", status: "In Progress")
+
+    visit "/applications/#{application.id}"
+
+    fill_in 'search', with: "tc"
+    click_on "commit"
+
+    click_on("Adopt this Pet")
+
+    fill_in 'description', with: "I am very good person"
+    click_on "Submit Application"
+
+    expect(page).to have_content("I am very good person")
+  end
 end
