61c12b2ddea2a907aeda6f3a714004fbe328e4f8
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-19 21:37:33 -0600

    Complete user story 24

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,8 +8,12 @@ class ApplicationsController < ApplicationController
 
   def create
     application = Application.create(application_params)
-    application.save
-    redirect_to "/applications/#{application.id}"
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error(s): #{error_message(application.errors)}"
+    end
   end
 
   private
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Application do
     it { should have_many :pet_applications }
     it { should have_many(:pets).through(:pet_applications)}
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_presence_of(:description) }
+    it { should validate_numericality_of(:zip_code) }
+    # it { should validate_length_of(:zip_code).is_equal_to(5) }
+  end
 end
