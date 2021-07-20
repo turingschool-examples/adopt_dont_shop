f5d5b6f9dce089f002cfa518e3d5e312c9c50839
Brett Jones <bhjones4545@gmail.com>
2021-07-19 22:01:12 -0600

    Add Faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Application.destroy_all
+Shelter.destroy_all
+Pet.destroy_all
+
+3.times do
+  Shelter.create do |shelter|
+    shelter.name = Faker::Company.name
+    shelter.city = Faker::Address.city
+    shelter.foster_program = [true, false].sample
+    shelter.rank = rand(1..10)
+    7.times do
+      shelter.pets.new do |pet|
+        pet.name = Faker::Creature::Dog.name
+        pet.breed = Faker:: Creature::Dog.breed
+        pet.age = rand(1..18)
+        pet.adoptable = [true, false].sample
+      end
+    end
+  end
+end
+
+3.times do
+  Application.create do |app|
+    app.name = Faker::Name.name
+    app.street_address = Faker::Address.street_address
+    app.city = Faker::Address.city
+    app.state = Faker::Address.state
+    app.zip_code = Faker::Address.zip_code
+    app.description = Faker::Fantasy::Tolkien.poem
+  end
+end
