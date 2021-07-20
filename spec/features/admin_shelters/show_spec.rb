f87ffd4531e1dbd41064aa860b7f452af716b8bf
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-20 18:26:35 -0400

    Create admin_shelters show spec

--- spec/features/admin_shelters/show_spec.rb
+++ spec/features/admin_shelters/show_spec.rb
@@ -1,1 +1,12 @@
-# FILL THIS IN
+require 'rails_helper'
+
+RSpec.describe 'the admin shelter show page' do
+  it "shows the shelter name and full address" do
+    shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+
+    visit "/admin/shelters/#{shelter.id}"
+
+    expect(page).to have_content(shelter.name)
+    expect(page).to have_content(shelter.city)
+  end
+end
