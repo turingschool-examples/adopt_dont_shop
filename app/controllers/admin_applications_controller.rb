7078feef563ce6d40114d6a3c575e50bd97a5dfa
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-19 19:32:04 -0600

    Reconfigure accept/reject logic

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -2,10 +2,17 @@ class AdminApplicationsController < ApplicationController
   def show
     @applicant = Applicant.find(params[:id])
     @pets = @applicant.associated_pets(params[:id])
-    if params[:pet_id].nil?
-
+    if @pet_application.nil?
+      @pet_application = PetApplicant.where("applicant_id = ?", params[:id]).limit(1).first
     else
-      @pet_id = (params[:pet_id])
+
     end
+  end
+
+  def update
+    @pet_application = PetApplicant.where(["pet_id = ? and applicant_id = ?", params[:pet_id], params[:id]])
+    @pet_application.update(status: params[:status])
+
+    redirect_to "/admin/applications/#{params[:id]}"
   end
 end
