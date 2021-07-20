22fb9c5394ace2168fc5090f2b678b5c2cce546c
Brett Jones <bhjones4545@gmail.com>
2021-07-19 22:04:53 -0600

    Add patch route for applications

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
   post '/applications', to: 'applications#create'
+  patch 'applications/:id', to: 'applications#update'
 
   post '/pet_applications', to: 'pet_applications#create'
 end
