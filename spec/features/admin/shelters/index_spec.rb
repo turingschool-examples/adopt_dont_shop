babf787b87ee7ec02a47408b8377f7bbce4c11d0
Jason Knoll <jason.p.knoll@gmail.com>
2021-07-21 10:45:31 -0700

    Add spec test for pending applications

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -6,13 +6,30 @@ RSpec.describe 'Admin Shelters Index' do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    @pirate = @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    @clawdia = @shelter_2.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    @ann = @shelter_3.pets.create!(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
+    @megan = Application.create!(name: "Megan", street_address: "12345 E street", city: "Phoenix", state: "AZ", zip_code: "99999", description: "Ipsum", status: "Pending")
+    @danny = Application.create!(name: "Danny", street_address: "5678 W road", city: "Scottsdale", state: "AZ", zip_code: "88888", description: "lorem", status: "In Progress")
   end
 
   it 'see all Shelters in the system listed in reverse alphabetical order by name' do
     visit "admin/shelters"
     save_and_open_page
 
-    expect(page).to have_content([@shelter_2, @shelter_3, @shelter_1])
+
+    expect(@shelter_2.name).to appear_before(@shelter_3.name)
+    expect(@shelter_3.name).to appear_before(@shelter_1.name)
   end
 
+  xit "can see shelters with pending applications" do
+    @megan.pets << @pirate
+    @megan.pets << @ann
+    @danny.pets << @clawdia
+
+    visit "admin/shelters"
+
+    expect(page).to have_content("Aurora shelter")
+    expect(page).to have_content("Fancy pets of Colorado")
+  end
 end
