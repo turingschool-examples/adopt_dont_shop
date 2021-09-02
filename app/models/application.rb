5b7f5c44b008bd5f2eb002c8faef322f83fae1eb
Weston Ellis <ellisweston112@gmail.com>
2021-09-02 17:22:56 -0600

    Create relationships

---
+++ app/models/application.rb
@@ -0,0 +1,13 @@
+class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :zip, presence: true
+
+  has_many :pet_applications
+  has_many :pets, through: :pet_applications
+
+
+
+end
---
+++ app/models/pet_application.rb
@@ -0,0 +1,5 @@
+class PetApplication < ApplicationRecord
+  belongs_to :application
+  belongs_to :pet
+
+end
