b80148be375404d5e5df7edba35be63e55a1dc6b
Tanner Dale <tanner@tannerdale.com>
2021-09-02 18:58:15 -0500

    Add pet searching model method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -12,4 +12,8 @@ class Pet < ApplicationRecord
   def self.adoptable
     where(adoptable: true)
   end
+
+  scope :search_for, ->(pet_name) {
+    where('name ILIKE ?', '%' + pet_name + '%')
+  }
 end
