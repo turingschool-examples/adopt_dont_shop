26bc27e91419b0d9afb99760644614a01b8f15a9
Tanner Dale <tanner@tannerdale.com>
2021-09-02 18:40:33 -0500

    Add new application tests, views, controller actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,9 +8,18 @@ class ApplicationsController < ApplicationController
   end
 
   def new
+    @application = Application.new
   end
 
   def create
+    new_app = Application.new(application_params)
+    if new_app.save
+      flash[:success] = 'Application Submitted!'
+      redirect_to application_path(new_app.id)
+    else
+      flash[:alert] = 'Incomplete Application'
+      redirect_to new_application_path
+    end
   end
 
   def edit
@@ -20,5 +29,11 @@ class ApplicationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def application_params
+    params.require(:application).permit(:name, :address, :city, :state, :zipcode, :reason)
   end
 end
