9af251ee9d28b3113e97b58a274158bbc6786c13
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-02 18:18:22 -0400

    Write original feature test, create controller and views page for show

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'the applications show' do
+  it "shows the application and all it's attributes" do
+    application = Application.create!(name: "Kevin Mugele", street_address: "694 Glen Road", city: "Sparta", state: "New Jersey", zip_code: 90210, status: "Open", statement: "I am looking to give a dog their forever home")
+
+    visit "/applications/#{application.id}"
+
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.street_address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zip_code)
+    expect(page).to have_content(application.status)
+    expect(page).to have_content(application.statement)
+  end
+end
