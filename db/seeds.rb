f3021cfeae96b8a929af22d112e52c927f67c659
Weston Ellis <ellisweston112@gmail.com>
2021-09-02 19:04:41 -0600

    Hunt bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+app = Application.create!(name: 'Billy', city: 'Denver', street_address: '123 lion st', state: 'CO', zip: 12345 )
+shelter = Shelter.create!(foster_program: true,
+ name: 'Bundle park',
+ city: 'Denver',
+ rank: 3
+)
+dog1 = shelter.pets.create!(adoptable: true,
+   age: 2,
+   breed: 'yes',
+   name: 'Bob'
+ )
+dog2 = shelter.pets.create!(adoptable: true,
+  age: 2,
+  breed: 'yes',
+  name: 'Billy'
+)
+PetApplication.create!(pet: dog1, application: app)
+PetApplication.create!(pet: dog2, application: app)
