113906f0143890876a35d1f2675313e9cf86f0ee
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-03 00:40:12 -0700

    Add has many relation to application pets; applications through application pets

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Pet, type: :model do
   describe 'relationships' do
     it { should belong_to(:shelter) }
+    it { should have_many :application_pets }
+    it { should have_many(:applications).through(:application_pets)}
   end
 
   describe 'validations' do
