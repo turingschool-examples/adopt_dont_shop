89972855524ff87aeb400b292ac81b971f7ec814
Laura Court <laura.mcourt@gmail.com>
2021-09-02 22:27:55 -0400

    Create relationship between application and pets

---
+++ spec/models/application_pet_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationPet, type: :model do
+  it { should belong_to :application}
+  it { should belong_to :pet}
+
+end
---
+++ spec/models/application_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Application do
+  it { should have_many :application_pets }
+  it { should have_many(:pets).through(:application_pets)}
+
+end
