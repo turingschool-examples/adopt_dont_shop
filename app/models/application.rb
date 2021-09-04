177d9afd14901e83edcf3e346bad5990898e45bd
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-04 10:50:57 -0600

    Add validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,12 @@ class Application < ApplicationRecord
   has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
 
+  validates :name,    presence: true
+  validates :address, presence: true
+  validates :city,    presence: true
+  validates :state,   presence: true
+  validates :zip,     presence: true
+
   def full_address
     "#{address}, #{city}, #{state} #{zip}"
   end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe Application, type: :model do
     it { should have_many(:pets).through(:application_pets) }
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+  end
+
   before(:each) do
     @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter.id)
