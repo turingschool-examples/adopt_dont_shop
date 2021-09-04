d7ca970d8d954e8e120c2cfd20cd8263e2aad912
Kelsey Thompson <kelsthompson2@gmail.com>
2021-09-03 18:15:08 -0600

    Write tests and implementation code for starting an application

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'show page' do
+RSpec.describe 'new page' do
   before(:each) do
     @shelter_1 = Shelter.create!(foster_program: true, name: "Best Shelter", city: "Daytona", rank: 4)
     @application_1 = Application.create!(name: "Freddy Thomas", street: "123 Sesame Street", city: "Daytona", state: "FL", zip_code: "12345", description: "I really love dogs", status: "Pending")
@@ -10,17 +10,14 @@ RSpec.describe 'show page' do
     PetApplication.create!(application: @application_1, pet: @pet_2)
   end
 
-  it 'displays all attributes of the application' do
-    visit "/applications/#{@application_1.id}"
-
-    expect(page).to have_content(@application_1.name)
-    expect(page).to have_content(@application_1.street)
-    expect(page).to have_content(@application_1.city)
-    expect(page).to have_content(@application_1.state)
-    expect(page).to have_content(@application_1.zip_code)
-    expect(page).to have_content(@application_1.description)
-    expect(page).to have_content(@application_1.status)
-    expect(page).to have_link(@pet_1.name)
-    expect(page).to have_link(@pet_2.name)
+  it 'has an application for a new pet' do
+    visit "applications/new"
+    fill_in :name, with: 'Sara Pounds'
+    fill_in :street_address, with: "2200 W Petty Dr"
+    fill_in :city, with: 'Muncie'
+    fill_in :state, with: 'IN'
+    fill_in :zip_code, with: '47304'
+    click_on "Submit"
+    expect(current_path).to eq("/applications/#{Application.last.id}")
   end
 end
