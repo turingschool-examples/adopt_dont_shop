c4d7e2619c694a7535eb1b424dd28c090ae559ef
Weston Ellis <ellisweston112@gmail.com>
2021-09-04 08:59:19 -0600

    Write test for story

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -27,5 +27,20 @@ RSpec.describe 'new application page' do
     expect(page).to have_content('In Progress')
   end
 
+  it 'can return a failure to fill message' do
+    visit '/application/new'
+
+    click_button 'submit'
+
+    expect(current_path).to eq('/application/new')
+    expect(page).to have_content('please fill the form completely')
+  end
 
 end
+#
+# As a visitor
+# When I visit the new application page
+# And I fail to fill in any of the form fields
+# And I click submit
+# Then I am taken back to the new applications page
+# And I see a message that I must fill in those fields.
