4fa98e4a52b61db9b425da07822c3980c22b534a
Ted S <tstaros23@gmail.com>
2021-09-04 21:53:59 -0600

    Finish user story 2

---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+# # When I fill in this form with my:
+#   - Name
+#   - Street Address
+#   - City
+#   - State
+#   - Zip Code
+# And I click submit
+# Then I am taken to the new application's show page
+# And I see my Name, address information, and description of why I would make a good home
+# And I see an indicator that this application is "In Progress"
+
+RSpec.describe 'New Application' do
+  describe 'As a visitor' do
+    describe 'When I visit the new application form by clicking a link on the pet index' do
+      it "I can create a new application" do
+        visit '/applications/new'
+
+        fill_in 'Name', with: 'Ted'
+        fill_in 'Street', with: '11 Revere Dr.'
+        fill_in 'City', with: 'Barrington'
+        fill_in 'State', with: 'Illinois'
+        fill_in 'Zip', with: '60010'
+        fill_in 'description', with: 'blahblah'
+
+        click_button 'Submit'
+
+        expect(current_path).to eq("/applications/#{Application.last.id}")
+      end
+    end
+  end
+end
