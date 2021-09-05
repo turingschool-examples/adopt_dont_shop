35fa466b748cc3a145fb8a29eb17d0a1da8ba132
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-04 19:43:27 -0600

    Add validations for name, address, and status

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,8 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :status, presence: true
+  
   has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
 end
