3dc674643a3f6b74f86e6b87ecfcf7a58b3c87e8
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-04 19:43:00 -0600

    Add tests for validations

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Application, type: :model do
     it {should have_many(:application_pets)}
     it {should have_many(:pets).through(:application_pets)}
   end
+
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:address)}
+    it {should validate_presence_of(:status)}
+  end
 end
