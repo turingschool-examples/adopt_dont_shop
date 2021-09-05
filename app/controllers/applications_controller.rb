4cade5cb98abb4d7b9671518f31f66a963365200
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-04 18:21:53 -0600

    Add new page with routes and functionality in applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,4 +6,18 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:app_id])
   end
+
+  def new
+  end
+
+  def create
+    application = Application.new({
+      name: params[:name],
+      address: "#{params[:street]} #{params[:city]}, #{params[:state]} #{params[:zip]}",
+      description: params[:description],
+      status: "In Progress"
+      })
+    application.save
+    redirect_to "/applications/#{application.id}"
+  end
 end
