8fc143401535c25cae7396137803fe9ebf52f31e
Blake Dempsey <bdempsey864@gmail.com>
2021-09-05 14:48:04 -0600

    Add seeds

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'applicant information' do
   before :each do
     @application = Application.create!(name: 'Murph', street: '123 Apple St', city: 'Boca Raton', state: 'FL', zip: 33498, description: 'I want Jack', status: 'in progress')
     @shelter_1 = Shelter.create!(name: 'Pups and Suds', city: 'Fort Collins', foster_program: false, rank: 2)
-    @pet1 = Pet.create!(name: 'Jack', age: 4, breed: 'Australian Shepherd', adoptable: true, shelter_id: @shelter_1.id)
-    @pet2 = Pet.create!(name: 'Cooper', age: 3, breed: 'Golden Retriever', adoptable: true, shelter_id: @shelter_1.id)
+    @pet1 = @shelter_1.pets.create!(name: 'Jack', age: 4, breed: 'Australian Shepherd', adoptable: true)
+    @pet2 = @shelter_1.pets.create!(name: 'Cooper', age: 3, breed: 'Golden Retriever', adoptable: true)
   end
 
   it 'displays applicant information' do
     visit "/applications/#{@application.id}"
-
+    
     expect(page).to have_content(@application.name)
     expect(page).to have_content(@application.street)
     expect(page).to have_content(@application.city)
