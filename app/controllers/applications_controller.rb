55a17a599d60dd1c2436ee7d82e50ccc904e3f41
Andrew Massey <acmassey@ncsu.edu>
2021-09-06 12:18:29 -0400

    Add #new and #create actions to controller. Include strong params

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationsController < ApplicationController
    def show
-     @application = Application.find(params[:application_id])
+     @application = Application.find(params[:id])
    end
+
+   def new
+   end
+
+   def create
+     application = Application.create(application_params)
+     if application.save
+       redirect_to "/applications/#{application.id}"
+     else
+       flash[:notice] = "Application not created: Required information missing."
+       redirect_to "/applications/new"
+     end
+   end
+
+
+private
+  def application_params
+    params
+    .permit(:name, :street, :city, :state, :zip, )
+    .with_defaults(status:"In Progress", message: "")
+  end
 end
