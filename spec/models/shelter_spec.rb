a4949dd481131bd413290a93ef282a3167a536a8
Weston Ellis <ellisweston112@gmail.com>
2021-09-06 14:00:47 -0600

    Write test for model side of story

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -41,6 +41,12 @@ RSpec.describe Shelter, type: :model do
         expect(Shelter.order_by_number_of_pets).to eq([@shelter_1, @shelter_3, @shelter_2])
       end
     end
+
+    describe '#order_by_name_reverse_alphabetical' do
+      it 'orders name by reverse alphabetical' do
+        expect(Shelter.order_by_name_reverse_alphabetical).to eq([@shelter_1, @shelter_3, @shelter_2])
+      end
+    end
   end
 
   describe 'instance methods' do
