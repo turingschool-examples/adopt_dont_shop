79912cc442598e75ed420ace03b59e1b77cdc048
Andrew Massey <acmassey@ncsu.edu>
2021-09-06 09:45:25 -0400

    Update Pet model with new relationships

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,8 @@
 class Pet < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true, numericality: true
+  has_many :application_pets
+  has_many :applications, through: :application_pets
   belongs_to :shelter
 
   def shelter_name
