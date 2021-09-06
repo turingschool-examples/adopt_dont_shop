4ae6879dc208ce04f782da7ee7c16d2c14883f8f
Blake Dempsey <bdempsey864@gmail.com>
2021-09-05 20:19:48 -0600

    Issue hitting 'create'

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,20 @@ class ApplicationsController < ApplicationController
   def show 
     @application = Application.find(params[:id])
   end
+
+  def new 
+  end
+
+  def create
+    require "pry"; binding.pry
+    @application = Application.new(app_params)
+    if @application.save 
+      redirect_to "/applications/#{@application.id}"
+    end
+  end
+
+  private
+  def app_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description)
+  end
 end
