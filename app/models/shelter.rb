3db285c971fa94551d06cf180d05e85a48c4628d
Weston Ellis <ellisweston112@gmail.com>
2021-09-06 14:05:55 -0600

    Write datalogic of story

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -16,6 +16,10 @@ class Shelter < ApplicationRecord
       .order("pets_count DESC")
   end
 
+  def self.order_by_name_reverse_alphabetical
+    find_by_sql("SELECT shelters.* FROM shelters ORDER BY shelters.name")
+  end
+
   def pet_count
     pets.count
   end
