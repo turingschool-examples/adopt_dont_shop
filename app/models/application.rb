130c02812b2511c4077ceaf2ebdb12fed4bab686
Andrew Massey <acmassey@ncsu.edu>
2021-09-06 12:17:51 -0400

    Add validations to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+  validates :name, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
 end
