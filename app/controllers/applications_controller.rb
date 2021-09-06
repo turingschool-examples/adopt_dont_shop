806740e90b6f5578587239311831bc811ade8162
Blake Dempsey <bdempsey864@gmail.com>
2021-09-06 10:25:12 -0600

    Finish ucreate application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,15 +7,17 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    require "pry"; binding.pry
     @application = Application.new(app_params)
     if @application.save 
       redirect_to "/applications/#{@application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "The one who fills out all the fields, has the greatest chance of getting a pet!"
     end
   end
 
   private
   def app_params
-    params.permit(:name, :street_address, :city, :state, :zip_code, :description)
+    params.permit(:name, :street, :city, :state, :zip, :description)
   end
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,3 @@
 class Application < ApplicationRecord
-  
+  validates_presence_of :name, :street, :city, :state, :zip
 end
