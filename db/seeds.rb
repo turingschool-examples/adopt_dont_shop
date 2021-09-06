6ab88e91769d6282c0060e8bc67563ee2f05e931
Andrew Massey <acmassey@ncsu.edu>
2021-09-06 19:34:37 -0400

    Create seeds for partial match testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,5 @@ application = Application.create!(name: "Andrew Massey", street: "4433 Old Forge
 shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
 pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
 pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-pet_3 = Pet.create!(adoptable: true, age: 10, breed: 'lab', name: "Lobsterlover", shelter_id: shelter.id)
+pet_3 = Pet.create!(adoptable: true, age: 10, breed: 'lab', name: "Lobsterman", shelter_id: shelter.id)
+pet_4 = Pet.create!(adoptable: true, age: 8, breed: 'lab', name: "Lobby", shelter_id: shelter.id)
