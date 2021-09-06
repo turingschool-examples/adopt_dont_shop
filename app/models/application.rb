e745c4dd88de19f55f742249db6ec202af541c86
Andrew Massey <acmassey@ncsu.edu>
2021-09-06 19:25:26 -0400

    Add dependent: :destroy to pet and application models

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
-  has_many :application_pets
+  has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
   validates :name, presence: true
   validates :street, presence: true
