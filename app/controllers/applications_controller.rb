c8bb2c0ff1493afa179e02e60ba8208f9cfb38b5
Ida Olson <idaolson@gmail.com>
2021-09-06 16:39:30 -0600

    Update applications controller with create and new methods

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,20 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    application = Application.create(app_params)
+
+    application.save
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+
+  def app_params
+    params.permit(:id, :name, :address, :city, :state, :zip, :description)
+  end
 end
