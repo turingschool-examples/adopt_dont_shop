4b47c4d7d905df19d9ad6f2dc4067b91c5310be4
Weston Ellis <ellisweston112@gmail.com>
2021-09-06 09:38:22 -0600

    Write model test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Application do
     it { should have_many(:pets).through(:pet_applications) }
   end
 
+  describe 'methods' do
+
+      it 'can tell if no pets' do
+        app = Application.create!(name: 'Hugh',
+          city: 'Aurora',
+          street_address: '300 quebec st',
+          state: 'CO',
+          zip: 12345
+        )
+
+        expect(app.dogs?).to eq(false)
+      end
+  end
 end
