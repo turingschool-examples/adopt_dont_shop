b2f52063b5251fde53637f4384f2f75b374e4ced
Andrew Massey <acmassey@ncsu.edu>
2021-09-06 21:39:53 -0400

    Add @pending_shelters to the index action

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,5 +1,6 @@
 class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.reverse_alphabetical_names
+    @pending_shelters = Shelter.pending_applications
   end
 end
