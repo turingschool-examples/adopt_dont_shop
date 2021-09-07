61ab41529ac12afedf438351fbd97cebb36205f0
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-06 19:11:15 -0600

    Add pet count instance method

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,11 @@ class Application < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :status, presence: true
-  
+
   has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
+
+  def pet_count
+    pets.count
+  end
 end
