111ccf617a4a490673316d5901cae01df3456b89
Andrew Massey <acmassey@ncsu.edu>
2021-09-06 21:38:22 -0400

    Refactor before :each and add test for showing pending applications

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'Admin shelters index' do
     @shelter_2 = Shelter.create!(name: 'Atlanta shelter', city: 'Atlanta, GA', foster_program: true, rank: 5)
     @shelter_3 = Shelter.create!(name: 'Charlotte shelter', city: 'Charlotte, NC', foster_program: true, rank: 2)
     @shelter_4 = Shelter.create!(name: 'Seattle shelter', city: 'Seattle, WA', foster_program: true, rank: 8)
+
+    @application = Application.create!(name: "Andrew Massey", street: "560 Wakefield Dr", city: "Charlotte", state: "NC", zip: 28056, message: "Please choose me!", status:"Pending")
+    @lucille = @shelter_1.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_1.id)
+    @application_pet = ApplicationPet.create(application: @application, pet: @lucille)
   end
 
   it "lists all the shelters" do
@@ -17,5 +21,12 @@ RSpec.describe 'Admin shelters index' do
     expect(page).to have_content(@shelter_2.name)
     expect(page).to have_content(@shelter_3.name)
     expect(page).to have_content(@shelter_4.name)
+  end
+
+  it "Shows shelters with pending applications" do
+    visit "/admin/shelters"
+
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_1.city)
   end
 end
