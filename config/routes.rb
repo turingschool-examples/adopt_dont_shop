f7f6255e14ede98cd63193385bedde09841b4d2a
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-06 21:38:49 -0600

    Add admin controller group with index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
 
+  get '/admin/shelters', to: 'admin/shelters#index'
+
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
   get '/pets/:id/edit', to: 'pets#edit'
