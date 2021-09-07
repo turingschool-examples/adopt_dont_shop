23997bbc4be7a3077780723486ecb545f23130cf
Ida Olson <idaolson@gmail.com>
2021-09-06 20:12:38 -0600

    Add validatation to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,6 @@
 class Application < ApplicationRecord
   has_many :application_pets, dependent: :destroy
   has_many :pets, through: :application_pets
+  validates_presence_of :name, :address, :city, :state, :zip
 
-end 
+end
