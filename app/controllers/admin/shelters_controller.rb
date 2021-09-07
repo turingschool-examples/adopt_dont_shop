88b836de54c36b9731d662c0b6df921524e74394
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-07 14:50:50 -0600

    Add logic to index action and view to return shelters with pending apps

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SheltersController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
+    @pending = Shelter.joins(pets: :applications).where('applications.status' => "Pending")
   end
 end
