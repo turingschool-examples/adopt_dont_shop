b5157bde77ca38535ac3c7eeb48eb17054a1718d
Jessica Jones <daddehjones@gmail.com>
2021-09-07 20:58:43 -0400

    Create controllers.

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationsController < ActionController::Base
+  def welcome
+  end
+
+  def show
+    @application = Application.find(params[:id])
+  end
+
+  private
+
+  def error_message(errors)
+    errors.full_messages.join(', ')
+  end
+
+
+end
