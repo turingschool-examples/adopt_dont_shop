e372de9ca67ee543ffa2776f9faefe03bcaedbf6
Jessica Jones <daddehjones@gmail.com>
2021-09-07 21:54:16 -0400

    Create user story 2 expectation.

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'the pets index' do
     shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
     pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
 
     visit "/pets"
 
@@ -80,5 +81,19 @@ RSpec.describe 'the pets index' do
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
+  end
+
+  it "can display a link to start a new application" do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+
+    visit "/pets"
+    save_and_open_page
+    
+    click_link "New Application Here"
+
+    expect(current_path).to eq("/applications/new")
   end
 end
