074387520fdca30bb1f4ea63cd3a43700b984884
Weston Ellis <ellisweston112@gmail.com>
2021-09-08 17:05:56 -0600

    Finish model helper and test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -33,6 +33,36 @@ RSpec.describe Application do
 
       expect(app.dogs?).to eq(true)
     end
+
+    it 'can get the corresponding pet app' do
+      app = Application.create!(name: 'Hugh',
+        city: 'Aurora',
+        street_address: '300 quebec st',
+        state: 'CO',
+        zip: 12345
+      )
+      shelter = Shelter.create!(foster_program: true,
+        name: 'Bundle park',
+        city: 'Denver',
+        rank: 3
+      )
+      dog1 = shelter.pets.create!(adoptable: true,
+        age: 2,
+        breed: 'yes',
+        name: 'Bob'
+      )
+      dog2 = shelter.pets.create!(adoptable: true,
+        age: 3,
+        breed: 'no',
+        name: 'Billy'
+      )
+
+      pet_app = PetApplication.create!(application: app, pet: dog1)
+      PetApplication.create!(application: app, pet: dog2)
+
+      # require "pry"; binding.pry
+      expect(app.find_pet_app(dog1.id)).to eq(pet_app)
+    end
   end
 
 end
