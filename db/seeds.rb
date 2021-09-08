91d15333316244dc0794f4dabe8f6d7bedf06c5e
Ida Olson <idaolson@gmail.com>
2021-09-07 19:37:06 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,18 @@ ApplicationPet.destroy_all
 @shelter = Shelter.create!(name: 'Cheyenne Animal Shelter', city: 'Cheyenne', foster_program: false, rank: 2)
 @pet_1 = @shelter.pets.create!(name: 'Cassio', age: 2, breed: 'Akbash', adoptable: true, shelter_id: @shelter.id)
 @pet_2 = @shelter.pets.create!(name: 'Bianca', age: 1, breed: 'Great Pyrenees',  adoptable: true, shelter_id: @shelter.id)
+@app_1 = Application.create!(
+  name: "Sarah Carter",
+  address: "1108 Ichabod St",
+  city: "North Pole",
+  state: "Alaska",
+  zip: "99705",
+  description: "I will name him George, and I will hug him, and pet him, and squeeze him")
+@app_2 = Application.create!(
+  name: "Doug Olson",
+  address: "2866 Villalobos Ave",
+  city: "North Pole",
+  state: "Alaska",
+  zip: "99705",
+  description: "Really more of a cat person but dogs are okay...I guess."
+)
