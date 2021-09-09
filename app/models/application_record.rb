29e1b07699c90d3918b75ecdf26f89e164e3c743
Ida Olson <idaolson@gmail.com>
2021-09-08 21:10:21 -0600

    Re-add something I removed

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  def self.search(search_params)
+    where("name ILIKE ?", "%#{search_params}%")
+  end
+
   def self.search_for(search_params)
     where("name ILIKE ?", "%#{search_params}%")
   end
