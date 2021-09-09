ed895d948e3245726ba7b1068cd080974f1ba10f
Ida Olson <idaolson@gmail.com>
2021-09-08 21:10:29 -0600

    Add missing test

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,8 @@
 class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
+
+  def self.find_by_ids(pet_id, app_id)
+    where(pet_id: pet_id, application_id: app_id).first
+  end
 end
