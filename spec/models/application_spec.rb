2430d9892d197cbc744f3b75289fc41124af3589
Gregory Fischer <gwhoisj@gmail.com>
2021-10-21 17:14:10 -0400

    Feat: relational tests written

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Application do
   describe 'relationships' do
     it {should have_many :application_pets}
+    it {should have_many(:pets).through(:application_pets)}
   end
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Pet, type: :model do
   describe 'relationships' do
     it { should belong_to(:shelter) }
     it {should have_many :application_pets}
+    it {should have_many(:applications).through(:application_pets)}
   end
 
   describe 'validations' do
