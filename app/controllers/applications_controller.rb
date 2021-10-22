4692491953181821ebff236e37e49e0855177756
Sam Devine <samuel.will.devine@gmail.com>
2021-10-22 15:29:29 -0600

    add name validation to new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,7 +3,23 @@ class ApplicationsController < ApplicationController
   def new
   end
 
+  def create
+    application = Application.new(application_params)
+
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error: #{error_message(application.errors)}"
+    end
+  end
+
   def show
     @application = Application.find(params[:id])
   end
+
+  private
+    def application_params
+      params.permit(:id, :name, :address, :city, :state, :zip, :description, :status)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,4 +39,5 @@ Rails.application.routes.draw do
 
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
+  post '/applications', to: 'applications#create'
 end
