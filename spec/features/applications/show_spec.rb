b88ee580be9ab4b49afbb36d3ee400308d98056c
Sam Devine <samuel.will.devine@gmail.com>
2021-10-22 16:51:16 -0600

    fiddling with applications pet search

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -50,5 +50,38 @@ RSpec.describe 'the application show page' do
 
       expect(current_path).to eq "/pets/#{fluffy.id}"
     end
+
+    describe 'when the application has not been submitted' do
+      describe 'Pet Lookup' do
+        it 'has a search bar to lookup pets by name' do
+          visit "applications/#{application.id}"
+
+          expect(page).to have_button 'Search'
+        end
+
+        it 'lists partial matches as search results' do
+          shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+          pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+          pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+          pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+
+          visit "/applications/#{application.id}"
+
+          fill_in 'Search', with: "Ba"
+          click_on("Search")
+
+          expect(current_path).to eq "/applications/#{application.id}"
+          expect(page).to have_content(pet_1.name)
+          expect(page).to have_content(pet_2.name)
+          expect(page).to_not have_content(pet_3.name)
+        end
+      end
+
+      it 'pets can be added to the application' do
+      end
+    end
+
+    describe 'when the application has been submitted' do
+    end
   end
 end
