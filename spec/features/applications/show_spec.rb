3f81582bef330b2dc0e4a0a4a750f530d45599c6
Nathan Brown <nathan.brown263@gmail.com>
2021-10-23 12:55:17 -0600

    Add search form to application show page (rendering matching pets to come)

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,26 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe 'the application show' do
-  it 'shows the application and all its attributes' do
-    application = Application.create!(
-                                      name: "Nate Brown",
-                            street_address: "2000 35th Avenue",
-                                      city: "Denver",
-                                     state: "CO",
-                                       zip: "90210",
-                               description: "I have a big backyard",
-                                    status: "In Progress"
-                                      )
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    application.pets.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-    application.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    visit "/applications/#{application.id}"
-    
-    expect(page).to have_content(application.name)
-    expect(page).to have_content(application.full_address)
-    expect(page).to have_content(application.description)
-    expect(page).to have_content(application.status)
-    page.has_link?("Lobster")
-    page.has_link?("Lucille Bald")
+RSpec.describe 'application show' do
+  let(:shelter) { Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9) }
+  let(:application) { Application.create!(
+                                    name: "Nate Brown",
+                          street_address: "2000 35th Avenue",
+                                    city: "Denver",
+                                   state: "CO",
+                                     zip: "90210",
+                             description: "I have a big backyard",
+                                  status: "In Progress"
+                                         )}
+  describe 'the application show page' do
+    it 'shows the application and all its attributes (with links to pets)' do
+      application.pets.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      application.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+
+      visit "/applications/#{application.id}"
+
+      expect(page).to have_content(application.name)
+      expect(page).to have_content(application.full_address)
+      expect(page).to have_content(application.description)
+      expect(page).to have_content(application.status)
+      page.has_link?("Lobster")
+      page.has_link?("Lucille Bald")
+    end
+
+    it 'renders the pet search' do
+      expect(page).to have_content("Add a Pet to this Application")
+      expect(find('form')).to have_content("Search for a pet:")
+    end
+  end
+
+  describe 'the pet search' do
+    context 'when valid data entered' do
+      it 'searches for an existing pet and displays it on the show page' do
+        pet = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Teddy', shelter_id: shelter.id)
+
+        fill_in 'Name', with: 'Teddy'
+        click_button 'Submit'
+
+        expect(page).to have_current_path("/applications/#{Application.last.id}")
+        expect(page).to have_content("Teddy")
+      end
+    end
+    context 'when no data entered' do
+      it 'returns to show page' do
+        click_button 'Submit'
+        expect(page).to have_current_path("/applications/#{Application.last.id}")
+      end
+    end
   end
 end
