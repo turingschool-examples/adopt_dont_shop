4a91adcaeb64e5d2958f89eafd52fb2da2f22164
Chaz Simons <chaz.simons@gmail.com>
2021-10-23 09:25:35 -0700

    Add many-to-many relationship between applications and pets

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,6 @@
 class Application < ApplicationRecord
   has_many :application_pets
+  has_many :pets, through: :application_pets
   validates :name, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
