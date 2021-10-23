b945bc0532391c1b4af50685b57ffb00703412bc
Nathan Brown <nathan.brown263@gmail.com>
2021-10-22 22:01:19 -0600

    Add links for pets on application show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -11,12 +11,16 @@ RSpec.describe 'the application show' do
                                description: "I have a big backyard",
                                     status: "In Progress"
                                       )
-
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    application.pets.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+    application.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
     visit "/applications/#{application.id}"
-
+    
     expect(page).to have_content(application.name)
     expect(page).to have_content(application.full_address)
     expect(page).to have_content(application.description)
     expect(page).to have_content(application.status)
+    page.has_link?("Lobster")
+    page.has_link?("Lucille Bald")
   end
 end
