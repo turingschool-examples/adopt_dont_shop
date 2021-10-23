0c980847af7c33fef9121a3594f1d569590bd146
Chaz Simons <chaz.simons@gmail.com>
2021-10-23 09:34:25 -0700

    Add test application validations

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Application, type: :model do
   describe 'relationships' do
     it { should have_many(:pets).through(:application_pets) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+  end
 end
