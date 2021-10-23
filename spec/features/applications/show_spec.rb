bd4d73278cfec0238b6a855845dc2e21230b8ccc
Karan Mehta <karanm645@gmail.com>
2021-10-23 12:41:59 -0500

    Create Show spec

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,7 @@
+# require 'rails_helper'
+
+# RSpec.describe 'the application show' do 
+#   it 'shows the applicant and all its attributes' do 
+#     applicant = Applicant.create(name: 'Shaggy Rogers', street_address: '544 Mystery Machine Lane', city: 'Los Angeles', state: 'CA', zip_code: '90210', reason: 'Because I am scared 24/7', status: "rejected")
+#   end 
+# end 
