2cf3b0c3466e9ebda5b2d007b07cf614c6082a1e
Matthew Kimball <mekimba6@gmail.com>
2021-10-23 13:18:51 -0600

    adds application show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   def welcome
   end
 
+  def show
+    @application = Application.find(params[:id])
+  end
+
   private
 
   def error_message(errors)
     errors.full_messages.join(', ')
   end
+
 end
