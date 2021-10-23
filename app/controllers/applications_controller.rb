0ccfcec95cf7751e7f6a2cdf0df4a64619c85360
Chaz Simons <chaz.simons@gmail.com>
2021-10-22 18:47:26 -0700

    Add test and method for create new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,4 +6,15 @@ class ApplicationsController < ApplicationController
 
   def new
   end
+
+  def create
+    application = Application.create(app_params)
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+
+    def app_params
+      params.permit(:name, :street_address, :city, :state, :zip_code)
+    end
 end
--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe 'Applications New' do
     expect(page).to have_content("Name")
     expect(page).to have_content("Street Address")
   end
+
+  it 'accepts user information and returns them to application show page' do
+    visit "/applications/new"
+
+    fill_in :name, with: 'Chaz Simons'
+    fill_in :street_address, with: '1234 Cool Guy Rd'
+    fill_in :city, with: 'Las Vegas'
+    fill_in :state, with: 'NV'
+    fill_in :zip_code, with: 89106
+
+    click_button "Submit"
+
+    expect(page).to have_content("Chaz Simons")
+  end
 end
