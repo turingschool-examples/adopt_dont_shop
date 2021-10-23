ada45b0a617fe9e516c288c3f754b1f3dd28e546
Ted S <tstaros23@gmail.com>
2021-10-23 16:40:29 -0600

    Finish setup except orderly issue

---
+++ spec/factories/applications.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :application do
+    address { "MyString" }
+    city { "MyString" }
+    state { "MyString" }
+    zip_code { "MyString" }
+    description { "MyString" }
+    status { "MyString" }
+  end
+end
