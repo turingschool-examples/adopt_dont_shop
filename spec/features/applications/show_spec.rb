bf12dddc423039629d54366d1234533d796f0227
Ted S <tstaros23@gmail.com>
2021-10-23 20:50:21 -0600

    Finish story 1

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,16 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'show page' do
-  # Application Show Page
-  #
-  # As a visitor
-  # When I visit an applications show page
-  # Then I can see the following:
-  # - Name of the Applicant
-  # - Full Address of the Applicant including street address, city, state, and zip code
-  # - Description of why the applicant says they'd be a good home for this pet(s)
-  # - names of all pets that this application is for (all names of pets should be links to their show page)
-  # - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
+
   describe 'Appication page content' do
     it "shows the application and pet attributes/links" do
       shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
