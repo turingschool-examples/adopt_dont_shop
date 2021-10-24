353c938399b44e9461b2a4f96d0b587ff9c451bc
Ted S <tstaros23@gmail.com>
2021-10-23 23:35:30 -0600

    Remove comment

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -83,22 +83,7 @@ RSpec.describe 'the pets index' do
   end
 
   it "can start an application" do
-#     As a visitor
-# When I visit the pet index page
-# Then I see a link to "Start an Application"
-# When I click this link
-# Then I am taken to the new application page where I see a form
-# When I fill in this form with my:
-#
-# Name
-# Street Address
-# City
-# State
-# Zip Code
-# And I click submit
-# Then I am taken to the new application's show page
-# And I see my Name, address information, and description of why I would make a good home
-# And I see an indicator that this application is "In Progress"
+    
     visit '/pets'
 
     click_on 'Start an Application'
