38b5e8f16ab9c345313dd22b3baec63bffc6833b
Nathan Brown <nathan.brown263@gmail.com>
2021-10-23 21:22:09 -0600

    refactor: Remove commented out User story

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -56,21 +56,6 @@ RSpec.describe 'application show' do
     end
   end
 
-#
-# Submit an Application
-#
-# As a visitor
-# When I visit an application's show page
-# And I have added one or more pets to the application
-# Then I see a section to submit my application
-# And in that section I see an input to enter why I would make a good owner for these pet(s)
-# When I fill in that input
-# And I click a button to submit this application
-# Then I am taken back to the application's show page
-# And I see an indicator that the application is "Pending"
-# And I see all the pets that I want to adopt
-# And I do not see a section to add more pets to this application
-
   context 'when the applicant has added pets' do
     it 'has a working submit application section' do
       pet = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Teddy', shelter_id: shelter.id)
