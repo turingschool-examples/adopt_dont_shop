1562fed1ad3bf603b537307808aa21a71fa9c619
ChloeLL <chloell@protonmail.ch>
2021-10-26 13:34:03 -0700

    Slim down show method

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,7 +1,5 @@
 class PetApplicationsController < ApplicationController
   def show
     @app = PetApplication.find(params[:id])
-
-    @pets = PetApplicationPet.where(pet_application_id: params[:id])
   end
 end
