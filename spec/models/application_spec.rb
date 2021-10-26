89b627917807797392971c39b9e397cf71d4f5d3
Sam Devine <samuel.will.devine@gmail.com>
2021-10-25 21:13:00 -0600

    test: validations for application model

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe Application, type: :model do
   end
 
   describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
   end
 
   describe 'instance methods' do
