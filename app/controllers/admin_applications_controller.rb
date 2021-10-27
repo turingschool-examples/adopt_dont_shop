6fda2e91ece3f6d1853a2077502bff15d88bea98
Sam Devine <samuel.will.devine@gmail.com>
2021-10-27 13:03:01 -0600

    check if all pets are approved when one is approved

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -12,8 +12,13 @@ class AdminApplicationsController < ApplicationController
 
     if petapp && params[:commit] == 'Approve'
       petapp.update(approved: true)
+      # check applications method to see if all pets are approved (move this approval check to model)
+      if application.pet_applications.where(approved: false || nil).count == 0
+        application.update(status: 'Approved')
+      end
     elsif petapp && params[:commit] == 'Reject'
       petapp.update(approved: false)
+      application.update(status: 'Rejected')
     end
 
     redirect_to "/admin/applications/#{application.id}"
