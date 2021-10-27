250e3565a7ee35c3b4ea8a2771b990e1cfd80878
Jacob Yarborough <jayarborough95@gmail.com>
2021-10-26 19:25:34 -0600

    Add submit app form

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationPetsController < ApplicationController 
     def create
-        Application.find(new_association[:app_id]).pets << Pet.find(new_association[:pet_id])
+    #    Application.find(new_association[:app_id]).pets << Pet.find(new_association[:pet_id]) # change this up 
+        ApplicationPet.create(pet_id: new_association[:pet_id], application_id: new_association[:app_id])
         redirect_to "/applications/#{new_association[:app_id]}"
     end 
 
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -118,10 +118,73 @@ RSpec.describe 'guest applications show page' do
         
         fill_in "Search for Pet by Name", with: "b"
         click_button "Search"
-
         click_on "Adopt this Pet"
-        save_and_open_page
+
         expect(current_path).to eq("/applications/#{application1.id}")
         expect(page).to have_content(pet1.name)
+    end 
+
+    it 'can submit an application' do 
+        shelter1 = Shelter.create!(foster_program: true,
+                                   name: 'Rocky Mountain Dog Shelter',
+                                   city: 'Denver',
+                                   rank: 1)
+        pet1 = shelter1.pets.create!(adoptable: true,
+                                     age: 3,
+                                     breed: "Labrador Retriever",
+                                     name: 'Bailey')
+        pet2 = shelter1.pets.create!(adoptable: true,
+                                     age: 1,
+                                     breed: "French Bulldog",
+                                     name: 'Bella')
+        pet3 = shelter1.pets.create!(adoptable: false,
+                                     age: 4,
+                                     breed: "German Shepard",
+                                     name: 'Max')
+        application1 = Application.create!(applicant_name: 'Jacob Yarborough', 
+                                           street_address: '123 Main Street', 
+                                           city: 'Denver', 
+                                           state: 'CO',
+                                           zipcode: '80223',
+                                           application_status: 'Pending',
+                                           why: 'I love dogs')
+
+        # When I visit an application's show page
+        # And I have added one or more pets to the application
+        # Then I see a section to submit my application
+        # And in that section I see an input to enter why I would make a good owner for these pet(s)
+        # When I fill in that input
+        # And I click a button to submit this application
+        # Then I am taken back to the application's show page
+        # And I see an indicator that the application is "Pending"
+        # And I see all the pets that I want to adopt
+        # And I do not see a section to add more pets to this application
+
+        visit "/applications/#{application1.id}"
+        expect(current_path).to eq("/applications/#{application1.id}")
+
+        fill_in "Search for Pet by Name", with: "b"
+        click_button "Search"
+        click_on "Adopt this Pet"
+
+        expect(current_path).to eq("/applications/#{application1.id}")
+        expect(page).to have_content(pet1.name)
+
+        expect(page).to have_content("Expression of Interest")
+        click_on "Submit Application"
+
+        expect(current_path).to eq("/applications/#{application1.id}")   
+        expect(application1.application_status).to eq("Pending") 
+        expect(page).to have_content(application1.applicant_name)
+        expect(page).to have_content(application1.street_address)
+        expect(page).to have_content(application1.city)
+        expect(page).to have_content(application1.state)
+        expect(page).to have_content(application1.zipcode)
+        expect(page).to have_content(application1.why)
+        expect(page).to have_content(application1.application_status)
+        expect(page).to have_content(pet1.name)
+        expect(page).not_to have_content("Adopt this Pet")
+        expect(page).not_to have_content(pet2.name)
+        expect(page).not_to have_content(pet3.name)     
     end 
 end 
