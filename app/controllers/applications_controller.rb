97d5fd9b948a924fac923256dec09b851b535987
Haewon Jeon <haewonito@gmail.com>
2021-10-29 18:44:22 -0600

    US2: creating a new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,26 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    if (app_params[:name] != nil) and (app_params[:street_address] != nil) and (app_params[:city] != nil) and (app_params[:state] != nil) and (app_params[:zip_code] != nil)
+      application = Application.create(app_params)
+      redirect_to "/applications/#{application.id}"
+    else
+      flash[:alert] = "Error: Please fill out all required information"
+      render :new
+    end
+  end
+
+  private
+
+    def app_params
+      params.permit(:name, :street_address, :city, :state, :zip_code)
+    end
 end
---
+++ spec/features/applications/index_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "application index page", type: :feature do
+  describe "start application" do
+    it "has a link to start an application " do
+      visit "/applications"
+      click_on "Start an Application"
+
+      expect(current_path).to eq("/applications/new")
+
+      fill_in :name, with: "Haewon Jeon"
+      fill_in :street_address, with: "4950 Hooker St."
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip_code, with: "80525"
+
+      click_on "Submit Application"
+
+      expect(current_path).to eq("/applications/#{Application.last.id}")
+      expect(page).to have_content("Haewon Jeon")
+      expect(page).to have_content("Denver")
+      expect(page).to have_content("In Progress")
+    end
+  end
+end
