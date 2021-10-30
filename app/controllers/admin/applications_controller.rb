aa52d9dc63c9f3715c095945c80bf7f0e0d6b824
Jacob Yarborough <jayarborough95@gmail.com>
2021-10-30 17:39:26 -0600

    Add user_story_11_testing

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -4,7 +4,6 @@ class Admin::ApplicationsController < ApplicationController
   end 
 
   def update 
-    binding.pry
     app_pet = ApplicationPet.where(pet_id: params[:pet_id], application_id: params[:app_id])
     if params[:commit].include? "Approve"
       app_pet.update(status: "Approved")
