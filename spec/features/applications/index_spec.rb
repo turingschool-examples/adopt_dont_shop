02bfa426434f25ec6d61256139add272e5e40c49
Haewon Jeon <haewonito@gmail.com>
2021-10-30 12:50:50 -0600

    US3 complete

--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -15,11 +15,24 @@ RSpec.describe "application index page", type: :feature do
       fill_in :zip_code, with: "80525"
 
       click_on "Submit Application"
-
       expect(current_path).to eq("/applications/#{Application.last.id}")
       expect(page).to have_content("Haewon Jeon")
       expect(page).to have_content("Denver")
       expect(page).to have_content("In Progress")
+    end
+
+    it "gives error message if form is not complete" do
+      visit "/applications"
+      click_on "Start an Application"
+
+      fill_in :name, with: "Haewon Jeon"
+      fill_in :street_address, with: "4950 Hooker St."
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+
+      click_on "Submit Application"
+      expect(current_path).to eq("/applications/new")
+      expect(page).to have_content("Error")
     end
   end
 end
