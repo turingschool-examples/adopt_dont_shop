b6131e132bf64306106cda6a714506a106d98453
John Napier <jcee.napier@gmail.com>
2021-12-09 22:05:18 -0700

    Add relationship tests for application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,7 @@
 class Application < ApplicationRecord 
+  has_many :application_pets 
+  has_many :pets, through: :application_pets
+
   validates_presence_of :name 
   validates_presence_of :street_address
   validates_presence_of :city
