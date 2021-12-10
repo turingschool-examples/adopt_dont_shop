c475c8f73a82ee8be259c7e1c9f1fea491cdd6ab
Chris Hewitt <agop5134@gmail.com>
2021-12-10 16:44:48 -0700

    Create start application user story and spec

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,28 @@
 class ApplicationsController < ApplicationController
-  
+
   def show
     @application = Application.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    application = Application.create!(application_params)
+    redirect_to "/applications/#{application.id}"
+
+  end
+
+  private
+    def application_params
+      params.permit(
+        :name,
+        :street_address,
+        :city,
+        :state,
+        :zip,
+        :description,
+        :status
+      )
   end
 end
---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "the application new page" do
+  it "displays a form with relevant fields" do
+    visit "/pets"
+    click_link('Start an Application')
+    expect(find('form')).to have_content('Name')
+    expect(find('form')).to have_content('Street address')
+    expect(find('form')).to have_content('City')
+    expect(find('form')).to have_content('State')
+    expect(find('form')).to have_content('Zip')
+  end
+
+  it "fills in form and creates application" do
+    visit "/pets"
+    click_link('Start an Application')
+    fill_in "Name", with: "Chris Hewitt"
+    fill_in "Street address", with: "19072"
+    fill_in "City", with: "Exeter"
+    fill_in "State", with: "CA"
+    fill_in "Zip", with: "93221"
+    click_button("Submit")
+    expect(Application.all.count).to eq(1)
+    expect(page).to have_current_path("/applications/#{Application.last.id}")
+    expect(page).to have_content("Chris Hewitt")
+    expect(page).to have_content("19072")
+    expect(page).to have_content("Exeter")
+    expect(page).to have_content("CA")
+    expect(page).to have_content("93221")
+    expect(page).to have_content("I like turtles")
+
+
+
+  end
+end
