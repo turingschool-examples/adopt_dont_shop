703a4f508d269431d9a612d884fb6ce52874f504
John Napier <jcee.napier@gmail.com>
2021-12-09 23:56:51 -0700

    complete user story application show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ @shelter_1 = Shelter.create!(foster_program: true, name: "Aspen Grove", city: "Fort Collins", rank: 1)
+
+ @application_1 = Application.create!(name: "John Napier", street_address: "936 Akin Ave.", city: "Fort Collins", state: "CO.", zip_code: 80521, description: "Good Owner", status: "Pending")
+ 
+ @pet_1 = @application_1.pets.create!(adoptable: true, age: 1, breed: "Boston Terrier", name: "Zelma", shelter_id: @shelter_1.id)
+ @pet_2 = @application_1.pets.create!(adoptable: true, age: 13, breed: "Tabby Cat", name: "Mazzy", shelter_id: @shelter_1.id)
