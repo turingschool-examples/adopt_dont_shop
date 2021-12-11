1b9ef9f8e039ed3d55268e99366447b143e6db6e
Hannah Warren <hannahkwarren@gmail.com>
2021-12-11 14:23:27 -0500

    Add validations and error messages for incomplete forms

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,10 @@
 class Application < ApplicationRecord
+    validates :name, presence: true
+    validates :address, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip, presence: true
+    validates :reason, presence: true
     has_many :pet_applications
     has_many :pets, through: :pet_applications
 end
