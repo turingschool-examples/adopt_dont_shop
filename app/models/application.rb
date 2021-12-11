5ef4dd5862f45dfd295ddb82703bc335ac554b1d
Joel Grant <joelmgrant@me.com>
2021-12-11 12:08:39 -0700

    Create application page and link

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+  after_initialize :set_defaults, unless: :persisted?
+
+  def set_defaults
+    self.status ||= "In Progress"
+  end
 end
