7d9302893d4f80b2b7ff67e6bee22c0446f1cb51
Hannah Warren <hannahkwarren@gmail.com>
2021-12-10 20:47:32 -0500

    Add more columns for address and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_024134) do
+ActiveRecord::Schema.define(version: 2021_12_10_215739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2021_12_10_024134) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.string "state"
+    t.string "zip"
   end
 
   create_table "pet_applications", force: :cascade do |t|
