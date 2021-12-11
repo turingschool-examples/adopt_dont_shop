a24dc4c8d32eb262f0bd369b49bf4f90a8564597
David Kassin <david.j.kassin@gmail.com>
2021-12-10 21:41:36 -0700

    working first story test and code

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -6,10 +6,26 @@ RSpec.describe 'the application show' do
     @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
     @pet_3 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
+    @application = Application.create!(name: 'David',street: '1023 Makeup',city: 'Chicago', state: 'IL', zip: '60657', description: 'greate person', status: 'pending')
+    @pet_application = ApplicationPet.create!(pet_id: @pet_1.id, application_id: @application.id)
   end
 
   it 'shows all of the applicants' do
 
-    application = Application.create!(name: 'David ',address: '1023 Makeup', description: 'greate person', pet: @pet_1, status: 'pending')
+
+    visit "/applications/#{@application.id}"
+
+    expect(page).to have_content(@application.name)
+    expect(page).to have_content(@application.street)
+    expect(page).to have_content(@application.city)
+    expect(page).to have_content(@application.state)
+    expect(page).to have_content(@application.zip)
+    expect(page).to have_content(@application.description)
+    expect(page).to have_content(@application.status)
+
+    expect(page).to have_link("#{@pet_1.name}")
+    click_link "#{@pet_1.name}"
+    expect(current_path).to eq("/pets/#{@pet_1.id}")
+
   end
 end
