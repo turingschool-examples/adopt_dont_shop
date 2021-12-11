7035c0b60a5dc8143b49b8126f7944033f1930bf
Wade Naughton <wadenaughton@gmail.com>
2021-12-10 23:43:45 -0500

    story 2 complete

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationsController < ApplicationController
+
+  def show
+    @application = Application.find(params[:id])
+    @pets = @application.pets
+  end
+
+end
---
+++ spec/features/application/show_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe 'application show page' do
+  before do
+
+    @shelter1 = Shelter.create!(foster_program: true, name:"wade shelter", city: "Bear", rank: 2)
+    @pet1 = @shelter1.pets.create!(adoptable: true, age: 5, breed:"golden", name:"gus")
+    @application1 = Application.create!(name:'Name1', address: '123 test st', city: 'Bear', state: 'Delaware', description: "this is a description", status: 'in progress' )
+    @application_pet = PetApplication.create!(pet_id:@pet1.id, application_id: @application1.id)
+
+    visit "/applications/#{@application1.id}"
+
+  end
+  it ' I see name, address, description, status' do
+    expect(page).to have_content(@application1.name)
+    expect(page).to have_content(@application1.address)
+    expect(page).to have_content(@application1.city)
+    expect(page).to have_content(@application1.state)
+    expect(page).to have_content(@application1.description)
+    expect(page).to have_content(@application1.status)
+    expect(page).to have_link("#{@pet1.name}")
+  end
+
+  it 'Links to all' do
+    expect(page).to have_link("#{@pet1.name}", href: "/pets/#{@pet1.id}")
+    click_link "#{@pet1.name}"
+    expect(current_path).to eq("/pets/#{@pet1.id}")
+  end
+end
