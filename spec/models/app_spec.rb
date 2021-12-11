31417da7ecd01c860e21945f9c03da1b2f657c97
Karan Mehta <karanm645@gmail.com>
2021-12-11 17:43:53 -0600

    Create validations in app_spec model test

--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe App do
     it { should have_many(:pets)}
     it { should have_many(:pets).through(:app_pets)}
   end 
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_numericality_of(:zip) }
+  end
+
 end 
