ff29b09fc2900df6a4718df9f67fc1575930628e
Karan Mehta <karanm645@gmail.com>
2021-12-11 17:43:13 -0600

    Create validation in app model

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,11 +1,16 @@
 class App < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true, numericality: true
+
   enum status: {
     "In Progress": 0,
     "Pending": 1,
     "Accepted": 2,
     "Rejected": 3
   }
-
   has_many :app_pets
   has_many :pets, through: :app_pets
 end 
