2e8834724cb94744362ce25554530c1dc3dc029f
Joel Grant <joelmgrant@me.com>
2021-12-11 12:02:22 -0700

    Adjust migrations

--- db/migrate/20211210232003_create_applications.rb
+++ db/migrate/20211210232003_create_applications.rb
@@ -2,7 +2,10 @@ class CreateApplications < ActiveRecord::Migration[5.2]
   def change
     create_table :applications do |t|
       t.string :name
-      t.string :address
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip
       t.string :description
       t.string :status
 
