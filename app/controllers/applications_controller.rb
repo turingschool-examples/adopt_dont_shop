346ffda79e4d2fcf6d14b645d15f860709ffdd27
David Kassin <david.j.kassin@gmail.com>
2021-12-11 11:42:42 -0700

    Add application form with test and working code

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,4 +3,19 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @pets = @application.pets
   end
+
+  def new
+  end
+
+  def create
+    application = Application.new(application_params)
+
+      redirect_to "/applications"
+  end
+
+private
+
+def application_params
+  params.permit(:name, :street, :city, :state, :zip)
+end
 end
