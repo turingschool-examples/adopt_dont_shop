c862f084e2ffc838d04b875aacf8c477e8f8b70e
Joel Grant <joelmgrant@me.com>
2021-12-11 19:22:05 -0700

    Correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   get '/', to: 'application#welcome'
 
-  get '/applications', to: 'applications#index'
+  # get '/applications', to: 'applications#index'
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
-  post '/applications', to: 'applications#create'
-  patch '/applications/:id', to: 'applications#show'
+  post '/applications/:id', to: 'applications#create'
+  # get '/applications', to: 'applications#edit'
+  # patch '/applications/:id', to: 'applications#update'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
