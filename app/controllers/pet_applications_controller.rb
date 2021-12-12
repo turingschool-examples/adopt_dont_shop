95d18895711f913869d70d169ec8b508cd978d0d
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-12 16:16:05 -0700

    redirect to application show page.

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,6 +1,7 @@
 class PetApplicationsController < ApplicationController
   def create
-    PetApplication.create(pet_id: :pet_id, application_id: :application_id)
+    PetApplication.create(pet_id: params[:pet_id], application_id: params[:application_id])
+    redirect_to("/applications/#{params[:application_id]}")
   end
 
   private
