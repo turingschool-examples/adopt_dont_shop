9b92e64cb6f6217860248a17ead32fcfc9d0dee4
Mallory Vining <mallorygallery12@gmail.com>
2021-12-11 18:01:46 -0700

    Add User Story 1

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Application show page', type: :feature do
-  xit 'displays applicants home info' do
+  it 'displays applicants home info' do
     sam = Application.create!(applicant_name: "Sam Neill",
                               address: "123 Wilderpeople Way",
                               city: "Eerie",
@@ -9,6 +9,20 @@ RSpec.describe 'Application show page', type: :feature do
                               zip_code: 80514,
                               reason: "Lots of love to give",
                               status: "Applied")
+    sam2 = Application.create!(applicant_name: "Samantha Neill",
+                              address: "123 Wilder Way",
+                              city: "Eerie",
+                              state: "Colorado",
+                              zip_code: 80514,
+                              reason: "hates dogs",
+                              status: "Applied")
     visit "/applications/#{sam.id}"
+    expect(page).to have_content(sam.applicant_name)
+    expect(page).to have_content(sam.address)
+    expect(page).to have_content(sam.city)
+    expect(page).to have_content(sam.state)
+    expect(page).to have_content(sam.zip_code)
+
+    expect(page).to_not have_content(sam2.applicant_name)
   end
 end
