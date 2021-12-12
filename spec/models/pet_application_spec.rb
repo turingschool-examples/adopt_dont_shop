244df8bda660d0f96cb997e6cc28f2ed9e4a968b
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-11 18:38:32 -0700

    Update model specs to test for relationships.

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe PetApplication, type: :model do
   it {should belong_to(:pet)}
-  it {shoudl belong_to(:application)}
+  it {should belong_to(:application)}
 end
