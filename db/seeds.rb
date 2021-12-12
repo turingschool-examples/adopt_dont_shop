1270eab09c4c3cfd6c91809e93b24e7a4f7b468f
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-12-11 23:45:14 -0700

    Gemfile lock

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Shelter.destroy_all
-Application.destroy_all
-Pet.destroy_all
-AdoptablePet.destroy_all
-shelter = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
-pet_1 = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
-pet_2 = Pet.create!(name: 'Lobster', age: 3, breed: 'Doberman', adoptable: true, shelter_id: shelter.id)
-application = pet_1.applications.create(name: 'Frank', street_address: '123 Main St', city: 'Long Beach', state: 'CA', zipcode: '90712', description: 'I have a fully fenced in acre property', status: 'pending')
-AdoptablePet.create!(application: application, pet: pet_2)
