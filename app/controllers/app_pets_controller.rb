6a8ebabe491a23c48f1d38a3229bcecd4f68d1dc
Karan Mehta <karanm645@gmail.com>
2021-12-12 11:58:55 -0600

    Create App Pets controller with create action

---
+++ app/controllers/app_pets_controller.rb
@@ -0,0 +1,13 @@
+class AppPetsController < ApplicationController
+  def create
+
+    @app_pets = AppPet.create!(app_pets_params)
+    redirect_to "/apps/#{@app_pets.app_id}"
+  end
+
+  private 
+  
+  def app_pets_params
+    params.permit(:app_id, :pet_id)
+  end
+end 
