52dd779bc7d55648ae79ba383b44c95f7e335a22
Darren Kulback <dkulback@gmail.com>
2021-12-12 09:30:07 -0700

    Test for adding a pet to an app

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -46,13 +46,13 @@ RSpec.describe 'application show page', type: :feature do
 
     visit "/applications/#{derek.id}"
     within '.add-pet' do
-    
+
     expect(page).to have_content("Add a Pet to this Application")
     expect(page).to have_button("Search")
     end
   end
 
-  it 'adds pets to the application show page' do
+  it 'searches pets on the application show page' do
     shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
     pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
