631d9daeec36729fe501d9395c24a6d25b103be8
Leland Curtis <lelandcurtis88@gmail.com>
2021-12-12 22:41:27 -0700

    Add test for shelters index page with pending application section.

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -9,10 +9,28 @@ RSpec.describe 'admin shelter index page' do
     @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
     @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
   end
-  
+
   it 'lists all the shelters in reverse alphabetical order when in admin mode' do
     visit '/admin/shelters'
     expect(@shelter_2.name).to appear_before(@shelter_1.name)
     expect(@shelter_3.name).to appear_before(@shelter_1.name)
+  end
+
+  it 'has a section that lists only shelters with pending applications' do
+
+    pet_1 = Pet.create!(adoptable: true, age: 10, breed: "Hound", name: "Pete", shelter_id: @shelter_1.id)
+    pet_2 = Pet.create!(adoptable: false, age: 3, breed: "GSP", name: "Newton", shelter_id: @shelter_2.id)
+    pet_3 = Pet.create!(adoptable: false, age: 13, breed: "German Shepard", name: "Molly", shelter_id: @shelter_3.id)
+    application_1 = Application.create!(name: 'Steve', address: '135 Waddle Road', city: 'Dallas', state: 'TX', zip: 75001, description: "I really want a dog", status: "Pending")
+    pet_application_1 = PetApplication.create!(pet_id: pet_1.id, application_id: application_1.id)
+    pet_application_2 = PetApplication.create!(pet_id: pet_2.id, application_id: application_1.id)
+
+    visit '/admin/shelters'
+    expect(page).to have_content("Shelter's with Pending Applications")
+    within('div.pending') do
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_2.name)
+      expect(page).to_not have_content(@shelter_3.name)
+    end
   end
 end
