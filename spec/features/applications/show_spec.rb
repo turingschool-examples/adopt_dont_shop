f3c5ef45ffadfd649580399a43b6a1ef64106fbd
David Kassin <david.j.kassin@gmail.com>
2021-12-13 13:40:08 -0700

    add one extra path test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -71,7 +71,7 @@ RSpec.describe 'the application show' do
         visit "/applications/#{@application.id}"
         fill_in 'Pet name', with: "#{@pet_2.name}"
         click_on("Submit")
-
+        expect(current_path).to eq("/applications/#{@application.id}")
 
         expect(page).to have_button("Adopt this Pet")
         click_on("Adopt this Pet")
@@ -81,7 +81,7 @@ RSpec.describe 'the application show' do
         expect(page).to have_button("Submit this application")
         fill_in 'Description', with: "I'm Incredible"
         click_on("Submit this application")
-        save_and_open_page
+        
         expect(current_path).to eq("/applications/#{@application.id}")
 
         expect(page).to have_content('Pending')
