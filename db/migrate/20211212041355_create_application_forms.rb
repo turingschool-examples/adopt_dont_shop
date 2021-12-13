655788099a25782757729bcd5a937be25e44becd
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-12-13 10:13:04 -0500

    Initial commit

---
+++ db/migrate/20211212041355_create_application_forms.rb
@@ -0,0 +1,15 @@
+class CreateApplicationForms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :application_forms do |t|
+      t.string :name
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :description
+
+
+      t.timestamps
+    end
+  end
+end
