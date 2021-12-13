f2f36ffc1d0e49f30e47f96785685412f7c0fd9e
Jessica Grazulis <jlgrazulis@gmail.com>
2021-12-13 10:11:33 -0700

    Clean up migration

--- db/migrate/20211213170515_create_pet_apps.rb
+++ db/migrate/20211213170515_create_pet_apps.rb
@@ -1,4 +1,4 @@
-class CreatePetApps < ActiveRecord::Migration[5.2]
+class PetApps < ActiveRecord::Migration[5.2]
   def change
     create_table :pet_apps do |t|
       t.string :name
@@ -7,6 +7,9 @@ class CreatePetApps < ActiveRecord::Migration[5.2]
       t.string :application_status
 
       t.timestamps
+
+      t.references :shelters, foreign_key: true
+      t.references :pets, foreign_key: true
     end
   end
 end
--- db/migrate/20211213170808_create_pet_app.rb
+++
@@ -1,8 +0,0 @@
-class CreatePetApp < ActiveRecord::Migration[5.2]
-  def change
-    create_table :pet_apps do |t|
-      t.references :shelters, foreign_key: true
-      t.references :pets, foreign_key: true
-    end
-  end
-end
