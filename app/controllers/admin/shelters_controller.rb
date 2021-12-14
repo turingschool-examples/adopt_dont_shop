2ef643854b6989937c017d5d9652f55f2f1f1746
Brad Breiten <jonathan@breiten.us>
2021-12-13 21:54:58 -0700

    Update: index method to house instance variable of all shelters that have associated pending applications

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -2,5 +2,7 @@ class Admin::SheltersController < ApplicationController
 
   def index
     @shelters = Shelter.reverse_alphabetical_by_name
+
+    @shelters_with_pending_applications = Shelter.pending_applications
   end
 end
