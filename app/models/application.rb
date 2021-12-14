41a514ea49488857880b363212d1972975b86304
Robin Dittrich <robinodittrich@gmail.com>
2021-12-13 16:02:29 -0800

    Add pet and pet_application files.

---
+++ app/models/application.rb
@@ -0,0 +1,12 @@
+class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :description, presence: true
+  validates :status, presence: true
+  
+  has_many :pet_applications
+  has_many :pets, through: :pet_applications
+end
