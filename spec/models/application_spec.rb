765000e7c341d75e397f8bc4354aa1359277c974
Robin Dittrich <robinodittrich@gmail.com>
2021-12-13 16:01:56 -0800

    Added spec files to pet and pet_applications

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  describe 'relationships' do
+    it { should have_many(:pet_applications) }
+    it { should have_many(:pets).through(:pet_applications) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:status) }
+  end
+end
