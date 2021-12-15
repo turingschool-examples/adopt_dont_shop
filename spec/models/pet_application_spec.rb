da9e7d650428d9d12b47d4efe66be162235fc539
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-12-15 18:53:12 -0500

    Add relationship tests

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe PetApplication, type: :model do
   describe 'relationships' do
     it { should belong_to :pet}
-    
+    it { should belong_to :application_form}
   end
 end
