8ee03d9ce0b528f0470c477019229f5a95c4e06c
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-12-15 18:51:34 -0500

    Add additional validation

--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -1,5 +1,5 @@
 class ApplicationForm < ApplicationRecord
-validates_presence_of :name, :street_address, :city, :state, :zip_code, :description
+validates_presence_of :name, :street_address, :city, :state, :zip_code, :description, :status
 
 has_many :pet_applications
 has_many :pets, through: :pet_applications
