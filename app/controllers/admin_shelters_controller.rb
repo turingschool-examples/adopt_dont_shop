bcc5af5dec490e36d799f94e80e8cb7534c68e16
Russell Rockwood <russell.rockwood@gmail.com>
2021-12-15 15:40:39 -0500

    approve or reject functionality

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -6,13 +6,31 @@ class AdminSheltersController < ApplicationController
 
   def show
     @applicant = Applicant.find(params[:id])
-    if params[:approved_pet_id]
-
+    if params[:commit] == 'Submit'
+      approval_status = verify_application(@applicant.pets, params)
+      if approval_status == 'none'
+        flash[:notice] = "Must Select Approve or Reject."
+      elsif approval_status == 'double'
+        flash[:notice] = 'Cannot Accept And Reject Pet. Select One Option'
+      elsif approval_status == 'Rejected'
+        @applicant.update({status: 'Rejected'})
+      else
+        @applicant.update({status: 'Approved'})
+      end
     end
   end
 
-  
-
+  def verify_application(pets, params)
+    pets.each do |pet|
+      if params[pet.id.to_s]['approved'] == 'no' && params[pet.id.to_s]['rejected'] == 'no'
+        return 'none'
+      elsif params[pet.id.to_s]['approved'] == 'yes' && params[pet.id.to_s]['rejected'] == 'yes'
+        return 'double'
+      end
+      if params[pet.id.to_s]['rejected'] == 'yes'
+        return 'Rejected'
+      end
+    end
+    'Approved'
   end
-
 end
