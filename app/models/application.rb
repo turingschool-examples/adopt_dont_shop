eca10024258c811bfbdc325e17052066d80269b6
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-15 15:47:26 -0600

    remove description requirement from Application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
-  validates :name, :street_address, :city, :state, :zip_code, :description, presence: true
+  validates :name, :street_address, :city, :state, :zip_code,  presence: true
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Application do
     it { should validate_presence_of(:street_address) }
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
-    it { should validate_presence_of(:zip_code) }
-    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:zip_code)}
   end
 end
