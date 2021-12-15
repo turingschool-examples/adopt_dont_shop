42e37c332792ce508251443772fe5b5166f58721
Brad Breiten <jonathan@breiten.us>
2021-12-15 12:33:43 -0700

    Add seeds for manual test pets on multiple applications

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-application_1 = Application.create!(applicant_name: "Mike Sloan", street_address: "134 Willow Lane", city: "Boulder", state: "CO", zip_code: "80034", description: "I'd be good because I love pets", application_status: "pending")
+application_1 = Application.create!(applicant_name: "Mike Sloan", street_address: "134 Willow Lane", city: "Boulder", state: "CO", zip_code: "80034", application_status: "In Progress")
+application_2 = Application.create!(applicant_name: "Ben Spiegel", street_address: "134 Windy meadows", city: "Boulder", state: "CO", zip_code: "80023", application_status: "In Progress")
 
 shelter_1 = Shelter.create!(foster_program: 'true', name: "Shelter 1", city: "Denver", rank: "5")
 shelter_2 = Shelter.create!(foster_program: 'true', name: "Bradburn Shelter", city: "Denver", rank: "1")
 shelter_3 = Shelter.create!(foster_program: 'true', name: "Home 4 Cats", city: "Denver", rank: "1")
 
-pet_1 = application_1.pets.create!(adoptable: "true", age: "3", breed: "Terrier", name: "Sparky", shelter_id: "#{shelter_1.id}")
+pet_1 = application_1.pets.create!(adoptable: "true", age: "5", breed: "Lab", name: "Bear", shelter_id: "#{shelter_1.id}")
+pet_5 = application_1.pets.create!(adoptable: "true", age: "3", breed: "Terrier", name: "Sparky", shelter_id: "#{shelter_1.id}")
+
 
 pet_2 = Pet.create!(adoptable: "true", age: "1", breed: "Black Lab", name: "Spot", shelter_id: "#{shelter_1.id}")
 
