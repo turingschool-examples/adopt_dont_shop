e7bb64d555851c6c2d49aa235ce201323c67b365
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-10 17:26:31 -0700

    feature: add has_many activerecord relationship to pet model

---
+++ spec/features/application/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+# As a visitor
+# When I visit an applications show page
+# Then I can see the following:
+# - Name of the Applicant
+# - Full Address of the Applicant including street address, city, state, and zip code
+# - Description of why the applicant says they'd be a good home for this pet(s)
+# - names of all pets that this application is for (all names of pets should be links to their show page)
+# - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
+RSpec.describe 'The application Show page' do 
+    it 'shows the user applicant attributes' do 
+        visit "/application/#{application.id}"
+    end
+end 
