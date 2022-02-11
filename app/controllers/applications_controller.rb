c315e8387f005297104547002d6cc58297cc17be
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-11 15:50:16 -0600

    Complete User story 1. Application show page displays all attributes

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationsController < ApplicationController
+
+    def index
+        @applications = Application.all 
+    end
+    
+    def show 
+        @application = Application.find(params[:id])
+    end
+end
