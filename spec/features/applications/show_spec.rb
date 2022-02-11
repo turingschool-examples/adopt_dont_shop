8db4de49e5086902c91c8ea488bc5e63f2e65a4a
Conor Barthel <conor.barthel@gmail.com>
2022-02-11 12:46:12 -0700

    Feat:application show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,13 +3,19 @@ require 'rails_helper'
 RSpec.describe 'Application show page' do
   before(:each) do
     @application = Application.create!(name: 'Mike',
-                                      address: '123 blake st., Denver, CO, 80211',
+                                      address: '123 Blake st., Denver, CO, 80211',
                                       description: 'I work from home',
                                       pet_names: 'Rufus',
                                       status:'In Progress')
   end
 
   it "should display application's attributes" do
-    visit "application/show"
+    visit "applications/#{@application.id}"
+
+    expect(page).to have_content("Mike")
+    expect(page).to have_content("23 Blake st., Denver, CO, 80211")
+    expect(page).to have_content("I work from home")
+    expect(page).to have_content("Rufus")
+    expect(page).to have_content("In Progress")
   end
 end
