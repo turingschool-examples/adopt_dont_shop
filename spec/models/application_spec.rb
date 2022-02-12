2366f1aebb1d8c35f0a36558e1db7292a222ea93
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-12 12:24:53 -0500

    Test/Feat: Validate application attributes

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Application, type: :model do
     it { should have_many :pet_applications }
     it { should have_many(:pets).through(:pet_applications) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zipcode) }
+  end
 end
