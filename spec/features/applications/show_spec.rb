d9127b69f3957de90487c2debdc126f8aaae27b0
Maximilian Wagner <maw3350@gmail.com>
2022-02-12 16:56:58 -0500

    Feat: Application show page complete (User story 2 complete)

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe 'application show page' do
     pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
     pet_3 = shelter_1.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
-    application_1 = Application.create!(name:'Alice', address: '357 test st', city: 'Frederickburg', state: 'VA', zip: '22406')
+    application_1 = Application.create!(name:'Alice', street: '357 test st', city: 'Frederickburg', state: 'VA', zip: '22406')
     PetApplication.create!(pet: pet_1, application: application_1)
 
-    visit "/applications"
+    visit "/applications/#{application_1.id}"
     within("#application-#{application_1.id}") do
       expect(page).to have_content(application_1.name)
       expect(page).to have_content(application_1.street)
@@ -21,5 +21,22 @@ RSpec.describe 'application show page' do
       expect(page).to have_content(application_1.description)
       expect(page).to have_content(application_1.status)
       expect(page).to have_content(pet_1.name)
+    end
+  end
+
+  it "pet names are links to that pet's id page" do
+
+    shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    pet_3 = shelter_1.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    application_1 = Application.create!(name:'Alice', street: '357 test st', city: 'Frederickburg', state: 'VA', zip: '22406')
+    PetApplication.create!(pet: pet_1, application: application_1)
+
+    visit "/applications/#{application_1.id}"
+    within("#application-#{application_1.id}") do
+      click_link "#{pet_1.name}"
+      expect(current_path).to eq("/pets/#{pet_1.id}")
+    end
   end
 end
