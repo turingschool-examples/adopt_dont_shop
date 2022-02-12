d005f3553378572775dae2c4d8febed193d3851b
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-12 14:43:58 -0500

    Add: enum status to display status type

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,9 +2,11 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
-  validates_presence_of :name
-  validates_presence_of :street_address
-  validates_presence_of :city
-  validates_presence_of :state
-  validates_presence_of :zipcode
+  validates_presence_of :name,
+                        :street_address,
+                        :city,
+                        :state,
+                        :zipcode
+  enum status: { "In Progress" => 0, "Pending" => 1, "Accepted" => 2, "Rejected" => 3 }
+
 end
