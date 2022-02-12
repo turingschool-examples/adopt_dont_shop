04fc826f3a7770f6c99eea86b49a8f62bacc2332
Mark Ertmer <mark.ertmer@gmail.com>
2022-02-12 13:45:46 -0700

    Add: validation for application description

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,7 @@
 class Application < ApplicationRecord
   validates :name, presence: true
   validates :street_address, presence: true
+  validates :description, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true, numericality: true
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Application, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
     it { should validate_presence_of(:street_address) }
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
