688f2acb1163936b265cca60a559ea89f4496390
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-12 14:31:32 -0700

    tweak seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@shelter = Shelter.create(foster_program: true,
+  name: "frank's felines",
+  city: "westminster",
+  rank: 10)
+@application= Application.create!(name: "Frank",
+  street: "123 blaine st",
+  city: "Sioux falls",
+  state: "SD",
+  zip: 80342,
+  )
 @bean = Pet.create!(
   name: "Bean",
   adoptable: true,
   age: 1,
   breed: "cat",
   shelter_id: @shelter.id)
-@shelter = Shelter.create(foster_program: true,
-              name: "frank's felines",
-              city: "westminster",
-              rank: 10)
-@application= Application.create!(name: "Frank",
-              street: "123 blaine st",
-              city: "Sioux falls",
-              state: "SD",
-              zip: 80342,
-              description: "I'm a meat popsicle looking for a companion.",
-              status: "Single")
