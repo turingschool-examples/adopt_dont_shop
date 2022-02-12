dfca7cfd25c35a52d8b9bdb27924bd531c38f94d
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-12 11:52:51 -0500

    Refactor: change create method

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -10,15 +10,16 @@ class ApplicantsController < ApplicationController
     @applicant = Applicant.create!(applicant_params)
     if @applicant.save
       redirect_to "/applicants/#{@applicant.id}"
-    else
-      redirect_to '/applicants/new'
-      # flash[:alert] = "Error: We will figure this out later"
-    end
+    end 
+    # else
+    #   redirect_to '/applicants/new'
+    #   # flash[:alert] = "Error: We will figure this out later"
+    # end
   end
 
   private
 
   def applicant_params
-    params.permit(:id, :name, :street_address, :city, :state, :zipcode, :description, :status)
+    params.permit(:id, :name, :street_address, :city, :state, :zipcode)
   end
 end
--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,8 +1,6 @@
 class Applicant < ApplicationRecord
   validates :name, presence: true
-  validates :description, presence: true
   validates_uniqueness_of :name
-  validates_uniqueness_of :description
 
   has_many :pet_applicants
   has_many :pets, through: :pet_applicants
