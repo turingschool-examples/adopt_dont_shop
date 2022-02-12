379ec8987b4b48388fb9e2875b01dae74c385d8f
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-12 11:33:06 -0500

    Refactor: edit routes for routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -39,5 +39,5 @@ Rails.application.routes.draw do
 
   get '/applicants/new', to: 'applicants#new'
   get '/applicants/:id', to: 'applicants#show'
-  post '/applicants/', to: 'applicants#create'
+  post '/applicants', to: 'applicants#create'
 end
