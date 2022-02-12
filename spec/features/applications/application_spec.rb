e39d772c364b0bc039e5d96525e29faccd427223
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-12 09:44:45 -0700

    test: for 25 added

--- spec/features/applications/application_spec.rb
+++ spec/features/applications/application_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'application' do
   it 'displays a link to all pets' do
     visit '/'
+    # save_and_open_page
     expect(page).to have_content("Adopt, don't shop!")
     expect(page).to have_link("Pets")
     click_link("Pets")
@@ -44,4 +45,5 @@ RSpec.describe 'application' do
     expect(page).to have_link("Veterinarians")
     expect(page).to have_link("Veterinary Offices")
   end
+
 end
---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'application' do
+  before(:each) do
+    @shelter = Shelter.create!(foster_program: true,
+                  name: "frankie's friendly felines",
+                  city: "westminster",
+                  rank: 10)
+    @application = Petapplication.create!(name: "Frank",
+                  street: "123 blaine st",
+                  city: "Sioux falls",
+                  state: "SD",
+                  zip: 80342,
+                  description: "I'm a meat popsicle looking for a companion.",
+                  status: "Single")
+    @bean = Pet.create!(
+                  name: "Bean",
+                  adoptable: true,
+                  age: 1,
+                  breed: "cat",
+                  shelter_id: @shelter.id)
+  end
+  it 'displays a link to applicant info' do
+    binding.pry
+    visit "/petapplications/#{@application.id}"
+    save_and_open_page
+    expect(page).to have_content(@application.name)
+    expect(page).to have_content(@application.street)
+    expect(page).to have_content(@application.city)
+    expect(page).to have_content(@application.state)
+    expect(page).to have_content(@application.description)
+    expect(page).to have_content(@application.status)
+  end
+end
