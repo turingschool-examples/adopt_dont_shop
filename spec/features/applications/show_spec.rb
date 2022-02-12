16ecf2616781caf9e01f51b8a19012b677a51f0b
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-12 12:11:01 -0600

    heroku fix

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -20,10 +20,12 @@ RSpec.describe 'the application show' do
       @lemon= Application.create(first_name: 'Lemon', last_name: 'Tiger', street_address: '1225 Alvaro Obgeron Dr.', city: 'Mexico City, GA', post_code: '67518', good_owner: 'Give the best cuddles!')
       @salty= Application.create(first_name: 'Salty', last_name: 'Hippo', street_address: '367 CBTIS Overton St.', city: 'Colima, DC', post_code: '14628', good_owner: 'Give the best treats!')
       @funky= Application.create(first_name: 'Funky', last_name: 'Platypus', street_address: '15 Overpath Pkwy.', city: 'Shenzhen, OH', post_code: '97627', good_owner: 'Give the best walks!')
+
+      ApplicationPets.create!(pet_id:@scooby.id, application_id: @lemon.id)
     end
 
     it "shows the all of the applicants and their details" do
-      # require "pry"; binding.pry
+  require "pry"; binding.pry
     visit "/applications/#{@salty.id}"
 
     expect(page).to have_content(@salty.first_name)
