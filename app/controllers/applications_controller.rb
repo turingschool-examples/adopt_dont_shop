f873ee5ce9f847630e3547a63911e0d0aba12c7f
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-12 14:44:18 -0500

    Add: create method and made params private

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    @pets = @application.pets 
+    @pets = @application.pets
+  end
+
+  def create
+    application = Application.create(applicant_params)
+    redirect_to "/application/#{application.id}"
+  end
+
+
+  private
+
+  def applicant_params
+    params.permit(:name, :street_address, :city, :state, :zipcode)
   end
 end
