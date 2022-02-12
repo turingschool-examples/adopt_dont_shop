05192521733a0b29161cd7caf39632eb6070be6b
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-12 14:43:09 -0700

    clear seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@shelter = Shelter.create(foster_program: true,
-  name: "frank's felines",
-  city: "westminster",
-  rank: 10)
-@application= Application.create(name: "Frank",
-  street: "123 blaine st",
-  city: "Sioux falls",
-  state: "SD",
-  zip: 80342,
-  )
-@bean = Pet.create(
-  name: "Bean",
-  adoptable: true,
-  age: 1,
-  breed: "cat",
-  shelter_id: @shelter.id)
