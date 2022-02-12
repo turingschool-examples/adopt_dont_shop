63a901ab64f5ffce6f7aab69262ac9853eec0484
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-12 16:16:44 -0600

    User story 22 complete

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,7 @@
 class Application < ApplicationRecord
-    has_many :pets
+    has_many :application_pets
+    has_many :pets, :through => :application_pets
+
     validates :name, presence: true 
     validates :street_address, presence: true
     validates :city, presence: true
