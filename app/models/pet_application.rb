b7eb50a5f8281349759caeebdc1632a96a0ab843
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-13 13:43:49 -0700

    test/feature: write model test/class method that retrieves pet ids for all joins table entries that have been accepted for a particular application

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -6,6 +6,11 @@ class PetApplication < ApplicationRecord
     def self.find_joins_row(application, pet)
         a_pet_id = pet.id
         app_id = application.id
-        where(pet_id: a_pet_id, application_id: app_id)  
+        where(pet_id: a_pet_id, application_id: app_id)
     end 
+
+    def self.find_approved_pet_ids(application)
+        app_id = application.id
+        where(application_id: app_id, status: "Accepted").pluck(:pet_id)
+    end
 end 
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -6,13 +6,19 @@ RSpec.describe PetApplication, type: :model do
         it {should belong_to :application}
     end 
 
+    before :each do 
+        PetApplication.destroy_all
+        Application.destroy_all
+        @application_1 = Application.create!(name: "Sedan Turtle", street_address: "3425 Gransfield ave", city: "Denver", state: "CO", zipcode: "80219", description: "I love me some animals.", status: "Accepted")
+        @dumb_friends_league = Shelter.create!(foster_program: true, name: "Dumb Friends League", city: "Englewood", rank: "1")
+        @mushu = @application_1.pets.create!(adoptable: true, age: "5", breed: "dog", name:"Mushu", shelter: @dumb_friends_league)
+    end 
+
     describe 'enum' do 
         it 'can update an applications status for one pet' do 
-        PetApplication.destroy_all
-        # application_1 = Application.create!(name: "Sedan Turtle", street_address: "3425 Gransfield ave", city: "Denver", state: "CO", zipcode: "80219", description: "I really love animals.", status: "Pending")
-        # dumb_friends_league = Shelter.create!(foster_program: true, name: "Dumb Friends League", city: "Englewood", rank: "1")
-        # mushu = application_1.pets.create!(adoptable: true, age: "5", breed: "dog", name:"Mushu", shelter: dumb_friends_league)
-        joins_row = PetApplication.create(id: 1, pet_id: 1, application_id: 2)
+        application = Application.create!(name: "Sedan Turtle", street_address: "3425 Gransfield ave", city: "Denver", state: "CO", zipcode: "80219", description: "I love me some animals.", status: "Accepted")
+        mantis = application.pets.create!(adoptable: true, age: "5", breed: "cat", name:"Mantis", shelter: @dumb_friends_league)
+        joins_row = PetApplication.create!(id: 1, pet_id: mantis.id, application_id: application.id)
         expect(joins_row.status).to eq(nil)
         joins_row.Accepted!
         expect(joins_row.status).to eq("Accepted")
@@ -21,15 +27,24 @@ RSpec.describe PetApplication, type: :model do
 
     describe 'class methods' do 
         it '#find_joins_row' do 
-            PetApplication.destroy_all
-            application_1 = Application.create!(name: "Sedan Turtle", street_address: "3425 Gransfield ave", city: "Denver", state: "CO", zipcode: "80219", description: "I really love animals.", status: "Pending")
-            dumb_friends_league = Shelter.create!(foster_program: true, name: "Dumb Friends League", city: "Englewood", rank: "1")
-            mushu = application_1.pets.create!(adoptable: true, age: "5", breed: "dog", name:"Mushu", shelter: dumb_friends_league)
-            joins_row = PetApplication.find_joins_row(application_1, mushu)
-            expect(joins_row.first.pet_id).to eq(mushu.id)
-            expect(joins_row.last.pet_id).to eq(mushu.id)
-            expect(joins_row.first.application_id).to eq(application_1.id)
-            expect(joins_row.last.application_id).to eq(application_1.id)
+            joins_row = PetApplication.find_joins_row(@application_1, @mushu)
+            expect(joins_row.first.pet_id).to eq(@mushu.id)
+            expect(joins_row.last.pet_id).to eq(@mushu.id)
+            expect(joins_row.first.application_id).to eq(@application_1.id)
+            expect(joins_row.last.application_id).to eq(@application_1.id)
         end
+
+        describe '#find_approved_pet_ids' do 
+        it 'finds all pet ids that have been accepted for a particular application' do 
+            PetApplication.destroy_all
+            Application.destroy_all
+            application = Application.create!(name: "Sedan Turtle", street_address: "3425 Gransfield ave", city: "Denver", state: "CO", zipcode: "80219", description: "I love me some animals.", status: "Accepted")
+            mantis = application.pets.create!(adoptable: true, age: "5", breed: "cat", name:"Mantis", shelter: @dumb_friends_league)
+            expect(application.pet_applications).to eq(mantis.pet_applications)
+            application.pet_applications.first.Accepted!
+            expect(application.pet_applications.first.status).to eq("Accepted")
+            expect(PetApplication.find_approved_pet_ids(application)).to eq([mantis.id])
+        end 
+      end 
     end
 end
