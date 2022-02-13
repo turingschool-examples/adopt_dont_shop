fba521235ef38bcc7be01dc4c056c8525b40874e
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-13 10:17:20 -0700

    test/feature: Add status integer column to joins table, add enumerator to joins table model. Write test for model enum and clas smethod find_joins_row. Write model method and pass tests.

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,11 @@
 class PetApplication < ApplicationRecord
+    enum status: {"Accepted": 0, "Rejected": 1}
     belongs_to :application
     belongs_to :pet
+
+    def self.find_joins_row(application, pet)
+        a_pet_id = pet.id
+        app_id = application.id
+        where(pet_id: a_pet_id, application_id: app_id)  
+    end 
 end 
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -1,8 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe PetApplication, type: :model do
-    describe "relationships" do 
-    it {should belong_to :pet}
-    it {should belong_to :application}
+    describe 'relationships' do 
+        it {should belong_to :pet}
+        it {should belong_to :application}
     end 
+
+    describe 'enum' do 
+        it 'can update an applications status for one pet' do 
+        PetApplication.destroy_all
+        # application_1 = Application.create!(name: "Sedan Turtle", street_address: "3425 Gransfield ave", city: "Denver", state: "CO", zipcode: "80219", description: "I really love animals.", status: "Pending")
+        # dumb_friends_league = Shelter.create!(foster_program: true, name: "Dumb Friends League", city: "Englewood", rank: "1")
+        # mushu = application_1.pets.create!(adoptable: true, age: "5", breed: "dog", name:"Mushu", shelter: dumb_friends_league)
+        joins_row = PetApplication.create(id: 1, pet_id: 1, application_id: 2)
+        expect(joins_row.status).to eq(nil)
+        joins_row.Accepted!
+        expect(joins_row.status).to eq("Accepted")
+        end
+    end 
+
+    describe 'class methods' do 
+        it '#find_joins_row' do 
+            PetApplication.destroy_all
+            application_1 = Application.create!(name: "Sedan Turtle", street_address: "3425 Gransfield ave", city: "Denver", state: "CO", zipcode: "80219", description: "I really love animals.", status: "Pending")
+            dumb_friends_league = Shelter.create!(foster_program: true, name: "Dumb Friends League", city: "Englewood", rank: "1")
+            mushu = application_1.pets.create!(adoptable: true, age: "5", breed: "dog", name:"Mushu", shelter: dumb_friends_league)
+            joins_row = PetApplication.find_joins_row(application_1, mushu)
+            expect(joins_row.first.pet_id).to eq(mushu.id)
+            expect(joins_row.last.pet_id).to eq(mushu.id)
+            expect(joins_row.first.application_id).to eq(application_1.id)
+            expect(joins_row.last.application_id).to eq(application_1.id)
+        end
+    end
 end
