a5d1351e3f46a611047f4d1198a9a46f744e51e9
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-12 22:03:29 -0500

    Test: Search box returns searched pet information on applicant show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,30 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe "Application Show Page" do
-  it "should show applicant information" do
+  before (:each) do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_1.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'dragon', name: 'Blake C', shelter_id: @shelter_1.id)
-    application = Application.create!(name: "Chaz Carmichael",
+    @pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_1.id)
+    @pet_2 = Pet.create(adoptable: true, age: 3, breed: 'dragon', name: 'Blake C', shelter_id: @shelter_1.id)
+    @application = Application.create!(name: "Chaz Carmichael",
       street_address: "10 lane",
       city: "Sandy Springs",
       state: "CO",
       zipcode: 12345,
       description: "I like pets",
       status: "In Progress")
-    pet_application = PetApplication.create!(pet_id: pet_1.id, application_id: application.id)
-    pet_application2 = PetApplication.create!(pet_id: pet_2.id, application_id: application.id)
-    visit "/application/#{application.id}"
-    expect(page).to have_content(application.name)
-    expect(page).to have_content(application.street_address)
-    expect(page).to have_content(application.city)
-    expect(page).to have_content(application.zipcode)
-    expect(page).to have_content(application.description)
-    expect(page).to have_content(application.status)
-    click_on('Lucille Bald')
-    expect(current_path).to eq("/application/pets/#{pet_1.id}")
-    visit "/application/#{application.id}"
-    click_on('Blake C')
-    expect(current_path).to eq("/application/pets/#{pet_2.id}")
+    @application2 = Application.create!(name: "Kirby",
+      street_address: "15 street",
+      city: "Jacob Drive",
+      state: "DE",
+      zipcode: 64523,
+      description: "Cheese is the best",
+      status: "In Progress")
+    @pet_application = PetApplication.create!(pet_id: @pet_1.id, application_id: @application.id)
+    @pet_application2 = PetApplication.create!(pet_id: @pet_2.id, application_id: @application.id)
   end
+
+  it "should show applicant information" do
+
+    visit "/application/#{@application.id}"
+    expect(page).to have_content(@application.name)
+    expect(page).to have_content(@application.street_address)
+    expect(page).to have_content(@application.city)
+    expect(page).to have_content(@application.zipcode)
+    expect(page).to have_content(@application.description)
+    expect(page).to have_content(@application.status)
+    click_on('Lucille Bald')
+    expect(current_path).to eq("/application/pets/#{@pet_1.id}")
+    visit "/application/#{@application.id}"
+    click_on('Blake C')
+    expect(current_path).to eq("/application/pets/#{@pet_2.id}")
+  end
+
+  describe "Add a pet section" do
+    it 'has a text box to search pets by name' do
+      visit "/application/#{@application2.id}"
+      expect(page).to have_button("Search")
+    end
+
+    it "searches for a pet by name and returns to show page with pets who match search" do
+      visit "/application/#{@application2.id}"
+      fill_in "Enter Pet Name:", with: "Lucille"
+      click_on('Search')
+      expect(current_path).to eq("/application/#{@application2.id}")
+      expect(page).to have_content(@pet_1.name)
+      expect(page).to have_content(@pet_1.breed)
+      expect(page).to have_content(@pet_1.age)
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to_not have_content(@pet_2.name)
+      expect(page).to_not have_content(@pet_2.breed)
+    end
+  end
+
 end
