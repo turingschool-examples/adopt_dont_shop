672fa6034a3efae42433ea2ffdcd740ff26e960e
Sam Smith <samlsmith424@gmail.com>
2022-02-13 17:30:50 -0800

    Feat user story 7

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -23,17 +23,21 @@ class ApplicationsController < ApplicationController
   end
 
   def search
-    # @pet = Pet.search(params[:search_pet_by_name])
+    @pet = Pet.search(params[:search_pet_by_name])
     redirect_to action: :show
   end
 
   def update
-    application = Application.find(params[:application_id])
-    pet = Pet.find(params[:pet_id])
-    if application.add_pet?(pet)
-    # pet_application = PetApplication.new(pet_id: @pet.id, application_id: @application.id)
-    # if pet_application.save
-      redirect_to action: :show
+    if params[:pet_id]
+      application = Application.find(params[:application_id])
+      pet = Pet.find(params[:pet_id])
+      application.add_pet?(pet)
+      redirect_to "/applications/#{application.id}"
+    elsif params[:description]
+      application = Application.find(params[:id])
+      application.update(application_params)
+      application.Pending!
+      redirect_to "/applications/#{application.id}"
     else
       flash[:alert] = "Error: you are already adopting this pet"
       redirect_to action: :show
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'application show page' do
   before(:each) do
-    @application_1 = Application.create!(name: "Britney Spears", address: "400 Hollywood Blvd", city: "Los Angeles", state: "CA", zipcode: 90027, description: "I love pets")
+    @application_1 = Application.create!(name: "Britney Spears", address: "400 Hollywood Blvd", city: "Los Angeles", state: "CA", zipcode: 90027)
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @pet_1 = Pet.create!(name: "Pork Chop", age: 6, breed: "Poodle", adoptable: true, shelter_id: @shelter_1.id)
     @pet_2 = Pet.create!(name: "Tiny", age: 4, breed: "Great Dane", adoptable: true, shelter_id: @shelter_1.id)
@@ -21,7 +21,7 @@ RSpec.describe 'application show page' do
       expect(page).to have_content(@application_1.city)
       expect(page).to have_content(@application_1.state)
       expect(page).to have_content(@application_1.zipcode)
-      expect(page).to have_content(@application_1.description)
+      # expect(page).to have_content(@application_1.description)
       expect(page).to have_content(@application_1.status)
       expect(page).to have_link(@pet_1.name)
     end
@@ -70,37 +70,77 @@ RSpec.describe 'application show page' do
 
       expect(current_path).to eq("/applications/#{@application_1.id}")
     end
+
     within('#app-attributes') do
       expect(page).to have_content("Pork Chop")
     end
   end
 
-  describe 'submitting an application' do
-    context 'without pets on the application' do
-      it 'does not allow submission' do
-        visit "/applications/#{@application_1.id}"
-        expect(page).to_not have_content('#application-submission')
-      end
+  it 'has a submit section after you add pets to the application' do
+    visit "/applications/#{@application_1.id}"
+
+    within('#adding-pets') do
+      expect(page).to have_content("Add a Pet to this Application")
+      expect(page).to_not have_content("Submit")
+      fill_in :search, with: "Pork Chop"
+      click_button('Search')
+
+      expect(current_path).to eq("/applications/#{@application_1.id}")
     end
 
-    context 'with pets on the application' do
-      before(:each) do
-        PetApplication.new(pet_id: @pet_1.id, application_id: @application_1.id).save
-        visit "/applications/#{@application_1.id}"
-      end
-
-      it 'has submission form with reason field' do
-        within('#application-submission') do
-          expect(page).to have_field("reason")
-          expect(page).to have_button("Submit")
-        end
-      end
-
-      it 'allows you to submit an application' do
-        
-      end
+    within('#pet-results') do
+      click_button("Adopt this Pet")
+      expect(current_path).to eq("/applications/#{@application_1.id}")
     end
 
+    within('#application-submission') do
+      expect(page).to have_content("Submit Your Application")
 
+      fill_in('Description', with: 'Pork Chop seems nice')
+      click_button('Submit')
+      # require "pry"; binding.pry
+
+      expect(current_path).to eq("/applications/#{@application_1.id}")
+    end
   end
+
+  # describe 'submitting an application' do
+  #   context 'without pets on the application' do
+  #     it 'does not allow submission' do
+  #       visit "/applications/#{@application_1.id}"
+  #       expect(page).to_not have_content('#application-submission')
+  #     end
+  #   end
+  #
+  #   context 'with pets on the application' do
+  #     before(:each) do
+  #       PetApplication.new(pet_id: @pet_1.id, application_id: @application_1.id).save
+  #       visit "/applications/#{@application_1.id}"
+  #     end
+  #
+  #     it 'has submission form with reason field' do
+  #       within('#pet-results') do
+  #         fill_in :search, with: "Pork Chop"
+  #         click_button('Search')
+  #         expect(current_path).to eq("/applications/#{@application_1.id}")
+  #       end
+  #
+  #       within('#application-submission') do
+  #         expect(page).to have_field("reason")
+  #         expect(page).to have_button("Submit")
+  #       end
+  #     end
+  #
+  #       within('#pet-results') do
+  #         click_button "Adopt this Pet"
+  #
+  #         expect(current_path).to eq("/applications/#{@application_1.id}")
+  #       end
+  #     it 'allows you to submit an application' do
+  #
+  #     end
+  #   end
+  #
+  #
+  # end
 end
