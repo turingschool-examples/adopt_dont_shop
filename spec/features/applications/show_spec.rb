cdaa717ae95532d6cb08d23ec532edb545433393
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-14 12:21:43 -0600

    app controller update

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -18,15 +18,17 @@ RSpec.describe 'the application show' do
       @peach = Pet.create!(name: 'Peach', age: 12, breed: 'Poodle', adoptable: true, shelter_id: @shelter_4.id)
 
 
-      @app_1= Application.create(first_name: 'Lemon', last_name: 'Tiger', street_address: '1225 Alvaro Obgeron Dr.', city: 'Mexico City, GA', post_code: '67518', status: "Pending")
-      @app_2= Application.create(first_name: 'Salty', last_name: 'Hippo', street_address: '367 CBTIS Overton St.', city: 'Colima, DC', post_code: '14628', status: "Pending")
-      @app_3= Application.create(first_name: 'Funky', last_name: 'Platypus', street_address: '15 Overpath Pkwy.', city: 'Shenzhen, OH', post_code: '97627', status: "Pending")
+      @app_1= Application.create(first_name: 'Lemon', last_name: 'Tiger', street_address: '1225 Alvaro Obgeron Dr.', city: 'Mexico City, GA', post_code: '67518', status: "In Progress")
+      @app_2= Application.create(first_name: 'Salty', last_name: 'Hippo', street_address: '367 CBTIS Overton St.', city: 'Colima, DC', post_code: '14628', status: "In Progress")
+      @app_3= Application.create(first_name: 'Funky', last_name: 'Platypus', street_address: '15 Overpath Pkwy.', city: 'Shenzhen, OH', post_code: '97627', status: "In Progress")
 
-      ApplicationPet.create!(pet_id:@scooby.id, application_id: @app_1.id)
-      ApplicationPet.create!(pet_id:@mango.id, application_id: @app_1.id)
     end
 
       it "shows the all of the applicants and their details" do
+
+      ApplicationPet.create!(pet_id:@scooby.id, application_id: @app_1.id)
+      ApplicationPet.create!(pet_id:@mango.id, application_id: @app_1.id)
+
       visit "/applications/#{@app_1.id}"
 
       expect(page).to have_content(@app_1.first_name)
@@ -42,6 +44,7 @@ RSpec.describe 'the application show' do
       expect(page).not_to have_content(@app_3.first_name)
 
       click_link 'Scooby'
+
       expect(page).to have_current_path("/pets/#{@scooby.id}")
       expect(page).to have_content(@scooby.name)
 
@@ -71,11 +74,27 @@ RSpec.describe 'the application show' do
 
       fill_in 'Search', with: "Apollo"
       click_on("Search")
-
       expect('Pet search results').to appear_before(@apollo.name)
 
       click_on "Adopt #{@apollo.name}"
 
+
       expect('Pets added to my application:').to appear_before(@apollo.name)
     end
+
+    it "can submit an application" do
+
+    ApplicationPet.create!(pet_id:@scooby.id, application_id: @app_1.id)
+    ApplicationPet.create!(pet_id:@mango.id, application_id: @app_1.id)
+
+    visit "/applications/#{@app_1.id}"
+
+    expect('Pets added to my application:').to appear_before(@scooby.name)
+    expect('Pets added to my application:').to appear_before(@mango.name)
+
+    expect(page).to have_content('Why would you make an excellent owner for these pets?')
+
+    fill_in 'Please describe', with: "Long walks on the beach"
+    click_on("Submit my application")
+  end
 end
