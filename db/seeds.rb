0f3749745c6d7d741db465badae9cf43275ce9ae
Mark Ertmer <mark.ertmer@gmail.com>
2022-02-14 10:29:00 -0700

    Add: destroy_all commands to top of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PetApplication.destroy_all
+Application.destroy_all
+Pet.destroy_all
+Shelter.destroy_all
+
 shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
 shelter.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
 shelter.pets.create!(name: 'Gaspir', breed: 'shorthair', age: 3, adoptable: true)
