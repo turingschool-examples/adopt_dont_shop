3693f942ef3148608da61f9ac7e620d32437675e
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-13 18:27:43 -0700

    feature: working on updating application status to accepted once all pets have been approved for the application

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,29 +6,41 @@ class AdminController < ApplicationController
 
     def show
         @application = Application.find(params[:id])
-        # Probably should move this to model level 
         @full_address = "#{@application.street_address} #{@application.city}, #{@application.state} #{@application.zipcode}"
         @undetermined_pets = PetApplication.find_nil_pets(@application)
         @approved_pets = PetApplication.find_approved_pets(@application)
         @rejected_pets = PetApplication.find_rejected_pets(@application)
+        if @approved_pets == @application.pets
+            @application.Accepted!
+            binding.pry
+            # @application.update(application_params)
+        end 
     end 
 
     def update 
         application = Application.find(params[:id])
+        approved_pets = PetApplication.find_approved_pets(application)
         if params[:approve]
             pet = Pet.find(params[:approve])
             accepted_joins_row = (PetApplication.find_joins_row(application, pet)).first
             accepted_joins_row.Accepted!
+            # approved_pets = PetApplication.find_approved_pets(application)
+            # if application.pets == approved_pets
+            #  application.Accepted!
+            # end 
             redirect_to "/admin/applications/#{application.id}"
         end 
 
         if params[:reject]
             pet = Pet.find(params[:reject])
             rejected_joins_row = (PetApplication.find_joins_row(application, pet)).first
-            # Is this persisting and saving in the database?
             rejected_joins_row.Rejected!
-            # rejected_joins_row.update
             redirect_to "/admin/applications/#{application.id}"
         end 
+    end 
+
+private 
+    def application_params
+        params.permit(:description, :status)
     end 
 end
--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'The admin applications show page' do 
     before :each  do 
+        PetApplication.destroy_all
+        Application.destroy_all
         @dumb_friends_league = Shelter.create!(foster_program: true, name: "Dumb Friends League", city: "Englewood", rank: "1")
         @smart_friends_league = Shelter.create!(foster_program: true, name: "Smart Friends League", city: "Lakewood", rank: "2")
 
-        @application_1 = Application.create!(name: "Sedan Turtle", street_address: "3425 Gransfield ave", city: "Denver", state: "CO", zipcode: "80219", status: "Pending")
+        @application_1 = Application.create!(name: "Denise Turtle", street_address: "3425 wave ave", city: "Louisville", state: "KY", zipcode: "80219", status: "Pending")
         @application_2 = Application.create!(name: "Roman Reigns", street_address: "2354 Narwal ct", city: "Littleton", state: "CO", zipcode: "23785", status: "Pending")
         @application_3 = Application.create!(name: "Doman Dens", street_address: "2354 Bart ct", city: "Middleton", state: "CO", zipcode: "23785", status: "In Progress")
 
@@ -15,7 +17,7 @@ RSpec.describe 'The admin applications show page' do
         @tesla = @application_3.pets.create!(adoptable: true, age: "9", breed: "cat", name:"Tesla", shelter: @smart_friends_league)
     end
     
-    it 'shows the admin user one appliciation and all of the pets on that application' do 
+    xit 'shows the admin user one appliciation and all of the pets on that application' do 
         visit "/admin/applications/#{@application_1.id}"
         expect(page).to have_content(@application_1.description)
         expect(page).to have_content(@application_1.status)
@@ -23,25 +25,37 @@ RSpec.describe 'The admin applications show page' do
         expect(page).to have_content(@mantis.name)
     end 
 
-    it 'has a button next to each pet on an application that allows the admin user to approve that specific pet' do 
+    xit 'has a button next to each pet on an application that allows the admin user to approve that specific pet' do 
         visit "/admin/applications/#{@application_1.id}"
         click_button("Approve #{@mushu.name}")
         expect(current_path).to eq("/admin/applications/#{@application_1.id}")
         expect(page).to have_content("#{@mushu.name} has been approved")
     end 
 
-    it 'has a button next to each pet on an application that allows the admin user to reject that specific pet' do 
+    xit 'has a button next to each pet on an application that allows the admin user to reject that specific pet' do 
         visit "/admin/applications/#{@application_1.id}"
         click_button("Reject #{@mushu.name}")
         expect(current_path).to eq("/admin/applications/#{@application_1.id}")
         expect(page).to have_content("#{@mushu.name} has been rejected")
     end
 
-    it 'Rejecting a pet on one application does not affect its status on another application' do 
+    xit 'rejecting a pet on one application does not affect its status on another application' do 
         visit "/admin/applications/#{@application_2.id}"
         expect(current_path).to eq("/admin/applications/#{@application_2.id}")
         expect(page).to have_no_content("#{@mushu.name} has been rejected")
         expect(page).to have_button("Approve #{@mushu.name}")
         expect(page).to have_button("Reject #{@mushu.name}")
+    end
+
+    it 'updates an applications status to approved once all pets have been approved' do 
+        visit "/admin/applications/#{@application_1.id}"
+        expect(@application_1.status).to eq("Pending")
+        click_button("Approve #{@mushu.name}")
+        expect(page).to have_content("#{@mushu.name} has been approved")
+        expect(@application_1.status).to eq("Pending")
+        click_button("Approve #{@mantis.name}")
+        expect(page).to have_content("#{@mantis.name} has been approved")
+        expect(@application_1.status).to eq("Accepted")
+        expect(page).to have content("Status: Accepted")
     end
 end 
