34ffd397f5064d2b2bd5851d1a16b35aae887988
Becky Nisttahuz <benistta@hotmail.com>
2022-02-14 11:28:39 -0700

    Fix: Test for application show spec

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -36,27 +36,20 @@ RSpec.describe 'application show page' do
 # Then I am taken back to the application show page
 # And under the search bar I see any Pet whose name matches my search
 
-it 'has a text box to filter results by keyword' do
+it 'has a section on the page with input to search for pets' do
+  shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+  pet_1 = shelter_1.pets.create(name: 'simba', age:12, breed: 'Doberman', adoptable: true)
+
   application_2 = Application.last
   visit "/applications/#{application_2 .id}"
 
   expect(page).to have_button("Add a Pet to this application")
+  fill_in "Search", with: "simba"
 
-end
-
-xit 'lists partial matches as search results' do
-  shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-  pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
-  pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
-  pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
-
-  visit "/pets"
-
-  fill_in 'Search', with: "Ba"
-  click_on("Search")
+  click_button "Add a Pet to this application"
+  expect(current_path).to eq("/applications/#{application_2.id}")
 
   expect(page).to have_content(pet_1.name)
-  expect(page).to have_content(pet_2.name)
-  expect(page).to_not have_content(pet_3.name)
 end
+
 end
