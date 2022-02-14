3957daa87c5f60b6c866bed5159cee272f0433e1
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-14 14:53:22 -0500

    Change: Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,30 @@
-Shelter.destroy_all
-Pet.destroy_all
-VeterinaryOffice.destroy_all
-Veterinarian.destroy_all
-Application.destroy_all
+# Shelter.destroy_all
+# Pet.destroy_all
+# VeterinaryOffice.destroy_all
+# Veterinarian.destroy_all
+# Application.destroy_all
 
 
-@shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-@shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-@shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+@shelter_1 = Shelter.find_or_create_by!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+@shelter_2 = Shelter.find_or_create_by!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+@shelter_3 = Shelter.find_or_create_by!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
 
-pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_1.id)
-pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_2.id)
-pet_3 = Pet.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: @shelter_3.id)
+pet_1 = Pet.find_or_create_by!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_1.id)
+pet_2 = Pet.find_or_create_by!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_2.id)
+pet_3 = Pet.find_or_create_by!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: @shelter_3.id)
 
-@vet_office_1 = VeterinaryOffice.create(name: 'Special Friends', boarding_services: true, max_patient_capacity: 100)
-@vet_office_2 = VeterinaryOffice.create(name: 'Pet Emergency Room', boarding_services: true, max_patient_capacity: 50)
-@vet_office_3 = VeterinaryOffice.create(name: 'The Country Vet', boarding_services: true, max_patient_capacity: 200)
+@vet_office_1 = VeterinaryOffice.find_or_create_by!(name: 'Special Friends', boarding_services: true, max_patient_capacity: 100)
+@vet_office_2 = VeterinaryOffice.find_or_create_by!(name: 'Pet Emergency Room', boarding_services: true, max_patient_capacity: 50)
+@vet_office_3 = VeterinaryOffice.find_or_create_by!(name: 'The Country Vet', boarding_services: true, max_patient_capacity: 200)
 
 
-vet_1 = Veterinarian.create(name: 'Taylor', review_rating: 10, on_call: true, veterinary_office_id: @vet_office_1.id)
-vet_2 = Veterinarian.create(name: 'Jim', review_rating: 8, on_call: true, veterinary_office_id: @vet_office_2.id)
+vet_1 = Veterinarian.find_or_create_by!(name: 'Taylor', review_rating: 10, on_call: true, veterinary_office_id: @vet_office_1.id)
+vet_2 = Veterinarian.find_or_create_by!(name: 'Jim', review_rating: 8, on_call: true, veterinary_office_id: @vet_office_2.id)
 
-application = Application.create!(name: "Chaz Carmichael",
+application = Application.find_or_create_by!(name: "Chaz Carmichael",
   street_address: "10 lane",
   city: "Sandy Springs",
   state: "CO",
   zipcode: 12345,
   description: "I like pets",
   status: "In Progress")
-
-application.id = 1
-application.save!
