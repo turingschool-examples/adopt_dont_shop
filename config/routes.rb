e78b3338ff448d152d43ac147bd180b82f5c7c15
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-14 15:42:38 -0600

    add route for application #create

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   post '/applications/:id', to: 'applications#create'
   patch '/applications/:id', to: 'applications#update'
 
+  get '/application_pets', to: 'application_pets#create'
   post '/application_pets', to: 'application_pets#create'
 
   get '/veterinary_offices', to: 'veterinary_offices#index'
