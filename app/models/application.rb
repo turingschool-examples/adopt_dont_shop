1ea16f2d5cd7f960e231014d4f3f0c976e107262
Becky Nisttahuz <benistta@hotmail.com>
2022-02-14 10:25:32 -0700

    Feat: Adding validations for attributes

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,11 +1,14 @@
 class Application < ApplicationRecord
+
   validates :name, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true
+  validates :description, presence: true
 
   # attribute :status, :string, deafult: "In Progress"
   has_many :application_pets
   has_many :pets, through: :application_pets
+
 end
