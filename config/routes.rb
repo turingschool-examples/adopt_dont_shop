34453945b2cb4da150333b28792a28ccee206fbe
Michael Hicks <hicks.michael04@gmail.com>
2022-02-14 09:21:35 -0700

    Add welcome index and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  root 'welcome#index'
+  
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
