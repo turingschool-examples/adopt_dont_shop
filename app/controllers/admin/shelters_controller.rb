98a126c353af465e20438cd6fac3a3e7cf5592a7
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-15 10:24:54 -0500

    Add: Pending applications method to controller and view page

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SheltersController < ApplicationController
   def index
+    @shelters_with_pending_apps = Shelter.pending_applications
     @shelters = Shelter.reverse_alphabetical
   end
 end
