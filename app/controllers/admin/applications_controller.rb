fde4868d1d65f1fae202ae08d8edbdbd799c3dfc
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-15 17:59:52 -0500

    Add: Routes and controller method for application approval button

---
+++ app/controllers/admin/applications_controller.rb
@@ -0,0 +1,15 @@
+class Admin::ApplicationsController < ApplicationController
+
+  def show
+    @application = Application.find(params[:id])
+    @pets = @application.pets
+  end
+
+  def update
+    @application = Application.find(params[:id])
+    pet_app = PetApplication.pet_app(@application.id, params[:petid])
+    pet_app.status = "1"
+    pet_app.save
+    redirect_to "/admin/applications/#{@application.id}/"
+  end
+end
