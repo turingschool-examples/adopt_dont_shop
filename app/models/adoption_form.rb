93c550ac956a770d2811847df0e14b3bddec9319
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-15 11:55:20 -0700

    Add default value for description

--- app/models/adoption_form.rb
+++ app/models/adoption_form.rb
@@ -6,11 +6,12 @@ class AdoptionForm < ApplicationRecord
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zip_code
+  attribute :description, :string, default: "Describe home here"
   validates_presence_of :description
 
-  # write tests for this later !!
   has_many :pet_adoption_forms
   has_many :pets, through: :pet_adoption_forms
 
   enum status: {"In Progress" => 0 , "Pending" => 1, "Accepted" => 2, "Rejected" => 3}
+
 end
