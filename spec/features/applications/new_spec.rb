9bebaf0727548e83fea8e67d239d615e1c2880da
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-15 10:30:54 -0500

    Fix: Application and applications new spec after changing form and title

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe "Starting an Application" do
   it "displays form for user to fill out and redirects to show page" do
     visit '/application/new'
-    fill_in 'Name:', with: 'Jerry'
-    fill_in 'Street Address:', with: '15 Glen Street'
-    fill_in 'City:', with: 'Los Angeles'
-    fill_in 'State:', with: 'CA'
-    fill_in 'Zip Code:', with: 34567
+    fill_in 'Full Name', with: 'Jerry'
+    fill_in 'Street Address', with: '15 Glen Street'
+    fill_in 'City', with: 'Los Angeles'
+    fill_in 'State', with: 'CA'
+    fill_in 'Zip Code', with: 34567
     click_on('Create Application')
     application = Application.first
     expect(current_path).to eq("/application/#{application.id}")
@@ -16,12 +16,12 @@ RSpec.describe "Starting an Application" do
 
   it "takes user back to the new applications page if form has blank fields" do
     visit '/application/new'
-    fill_in 'Name:', with: 'Jerry'
-    fill_in 'Street Address:', with: '15 Glen Street'
-    fill_in 'City:', with: ''
-    fill_in 'State:', with: ''
-    fill_in 'Zip Code:', with: 123
+    fill_in 'Full Name', with: 'Jerry'
+    fill_in 'Street Address', with: '15 Glen Street'
+    fill_in 'City', with: ''
+    fill_in 'State', with: ''
+    fill_in 'Zip Code', with: 123
     click_on('Create Application')
-    expect(current_path).to eq("/application/new")
+    expect(current_path).to eq("/create_application")
   end
 end
