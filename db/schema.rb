1bc35f78fb13aa15715f397c83200dafecebaec5
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-15 14:42:41 -0700

    create migration to add street_address, state and zipcode to shelter table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_163306) do
+ActiveRecord::Schema.define(version: 2022_02_15_194916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 2022_02_13_163306) do
     t.integer "rank"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "street_address"
+    t.string "state"
+    t.integer "zipcode"
   end
 
   create_table "veterinarians", force: :cascade do |t|
