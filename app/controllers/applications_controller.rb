8959d61358d7ad51266d7b0bd8dce08d0be377ae
Becky Nisttahuz <benistta@hotmail.com>
2022-02-14 22:00:32 -0700

    Feat: Updated show method to find pet

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,31 +1,57 @@
 class ApplicationsController < ApplicationController
 
-
   def show
     @application = Application.find(params[:id])
+
+    if params[:search].present?
+      @pets = Pet.where(name: params[:search])
+    else
+      @pets = []
+    end
+    # @pets = @application.pets
+    # @pets = Pet.find_by(name: params[:search])
+    # @approved_pets = @pets.find_all {|pet| !pet.has_approved_applicant}
   end
 
   def new
-    @application = Application.new
+    # @application = Application.new
   end
+
 
   def create
     # application = Application.create(application_params)
-    # require "pry"; binding.pry
-    application = Application.create(application_params)
+    application = Application.new(application_params)
     # application.status = "In Progress"
     # application.update(status: "In Progress")
     # redirect_to "/applications/#{application.id}"
+    # if application.save && params[:pet_names] != nil
     if application.save
+    #   pets = Pet.where(id: params.keys)
+    # application.pets << pets
+
       redirect_to "/applications/#{application.id}"
     else
-      flash[:alert] = "Error: #{error_message(application.errors)}"
       redirect_to "/applications/new"
-      
+      flash[:alert] = "Error: #{error_message(application.errors)}"
     end
-
   end
 
+  def update
+    application = Application.find(params[:id])
+
+    application.update(application_params)
+    redirect_to "/applications/#{application.id}"
+    # pet = Pet.find_by(name: params[:search])
+    # application.pets << pet
+    # redirect_to "/applications/#{application.id}"
+  end
+
+  # def search
+  #   @pets = Pet.find_by(name: params[:search])
+  #
+  #   redirect_to "/applications/#{params[:id]}"
+  #
+  # end
 
 private
   def application_params
