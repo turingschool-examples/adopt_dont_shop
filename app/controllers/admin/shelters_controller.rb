9a6b7b55085daea88a91a1e3c79ffed754ae06e5
TJ Williams <williamsjr.timothy1@gmail.com>
2022-02-16 18:09:10 -0500

    Feat: Displays shelters with pending apps in alphabetical order

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,6 +1,6 @@
 class Admin::SheltersController < ApplicationController
   def index
-    @shelters_with_pending_apps = Shelter.pending_applications
+    @shelters_with_pending_apps = Shelter.pending_alphabetical_order
     @shelters = Shelter.reverse_alphabetical
   end
 
