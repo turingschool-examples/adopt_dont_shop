4144043be508fc66973886f9228d111fcd2e3478
Blake Dempsey <bdempsey864@gmail.com>
2022-02-16 16:38:33 -0500

    user story 18 complete

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -112,6 +112,21 @@ RSpec.describe 'application show page' do
     expect(page).to have_content(happy.name)
     expect(page).to_not have_content(honus.name)
   end
+
+  it 'returns matches regardless of case' do
+    murph = Application.create!(name: "Murph", street_address: "456 Acres Ln", city: "Boca Rotan", state: "FL", zip_code: "33481", description: "Jack would have a brother", status: "In Progress")
+    cyle = Application.create!(name: "Cyle", street_address: "139 Corvette St", city: "Inman", state: "SC", zip_code: "29349", description: "I would take him disc'n", status: "In Progress")
+    homing_homies = Shelter.create!(name: "Homing Homies", city: "Houston", rank: 1, foster_program: true)
+    hank = homing_homies.pets.create!(name: "Hank", breed: "Aussie", adoptable: true, age: 6)
+    happy = homing_homies.pets.create!(name: "Happy", breed: "Pit Bull", adoptable: true, age: 1)
+    honus = homing_homies.pets.create!(name: "Honus", breed: "Blood Hound", adoptable: false, age: 2)
+
+    visit "/applications/#{murph.id}"
+
+    fill_in :search_name, with: "hApPy"
+    click_button "Search"
+    expect(page).to have_content(happy.name)
+  end
 end
 
 
