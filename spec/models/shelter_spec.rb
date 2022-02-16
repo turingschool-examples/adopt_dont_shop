5e34eb3f2425d8de46b421594812e53fe66aaab3
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-16 12:10:35 -0700

    add activerecord association. pass test

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Shelter, type: :model do
   describe 'relationships' do
     it { should have_many(:pets) }
+    it { should have_many(:applications).through(:pets)}
   end
 
   describe 'validations' do
