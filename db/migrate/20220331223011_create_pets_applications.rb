7de443f89548c7872b081c37d3b3ba61b4ddd638
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-31 16:39:13 -0600

    create migrations for Applications and PetApplications

---
+++ db/migrate/20220331223011_create_pets_applications.rb
@@ -0,0 +1,8 @@
+class CreatePetsApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets_applications do |t|
+      t.references :pet, foreign_key: true
+      t.references :application, foreign_key: true
+    end
+  end
+end
