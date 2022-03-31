171fda35fc31942342c79afbdf12a60af297d3e5
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-31 15:34:25 -0600

    add asset configuration for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
