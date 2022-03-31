49d9368e6eceac89b34fef14ed1c7503ba1dc690
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-31 14:47:19 -0600

    add application model test

---
+++ spec/features/applications/index_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Applications Index' do
+  it "lists all applications" do
+    @applicantion_1 = Application.create!(name: "Susan", street_address:)
+  end
+end
---
+++ spec/models/application_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Application, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_presence_of(:reasons_to_approve) }
+    it { should validate_presence_of(:status) }
+  end
+
+end
