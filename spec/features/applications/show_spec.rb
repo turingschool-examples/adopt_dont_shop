08d03d43202a3c04e3e351c49f91379e95b46d42
Kim G <xiaole.guo@yahoo.com>
2022-03-31 14:05:12 -0600

    write test for application show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'the applications show' do
 	
-	# it 'shows application attributes' do 
-	# 	application = Application.create(name:)
-	# 	visit '/applications/show'
-
+	it 'shows application attributes' do 
+		application = Application.create!(name: "Tyler R", full_address:"1000 Something Blvd, Denver, 80221", 
+										  description: "I'm lonely and need fluffy mammals")
+		shelter = Shelter.create!(name: "Max Fund", city: "Denver", rank: 100, foster_program:true)
+		pet_1 = shelter.pets.create!(name: "Dianne", age: 3, breed: "cat?", adoptable: true)
+		visit '/applications/show'
+	end
 
 
 
