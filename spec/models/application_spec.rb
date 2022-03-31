bbb47aea3d5f4e24416b7a2e9f89585a7fd67fcf
Chris Romero <chrisleeromero1@gmail.com>
2022-03-31 17:41:59 -0600

    test: add validation tests for application model

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Application, type: :model do
     it { should have_many :application_pets}
     it { should have_many(:pets).through(:application_pets)}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :address}
+    it { should validate_presence_of :city}
+    it { should validate_presence_of :state}
+    it { should validate_presence_of :zipcode}
+    it { should validate_presence_of :description}
+    it { should validate_presence_of :status}
+  end
 end
