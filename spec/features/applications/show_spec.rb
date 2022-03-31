bc3bcaf4999f23debd831c63a31b8a387b618625
Kim G <xiaole.guo@yahoo.com>
2022-03-31 13:55:58 -0600

    complete setup for application model and many-to-many association

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'the applications show' do
+	
+	# it 'shows application attributes' do 
+	# 	application = Application.create(name:)
+	# 	visit '/applications/show'
+
+
+
+	end
+
+end
---
+++ spec/models/application_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  describe 'relationships' do
+    it { should have_many(:application_pets)}
+    it { should have_many(:pets).through(:application_pets)}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:full_address) }
+    it { should validate_presence_of(:description) }
+  end
+
+
+
+end
