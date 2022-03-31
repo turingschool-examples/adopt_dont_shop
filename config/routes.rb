2bcb402774536b09a71ab23d03ba84b83e45e94f
Kim G <xiaole.guo@yahoo.com>
2022-03-31 10:19:21 -0600

    Initial commit:heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/', to: 'application#welcome'
+  root 'welcome#index'
+  #get '/', to: 'application#welcome'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
