8e193f9e8c8113857b148c99d231f005786a17e1
Chris Romero <chrisleeromero1@gmail.com>
2022-04-01 13:26:30 -0600

    test: add test for pet names on application show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -14,7 +14,9 @@ RSpec.describe 'the applications index' do
   it 'displays the applicant attributes ' do
     application = Application.create!(name: 'Chris', address: '505 Main St.', city: 'Denver', state: 'CO', zipcode: '80205', description: "I'm great with dogs.", status: 'In-progress')
     shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
-    pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    pet_1 = application.pets.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    pet_2 = application.pets.create(name: 'Sparky', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    pet_3 = Pet.create(name: 'Spot', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
     visit "/applications/#{application.id}"
     save_and_open_page
     expect(page).to have_content(application.name)
@@ -24,5 +26,8 @@ RSpec.describe 'the applications index' do
     expect(page).to have_content(application.zipcode)
     expect(page).to have_content(application.description)
     expect(page).to have_content(application.status)
+    expect(page).to_not have_content(pet_3.name)
+    click_link "#{pet_1.name}"
+    expect(current_path).to eq("/pets/#{pet_1.id}")
   end
 end
