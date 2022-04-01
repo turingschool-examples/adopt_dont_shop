5ee5d64811863570d10376d19adf06e3ea65b76e
Kim G <xiaole.guo@yahoo.com>
2022-03-31 22:25:16 -0600

    reformat show page and reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-application = Application.create!(name: "Tyler R", full_address:"1000 Something Blvd, Denver, 80221", description: "I'm lonely and need fluffy mammals")
+Application.destroy_all
+Shelter.destroy_all
+Pet.destroy_all
+Veterinarian.destroy_all
+VeterinaryOffice.destroy_all
+application = Application.create!(name: "Tyler R", street_address:"1000 Something Blvd", city: "Denver", state: "CO", zipcode: "80123")
 shelter = Shelter.create!(name: "Max Fund", city: "Denver", rank: 100, foster_program:true)
 pet_1 = shelter.pets.create!(name: "Dianne", age: 3, breed: "cat?", adoptable: true)
