91dbdb84f633b3cc4b85f5a92ebd27aab1819c63
Susan Boyd <sueboyd922@gmail.com>
2022-04-01 10:58:16 -0400

    Test: write test for new application page

---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'new application page' do
+  it 'creates a new application' do
+    visit '/applications/new'
+
+    fill_in("Name", with: "Frodo Baggins")
+    fill_in("Street Address", with: "1 Shire Ave")
+    fill_in("City", with: "Las Vegas")
+    fill_in("State", with: "NV")
+    fill_in("Zip Code", with: "88901")
+
+    click_on("Submit")
+    expect(current_path).to eq("/applications/1")
+
+    expect(page).to have_content("Frodo Baggins")
+    expect(page).to have_content("1 Shire Ave")
+    expect(page).to have_content("Las Vegas")
+    expect(page).to have_content("Description")
+    expect(page).to have_content("Pending")
+    expect(page).not_to have_content("Accepted")
+  end
+
+end
