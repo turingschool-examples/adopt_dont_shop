f453ae4b409c41bcb6e19275374654e654fbebf4
Will Medders <wmedders@gmail.com>
2022-04-01 16:45:14 -0400

    Test: application show shows applicant and attributes

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'the application show' do
+  it "shows the applicant and all it's attributes" do
+    shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    scooby = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    application = Application.create!(status: "In Progress", name: "Murmuring Savannah", street: 'Main',city: 'Leadville', state: 'CO', zip: 11111)
+
+    visit "/applications/#{application.id}"
+
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.street)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zip)
+    expect(page).to have_content(application.description)
+    expect(page).to have_content(scooby.name)
+    expect(page).to have_content(application.status)
+  end
+end
