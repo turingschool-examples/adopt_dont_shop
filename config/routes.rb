7c0bb2456bd81458cb04b1dd55ba77cfea83a809
Tyler Ross <tross020890@gmail.com>
2022-03-31 19:34:07 -0500

    restore welcome path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #root 'welcome#index'
-  #get '/', to: 'application#welcome'
+  get '/', to: 'application#welcome'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
