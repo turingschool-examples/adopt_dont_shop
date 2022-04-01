57311a8af7a4a797cb60bcbd2d92603e59cf9869
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-01 06:11:53 -0600

    test: validations to application

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,12 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
+  validates :description, presence: true
+  validates :status, presence: true
+
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Application do
     it {should have_many(:pet_applications)}
     it {should have_many(:pets).through(:pet_applications)}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:street_address)}
+    it { should validate_presence_of(:city)}
+    it { should validate_presence_of(:state)}
+    it { should validate_presence_of(:zipcode)}
+    it { should validate_presence_of(:description)}
+    it { should validate_presence_of(:status)}
+  end
 end
