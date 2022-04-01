3644e144618851884422c6d2302a0481cb6330e3
Joseph Downs <jos.w.downs@gmail.com>
2022-04-01 17:25:20 -0600

    Added @pets.name to view

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    @pets = Pet.find(@application.pet_id)
+    # binding.pry
   end
 end
