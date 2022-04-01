897de1ba7f166720cdd0c67ce940a9b9a2f8cbb6
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-01 14:35:59 -0600

    Test: add test for new application link from pets#index page

---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Application do
+  describe 'new applications' do
+
+    describe 'as a visitor' do
+
+      it 'visit pet index page, click link to start an application' do
+        Application.destroy_all
+        shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+        pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+        pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+
+        visit "/pets"
+
+
+        click_link "Start an Application"
+        expect(current_path).to eq("/applications/new")
+
+        fill_in 'Name', with: 'Barry Bonds'
+        fill_in 'Full Address', with: '513 Maggie Lane, Evergreen, CO, 80439'
+        click_on 'Submit'
+        expect(current_path).to eq("/applications/1")
+
+        expect(page).to have_content("Name: Barry Bonds")
+        expect(page).to have_content("Address: 513 Maggie Lane, Evergreen, CO, 80439")
+        expect(page).to have_content("Description: ")
+        expect(page).to have_content("Status: In Progress")
+      end
+
+
+
+
+    end
+
+
+  end
+end
