bbd05df099fad5e1072c09e5b0ffea62074e47d3
Chris Romero <chrisleeromero1@gmail.com>
2022-03-31 19:05:20 -0600

    refactor: update show index file to iterate over array

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -12,16 +12,17 @@ RSpec.describe 'the applications index' do
 # names of all pets that this application is for (all names of pets should be links to their show page)
 # The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
   it 'displays the applicant attributes ' do
-    applicantion = Application.create!(name: 'Chris', address: '505 Main St.', city: 'Denver', state: 'CO', zipcode: '80205', description: "I'm great with dogs.", status: 'In-progress')
+    application = Application.create!(name: 'Chris', address: '505 Main St.', city: 'Denver', state: 'CO', zipcode: '80205', description: "I'm great with dogs.", status: 'In-progress')
     shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
     pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
-    visit "/applications/#{applicantion.id}"
-    expect(page).to have_content(applicantion.name)
-    expect(page).to have_content(applicantion.address)
-    expect(page).to have_content(applicantion.city)
-    expect(page).to have_content(applicantion.state)
-    expect(page).to have_content(applicantion.zipcode)
-    expect(page).to have_content(applicantion.description)
-    expect(page).to have_content(applicantion.status)
+    visit "/applications/#{application.id}"
+    save_and_open_page
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zipcode)
+    expect(page).to have_content(application.description)
+    expect(page).to have_content(application.status)
   end
 end
