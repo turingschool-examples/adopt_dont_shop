4313387817d0a5395cf3ecc61c65b73d7579817b
Tyler Ross <tross020890@gmail.com>
2022-03-31 19:12:15 -0500

    removed spacing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-application = Application.create!(name: "Tyler R", full_address:"1000 Something Blvd, Denver, 80221",
-                  description: "I'm lonely and need fluffy mammals")
+application = Application.create!(name: "Tyler R", full_address:"1000 Something Blvd, Denver, 80221", description: "I'm lonely and need fluffy mammals")
 shelter = Shelter.create!(name: "Max Fund", city: "Denver", rank: 100, foster_program:true)
 pet_1 = shelter.pets.create!(name: "Dianne", age: 3, breed: "cat?", adoptable: true)
