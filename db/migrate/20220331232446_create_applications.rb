69a5ed2cf0206d87ac9e8835ca525b8054b20adf
Alex Psitos <apsitos08@gmail.com>
2022-03-31 20:41:52 -0400

    Create tests and models for application and pet application

---
+++ db/migrate/20220331232446_create_applications.rb
@@ -0,0 +1,14 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :street_address
+      t.string :city
+      t.integer :zip_code
+      t.string :description
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
