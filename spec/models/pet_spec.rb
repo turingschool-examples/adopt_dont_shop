9f6695bd813ebca2e308be28a11c11aa326bd140
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-01 06:01:42 -0600

    relationships tests passing

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Pet, type: :model do
   describe 'relationships' do
     it { should belong_to(:shelter) }
     it { should have_many(:pet_applications) }
-    it { should have_many(:pets).through(:pet_applications) }
+    it { should have_many(:applications).through(:pet_applications) }
   end
 
   describe 'validations' do
