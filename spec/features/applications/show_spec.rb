ec12e6334e8b4df61f641170a83d3539a51e11fe
Sherman Au <shermau13@gmail.com>
2022-04-02 17:57:12 -0600

    add:application#show shows attributes

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,10 +5,10 @@ require 'rails_helper'
 # names of all pets that this application is for (all names of pets should be links to their show page)
 # The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
 
-RSpec.describe 'the applications show' do
+RSpec.describe 'the applications show page' do
   before do
     @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @pet = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+    @pet_1 = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
     @pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
 
     @application = Application.create(
@@ -17,11 +17,14 @@ RSpec.describe 'the applications show' do
       description: 'Loves animals',
       status: 'Accepted'
     )
+    PetApplication.create!(pet_id: @pet_1.id, application_id: @application.id)
+    PetApplication.create!(pet_id: @pet_2.id, application_id: @application.id)
 
     visit "/applications/#{@application.id}"
   end
 
   it 'shows the attributes of the application' do
+    save_and_open_page
     expect(page).to have_content(@application.name)
     expect(page).to have_content(@application.address)
     expect(page).to have_content(@application.city)
@@ -29,5 +32,12 @@ RSpec.describe 'the applications show' do
     expect(page).to have_content(@application.zipcode)
     expect(page).to have_content(@application.description)
     expect(page).to have_content(@application.status)
+  end
+
+  it 'pet names has redirect link' do
+    expect(page).to have_link(@pet_1.name.to_s, href: "/pets/#{@pet_1.id}")
+    click_link @pet_1.name.to_s
+    save_and_open_page
+    expect(current_path).to eq("/pets/#{@pet_1.id}")
   end
 end
