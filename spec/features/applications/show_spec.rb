53124b1d2fcfc879ef8c22a398fc589b04e74d01
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-02 15:34:26 -0600

    Test: add test for button to adopt a pet from search results, add plain text description of app's displayed attriubtes

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe 'Application show page' do
     Shelter.destroy_all
 
     shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+    @pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    @pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
     @pet_3 = Pet.create(adoptable: true, age: 7, breed: 'corgie', name: 'Skippy', shelter_id: shelter.id)
     @pet_4 = Pet.create(adoptable: true, age: 5, breed: 'lab', name: 'skippy', shelter_id: shelter.id)
     @app_1 = Application.create!(name: 'bob', address: '100 main street, Aurora, CO, 80014', description: 'I love dogs', pet_names: 'Joe, Champ, Pixie', status: 2)
@@ -28,11 +28,11 @@ RSpec.describe 'Application show page' do
     it 'Then I can see application attributes' do
       visit "/applications/#{@app_1.id}"
 
-      expect(page).to have_content("#{@app_1.name}")
-      expect(page).to have_content("#{@app_1.address}")
-      expect(page).to have_content("#{@app_1.description}")
-      expect(page).to have_content("#{@app_1.pet_names}")
-      expect(page).to have_content("#{@app_1.status}")
+      expect(page).to have_content("Applicant Name: #{@app_1.name}")
+      expect(page).to have_content("Full Address: #{@app_1.address}")
+      expect(page).to have_content("Why I am a Good Home: #{@app_1.description}")
+      expect(page).to have_content("Adoptable Pets: #{@app_1.pet_names}")
+      expect(page).to have_content("Appplication Status: #{@app_1.status}")
     end
 
     describe 'When and application has not been submitted' do
@@ -49,6 +49,28 @@ RSpec.describe 'Application show page' do
         expect(page).to have_content("#{@pet_4.name}")
       end
     end
+
+    describe 'Adding pets to an application' do
+
+      it 'I see button to adopt a pet, clicking it adds pet to my application' do
+        visit "/applications/#{@app_2.id}"
+
+        expect(page).to have_content("Adoptable Pets:")
+        fill_in "pet names", with: 'Lobster'
+        click_on 'Submit'
+
+        within "#pet-#{@pet_2.id}" do
+          click_on "Adopt This Pet"
+        end
+        expect(current_path).to eq("/applications/#{@app_2.id}")
+
+        expect(page).to have_content("Adoptable Pets: #{@pet_2.name}")
+
+
+      end
+
+
+
+    end
   end
 end
-
