9472b4d2a38ff16c0fe30ce6ee2341b451521c3e
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-02 15:03:40 -0600

    initial commit: application show test

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+# Name of the Applicant
+# Full Address of the Applicant including street address, city, state, and zip code
+# Description of why the applicant says they'd be a good home for this pet(s)
+# names of all pets that this application is for (all names of pets should be links to their show page)
+# The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
+
+RSpec.describe 'the applications show' do
+    before do
+      @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      @pet = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+      @pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
+      @applicantion = Applicantion.create(name: "Sherman",address: "123 Main St",city: "Longmont", state: "CO", zipcode: "80501", description: "Loves animals", status: "Accepted")
+
+      visit "/applications/#{@application.id}"
+    end
+
+    it "shows the attributes of the application" do
+      expect(page).to have_content(@application.name)
+      expect(page).to have_content(@application.address)
+      expect(page).to have_content(@application.city)
+      expect(page).to have_content(@application.state)
+      expect(page).to have_content(@application.zipcode)
+      expect(page).to have_content(@application.description)
+      expect(page).to have_content(@application.status)
+    end
+
+  end
