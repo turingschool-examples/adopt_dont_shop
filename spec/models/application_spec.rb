a4febd7867d335f3b9d5a53461832a55e633c9ca
craig cardwell <wizeduvcc@gmail.com>
2022-04-01 19:48:25 -0600

    feat: add relationship between application and pets through application_pet model

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe Application, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :address }
-    it { should validate_presence_of :description }
-    it { should validate_presence_of :pet_names }
-    it { should validate_presence_of :status }
+    # it { should validate_presence_of :description }
+    # it { should validate_presence_of :pet_names }
+    # it { should validate_presence_of :status }
+  end
+
+  describe 'relationships' do
+    it { should have_many :application_pets }
+    it { should have_many(:pets).through(:application_pets) }
   end
 end
