2814c547c4f8c7ae88d7a0ceb254802173e3ad49
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-04-01 23:12:17 -0700

    Feature tests for application form

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,8 +1,28 @@
 class ApplicationsController < ApplicationController
 
+  def index
+
+  end
+
   def show
     @application = Application.find(params[:application_id])
     @pets = Pet.all
+  end
+
+  def new
+    # @application = Application.find(params[:id])
+  end
+
+  def create
+    @application = Application.create(application_params)
+    redirect_to "/applications/#{@application.id}"
+  end
+
+
+  private
+
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zipcode, :description)
   end
 
 end
--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -81,4 +81,11 @@ RSpec.describe 'the pets index' do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
+
+  it 'has link to start new application' do
+    visit "/pets"
+
+    click_link "Start an Application"
+    expect(current_path).to eq("/applications/new")
+  end
 end
