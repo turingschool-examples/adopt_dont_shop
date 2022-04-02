be217985814b350d5501531391eb224493d0e642
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-01 23:08:47 -0600

    Test: pet index displays link to start new application

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -81,4 +81,11 @@ RSpec.describe 'the pets index' do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
+
+  it 'displays a link to start a new application' do
+    visit "/pets"
+    click_link("Start an Application")
+
+    expect(current_path).to eq("/applications/new")
+  end
 end
