1d6ff0f89ce1a9ef8ab5ae6314a5beab2b049c85
Susan Boyd <sueboyd922@gmail.com>
2022-04-02 09:42:48 -0400

    Test: Write tests for approving and rejecting pets on applications from admin view

---
+++ spec/features/admin/show_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'admin application show page' do
+  before (:each) do
+    @application_1 = Application.create!(
+      name: 'Anita Barker',
+      street_address: '2468 Park Blvd.',
+      city: 'Denver',
+      state: 'CO',
+      zipcode: '80202',
+      description: 'none',
+      status: 'Pending')
+    @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    PetApplication.create!(pet: @pet_1, application: @application_1)
+    PetApplication.create!(pet: @pet_2, application: @application_1)
+  end
+
+  it 'has a button to approve each pet on an application' do
+    visit "/admin/applications/#{@application_1.id}"
+
+    within "pet-#{@pet_1.id}" do
+      click_on("Approve")
+    end
+    expect(current_path).to eq("/admin/applications/#{@pet_1.id}")
+    within "pet-#{@pet_1.id}" do
+      expect(page).not_to have_button("Approve")
+      expect(page).to have_content("Approved for Adoption")
+    end
+
+    within "pet-#{@pet_2.id}" do
+      click_on("Approve")
+    end
+    expect(current_path).to eq("/admin/applications/#{@pet_2.id}")
+    within "pet-#{@pet_2.id}" do
+      expect(page).not_to have_button("Approve")
+      expect(page).to have_content("Approved for Adoption")
+    end
+  end
+
+  it 'can reject a pet for adoption' do
+    visit "/admin/applications/#{@application_1.id}"
+
+    Pet.all.each do |pet|
+      within "pet-#{pet.id}" do
+        click_on("Reject")
+      end
+      expect(current_path).to eq("/admin/applications/#{pet.id}")
+      within "pet-#{pet.id}" do
+        expect(page).not_to have_button("Reject")
+        expect(page).to have_content("Rejected")
+      end
+    end
+  end
+end
