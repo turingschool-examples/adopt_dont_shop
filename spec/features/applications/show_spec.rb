de49da4afec2da3ae3062e352d40077420b059a3
Will Medders <wmedders@gmail.com>
2022-04-01 20:48:26 -0400

    refactor application show html formatting

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe 'the application show' do
     shelter = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
     scooby = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
     application = scooby.applications.create!(status: "In Progress", name: "Murmuring Savannah",
-                street: 'Main',city: 'Leadville', state: 'CO',
+                street: '123 Main',city: 'Leadville', state: 'CO',
                 zip: 11111, description: "I'm a real cool cat",
                 pet_id: scooby.id)
 
     # binding.pry
     visit "/applications/#{application.id}"
-# save_and_open_page
+save_and_open_page
     expect(page).to have_content(application.name)
     expect(page).to have_content(application.street)
     expect(page).to have_content(application.city)
