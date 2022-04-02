833c1acf46cf536d9069f6b565f1bd00bdb1d04e
James Harkins <jh.harkins@protonmail.com>
2022-04-02 15:56:22 -0600

    feat: pass model test for shelters#with_pending_applications class method

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -20,6 +20,14 @@ class Shelter < ApplicationRecord
     find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
   end
 
+  def self.with_pending_applications
+    select("shelters.*")
+      .joins("INNER JOIN pets ON pets.shelter_id = shelters.id
+              INNER JOIN application_pets ON pets.id = application_pets.pet_id
+              INNER JOIN applications ON application_pets.application_id = applications.id")
+      .where("applications.status = 'Pending'")
+  end
+
   def pet_count
     pets.count
   end
