61a6c524ba757e63d7856a143ce488b1dab421d4
Will Medders <wmedders@gmail.com>
2022-04-01 20:40:38 -0400

    feat: Application model validations

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -11,9 +11,10 @@ RSpec.describe Application, type: :model do
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:zip) }
+    it { should validate_numericality_of(:zip) }
     it { should validate_presence_of(:status) }
     it { should validate_presence_of(:description) }
-  
+
 end
   #
   # before(:each) do
