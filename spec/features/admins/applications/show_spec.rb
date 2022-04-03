fba998580cc1b46b98b463a8a355f1f320f42540
James Harkins <jh.harkins@protonmail.com>
2022-04-03 14:49:25 -0600

    test: create feature tests for admin_application#show page status when all pets are approved for user story 12

--- spec/features/admins/applications/show_spec.rb
+++ spec/features/admins/applications/show_spec.rb
@@ -131,6 +131,31 @@ RSpec.describe 'admin_applications show page' do
           expect(page).not_to have_content("Rejected!")
         end
       end
+
+      it 'shows that the application has been approved if all pets are approved' do
+        application = Application.create!(name: 'Chris', address: '505 Main St.', city: 'Denver', state: 'CO', zipcode: '80205', description: "I'm great with dogs.", status: 'In-progress')
+        shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+        pet_1 = application.pets.create!(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+        pet_2 = application.pets.create!(name: 'Sparky', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+        pet_3 = application.pets.create!(name: 'Spot', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+
+        visit "/admin/applications/#{application.id}"
+
+        within "#pet-#{pet_1.id}" do
+          click_button "Approve"
+        end
+
+        within "#pet-#{pet_2.id}" do
+          click_button "Approve"
+        end
+
+        within "#pet-#{pet_3.id}" do
+          click_button "Approve"
+        end
+
+        expect(current_path).to eq("admin/applications/#{application.id}")
+        expect(page).to have_content("Application Status: Approved!")
+      end
     end
   end
 end
