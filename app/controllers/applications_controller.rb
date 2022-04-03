a4ce069ba658284dc8ccd7cdb5477ffb217f21c0
reuben <creubend@gmail.com>
2022-04-03 17:59:40 -0600

    feat: form for appliction creates a new app and takes user to show page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,7 +4,17 @@ def show
 end
 
 def new
-  
 end
+
+def create 
+  app = Application.create!(app_params)
+  redirect_to "/applications/#{app.id}"
+end
+
+private
+
+  def app_params
+    params.permit(:id, :name, :address, :city, :state, :zipcode, :description, :app_status)
+  end
 
 end 
--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -2,17 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'application creation' do
   
-  describe 'the application new' do
+  describe 'the new application' do
     it 'there is a form with fields' do
-      visit "/applications/new"
-save_and_open_page
+      visit "/applications/new" 
+      
       expect(page).to have_content('New Application')
       expect(find('form')).to have_content('Name')
-      expect(find('form')).to have_content('Street address')
+      expect(find('form')).to have_content('Address')
       expect(find('form')).to have_content('City')
       expect(find('form')).to have_content('State')
       expect(find('form')).to have_content('Zipcode')
       expect(find('form')).to have_button('Submit')
+    end
+    
+    it "can be filled in with info and creates a new app" do
+      visit "/applications/new" 
+      fill_in 'Name', with: 'Joe Blow'
+      fill_in 'Address', with: '3242 W 12st'
+      fill_in 'City', with: 'Castle Rock'
+      fill_in 'State', with: 'CO'
+      fill_in 'Zipcode', with: '1254'
+      click_on 'Submit'
+      application = Application.last
+      expect(current_path).to eq("/applications/#{application.id}")
+      expect(page).to have_content('Joe Blow')
+      expect(page).to have_content('In progress')
+      
+      
     end
   end
 end 
