4296c798b1778eac1d9c57c8d5c437d5e0c65c95
James Harkins <jh.harkins@protonmail.com>
2022-04-02 19:42:01 -0600

    test: create feature tests for admin_applications#show page for reject button for all listed pets for user story 14

--- spec/features/admins/applications/show_spec.rb
+++ spec/features/admins/applications/show_spec.rb
@@ -32,6 +32,36 @@ RSpec.describe 'admin_applications show page' do
           expect(page).to have_content("Approved!")
         end
       end
+
+      it 'for every pet, i see a button to reject the application for that specific pet' do
+        application = Application.create!(name: 'Chris', address: '505 Main St.', city: 'Denver', state: 'CO', zipcode: '80205', description: "I'm great with dogs.", status: 'In-progress')
+        shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+        pet_1 = application.pets.create!(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+        pet_2 = application.pets.create!(name: 'Sparky', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+        pet_3 = application.pets.create!(name: 'Spot', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+
+        visit "/admin/applications/#{application.id}"
+
+        within "#pet-#{pet_1.id}" do
+          expect(page).to have_button("Reject")
+        end
+
+        within "#pet-#{pet_2.id}" do
+          expect(page).to have_button("Reject")
+        end
+
+        within "#pet-#{pet_3.id}" do
+          expect(page).to have_button("Reject")
+          click_button "Reject"
+        end
+
+        expect(current_path).to eq("/admin/applications/#{application.id}")
+
+        within "#pet-#{pet_3.id}" do
+          expect(page).not_to have_button("Reject")
+          expect(page).to have_content("Rejected!")
+        end
+      end
     end
   end
 end
