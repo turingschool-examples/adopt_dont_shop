07d4f84d15bf05607c4964aea5aae20be907c149
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-03 11:52:21 -0600

    feat: default description and status

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,9 +4,14 @@ class Application < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :zipcode, presence: true
-  validates :description, presence: true
-  validates :status, presence: true
+  # validates :description, presence: true
+  # validates :status, presence: true
 
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  def set_defaults
+    self.description ||= "I'm a qualified pet parent!"
+    self.status ||= "Pending"
+  end
 end
