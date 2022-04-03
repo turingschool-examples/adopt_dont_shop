3b960094cb7e234592ee379ac01b1792e8744060
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-02 20:29:04 -0600

    ModelTest: Applications should validate presence of attributes

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Application do
   describe 'relationships' do
     it { should have_many(:pets).through(:application_pets) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_presence_of(:status) }
+  end
 end
