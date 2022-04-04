5bee41b182323f5aacbfebbeb413f17d4ab1c337
Luke Pascale <lpluke5@gmail.com>
2022-04-04 15:29:20 -0500

    FeatTest: Admin index should show shelters with pending applications

--- spec/features/admins/index_spec.rb
+++ spec/features/admins/index_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'the admin shelters index' do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-    @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @pet1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    @pet2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    @pet3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
   end
 
   it 'shows the admin/index in reverse alphabetical order' do
@@ -17,5 +17,21 @@ RSpec.describe 'the admin shelters index' do
     expect(@shelter_3.name).to appear_before(@shelter_1.name)
     expect(@shelter_1.name).to_not appear_before(@shelter_3.name)
     expect(@shelter_1.name).to_not appear_before(@shelter_2.name)
+  end
+
+  it 'shows the shelters with pending applications' do
+    application1 = @pet1.applications.create!(name: "Billy Swanson", street_address: "123 Main Lane", city: "Dallas", state: "New Hampshire", zip_code: "12121")
+
+    visit "/applications/#{application1.id}"
+
+    fill_in :description, with: "I work from home."
+
+    click_button "Submit Application"
+
+    visit '/admin/shelters'
+
+    within '#shelters-with-pending-apps' do
+      expect(page).to have_content("Aurora shelter")
+    end
   end
 end
