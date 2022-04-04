90f965a564665aabc805601af20370b578850ef4
Joseph Senecal <jasenecal2@gmail.com>
2022-04-03 22:22:52 -0600

    Test: Application model verification and relationship tests

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+
+  describe 'validation' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :street_address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zipcode}
+    it {should validate_presence_of :status}
+  end
+
+  describe 'relationships' do
+    it {should have_many(:pets).through(:pet_applications)}
+  end
+end
