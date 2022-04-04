ea69b7829e91eb31ee9efd7a6defcba81c68b3fc
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-04-03 21:48:46 -0700

    Start tests for adopt a pet button adoption form

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -27,15 +27,14 @@ class ApplicationsController < ApplicationController
     end
   end
 
-  # def update
-  #   application = Application.find(params[:id])
-  #   if application.update(application_params)
-  #     redirect_to "/applications/#{application.id}"
-  #   else
-  #     redirect_to "/applications/#{application.id}"
-  #     flash[:alert] = "Error: #{error_message(application.errors)}"
-  #   end
-  # end
+  def update
+    application = Application.find(params[:id])
+    @pets = Pet.search(params[:pets])
+    require "pry"; binding.pry
+
+    application.update(application_params)
+    redirect_to "/applications/#{application.id}"
+  end
 
 
   private
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -47,4 +47,26 @@ RSpec.describe 'application show page' do
     expect(page).to have_link("Joey")
     expect(current_path).to eq("/applications/#{application.id}")
   end
+
+  it "has button to add a pet to application for adoption" do
+    shelter = Shelter.create!(name: 'Save The Animals', city: 'Denver', rank: 4, foster_program: true)
+    pet1 = Pet.create!(name: 'Joey', age: 2, breed: 'Poodle', adoptable: true, shelter_id: shelter.id)
+    dog = Pet.create!(name: 'Dog', age: 4, breed: 'German', adoptable: true, shelter_id: shelter.id)
+    application = Application.create!(name: 'Andrew',
+      street_address: '112 Greenbrook',
+      city: 'Denver',
+      state: 'CO',
+      zipcode: '80207',
+      description: 'Happy, friendly, cool',
+      status: 'In Progress',
+    )
+    # application_pet_1 = ApplicationPet.create!(application_id: application.id, pet_id: pet1.id)
+    visit "/applications/#{application.id}"
+    fill_in 'Pets', with: "Joey"
+    click_button "Search"
+    expect(page).to have_link("Joey")
+    click_button 'Adopt this Pet'
+    expect(current_path).to eq("/applications/#{application.id}")
+    expect(page).to have_content("Joey")
+  end
 end
