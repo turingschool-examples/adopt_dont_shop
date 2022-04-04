57170d769797244e6e850f8b4d09ba1f791e36e1
Joseph Downs <jos.w.downs@gmail.com>
2022-04-04 00:17:53 -0600

    Add show view for admin/shelters

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -20,6 +20,10 @@ class Shelter < ApplicationRecord
     find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
   end
 
+  def self.show_list(id)
+    find_by_sql("SELECT name, city FROM shelters WHERE id = #{id}")
+  end
+
   def self.pending
     select("shelters.*")
     .joins(pets: :applications)
--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AdminShelters, type: :feature do
+RSpec.describe "AdminShelters", type: :feature do
   before (:each) do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
@@ -13,6 +13,7 @@ RSpec.describe AdminShelters, type: :feature do
 
       expect(page).to have_content(@shelter_1.name)
       expect(page).to have_content(@shelter_1.city)
+      expect(page).to_not have_content(@shelter_3.name)
     end
   end
 
