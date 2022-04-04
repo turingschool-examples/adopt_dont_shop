fc339179cc1ebe3984bcbd8d5cb3fb2dd13e9e9f
Susan Boyd <sueboyd922@gmail.com>
2022-04-04 14:57:40 -0400

    Refactor: update admin applicatons show view using pet_applications

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,12 +1,9 @@
 class Admin::ApplicationsController < ApplicationController
 
   def show
+    # require "pry"; binding.pry
     @application = Application.find(params[:id])
-    @pets = []
-    @application.pets.each {|pet| @pets << pet}
     if !params[:approve].nil?
-      # require "pry"; binding.pry
-      Pet.find(params[:approve]).adoptable = false
     end
   end
 end
--- spec/features/admin/show_spec.rb
+++ spec/features/admin/show_spec.rb
@@ -23,29 +23,29 @@ RSpec.describe 'admin application show page' do
     @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    PetApplication.create!(pet: @pet_1, application: @application_1)
-    PetApplication.create!(pet: @pet_2, application: @application_1)
-    PetApplication.create!(pet: @pet_1, application: @application_2)
-    PetApplication.create!(pet: @pet_2, application: @application_2)
+    @pet_app1 = PetApplication.create!(pet: @pet_1, application: @application_1)
+    @pet_app2 = PetApplication.create!(pet: @pet_2, application: @application_1)
+    @pet_app3 = PetApplication.create!(pet: @pet_1, application: @application_2)
+    @pet_app4 = PetApplication.create!(pet: @pet_2, application: @application_2)
   end
 
   it 'has a button to approve each pet on an application' do
     visit "/admin/applications/#{@application_1.id}"
 
-    within ".pet-#{@pet_1.id}" do
+    within ".pet_app-#{@pet_app1.id}" do
       click_on("Approve")
     end
     expect(current_path).to eq("/admin/applications/#{@application_1.id}")
-    within ".pet-#{@pet_1.id}" do
+    within ".pet-#{@pet_app1.id}" do
       expect(page).not_to have_button("Approve")
       expect(page).to have_content("Approved for Adoption")
     end
 
-    within ".pet-#{@pet_2.id}" do
+    within ".pet-#{@pet_app2.id}" do
       click_on("Approve")
     end
-    expect(current_path).to eq("/admin/applications/#{@pet_2.id}")
-    within "pet-#{@pet_2.id}" do
+    expect(current_path).to eq("/admin/applications/#{@application_1.id}")
+    within "pet-#{@pet_app2.id}" do
       expect(page).not_to have_button("Approve")
       expect(page).to have_content("Approved for Adoption")
     end
