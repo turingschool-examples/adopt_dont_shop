f8f312319e68d34d707cc5fa420c64263455ca6f
Drew Proebstel <d.proebstel@gmail.com>
2022-04-04 16:00:10 -0700

    admin done

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -69,7 +69,36 @@ RSpec.describe 'the admin shelter show page ' do
     expect(page).to have_button("Reject Application for Poopy")
     expect(page).to have_content("Spot Rejected")
     expect(page).to have_content("Bognart Rejected")
+  end
 
+  it 'one application does not affect another' do
+    shelter = Shelter.create!(foster_program: false, name: 'Humane Society', city: 'Phoenix', rank: 20)
+    application1 = Application.create!(name: 'Cory', city: 'Tempe', state: 'AZ', street_address: '3030 Westroad', zip_code: '85282', description: 'I love dogs and I have a lot of free time to take care of one.', status: 'Pending')
+    application2 = Application.create!(name: 'Drew', city: 'Los Angeles', state: 'CA', street_address: '6969 Bonky', zip_code: '69420', description: 'I have a fever and the only cure is more dog bell.', status: 'Pending')
+    spot = Pet.create!(adoptable: true, age: 2, breed: 'Mix', name: 'Spot', shelter_id: shelter[:id])
+    bognart = Pet.create!(adoptable: true, age: 2, breed: 'Mix', name: 'Bognart', shelter_id: shelter[:id])
+    poopy = Pet.create!(adoptable: true, age: 2, breed: 'Mix', name: 'Poopy', shelter_id: shelter[:id])
+    ApplicationPet.create!(pet_id: spot.id, application_id: application1.id)
+    ApplicationPet.create!(pet_id: poopy.id, application_id: application1.id)
+    ApplicationPet.create!(pet_id: bognart.id, application_id: application2.id)
+    ApplicationPet.create!(pet_id: spot.id, application_id: application2.id)
+
+    visit "/admin/applications/#{application1.id}"
+
+    expect(page).to have_button("Reject Application for Spot")
+    expect(page).to have_button("Reject Application for Poopy")
+    expect(page).to have_button("Approve Application for Spot")
+    expect(page).to have_button("Approve Application for Poopy")
+
+    click_button("Reject Application for Spot")
+
+    visit "/admin/applications/#{application2.id}"
+
+    expect(page).to have_button("Reject Application for Spot")
+    expect(page).to have_button("Reject Application for Bognart")
+    expect(page).to have_button("Approve Application for Spot")
+    expect(page).to have_button("Approve Application for Bognart")
 
   end
+
 end
