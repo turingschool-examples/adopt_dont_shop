e4e174bf2f4d3d5eaba28d0f1526288d0f626142
Andrew Speth <speth.andrew@yahoo.com>
2022-04-03 22:01:34 -0600

    test: admin/shelters#index has a list of shelters with pending applications

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -11,7 +11,32 @@ RSpec.describe 'the admin shelters index' do
     expect(shelter2.name).to appear_before(shelter1.name)
     expect(shelter1.name).to appear_before(shelter3.name)
   end
+  
+  it 'lists all the shelters with pending applications' do
+    shelter1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    shelter2 = Shelter.create(name: 'Denver shelter', city: 'Denver, CO', foster_program: true, rank: 10)
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter1.id)
+    application = Application.create!(name: 'Andrew',
+      street_address: '112 Greenbrook',
+      city: 'Denver',
+      state: 'CO',
+      zipcode: '80207',
+      description: 'Happy, friendly, cool',
+      status: 'Pending',
+    )
+    application_pet = ApplicationPet.create(pet_id: pet_1.id, application_id: application.id)
+    
+    visit '/admin/shelters'
+
+    within "Shelters with Pending Applications" do
+      expect(page).to have_content('Aurora shelter')
+      expect(page).to_not have_content('Denver shelter')
+    end
+  end
+
+
 end
 # As a visitor
 # When I visit the admin shelter index ('/admin/shelters')
-# Then I see all Shelters in the system listed in reverse alphabetical order by name
+# Then I see a section for "Shelter's with Pending Applications"
+# And in this section I see the name of every shelter that has a pending application
