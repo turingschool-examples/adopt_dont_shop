2df4c0fcfe817cf7e09a9d18ffb8ca0fb299a2f2
Chris Romero <chrisleeromero1@gmail.com>
2022-04-03 18:19:34 -0600

    test: add test for user story 23, searching for pets on an application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -22,4 +22,34 @@ RSpec.describe 'the applications index' do
     click_link "#{pet_1.name}"
     expect(current_path).to eq("/pets/#{pet_1.id}")
   end
+
+  # As a visitor
+  # When I visit an application's show page
+  # And that application has not been submitted,
+  # Then I see a section on the page to "Add a Pet to this Application"
+  # In that section I see an input where I can search for Pets by name
+  # When I fill in this field with a Pet's name
+  # And I click submit,
+  # Then I am taken back to the application show page
+  # And under the search bar I see any Pet whose name matches my search
+  it 'allows the user to serach for pets' do
+    # application_1 = Application.create!(name: 'Chris', address: '505 Main St.', city: 'Denver', state: 'CO', zipcode: '80205', description: "I'm great with dogs.", status: 'In-progress')
+    application = Application.create!(name: 'Chris', address: '505 Main St.', city: 'Denver', state: 'CO', zipcode: '80205', description: "I'm great with dogs.", status: 'In-progress')
+    shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    pet_1 = application.pets.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    pet_2 = application.pets.create(name: 'Sparky', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    pet_3 = Pet.create(name: 'Spot', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+
+    visit "/applications/#{application.id}"
+
+    expect(page).to have_content("Add a Pet to this Application")
+    expect(page).to have(application.status)
+    expect(page).to_not have_content(pet_1.name)
+    expect(page).to_not have_content(pet_2.name)
+    fill_in 'Search for pets:', with: 'Sparky'
+    click_on 'Search'
+    expect(page).to have_content("Add a Pet to this Application")
+    expect(page).to_not_not have_content(pet_1.name)
+    expect(page).to have_content(pet_2.name)
+  end
 end
