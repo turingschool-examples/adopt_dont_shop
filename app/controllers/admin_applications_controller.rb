41d9f1a6277c684f74d53deacd2cf2f7b071a346
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-04 15:26:55 -0600

    Feat: add button to approve pet in admin/apps/:id show page

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -2,6 +2,6 @@ class AdminApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
   end
 end
--- spec/features/admin_applications/show_spec.rb
+++ spec/features/admin_applications/show_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe 'Admin Applications Show Page' do
     @skeeter = Pet.create!(adoptable: true, age: 7, breed: 'corgie', name: 'Skeeter', shelter_id: @shelter.id)
     @skippy = Pet.create!(adoptable: true, age: 5, breed: 'lab', name: 'skippy', shelter_id: @shelter.id)
     @bonds= Application.create!(name: 'Barry Bonds', address: '100 main street, Aurora, CO, 80014', description: 'I love dogs', pet_names: "#{@skeeter.id}, #{@lobster.id}", status: 1)
-    @app_pet1 = ApplicationPet.create!(application: @bonds, pet: @skeeter)
-    @app_pet2 = ApplicationPet.create!(application: @bonds, pet: @lobster)
+    @app_pet1 = ApplicationPet.create!(application: @bonds, pet: @skeeter, pet_status: 0)
+    @app_pet2 = ApplicationPet.create!(application: @bonds, pet: @lobster, pet_status: 0)
   end
 
   describe 'As a visitor' do
@@ -22,8 +22,8 @@ RSpec.describe 'Admin Applications Show Page' do
 
       visit "/admin/applications/#{@bonds.id}"
       save_and_open_page
-      within "pet-#{@skeeter.id}" do
-          click_on "APPROVE"
+      within "#pet-#{@skeeter.id}" do
+          click_on "APPROVE THIS PET"
       end
       expect(current_path).to eq("/admin/applications/#{@bonds.id}")
       within "pet-#{@skeeter.id}" do
