dcb46efd11ef29df7c89344f892e689f00987bc8
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-03 18:31:00 -0600

    plant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PetApplication.destroy_all
+Application.destroy_all
+Pet.destroy_all
+Shelter.destroy_all
+application_1 = Application.create!(name: "Anita Barker", street_address: '2468 Park Blvd.', city: "Denver", state: "CO", zipcode: "80202")
+application_2 = Application.create!(name: "Bob Barker", street_address: "1357 20th st.", city: "Golden", state: "CO", zipcode: "80209", description: "I'm a great pet parent!", status: "Pending")
+application_3 = Application.create!(name: "Doug Shruggs", street_address: "2024 Baker Blvd", city: "Littleton", state: "CO", zipcode: "80120", description: "I'm a great dog dad!", status: "Pending")
+
+shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+shelter_4 = Shelter.create(name: 'Dumb Friends League', city: 'Boulder, CO', foster_program: true, rank: 8)
+
+pet_1 = Pet.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true, shelter_id: shelter_1.id)
+pet_2 = Pet.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true, shelter_id: shelter_1.id)
+pet_3 = Pet.create!(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false, shelter_id: shelter_3.id)
+pet_4 = Pet.create!(name: 'Purrdy', breed: 'persian', age: 6, adoptable: true, shelter_id: shelter_3.id)
+
+
+PetApplication.create!(pet: pet_1, application: application_2)
+PetApplication.create!(pet: pet_2, application: application_2)
+
+PetApplication.create!(pet: pet_3, application: application_3)
+PetApplication.create!(pet: pet_4, application: application_3)
