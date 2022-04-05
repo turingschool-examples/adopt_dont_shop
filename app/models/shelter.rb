366efff91c5e0a3774e0140107f2076f70ea3332
Andrew Speth <speth.andrew@yahoo.com>
2022-04-05 08:36:19 -0600

    removed unused method from shelter.rb

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -33,10 +33,6 @@ class Shelter < ApplicationRecord
     adoptable_pets.where('age >= ?', age_filter)
   end
 
-  def  average_age
-    require "pry"; binding.pry
-  end
-
   def self.pending
     joins(pets: :applications).where(applications: {:status => "Pending"})
   end
