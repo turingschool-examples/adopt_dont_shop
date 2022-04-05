cb56322a7a7721035d9d7aff5ef17130046f0bdf
Andrew Speth <speth.andrew@yahoo.com>
2022-04-05 15:55:07 -0600

    test: action required section has links

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -81,8 +81,39 @@ RSpec.describe 'the admin shelter show page' do
     end
   end
 
+  it "has links in 'Action Required' section that go to admin/application/:id" do
+    shelter1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    shelter2 = Shelter.create!(name: 'Denver shelter', city: 'Denver, CO', foster_program: false, rank: 9)
+    luna = shelter1.pets.create!(name: 'luna', age: 1, breed: 'Cat', adoptable: true)
+    booth = Pet.create!(name: 'booth', age: 11, breed: 'Cat', adoptable: true, shelter_id: shelter1.id)
+    application1 = Application.create!(name: 'Andrew',
+      street_address: '112 Greenbrook',
+      city: 'Denver',
+      state: 'CO',
+      zipcode: '80207',
+      description: 'Happy, friendly, cool',
+      status: 'Pending'
+    )
+    application2 = Application.create!(name: 'Andrew',
+      street_address: '112 Greenbrook',
+      city: 'Denver',
+      state: 'CO',
+      zipcode: '80207',
+      description: 'Happy, friendly, cool',
+      status: 'In Progress'
+    )
+    application_pet = ApplicationPet.create!(application_id: application1.id, pet_id: luna.id)
+    application_pet = ApplicationPet.create!(application_id: application2.id, pet_id: booth.id, pet_status: "Approved")
+    
+    visit "/admin/shelters/#{shelter1.id}"
+    expect(page).to have_content("Action Required")
+    within "#action_required" do
+      click_link("luna")
+      expect(current_path).to eq("/admin/applications/#{application1.id}")
+    end
+  end
 end
 # As a visitor
 # When I visit an admin shelter show page
-# Then I see a section titled "Action Required"
-# In that section, I see a list of all pets for this shelter that have a pending application and have not yet been marked "approved" or "rejected"
+# And I look in the "Action Required" section
+# Then next to each pet's name I see a link to the admin application show page where I can accept or reject the pet.
