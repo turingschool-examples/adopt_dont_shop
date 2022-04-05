297adecb61694493fe9e9be939609c76c3b6bdce
Sherman Au <shermau13@gmail.com>
2022-04-05 12:25:59 -0600

    refactor: params

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -33,51 +33,49 @@ RSpec.describe 'the applications show page' do
 
     expect(current_path).to eq("/pets/#{@pet_1.id}")
   end
-end
-describe 'search in applicatioins' do
-  before do
-    @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @pet_1 = @shelter.pets.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
-    @pet_2 = @shelter.pets.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
-    @pet3 = @shelter.pets.create!(adoptable: true, age: 7, breed: 'Golden Retriever', name: 'Carter')
-    @application1 = Application.create!(name: 'Chris', address: '123 Main St', city: 'Hometown', state: 'CO',
-                                        zipcode: '80504')
-    visit "/applications/#{@application1.id}"
-  end
 
-  it 'Unsubmitted applications have a search box to select pets by name and when adopt button clicked, pet is attached to application' do
-    fill_in(:pet_name, with: 'Carter')
-    click_button('Search')
-    expect(current_path).to eq("/applications/#{@application1.id}")
-    within ".pet-#{@pet3.id}" do
-      expect(page).to have_content(@pet3.name)
-      click_button('Adopt')
+  describe 'search in applicatioins' do
+    before do
+      @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      @pet_1 = @shelter.pets.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true,
+                                    shelter_id: @shelter.id)
+      @pet_2 = @shelter.pets.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster',
+                                    shelter_id: @shelter.id)
+      @pet3 = @shelter.pets.create!(adoptable: true, age: 7, breed: 'Golden Retriever', name: 'Carter')
+      @application1 = Application.create!(name: 'Chris', address: '123 Main St', city: 'Hometown', state: 'CO',
+                                          zipcode: '80504')
+      visit "/applications/#{@application1.id}"
+    end
+
+    it 'Unsubmitted applications have a search box to select pets by name and when adopt button clicked, pet is attached to application' do
+      fill_in(:pet_name, with: 'Carter')
+      click_button('Search')
       expect(current_path).to eq("/applications/#{@application1.id}")
-    end
-
-    within '.requested_pets' do
-      expect(page).to have_content(@pet3.name)
-    end
-
-    describe 'sumbit an application' do
-      it 'shows a section to input on why applicant would be a good owner' do
-        expect(page).to_not have_content('Describe why you make a good owner for these pets:')
-        fill_in(:pet_name, with: "#{@pet3.name}")
-
-        click_button 'Search'
-        click_button 'Adopt'
-
-        expect(page).to have_content('Describe why you make a good owner for these pets:')
-        expect(find('form')).to have_content('Description')
-        fill_in ('Description', with: 'Dogs are amazing')
-        click_button 'Submit Application'
+      within ".pet-#{@pet3.id}" do
+        expect(page).to have_content(@pet3.name)
+        click_button('Adopt')
         expect(current_path).to eq("/applications/#{@application1.id}")
-        expect(page).to have_content('Dogs are amazing')
-        expect(page).to have_content("Pending")
-        expect(page).to_not have_content("Accepted")
-        expect(page).to_not have_content("In Progress")
-        expect(page).to_not have_content("Rejected")
       end
+
+      within '.requested_pets' do
+        expect(page).to have_content(@pet3.name)
+      end
+    end
+
+    it 'shows a section to input on why applicant would be a good owner' do
+      expect(page).to_not have_content('Describe why you make a good owner for these pets:')
+      fill_in(:pet_name, with: @pet3.name.to_s)
+
+      click_button 'Search'
+      click_button 'Adopt'
+      expect(page).to have_content('Describe why you make a good owner for these pets:')
+      fill_in('Description', with: 'Dogs are amazing')
+      click_button 'Submit Application'
+      expect(current_path).to eq("/applications/#{@application1.id}")
+      expect(page).to have_content('Pending')
+      expect(page).to_not have_content('Accepted')
+      expect(page).to_not have_content('In Progress')
+      expect(page).to_not have_content('Rejected')
     end
   end
 end
