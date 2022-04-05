36901dcef3d7f2e0bc74947e2f74ece79fd95720
John Hennerich <john.hennerich@gmail.com>
2022-04-05 11:56:34 -0600

    Feat: implement update for submit app

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -19,8 +19,13 @@ class ApplicationsController < ApplicationController
   def new
   end
 
+  def update
+    @application = Application.find(params[:id])
+    @application.status = "Pending"
+    @application.save
+  end
+
   def create
-    binding.pry
     @application = Application.new(application_params)
     if @application.save
       redirect_to "/applications/#{@application.id}"
