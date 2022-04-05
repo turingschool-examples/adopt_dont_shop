e37276f7550a0bbfbe5678c13c623f609968b5a8
Joseph Senecal <jasenecal2@gmail.com>
2022-04-05 13:15:12 -0600

    Feat: application submission

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -17,6 +17,16 @@ class ApplicationsController < ApplicationController
     end
   end
 
+  def update
+    if !params[:description]
+      flash[:notice] = "Error: Applicant must answer all questions before submitting application"
+      render :show
+    else
+      Application.update(params[:id], description: params[:description], status: "Pending")
+      redirect_to "/applications/#{params[:id]}"
+    end
+  end
+
   private
 
   def new_application_params
