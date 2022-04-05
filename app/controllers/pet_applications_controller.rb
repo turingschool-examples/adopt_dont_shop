741a86e3c00f7fb38ae40e7578a658ea057cb1f1
Sherman Au <shermau13@gmail.com>
2022-04-05 12:29:09 -0600

    refactor

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -3,4 +3,10 @@ class PetApplicationsController < ApplicationController
     PetApplication.create(application_id: params[:application_id], pet_id: params[:pet_id])
     redirect_to "/applications/#{params[:application_id]}"
   end
+
+  private
+
+  def pet_application_params
+    params.permit(:application_id, :pet_id)
+  end
 end
