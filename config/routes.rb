9806d1695f7ba2aba1034979c779d8cfaf8f9028
Tyler Ross <tross020890@gmail.com>
2022-04-04 20:27:33 -0500

    Feat: add description, change status

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
   post '/applications', to: 'applications#create'
+  patch '/applications/:id', to: 'applications#update'
   #post app/id tto pet_app#create, create in p_a controller, redirect
 
   get '/admin/shelters', to: 'admin_shelters#index'
