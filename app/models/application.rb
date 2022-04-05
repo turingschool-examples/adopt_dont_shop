3b62020305f9c81a50f6d104513880ee907e010a
Cory Bethune <corybbethune@gmail.com>
2022-04-04 20:55:58 -0700

    Card 23 is done and passing tests

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+
+  # def self.search_pets
+  #   Pet.name.upcase.find(params[:search].upcase)
+  #
+  # end
 end
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -17,6 +17,26 @@ RSpec.describe 'the application show' do
     expect(page).to have_content(application.description)
     expect(page).to have_content(application.status)
     expect(page).to have_content('Pet(s) applying for: Spot')
+    save_and_open_page
+  end
+
+  it "searches for pets and adds them to an application" do
+    shelter = Shelter.create!(foster_program: false, name: 'Humane Society', city: 'Phoenix', rank: 20)
+    spot = Pet.create!(adoptable: true, age: 2, breed: 'Mix', name: 'Spot', shelter_id: shelter[:id])
+    chungus = Pet.create!(adoptable: true, age: 15, breed: 'Chihuahua', name: 'Chungus', shelter_id: shelter[:id])
+    application = Application.create!(name: 'Cory', city: 'Tempe', state: 'AZ', street_address: '3030 Westroad', zip_code: '85282', description: 'I love dogs and I have a lot of free time to take care of one.', status: 'Pending')
+    app_pet = ApplicationPet.create!(pet_id: spot.id, application_id: application.id)
+
+    visit "/applications/#{application.id}"
+    expect(page).to have_content("Add a Pet to this Application")
+
+    fill_in 'Search', with: 'Spot'
+    click_button 'Submit'
+    save_and_open_page
+    expect(current_path).to eq("/applications/#{application.id}/")
+    # binding.pry
+    expect(page).to have_content("Spot")
+    expect(page).to_not have_content("Chungus")
   end
 
 
