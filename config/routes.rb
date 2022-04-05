fccc5d148a1b147d1149031540d350b3d6aa0bea
Joseph Senecal <jasenecal2@gmail.com>
2022-04-05 12:42:42 -0600

    rails c

--- config/routes.rb
+++ config/routes.rb
@@ -39,5 +39,6 @@ Rails.application.routes.draw do
 
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
+  patch '/applications/:id', to: 'applications#update'
   post '/applications', to: 'applications#create'
 end
