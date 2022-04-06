f14521d8f18d5f9244af7de8680028ce1af08bf6
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-05 20:39:57 -0600

    Refactor: fix bug causing duplicate shelters to appear on admin/shelter #index page under shelters with pending apps

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -2,9 +2,25 @@ class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.reverse_order_by_name
     @applications = Application.pending_apps
+    @pending_shelters = no_duplicate_pets
   end
 
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+
+  private
+    def no_duplicate_pets
+      shelters = Shelter.reverse_order_by_name
+      applications = Application.pending_apps
+      pending_shelters = []
+      applications.each do |app|
+        app.pets.each do |pet|
+          pending_shelters << pet.shelter
+        end
+      end
+      pending_shelters.uniq
+      # require "pry"; binding.pry
+    end
 end
