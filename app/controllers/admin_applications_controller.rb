a5a979620542347179c3b5535f42e4a760975a32
Andrew Speth <speth.andrew@yahoo.com>
2022-04-06 12:26:55 -0600

    refactor: fix bug in 1+ pets rejected makes app rejected

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -6,29 +6,30 @@ class AdminApplicationsController < ApplicationController
   end
 
   def update
-    i = 0
-    @application = Application.find(params[:id])
     @app_pet = ApplicationPet.find(params[:app_id])
-    @application.application_pets.each do |app_pet|
-      if params[:status] == "Approve"
-        app_pet.pet_status = 'Approved'
-        @app_pet.pet_status = 'Approved'
-      elsif params[:status] == 'Reject'
-        app_pet.pet_status = 'Rejected'
-        @app_pet.pet_status = 'Rejected'
-        @application.status = 'Rejected'
-      end
+    
+    if params[:status] == "Approve"
+      @app_pet.pet_status = 'Approved'
+    elsif params[:status] == 'Reject'
+      @app_pet.pet_status = 'Rejected'
+      @app_pet.application.status = 'Rejected'
     end
-    @application.application_pets.each do |app|
-      if app.pet_status == 'Approved'
+    
+    @app_pet.save
+    
+    i = 0
+    all_app_pets = ApplicationPet.all
+    all_app_pets.each do |app|
+      if app.pet_status == "Approved"
         i += 1
       end
     end
-    if @application.application_pets.count == i
-      @application.status = 'Approved'
+    
+    if all_app_pets.count == i
+      @app_pet.application.status = 'Approved'  
     end
-    @application.save
-    @app_pet.save
-    redirect_to "/admin/applications/#{@application.id}"
+    
+    @app_pet.application.save
+    redirect_to "/admin/applications/#{@app_pet.application.id}"
   end
 end
