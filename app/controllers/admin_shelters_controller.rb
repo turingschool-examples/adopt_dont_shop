158f6cd9276d49f93e4138361aa03ea172fc5a69
craig cardwell <wizeduvcc@gmail.com>
2022-04-05 20:31:54 -0600

    feat: add link to admin shelters index page for each shelter name

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -2,6 +2,7 @@ class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.reverse_order_by_name
     @applications = Application.pending_apps
+    # require "pry"; binding.pry
   end
 
   def show
