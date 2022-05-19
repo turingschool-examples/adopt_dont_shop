f25688990b3d65f3379622b51a03b0fa318ccde7
Zachary P <zachary.s.prince@outlook.com>
2022-05-19 17:40:28 -0400

    Refactor test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'application' do
   it 'shows full address of applicant' do
+    shelter = Shelter.create!(foster_program: true, name: 'Gally', city: 'Denver', rank: 21)
     application = Application.create!(name: 'Sylvester Tommy', street_address: '1827 Vincent Ave', city: 'Halifax',
                                       state: 'Colorado', zip_code: '19274', description: 'I LOVE pets', status: 'In Progress')
+    pet1 = Pet.create!(adoptable: true, age: 9, breed: 'Labrador', name: 'Suzan', shelter_id: shelter.id)
+    ap1 = ApplicationPet.create!(application_id: application.id, pet_id: pet1.id)
     visit "/applications/#{application.id}"
 
     expect(page).to have_content('Sylvester Tommy')
@@ -13,5 +16,6 @@ RSpec.describe 'application' do
     expect(page).to have_content('19274')
     expect(page).to have_content('I LOVE pets')
     expect(page).to have_content('In Progress')
+    expect(page).to have_content('Suzan')
   end
 end
