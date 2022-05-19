e3a23e515e53401fcd66f4e1dfa63b20f7a37c12
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-19 13:35:07 -0500

    Initial commit

---
+++ db/migrate/20220519175132_create_applications.rb
@@ -0,0 +1,13 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :address
+      t.string :description
+      t.string :pets
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
