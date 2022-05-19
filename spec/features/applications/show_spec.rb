a284de805ee0a7f20447b470f9797aa8a4cbf234
Sage Skaff <skaffsage@gmail.com>
2022-05-19 12:47:44 -0700

    Add application attributes test

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Applications Show Page' do
+  it 'displays the applicants attributes' do
+    application = Application.create!(name: 'Jenn', address: '123 Water Street, Denver, CO, 80111',
+                                      description: 'I like animals!', status: 'In Progress')
+    visit "/applications/#{application.id}"
+    expect(page).to have_content('Jenn')
+    expect(page).to have_content('123 Water Street, Denver, CO, 80111')
+    expect(page).to have_content('I like animals!')
+    expect(page).to have_content('In Progress')
+  end
+end
