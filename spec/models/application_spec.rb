0d1b60f11e86801fce6b5b1376ce0a83e85abb22
Sage Skaff <skaffsage@gmail.com>
2022-05-19 12:34:33 -0700

    Add validations test

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :status }
+  end
+end
