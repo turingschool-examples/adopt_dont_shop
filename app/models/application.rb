3ed581dd63dc355b5a1df6a479b4c39ae000aeb6
Zachary P <zachary.s.prince@outlook.com>
2022-05-19 16:56:55 -0400

    Add application model and validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,9 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates :description, presence: true
+  validates :status, presence: true
 end
