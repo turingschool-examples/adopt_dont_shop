e59167a141c7b90b2847a78784086d526844b1b6
Caden Jarrett <caden.jarrett99@gmail.com>
2022-05-19 15:58:06 -0600

    Test: application relationships test written

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should have_many(:pets).through(:pet_applications)}
+  end
 end
