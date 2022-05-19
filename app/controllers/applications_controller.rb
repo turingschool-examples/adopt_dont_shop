d929a4289e985c905706d57dffe3a043a51886ac
Zachary P <zachary.s.prince@outlook.com>
2022-05-19 19:25:17 -0400

    Add last app variable

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,5 +3,19 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
-  def new; end
+  def new
+    @status = 'In Progress'
+    @last_app = Application.last
+  end
+
+  def create
+    application = Application.create(application_params)
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description, :status)
+  end
 end
