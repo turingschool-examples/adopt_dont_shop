bbf7403e7b80c5d050c098fc9f23275cd2fca2eb
Stirling Hostetter <stirhoss@icloud.com>
2022-05-19 16:22:50 -0500

    Refactor: visit show page fix and complete

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,14 +4,17 @@ RSpec.describe "Applications Show Page" do
   before :each do
     @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @pet_1 = @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @application_1 = Application.create!(name: "Mike", address: "1234 Street St Denver CO 69420", description: "I care about pets", pets: @pet_1.name, status: "Pending")
+    @application_1 = Application.create!(name: "Mike", address: "1234 Street St", city: 'Denver', state: 'CO', zipcode: '69420', description: "I care about pets", status: "Pending")
   end
   it "visit show page and shows application attributes" do
     visit "/applications/#{@application_1.id}"
     expect(page).to have_content("Mike")
-    expect(page).to have_content("1234 Street St Denver CO 69420")
+    expect(page).to have_content("1234 Street St")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("CO")
+    expect(page).to have_content("69420")
     expect(page).to have_content("I care about pets")
-    expect(page).to have_content(@pet_1.name)
     expect(page).to have_content("Pending")
+    expect(page).to have_content("Pets applied for:")
   end
 end
