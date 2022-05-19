33fad22e0a6074f6dc4a6791df09644955c8a85c
Sage Skaff <skaffsage@gmail.com>
2022-05-19 13:34:08 -0700

    Add relationships

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,5 @@
 class Application < ApplicationRecord
   validates_presence_of :name, :address, :status, :description
+  has_many :application_pets
+  has_many :pets, through: :application_pets
 end
