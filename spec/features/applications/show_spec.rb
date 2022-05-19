19a0f97a601e8bcba007f66e4927dee836cd1c74
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-19 13:34:38 -0500

    Initial commit

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Applications Show Page" do
+  before :each do
+    @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @pet_1 = @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    @application_1 = Application.create!(name: "Mike", address: "1234 Street St Denver CO 69420", description: "I care about pets", pets: @pet_1.name, status: "Pending")
+  end
+  it "visit show page and shows application attributes" do
+    visit "/applications/#{@application_1.id}"
+    expect(page).to have_content("Mike")
+    expect(page).to have_content("1234 Street St Denver CO 69420")
+    expect(page).to have_content("I care about pets")
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content("Pending")
+  end
+end
