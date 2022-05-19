51515df5cf7a5b31938debd12817205cc4fc5a17
Caden Jarrett <caden.jarrett99@gmail.com>
2022-05-19 16:27:16 -0600

    Test: wrote validations for pet application to belong to pets and applications

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PetApplication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it {should belong_to(:pet)}
+    it {should belong_to(:application)}
+  end
 end
