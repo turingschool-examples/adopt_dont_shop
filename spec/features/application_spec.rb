26893a4b21d4e53eaa1fb2375ab14e60cee11bc4
Zachary P <zachary.s.prince@outlook.com>
2022-05-19 16:45:01 -0400

    Add application show page test

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -46,6 +46,16 @@ RSpec.describe 'application' do
   end
 
   it 'shows full address of applicant' do
-    visit '/:id'
+    application = Application.create!(name: 'Sylvester Tommy', street_address: '1827 Vincent Ave', city: 'Halifax',
+                                      state: 'Colorado', zip_code: '19274', description: 'I LOVE pets', status: 'In Progress')
+    visit '/applications/:id'
+
+    expect(page).to have_content('Sylvester Tommy')
+    expect(page).to have_content('1827 Vincent Ave')
+    expect(page).to have_content('Halifax')
+    expect(page).to have_content('Colorado')
+    expect(page).to have_content('19274')
+    expect(page).to have_content('I LOVE pets')
+    expect(page).to have_content('In Progress')
   end
 end
