6066fc414b7b52586567e4bb4ac597164367e740
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-19 14:27:19 -0600

    Test: shows an application and all it's attributes

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+# TODO: merge seeds files to make objects in tests consistent
+
+RSpec.describe 'the shelter show' do
+  let(:application) { Application.create!(name: 'Debbie', street_address: '1234 dog way', city: "San Francisco", zip_code: 66012, description: 'I love dogs', pet_names: 'Pickle, Brownie', application_status: 'Accepted') }
+
+  it "shows the application and all it's attributes" do
+    visit "/applications/#{application.id}"
+    save_and_open_page
+
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.street_address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.zip_code)
+    expect(page).to have_content(application.description)
+    expect(page).to have_content(application.all_pet_names)
+    expect(page).to have_content(application.application_status)
+  end
+end
