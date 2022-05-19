53868751cc971f70f30ed4762842f8de066c5567
Sage Skaff <skaffsage@gmail.com>
2022-05-19 13:32:36 -0700

    Add relationships test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Application, type: :model do
     it { should validate_presence_of :address }
     it { should validate_presence_of :status }
   end
+
+  describe 'relationships' do
+    it { should have_many :application_pets }
+    it { should have_many(:pets).through(:application_pets) }
+  end
 end
