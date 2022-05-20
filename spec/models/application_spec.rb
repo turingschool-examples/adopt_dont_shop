3458d096cda4fe545aba007c24d7e78d3a98031b
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-20 11:03:42 -0700

    create application model test

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  describe 'relationships' do
+    it { should have_many :pet_applications }
+    it { should have_many(:pets).through(:pet_applications) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:application_status) }
+  end
+
+end
