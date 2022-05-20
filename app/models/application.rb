92db6cb8ee78f9a88fd454667ce11712d0894903
Deannah Burke <deannah@bierstadtlager.com>
2022-05-20 16:53:18 -0600

    Add: application show spec, page

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+  enum status:["In Progress", "Pending", "Accepted", "Rejected"]
+  validates_presence_of :name
+  validates_presence_of :street_address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip_code
+  validates_presence_of :description
 end
