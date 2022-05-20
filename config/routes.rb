793f88bc6c337bc0fef91a1452c845b1dd118ddb
Sage Skaff <skaffsage@gmail.com>
2022-05-19 18:45:49 -0700

    Add application index route

--- config/routes.rb
+++ config/routes.rb
@@ -38,4 +38,5 @@ Rails.application.routes.draw do
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
 
   get '/applications/:application_id', to: 'applications#show'
+  get '/applications', to: 'applications#index'
 end
