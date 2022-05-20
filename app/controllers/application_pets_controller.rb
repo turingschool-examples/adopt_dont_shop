562a3d05a328315cec2c2eaad414b3078af44383
Zachary P <zachary.s.prince@outlook.com>
2022-05-20 19:47:18 -0400

    Remov debugger entry point

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationPetsController < ApplicationController
   def create
-    # binding.pry
     app_pet = ApplicationPet.create(application_pets_params)
     redirect_to "/applications/#{app_pet.application_id}"
   end
