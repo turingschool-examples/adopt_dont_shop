345c091bd7961a9c1379d1d66e26f62c23d07543
Sage Skaff <skaffsage@gmail.com>
2022-05-19 18:47:06 -0700

    Add index method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationsController < ApplicationController
+  def index
+    @applications = Application.all
+  end
+
   def show
     @application = Application.find(params[:application_id])
   end
