a56a93704446efa4c566985e28402871f09f3258
Wesley Garcia <wes@alltradestech.com>
2022-05-20 13:42:18 -0700

    Add test for first user story

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ApplicationPet.destroy_all
+Pet.destroy_all
+Shelter.destroy_all
+ApplicationForm.destroy_all
+VeterinaryOffice.destroy_all
+Veterinarian.destroy_all
+
+shelter_1 = Shelter.create(name: 'Aurora Shelter', city: 'Aurora, CO', foster_program: false, rank: 5)
+shelter_2 = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: true, rank: 9)
+
+pet_1 = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
+pet_2 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter_1.id)
+pet_3 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter_2.id)
+
+applicationform_1 = ApplicationForm.create(name: "Hank", street_address: "1234 Some Street", city: "Aurora", state: "CO", zip_code: "80015", description: "I'd like an animal please", status: "In Progress")
+applicationform_2 = ApplicationForm.create(name: "Levi", street_address: "4321 Another Street", city: "Los Angeles", state: "CA", zip_code: "12345", description: "wanna animal", status: "Pending")
+applicationform_3 = ApplicationForm.create(name: "Diana", street_address: "4444 Oneother Court", city: "Detroit", state: "MI", zip_code: "54321", description: "I love animals", status: "Accepted")
+applicationform_4 = ApplicationForm.create(name: "Michael", street_address: "621311 Thisdude Street", city: "Philadelphia", state: "PA", zip_code: "19147", description: "Definitely NOT for fighting", status: "Rejected")
+
+application_pets_1 = ApplicationPet.create(pet: pet_1, application_form: applicationform_1)
+application_pets_2 = ApplicationPet.create(pet: pet_3, application_form: applicationform_2)
+
+vet_office_1 = VeterinaryOffice.create(name: 'Special Friends', boarding_services: true, max_patient_capacity: 100)
+vet_office_2 = VeterinaryOffice.create(name: 'Pet Emergency Room', boarding_services: false, max_patient_capacity: 50)
+vet_office_3 = VeterinaryOffice.create(name: 'The Country Vet', boarding_services: true, max_patient_capacity: 200)
+
+vet_1 = vet_office_1.veterinarians.create(name: 'Morgan', on_call: true, review_rating: 10)
+vet_2 = vet_office_2.veterinarians.create(name: 'Heather', on_call: true, review_rating: 9)
+vet_3 = vet_office_3.veterinarians.create(name: 'John', on_call: false, review_rating: 7)
