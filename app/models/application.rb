e799aa72f184fec43dca6a13aed871c613aaa48b
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-21 10:35:11 -0600

    Removing unnecessary tests for first user story

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,11 +2,4 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
-
-#   def pets_wanted
-#     # binding.pry
-#     # desired_pets = Application.joins(:pets).where("application_id = #{id}")
-#     # desired_pets.first.pets
-#     pets
-#   end
 end
