a37680a9f2e703fc4838577749dd397d2d751a96
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-21 12:04:50 -0600

    Feat: add test and functionality for partial form completion and render partial form

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -22,4 +22,19 @@ RSpec.describe 'Application New Page', type: :feature do
         expect(page).to have_content("Applicant statement:")
     end
   end
+
+  describe 'Starting an Application, Form not Completed' do 
+    it 'does not allow an incomplete application to be submitted' do 
+      visit "/applications/new"
+
+      fill_in(:name, with: 'Spongebob')
+      fill_in(:street_address, with: '124 Conch')
+      fill_in(:city, with: 'Bikini Bottom')
+      fill_in(:zip_code, with: 80250)
+      click_on('Submit')
+      save_and_open_page
+      expect(current_path).to eq('/applications/new')
+      expect(page).to have_content('Application not created: Required information missing')
+    end
+  end
 end
