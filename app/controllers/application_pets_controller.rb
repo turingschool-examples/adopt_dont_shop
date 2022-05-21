347061c6e044800c82506f93c543bcb0a7d1381a
Zachary P <zachary.s.prince@outlook.com>
2022-05-21 10:28:15 -0400

    Change create to post

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationPetsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def create
     app_pet = ApplicationPet.create(application_pets_params)
     redirect_to "/applications/#{app_pet.application_id}"
