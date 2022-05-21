1913fce106d71c200f3ef53aa4c2960ab5f30f06
Zachary P <zachary.s.prince@outlook.com>
2022-05-21 19:03:08 -0400

    Change quotes

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -35,4 +35,8 @@ class Shelter < ApplicationRecord
   def self.reverse_alpha
     find_by_sql('SELECT * FROM shelters ORDER BY name DESC;')
   end
+
+  def self.pending_apps
+    joins(books: [{ reviews: { customer: :orders } }, :supplier])
+  end
 end
