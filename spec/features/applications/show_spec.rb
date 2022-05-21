494220b107d583c46661a5bd30d86ec28e616339
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-21 15:13:05 -0600

    Test: can searche for partial matches for pet names on application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'the applications show page' do
   let!(:application) { Application.create!(name: 'Debbie Yang', street_address: '1234 dog way', city: "San Francisco", state: 'CA', zip_code: 66012) }
   let!(:shelter_1) { Shelter.create!(foster_program: true, name: 'DogsBySeth', city: 'Denver', rank: 1) }
   let!(:pet_1) { shelter_1.pets.create!(adoptable: true, age: 3, breed: 'Yorkie', name: 'Pickle') }
-  let!(:pet_2) { shelter_1.pets.create!(adoptable: true, age: 5, breed: 'German Shephard', name: 'Brownie') }
+  let!(:pet_2) { shelter_1.pets.create!(adoptable: true, age: 5, breed: 'German Shephard', name: 'Pickles') }
 
   it "shows an application and all it's attributes" do
     visit "/applications/#{application.id}"
@@ -49,7 +49,7 @@ RSpec.describe 'the applications show page' do
     expect(current_path).to eq("/pets/#{pet_1.id}")
   end
 
-  it "can submit an application" do
+  it "can Submit an application" do
     visit "applications/#{application.id}"
 
     expect(page).to_not have_content("Submit")
@@ -102,5 +102,14 @@ RSpec.describe 'the applications show page' do
     expect(page).to_not have_content("Submit")
     expect(app_1.application_status).to eq("Pending")
     expect(app_1.description).to eq("I love dogs")
+  end
+
+  it "searches for partial matches for pet names" do
+    visit "/applications/#{application.id}"
+
+    fill_in :pet_name, with: "Pic"
+    click_button "Search"
+    expect(page).to have_content("Pickle")
+    expect(page).to have_content("Pickles")
   end
 end
