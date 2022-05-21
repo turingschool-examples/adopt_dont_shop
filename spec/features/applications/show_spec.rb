6f47db3df652eb444fc705744edceffc68125f73
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-20 21:44:32 -0400

    Add form to add pet to application, not yet working

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe 'the application show' do
   let!(:app_1) {Application.create(name: 'Stephen', street_address: '3 Green St', city: 'Boulder', state: 'CO', zip_code: 80303, description: 'I need to combat loneliness' )}
   let!(:app_2) {Application.create(name: 'Jenn', street_address: '2 Wildflower Lane', city: 'Aurora', state: 'CO', zip_code: 80010, description: 'I need fluffy pets to cuddle' )}
   let!(:pet_1) {Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)}
-  let!(:pet_application_1) {PetApplication.create(pet_id: pet_1.id, application_id: app_1.id)}
-  let!(:pet_application_2) {PetApplication.create(pet_id: pet_1.id, application_id: app_2.id)}
+  # let!(:pet_application_1) {PetApplication.create(pet_id: pet_1.id, application_id: app_1.id)}
+  # let!(:pet_application_2) {PetApplication.create(pet_id: pet_1.id, application_id: app_2.id)}
 
   it "shows the application and all it's attributes" do
     visit "/applications/#{app_1.id}"
@@ -26,13 +26,15 @@ RSpec.describe 'the application show' do
   end
 
   it "allows you to add a pet to the application if the application has not been submitted" do
+    visit "/applications/#{app_1.id}"
     expect(page).to have_content('In Progress')
     expect(page).to have_content('Add a Pet to this Application')
 
-    fill_in("Pet name", with: "Lucille Bald")
-    click_button "Submit"
+    fill_in("Search by name", with: "Lucille Bald")
+    click_button "Search"
 
     expect(current_path).to eq("/applications/#{app_1.id}")
+    save_and_open_page
     expect(page).to have_content('Lucille Bald')
 
   end
