e2589e207e88f810600ed51b8d3186156bbb9b08
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-21 10:00:42 -0600

    Refactor: first user story to remove unnecessary method

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,9 +3,10 @@ class Application < ApplicationRecord
   has_many :pets, through: :pet_applications
 
 
-  def pets_wanted
-    # binding.pry
-    desired_pets = Application.joins(:pets).where("application_id = #{id}")
-    desired_pets.first.pets
-  end
+#   def pets_wanted
+#     # binding.pry
+#     # desired_pets = Application.joins(:pets).where("application_id = #{id}")
+#     # desired_pets.first.pets
+#     pets
+#   end
 end
