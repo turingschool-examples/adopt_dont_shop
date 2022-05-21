b4008f340f3b412ac1b1f5f091456f5143c52c09
Sarah Hall <swatson1994@yahoo.com>
2022-05-21 12:43:05 -0400

    "Add routes and method for create"

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,4 +5,13 @@ class ApplicationsController < ApplicationController
 
   def new
   end
+
+  def create
+    new_app = Application.create(app_params)
+    redirect_to "/applications/#{new_app.id}"
+  end
+
+  def app_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :status)
+  end
 end
