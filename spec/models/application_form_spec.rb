082dfc79b1d4fc0549f9eabf8e3c3dabbd33bbf3
Jim Riddle <jimriddle0@gmail.com>
2022-05-21 10:58:28 -0500

    write tests to validate presence of app forms attrs

--- spec/models/application_form_spec.rb
+++ spec/models/application_form_spec.rb
@@ -6,38 +6,13 @@ RSpec.describe ApplicationForm, type: :model do
     it { should have_many(:pets).through(:application_pets)}
   end
 
-  # describe 'validations' do
-  #   it { should validate_presence_of(:name) }
-  #   it { should validate_presence_of(:age) }
-  #   it { should validate_numericality_of(:age) }
-  # end
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_numericality_of(:zip_code) }
+  end
 
-  # before(:each) do
-  #   @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-  #   @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-  #   @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-  #   @pet_3 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
-  # end
-
-  # describe 'class methods' do
-  #   describe '#search' do
-  #     it 'returns partial matches' do
-  #       expect(Pet.search("Claw")).to eq([@pet_2])
-  #     end
-  #   end
-  #
-  #   describe '#adoptable' do
-  #     it 'returns adoptable pets' do
-  #       expect(Pet.adoptable).to eq([@pet_1, @pet_2])
-  #     end
-  #   end
-  # end
-  #
-  # describe 'instance methods' do
-  #   describe '.shelter_name' do
-  #     it 'returns the shelter name for the given pet' do
-  #       expect(@pet_3.shelter_name).to eq(@shelter_1.name)
-  #     end
-  #   end
-  # end
 end
