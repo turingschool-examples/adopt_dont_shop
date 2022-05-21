2542d7c218389dbf5f3539c3669b8b5adbf2437a
Zachary P <zachary.s.prince@outlook.com>
2022-05-21 17:45:21 -0400

    Add shelter reverse alpha test

---
+++ spec/features/admin/shelters/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'the admin shelters index page' do
+  it 'shows all shelters in the system in reverse alphabetical order' do
+    shelter1 = Shelter.create!(foster_program: true, name: 'Gally', city: 'Denver', rank: 21)
+    shelter2 = Shelter.create!(foster_program: true, name: 'Fally', city: 'Denver', rank: 21)
+    shelter3 = Shelter.create!(foster_program: true, name: 'Eally', city: 'Denver', rank: 21)
+    shelter4 = Shelter.create!(foster_program: true, name: 'Dally', city: 'Denver', rank: 21)
+
+    visit '/admin/shelters'
+
+    expect(shelter1.name).to appear_before(shelter2.name)
+
+    expect(shelter1.name).to appear_before(shelter4.name)
+
+    expect(shelter1.name).to appear_before(shelter3.name)
+
+    expect(shelter3.name).to appear_before(shelter4.name)
+
+    expect(shelter2.name).to appear_before(shelter4.name)
+
+    expect(shelter4.name).to appear_after(shelter2.name)
+  end
+end
