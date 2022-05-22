f2d91e1a7de85e758c344af67152b2659d56cf2c
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-21 18:05:52 -0600

    add order_by_name method to shelter.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,15 @@ Pet.destroy_all
 Shelter.destroy_all
 
 shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-# shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-# shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-# shelter.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-# shelter.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-# shelter.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+shelter.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+shelter.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+shelter.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
 
-# vet_office = VeterinaryOffice.create(name: 'Best Vets', boarding_services: true, max_patient_capacity: 20)
-# vet_1 = Veterinarian.create(name: 'Taylor', review_rating: 10, on_call: true, veterinary_office_id: vet_office.id)
-# vet_2 = Veterinarian.create(name: 'Jim', review_rating: 8, on_call: true, veterinary_office_id: vet_office.id)
+vet_office = VeterinaryOffice.create(name: 'Best Vets', boarding_services: true, max_patient_capacity: 20)
+vet_1 = Veterinarian.create(name: 'Taylor', review_rating: 10, on_call: true, veterinary_office_id: vet_office.id)
+vet_2 = Veterinarian.create(name: 'Jim', review_rating: 8, on_call: true, veterinary_office_id: vet_office.id)
 
 application = Application.create!(name: 'Debbie Yang', street_address: '1234 dog way', city: "San Francisco", state: 'CA', zip_code: 66012)
 
