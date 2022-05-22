5a1304771d556b18825dd05964c0782f52eec5dd
Sarah Hall <swatson1994@yahoo.com>
2022-05-22 16:50:53 -0400

    "Add route for admin aplpications show page"

--- config/routes.rb
+++ config/routes.rb
@@ -45,4 +45,5 @@ Rails.application.routes.draw do
   post '/pet_applications/new', to: 'pet_applications#create'
 
   get '/admin/shelters', to: 'admin_shelters#index'
+  get '/admin/applications/:id', to: 'admin_applications#show'
 end
