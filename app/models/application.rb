e27d59692a1642313c8a8e229bc989e241e3a585
Andrew King <dev.andrewking@gmail.com>
2022-05-21 20:25:07 -0500

    add show all pets method to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,8 @@ class Application < ApplicationRecord
 
   validates_presence_of :name, :street, :city, :state, :zip, :description,
    :application_status
+
+  def self.show_pets 
+    Pet.all
+  end
 end
