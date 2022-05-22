4f5c23e14b08a2f0cd9218859453fd31f4a8b483
Matt Deming <vballmatt44@gmail.com>
2022-05-22 15:40:22 -0600

    Add tests and validations for application model

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+  end
+
   describe 'relatioinships' do
     it { should have_many(:application_pets) }
   end
