18755f3e61bc3419ee8f90ba46da3c1d20ec0f2c
Caden Jarrett <caden.jarrett99@gmail.com>
2022-05-23 14:29:56 -0600

    created test for status change

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
+  before(:each) do
+    @shelter1 = Shelter.create(name: 'Denver Dogs', city: 'Denver', foster_program: true, rank: 9)
+    @pet12 = @shelter1.pets.create(name: 'Spike', age: 3, breed: 'doberman', adoptable: true)
+    @application1 = Application.create!(name: 'Chris', street_address: '123 Main St', city: 'Hometown', state: 'CO', zipcode: "00004")
+  end
+
   describe "relationships" do
     it {should have_many(:pet).through(:pet_applications)}
     it {should have_many(:pet_applications)}
@@ -13,5 +19,11 @@ RSpec.describe Application, type: :model do
     it {should validate_presence_of(:state)}
     it {should validate_presence_of(:zipcode)}
     it {should validate_presence_of(:status)}
+  end
+
+  it 'can change the status of a application to pending when submitted' do
+    expect(@application1.status).to eq("In Progress")
+    @application1.status_pending
+    expect(@application1).to eq("Pending")
   end
 end
