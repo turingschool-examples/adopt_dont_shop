e677f3577f2aa4ebad8805bea01bd318bd567e98
Sarah Hall <swatson1994@yahoo.com>
2022-05-23 13:44:01 -0400

    "Add tests approve button and indicator"

--- spec/features/admin/admin_applications/show_spec.rb
+++ spec/features/admin/admin_applications/show_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'admin applications show page', type: :feature do
           street_address: '-1 Bermuda trgl',
           city: 'Atlantis',
           state: 'Confusion',
-          zip_code: 42069,
+          zip_code: 42070,
           description: "I'm really lonely.",
           status: 'Pending'
       )
@@ -21,8 +21,35 @@ RSpec.describe 'admin applications show page', type: :feature do
 
       visit "/admin/applications/#{application1.id}"
 
-      expect(page).to have_content(pet_1.name)
-      expect(page).to have_content(pet_2.name)
+      expect(page).to have_button("Approve #{pet_1.name}")
+      expect(page).to have_button("Approve #{pet_2.name}")
+    end
+
+    it 'shows pet name and aproval indicator on application show page after button clicked' do
+      shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+      pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+
+      application1 = Application.create!(
+          name: 'Waldo Werziat',
+          street_address: '-1 Bermuda trgl',
+          city: 'Atlantis',
+          state: 'Confusion',
+          zip_code: 42070,
+          description: "I'm really lonely.",
+          status: 'Pending'
+      )
+      pet_application_1 = PetApplication.create!(pet: pet_1, application: application1)
+      pet_application_2 = PetApplication.create!(pet: pet_2, application: application1)
+
+      visit "/admin/applications/#{application1.id}"
+
+      within "#pet#{pet_1.id}" do
+        click_button("Approve #{pet_1.name}")
+
+        expect(page).to have_current_path("/admin/applications/#{application1.id}")
+        expect(page).to have_content("#{pet_1.name}: Approved")
+      end
     end
   end
 end
