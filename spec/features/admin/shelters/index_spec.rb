08244b86cfdb6771d03271656d8ad70c3a9bc547
Clay Ash <claycourtneyash@gmail.com>
2022-05-23 09:24:14 -0600

    Add tests and view for shelters with pending applications in admin shelter index

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe 'admin shelters index page' do
   let!(:shelter_2) { Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5) }
   let!(:shelter_3) { Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10) }
 
+  let!(:pet_1) { shelter.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true) }
+  let!(:pet_2) { shelter_2.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true) }
+  let!(:pet_3) { shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true) }
+
+  let!(:app_1) { pet_1.applications.create(name: "Bob", street_address: "234 York st", city: "Denver", state: "CO", zip_code: "83201", application_status: "Pending") }
+  let!(:app_2) { pet_2.applications.create(name: "Violet", street_address: "678 Main st", city: "Denver", state: "CO", zip_code: "83302", application_status: "Pending") }
+  let!(:app_3) { pet_3.applications.create(name: "Hannah", street_address: "MLK blvd", city: "Denver", state: "CO", zip_code: "83402", application_status: "In Progress") }
+
+
   describe "#order_by_name"
   it "can see all shelters in the system listed in reverse alphabetical order by name" do
     visit '/admin/shelters'
@@ -12,5 +21,18 @@ RSpec.describe 'admin shelters index page' do
     expect('RGV animal shelter').to appear_before('Aurora shelter')
     expect('Fancy pets of Colorado').to appear_before('Aurora shelter')
     expect('Aurora shelter').to_not appear_before('RGV animal shelter')
+  end
+
+
+  it 'lists shelters that have pending applications' do 
+    visit '/admin/shelters'
+
+    expect(page).to have_content("Shelters with Pending Applications")
+
+     within("#pending-#{shelter.id}") do
+      expect(page).to have_content(shelter.name)
+      expect(page).to have_content(shelter_2.name)
+      expect(page).to_not have_content(shelter_3.name)
+     end 
   end
 end
