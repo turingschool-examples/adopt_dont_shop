71ad83c114f69f8aa0ede1796ee7eb19f5bfa140
Andrew King <dev.andrewking@gmail.com>
2022-05-23 15:04:15 -0500

    remove description from validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,8 +2,7 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
-  validates_presence_of :name, :street, :city, :state, :zip, :description,
-   :application_status
+  validates_presence_of :name, :street, :city, :state, :zip, :application_status
 
   def self.show_pets
     Pet.all
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe Application, type: :model do
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:zip) }
-    it { should validate_presence_of(:description) }
     it { should validate_presence_of(:application_status) }
   end
 
