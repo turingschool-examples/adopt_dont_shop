4518f8dbd9856045088410e4925009ac4545451f
Andrew King <dev.andrewking@gmail.com>
2022-05-23 14:58:15 -0500

    remove description from test data

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe 'the application show' do
     it "shows the application and attributes" do
         application = Application.create!(name: 'Andrew', street: "123 Street", city: "Kenosha",
-        state: "WI", zip: 53144, description: "This is a description of why I'm a good fit.",
-        application_status: "In Progress")
+        state: "WI", zip: 53144, application_status: "In Progress")
 
         visit "/applications/#{application.id}"
 
@@ -19,8 +18,7 @@ RSpec.describe 'the application show' do
 
     it "can search for pets by name" do
         application = Application.create!(name: 'Andrew', street: "123 Street", city: "Kenosha",
-        state: "WI", zip: 53144, description: "This is a description of why I'm a good fit.",
-        application_status: "In Progress")
+        state: "WI", zip: 53144, application_status: "In Progress")
         shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
         pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
         pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
@@ -38,25 +36,36 @@ RSpec.describe 'the application show' do
     end
 
 
-it "can add a pet to an application" do
-    application = Application.create!(name: 'Andrew', street: "123 Street", city: "Kenosha",
-    state: "WI", zip: 53144, description: "This is a description of why I'm a good fit.",
-    application_status: "In Progress")
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
-    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+    it "can add a pet to an application" do
+        application = Application.create!(name: 'Andrew', street: "123 Street", city: "Kenosha",
+        state: "WI", zip: 53144, application_status: "In Progress")
+        shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+        pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+        pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+        pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
 
 
-    visit "/applications/#{application.id}"
+        visit "/applications/#{application.id}"
 
-    fill_in "Search", with: "Babe"
-    click_button "Search"
+        fill_in "Search", with: "Babe"
+        click_button "Search"
 
-    click_button "Adopt Babe"
+        click_button "Adopt Babe"
 
-    expect(current_path).to eq("/applications/#{application.id}")
-    expect(page).to have_link("Babe", href: "/pets/#{pet_2.id}")
-end
+        expect(current_path).to eq("/applications/#{application.id}")
+        expect(page).to have_link("Babe", href: "/pets/#{pet_2.id}")
+    end
+
+    it 'can submit an application' do
+        application = Application.create!(name: 'Andrew', street: "123 Street", city: "Kenosha",
+        state: "WI", zip: 53144, application_status: "In Progress")
+        shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+        pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+        pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+        pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+
+
+        visit "/applications/#{application.id}"
+    end
 
 end
