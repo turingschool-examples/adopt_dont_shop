a6f47955299309225236cc91cf74670b7bb901ac
Colin Reinhart <colinbreinhartgmail.com>
2022-05-23 13:15:10 -0600

    Delete pry from controller and added seeds- heroku troubleshoot

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,15 @@ Application.destroy_all
 Shelter.destroy_all
 
 @ddfl = Shelter.create!(name: "Denver Dumb Friends League", foster_program: true, city: "Denver", rank: 5)
-# @max = Shelter.create!(name: "Max Fund", foster_program: true, city: "Denver", rank: 5)
+@max = Shelter.create!(name: "Max Fund", foster_program: true, city: "Denver", rank: 5)
 
 @doggo = @ddfl.pets.create!(adoptable: true, age: 12, breed: "Cairn Terrior", name: "Sylus")
-# @meow = @max.pets.create!(adoptable: true, age: 2, breed: "short hair", name: "Hopps")
+@meow = @max.pets.create!(adoptable: true, age: 2, breed: "short hair", name: "Hopps")
 
 # Creates a playlist
 
-# @bob = Application.create!(name: "Bob Ross", street_address: "123 Trees St.", city: "Nantuket", state: "MA", zip: "12554")
-# @bill = Application.create!(name: "Bill Nye", street_address: "1265 S Main St.", city: "Los Angeles", state: "CA", zip: "90006")
+@bob = Application.create!(name: "Bob Ross", street_address: "123 Trees St.", city: "Nantuket", state: "MA", zip: "12554")
+@bill = Application.create!(name: "Bill Nye", street_address: "1265 S Main St.", city: "Los Angeles", state: "CA", zip: "90006")
 
 # Creates playlist and associates it with a song. Under the hood, creates a row in the playlist_songs table
 
