d427f67f5b906c5feef93de8640df8bc2731f1c3
Andrew King <dev.andrewking@gmail.com>
2022-05-23 14:57:50 -0500

    change patch to post for adopt_pet

--- config/routes.rb
+++ config/routes.rb
@@ -41,5 +41,5 @@ Rails.application.routes.draw do
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
   post '/applications', to: 'applications#create'
-  patch '/applications/:id', to: 'applications#adopt_pet'
+  post '/applications/:id', to: 'applications#adopt_pet'
 end
