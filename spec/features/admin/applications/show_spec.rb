d9091d1f5d351afab30c3f7506a2515b524bdb66
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-24 15:42:59 -0600

    Test: Approved/Rejected Pets on one Application do not affect other Applications

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe 'admin application show page' do
   let!(:shelter) { Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9) }
   let!(:pet_1) { shelter.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false) }
-  # let!(:pet_2) { shelter.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true) }
-  # let!(:pet_3) { shelter.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true) }
+  let!(:pet_2) { shelter.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true) }
+  let!(:pet_3) { shelter.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true) }
   let!(:app_1) { Application.create(name: "Bob", street_address: "234 York st", city: "Denver", state: "CO", zip_code: "83201", application_status: "Pending") }
+  let!(:app_2) { Application.create(name: "Violet", street_address: "678 Main st", city: "Denver", state: "CO", zip_code: "83302", application_status: "Pending") }
   let!(:pet_app1) { PetApplication.create!(application_id: app_1.id, pet_id: pet_1.id) }
-  # let!(:pet_app2) { PetApplication.create!(application_id: app_1.id, pet_id: pet_2.id) }
-  # let!(:pet_app3) { PetApplication.create!(application_id: app_1.id, pet_id: pet_3.id) }
+  let!(:pet_app2) { PetApplication.create!(application_id: app_1.id, pet_id: pet_2.id) }
+  let!(:pet_app3) { PetApplication.create!(application_id: app_1.id, pet_id: pet_3.id) }
+  let!(:pet_app4) { PetApplication.create!(application_id: app_2.id, pet_id: pet_1.id) }
 
   it 'can approve an application for a pet' do
     visit "/admin/applications/#{app_1.id}"
@@ -17,25 +19,41 @@ RSpec.describe 'admin application show page' do
       expect(page).to have_button("Approve")
       expect(page).to have_button("Reject")
       click_button "Approve"
-      # save_and_open_page
+      expect(current_path).to eq("/admin/applications/#{app_1.id}")
+      expect(page).to have_no_button("Approve")
+      expect(page).to have_content("Approved")
     end
-    expect(current_path).to eq("/admin/applications/#{app_1.id}")
-    expect(page).to have_no_button("Approve")
-    expect(page).to have_content("Approved")
-    #
-    # within("##{pet_app2.id}") do
-    #   expect(page).to have_button("Approve")
-    #   expect(page).to have_button("Reject")
-    #   expect(page).to_not have_content("Approved")
-    # end
-    #
-    # within("##{pet_app3.id}") do
-    #   expect(page).to have_button("Approve")
-    #   expect(page).to have_button("Reject")
-    #   click_button "Reject"
-    #   save_and_open_page
-    #   expect(page).to have_content("Rejected")
-    #   expect(page).to_not have_content("Approved")
-    # end
+
+    within("##{pet_app2.id}") do
+      expect(page).to have_button("Approve")
+      expect(page).to have_button("Reject")
+      expect(page).to_not have_content("Approved")
+    end
+
+    within("##{pet_app3.id}") do
+      expect(page).to have_button("Approve")
+      expect(page).to have_button("Reject")
+      click_button "Reject"
+      save_and_open_page
+      expect(page).to have_content("Rejected")
+      expect(page).to_not have_content("Approved")
+    end
+  end
+
+  it 'Approved/Rejected Pets on one Application do not affect other Applications' do
+
+    visit "/admin/applications/#{app_1.id}"
+    within("##{pet_app1.id}") do
+    expect(page).to have_content('Mr. Pirate')
+
+      click_button "Approve"
+      expect(page).to have_content("Approved")
+    end
+
+    visit "/admin/applications/#{app_2.id}"
+    within("##{pet_app4.id}") do
+      expect(page).to have_content('Mr. Pirate')
+      expect(page).to have_button("Approve")
+    end
   end
 end
