dd0991eca081cf25b24ca2568d1e2cc01bc11984
Matt Deming <vballmatt44@gmail.com>
2022-05-25 10:51:00 -0600

    Add functionality for approving pet

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get '/admin/shelters', to: 'admin_shelters#index'
 
+  get '/admin/applications/:id', to: 'admin_applications#show'
+  post '/admin/applications/:id/edit', to: 'admin_applications#edit'
+  patch '/admin/applications/:id', to: 'admin_applications#update'
+
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
   get '/applications', to: 'applications#index'
