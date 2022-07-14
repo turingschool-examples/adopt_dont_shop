e6bc3746fe6cbdc2d4b29bb544fe1f37ee1644ff
Sage Skaff <skaffsage@gmail.com>
2022-07-14 10:52:39 -0700

    Update schema

---
+++ db/migrate/20220714174146_create_application.rb
@@ -0,0 +1,12 @@
+class CreateApplication < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :address
+      t.string :description
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
