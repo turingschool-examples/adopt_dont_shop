4930bdf18506e5080e3c394b5d94df18a951570c
mkbonini <mkbonini@gmail.com>
2022-07-14 12:59:58 -0600

    Create migration for pet application join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_185336) do
+ActiveRecord::Schema.define(version: 2022_07_14_185826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2022_07_14_185336) do
     t.string "zipcode"
     t.string "description"
     t.string "status"
+  end
+
+  create_table "pet_applications", force: :cascade do |t|
+    t.bigint "pet_id"
+    t.bigint "application_id"
+    t.index ["application_id"], name: "index_pet_applications_on_application_id"
+    t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -63,6 +70,8 @@ ActiveRecord::Schema.define(version: 2022_07_14_185336) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pet_applications", "applications"
+  add_foreign_key "pet_applications", "pets"
   add_foreign_key "pets", "shelters"
   add_foreign_key "veterinarians", "veterinary_offices"
 end
