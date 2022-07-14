b30958ec9f0ec83968464dfe7313c9092b24ee30
Drew MacNicholas <dmacnicholas@gmail.com>
2022-07-14 18:27:09 -0500

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Shelter.destroy_all
+Pet.destroy_all
+Application.destroy_all
+
+
+@shelter_1 = Shelter.create!(foster_program: true, name: "ABC Shelter", city: "Nashville", rank: 1)
+@pet_1 = @shelter_1.pets.create!(adoptable: true, age: 3, breed: "Goldendoodle", name: "Daisy")
+@pet_2 = @shelter_1.pets.create(adoptable: false, age: 7, breed: "Terrier", name: "Chewie")
+@application_1 = Application.create!(name: 'John', address: '123 Main Street', city: 'Nashville', state: 'TN', zip: 37067, description: "I want a nice dog.", status: "In Progress")
