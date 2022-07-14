92f2852f428a6fd168a9b975b0d2dbccc7f2b43e
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-14 13:49:54 -0600

    Add application model and spec, add pet applications join table and model, test validations and relationships

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe Pet, type: :model do
   describe 'relationships' do
     it { should belong_to(:shelter) }
+    it { should have_many(:applications).through(:pet_applications) }
   end
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:age) }
     it { should validate_numericality_of(:age) }
+    
   end
 
   before(:each) do
