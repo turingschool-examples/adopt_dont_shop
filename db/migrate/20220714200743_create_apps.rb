fa3c8f972d58eb9952b1124fb18e4f9545270f39
Eli Sachs <easachs13@gmail.com>
2022-07-14 15:20:48 -0600

    Add migrations

---
+++ db/migrate/20220714200743_create_apps.rb
@@ -0,0 +1,12 @@
+class CreateApps < ActiveRecord::Migration[5.2]
+  def change
+    create_table :apps do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :description
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# shelter = Shelter.create!(name: "Dumb Friends League", city: "Denver", rank: 1, foster_program: true)
+# pet = Pet.create!(name: "Spot", breed: "Beagle", age: 2, adoptable: true)
+# vetoffice = VeterinaryOffice.create!(name: "Banfield", max_patient_capacity: 15, boarding_services: true)
+# vet = Veterinarian.create!(name: "Bob", review_rating: 3, on_call: true)
