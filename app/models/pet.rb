cd1baf71e33608ea5cb29ec070e30b5b68619c28
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-14 17:52:54 -0600

    US2: update pet model; adds has_many applicants

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ApplicationRecord
+  has_many :applicants, through: :applicant_pets, cache_counter: true
+  
   validates :name, presence: true
   validates :age, presence: true, numericality: true
   belongs_to :shelter
