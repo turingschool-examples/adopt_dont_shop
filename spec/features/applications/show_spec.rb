0b622835161606f8e61201225c34a2ed9ba89613
mkbonini <mkbonini@gmail.com>
2022-07-14 13:49:55 -0600

    Add test for application show page

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'the application show' do
+  it "shows the application and all it's attributes" do
+    application = Application.create(name: 'John Doe', street_address: '123 apple street' city: 'Denver', state: 'CA', zipcode: '90210', description: 'we love pets', status: 'in progress')
+    shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    pet = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+
+    PetApplication.create!(pet: pet, application: application)
+
+    visit "/applications/#{application.id}"
+
+    expect(page).to have_content('John Doe')
+    expect(page).to have_content('123 apple street')
+    expect(page).to have_content('Denver')
+    expect(page).to have_content('CA')
+    expect(page).to have_content('90210')
+    expect(page).to have_content('we love pets')
+    expect(page).to have_content('in progress')
+
+    expect(page).to have_content('Scooby')
+  end
