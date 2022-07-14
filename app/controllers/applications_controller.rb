5ac977a28639de62a4099d8ac5192b9d52d91235
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-14 15:55:24 -0600

    feat: add pet search on application show page with test

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,6 +2,11 @@ class ApplicationsController < ApplicationController
 
     def show
         @application = Application.find(params[:application_id])
+        if params[:search] != nil 
+            @pets = Pet.find_by_name(params[:search])
+        else     
+            @pets = []
+        end 
     end
 
 end  
