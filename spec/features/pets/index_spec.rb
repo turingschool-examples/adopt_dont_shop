d7464760922edb1e5065d9752de89773e475d2c6
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-14 15:23:11 -0600

    feat: add link to start an application on pets index page

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -92,4 +92,18 @@ RSpec.describe 'the pets index' do
 
     expect(page).to have_link('Start an Application')
   end
+
+  it 'click link to go to new pet application page' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+
+    visit "/pets"
+
+    click_link 'Start an Application' 
+
+    expect(current_path).to eq('/applications/new')
+  end
+
 end
