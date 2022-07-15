5a6eb5831b139dd77a7975873f826c2123b9ee99
Thiago Silveira <thiago1992@yahoo.com>
2022-07-15 15:40:26 -0600

    Test: View for application form

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -81,4 +81,27 @@ RSpec.describe 'the pets index' do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
+
+  it 'displays link that starts an application' do
+    visit "/pets"
+    expect(page).to have_link("Start Application")
+  end
+  
+  it 'redirects to new application form' do
+    visit "/pets"
+    click_link("Start Application")
+    save_and_open_page
+    expect(page).to have_current_path("/applications/new")
+  end
+
+  it 'displays new content in new page' do
+    visit "/applications/new"
+    expect(page).to have_content("New Application")
+  end
+
+  it 'has a submit button' do
+    visit "/applications/new"
+    click_button "Submit Application"
+    expect(page).to have_current_path("/applications")
+  end
 end
