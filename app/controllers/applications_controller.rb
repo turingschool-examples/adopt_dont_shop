505ccd316620f164ee06139dccc0562f9afabc63
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-07-15 12:48:59 -0500

    add new application form

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,20 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new 
+
+  end
+
+  def create
+    params[:address] = "#{params[:street_address]}, #{params[:city]}, #{params[:state]} #{params[:zip_code]}"
+    application = Application.create(application_params)
+
+    redirect_to "/applications/#{application.id}"
+  end
+  
+  private 
+  def application_params 
+    params.permit(:name, :address, :description, :status)
+  end
 end
---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper' 
+
+RSpec.describe 'applications new' do 
+    it 'has a form to create new application' do 
+        visit '/applications/new' 
+        fill_in "Name", with: "Tony Soprano" 
+        fill_in "Street Address", with: "1234 Ever Lane"
+        fill_in "City", with: "Jersey City" 
+        fill_in "State", with: "NJ" 
+        fill_in "Zip Code", with: "12345"
+        click_button "Submit"
+
+        expect(current_path).to eq "/applications/#{Application.all.last.id}"
+        expect(page).to have_content("Tony Soprano")
+        expect(page).to have_content("1234 Ever Lane, Jersey City, NJ 12345")
+        expect(page).to have_content("In Progress")
+    end
+end
