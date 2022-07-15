b7d7d575ec7b92cf2cd78a2a4dd49210a95c0d20
Gauri Joshi <gauri728@gmail.com>
2022-07-15 15:04:38 -0700

    updates strong params

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -5,17 +5,18 @@ class ApplicantsController < ApplicationController
     end
 
     def new
-        # @applicant = Applicant.find(params[:applicant_id])
+        # @applicant = Applicant.new
+        # @applicant.id = (params[:applicant_id])
     end
 
     def create 
-        applicant = Applicant.create(applicant_params)
-        applicant.submit
-        redirect_to "/applications/#{applicant.id}" #this needs fixing
+        Applicant.create(applicant_params)
+        # applicant.submit
+        redirect_to "/applications" #this needs fixing
     end
 end
 
 private 
 def applicant_params 
-    params.permit(:name, :address, :city, :state, :zip, :description)
+    params.permit(:name, :address, :city, :state, :zip, :description, :names_pets_wanted, :application_status)
 end
