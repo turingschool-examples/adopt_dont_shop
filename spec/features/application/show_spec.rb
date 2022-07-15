02472fd6a2805a3182c736233214ac7ed91e1558
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-14 20:36:11 -0700

    Created and passed tests for applications class

--- spec/features/application/show_spec.rb
+++ spec/features/application/show_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Application Show' do
   before :each do
     @shelter_1 = Shelter.create!(foster_program: true, name: "ABC Shelter", city: "Nashville", rank: 1)
-    @pet_1 = @shelter_1.pets.create!(adoptable: true, age: 3, breed: "Goldendoodle", name: "Daisy")
-    @pet_2 = @shelter_1.pets.create(adoptable: false, age: 7, breed: "Terrier", name: "Chewie")
+    @pet_1 = Pet.create!(adoptable: true, age: 3, breed: "Goldendoodle", name: "Daisy", shelter_id: @shelter_1.id)
+    @pet_2 = Pet.create!(adoptable: true, age: 7, breed: "Terrier", name: "Chewie", shelter_id: @shelter_1.id)
     @application_1 = Application.create!(name: 'John', address: '123 Main Street', city: 'Nashville', state: 'TN', zip: 37067, description: "I want a nice dog.", status: "In Progress")
-    @pet_application_1 = PetApplication.create(pet_id: @pet_1.id, application_id: @application_1.id)
+    @pet_application_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: @application_1.id)
+    @pet_application_1 = PetApplication.create!(pet_id: @pet_2.id, application_id: @application_1.id)
 end
 
   describe 'attributes' do
@@ -20,6 +21,8 @@ end
       expect(page).to have_content(@application_1.zip)
       expect(page).to have_content(@application_1.description)
       expect(page).to have_content(@application_1.status)
+      expect(page).to have_content(@pet_1.name)
+      expect(page).to have_content(@pet_2.name)
     end
   end
 end
