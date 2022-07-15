06064cd80410f2bcdccdcf8f8bdb47f194d3bd94
Anna Marie Sterling <sterling.am@gmail.com>
2022-07-14 20:14:21 -0600

    Applicant Index page

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,6 +1,5 @@
 class ApplicantsController < ApplicationController
  def index
-   require "pry"; binding.pry
    @applicants = Applicant.all
  end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,30 @@ Pet.destroy_all
 Applicant.destroy_all
 
 mike = Applicant.create!(
-                      name: 'Mike Dao',
-                      street_address: '245 Maple St',
-                      city: 'Centennial',
-                      state: 'Colorado',
-                      zip_code: 80112,
-                      applicant_bio: 'My dog needs another to chase up trees.',
-                      application_status: 'Pending')
-
+              name: 'Mike Dao',
+              street_address: '245 Maple St',
+              city: 'Centennial',
+              state: 'Colorado',
+              zip_code: '80112',
+              applicant_bio: 'My dog needs another to chase up trees.',
+              application_status: 'Pending')
+chris = Applicant.create!(
+              name: 'Chris Simmons',
+              street_address: '533 Oak St',
+              city: 'Columbus',
+              state: 'Ohio',
+              zip_code: '43004',
+              applicant_bio: 'Because how much more work could a third cat be?',
+              application_status: 'In Progress')
+dani = Applicant.create!(
+              name: 'Dani Coleman',
+              street_address: '912 Willow St',
+              city: 'Arvada',
+              state: 'Colorado',
+              zip_code: '80003',
+              applicant_bio: 'Because I am just awesome.',
+              application_status: 'Accepted')
+              
 pet = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
 pet = Pet.create!(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
 pet = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'George Hairlesson', shelter_id: shelter.id)
