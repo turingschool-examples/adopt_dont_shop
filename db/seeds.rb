dbfb5e41d2eb448c508c82a07b2f1c6fa352dcd3
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-14 20:34:53 -0700

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Shelter.destroy_all
+PetApplication.destroy_all
 Pet.destroy_all
 Application.destroy_all
-
+Shelter.destroy_all
 
 @shelter_1 = Shelter.create!(foster_program: true, name: "ABC Shelter", city: "Nashville", rank: 1)
-@pet_1 = @shelter_1.pets.create!(adoptable: true, age: 3, breed: "Goldendoodle", name: "Daisy")
-@pet_2 = @shelter_1.pets.create(adoptable: false, age: 7, breed: "Terrier", name: "Chewie")
+
+@pet_1 = Pet.create!(adoptable: true, age: 3, breed: "Goldendoodle", name: "Daisy", shelter_id: @shelter_1.id)
+@pet_2 = Pet.create!(adoptable: true, age: 7, breed: "Terrier", name: "Chewie", shelter_id: @shelter_1.id)
+
 @application_1 = Application.create!(name: 'John', address: '123 Main Street', city: 'Nashville', state: 'TN', zip: 37067, description: "I want a nice dog.", status: "In Progress")
+
+@pet_application_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: @application_1.id)
+@pet_application_1 = PetApplication.create!(pet_id: @pet_2.id, application_id: @application_1.id)
