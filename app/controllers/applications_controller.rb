6b88861dae62076e2d437d83ee1a5d566b89dddf
Thiago Silveira <thiago1992@yahoo.com>
2022-07-15 15:42:40 -0600

    Feature: Application controller new and create

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationsController < ApplicationController
+    def index
+        @applications = Application.all
+    end
+
     def show
         @application = Application.find(params[:id])
         @pets = @application.pets
+    end
+
+    def new
+    end
+
+    def create
+        # Application.create
     end
 end
