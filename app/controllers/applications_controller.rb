ea9cb6b3605ac7b0e470f951ce382ee2669701b3
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-14 22:14:09 -0700

    testing and updates to applications controller for us 2

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,4 +6,21 @@ class ApplicationsController < ApplicationController
 
   def new
   end
+
+  def create
+    @application = Application.create(application_params)
+    redirect_to "/applications/#{@application.id}"
+  end
+
+  private
+
+  def application_params
+    params.permit(:name,
+                  :address,
+                  :city,
+                  :state,
+                  :zip,
+                  :description,
+                  :status)
+  end
 end
