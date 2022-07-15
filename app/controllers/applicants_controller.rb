0da908d3d067f369181276ed0bf146878522de8c
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-14 18:10:44 -0600

    US2: update applicant controller

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,17 +1,32 @@
 class ApplicantsController < ApplicationController
   def index
-    
+    @applicants = Applicant.all
   end
 
   def new
+    @applicant = Applicant.new
   end
 
   def edit
   end
 
   def show
+    @applicant = Applicant.find(params[:id])
   end
 
   def create
+    applicant = Applicant.create!(applicant_params)
   end
+
+  def update
+    applicant = Applicant.find(params[:id])
+    applicant.update(applicant_params)
+    redirect_to
+  end
+
+  def destroy
+  end
+
+  private
+
 end
