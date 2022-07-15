a21d36fae1cef92ec4daa6ed1aa757fb3186da81
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-14 19:45:22 -0600

    feat: Add flash warning to application page if fields are left empty when submitted

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -13,7 +13,18 @@ RSpec.describe 'New Application' do
     fill_in('Description', with: 'I love dogs so much and have lots of food for them')
 
     click_button('Submit')
-    
+
     expect(page).to have_content("Bob Bobbicus")
+  end
+
+  it 'clicking submit on an empty form' do
+
+    visit '/applications/new'
+
+    click_button('Submit')
+
+    expect(page).to have_content("*Fields can not be empty*")
+    save_and_open_page
+    expect(current_path).to eq('/applications')
   end
 end
