fff5c2e649353305d3cb673cbbb8d433f8eadd98
Shirley DeCesari <shirley.decesari@gmail.com>
2022-07-14 22:05:28 -0700

    Test -Add test to create new application

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'application creation' do 
-  
+   it "creates a new application" do 
+      visit '/applications/new'
+
+      fill_in 'Name', with: 'Shirley DeCesari'
+      fill_in 'street_address', with: '2482 Magic Flower Ave.'
+      fill_in 'city', with: 'Reno'
+      fill_in 'state', with: 'Nevada'
+      fill_in 'zip_code', with: '89231'
+
+      click_button 'Submit'
+
+      epxect(current_path).to eq('/applications/new')
+      expect(page).to have_content('Shirley Decesari')
+      expect(page).to have_button('Submit')
+   end
 end
