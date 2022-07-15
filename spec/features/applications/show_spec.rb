695353fb7e39364fab11d641fba546b4c8796c57
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-15 19:06:17 -0400

    add applications show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -30,7 +30,23 @@ RSpec.describe 'Show application', type: :feature do
     expect(page).to have_no_content("MA")
     expect(page).to have_no_content(10002)
     expect(page).to have_no_content("I love cats")
+    end
   end
-end
+
+  it 'clicks an application pets and it opens that pets show page' do
+    dog_homes = Shelter.create!(id: 1, name: 'Dog Homes', city: 'Miami', rank: 1, foster_program: true)
+    application_1 = Application.create!(id: 1, name: 'John Doe', street_address: "123 Main St", city: "New York", state: "NY", zipcode: 10001, description: "I love dogs", status: "pending")
+    application_2 = Application.create!(id: 2, name: 'Jane Doe', street_address: "456 Main St", city: "Boston", state: "MA", zipcode: 10002, description: "I love cats", status: "pending")
+    roofus = Pet.create!(id: 1, name: 'Roofus', age: 2, breed: 'pit bull', adoptable: true, shelter_id: 1)
+    bowow = Pet.create!(id: 2, name: 'Bowow', age: 3, breed: 'labrador', adoptable: true, shelter_id: 1)
+    pet_application_1 = PetApplication.create!(id: 1, application_id: 1, pet_id: 1)
+    pet_application_2 = PetApplication.create!(id: 2, application_id: 1, pet_id: 2)
+
+    visit "applications/#{application_1.id}"
+
+    click_link "Roofus"
+
+    expect(current_path).to eq("/pets/1")
+  end
 
 end
