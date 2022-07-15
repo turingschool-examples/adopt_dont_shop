336e58d77785e20b272cdf4d0f755aa7482fdc6a
Eli Sachs <easachs13@gmail.com>
2022-07-15 12:53:05 -0600

    Add Pet search spec

--- spec/features/apps/show_spec.rb
+++ spec/features/apps/show_spec.rb
@@ -39,22 +39,11 @@ RSpec.describe 'app show' do
     expect(page).to have_link("Veterinarians")
     expect(page).to have_link("Veterinary Offices")
   end
-
-  # Application Show Page
-
-  # As a visitor
-  # When I visit an applications show page
-  # Then I can see the following:
-  # - Name of the Applicant
-  # - Full Address of the Applicant including street address, city, state, and zip code
-  # - Description of why the applicant says they'd be a good home for this pet(s)
-  # - names of all pets that this application is for (all names of pets should be links to their show page)
-  # - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
-
+  
   it 'displays a link to all veterinarians' do
     app_1 = App.create!(name: "Bob", address: "2020 Maple Lane", city: "Denver", state: "CO", zip: "80202", description: "ABC", status: "in progress")
     visit "/apps/#{app_1.id}"
-
+    
     expect(page).to have_link("Veterinarians")
     click_link("Veterinarians")
     expect(page).to have_current_path('/veterinarians')
@@ -63,7 +52,18 @@ RSpec.describe 'app show' do
     expect(page).to have_link("Veterinarians")
     expect(page).to have_link("Veterinary Offices")
   end
-
+  
+    # Application Show Page
+  
+    # As a visitor
+    # When I visit an applications show page
+    # Then I can see the following:
+    # - Name of the Applicant
+    # - Full Address of the Applicant including street address, city, state, and zip code
+    # - Description of why the applicant says they'd be a good home for this pet(s)
+    # - names of all pets that this application is for (all names of pets should be links to their show page)
+    # - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
+  
   it 'displays app attributes' do
     app_1 = App.create!(name: "Bob", address: "2020 Maple Lane", city: "Denver", state: "CO", zip: "80202", description: "ABC", status: "in progress")
     app_2 = App.create!(name: "John", address: "2021", city: "San Fran", state: "CA", zip: "90909", description: "XYZ", status: "in progress")
@@ -95,5 +95,43 @@ RSpec.describe 'app show' do
     expect(page).to_not have_content(pet_3.name)
     click_link "#{pet_1.name}"
     expect(current_path).to eq("/pets/#{pet_1.id}")
+  end
+
+  # Search for Pets for an Application
+
+  # As a visitor
+  # When I visit an application's show page
+  # And that application has not been submitted,
+  # Then I see a section on the page to "Add a Pet to this Application"
+  # In that section I see an input where I can search for Pets by name
+  # When I fill in this field with a Pet's name
+  # And I click submit,
+  # Then I am taken back to the application show page
+  # And under the search bar I see any Pet whose name matches my search
+
+  it 'can search for pets on in progress application' do
+    app_1 = App.create!(name: "Bob", address: "2020 Maple Lane", city: "Denver", state: "CO", zip: "80202", description: "ABC", status: "in progress")
+    shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+
+    visit "/apps/#{app_1.id}"
+
+    expect(page).to have_content('Search')
+    fill_in 'Search', with: 'Lobster'
+    click_button 'Submit'
+    expect(page).to have_current_path("/apps/#{app_1.id}?search=Lobster")
+    expect(page).to have_content('Lobster')
+  end
+
+  it 'doesnt search if app status not in progress' do
+    app_1 = App.create!(name: "Bob", address: "2020 Maple Lane", city: "Denver", state: "CO", zip: "80202", description: "ABC", status: "pending")
+    shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+
+    visit "/apps/#{app_1.id}"
+
+    expect(page).to_not have_content('Search')
   end
 end
