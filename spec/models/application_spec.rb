27b2715a024b1fa9b2989dcea2323359f5a10212
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-15 18:29:14 -0700

    completed Starting an Application, Form not Completed

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Application, type: :model do
     it { should have_many(:pet_applications) }
     it { should have_many(:pets).through(:pet_applications) }
   end
+
+  describe 'application requirements' do
+    @application_1 = Application.create!(name: 'John', address: '123 Main Street', city: 'Nashville', state: 'TN', zip: 37067, description: "I want a nice dog.", status: "In Progress")
+    
+    it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_presence_of(:address)}
+    it { is_expected.to validate_presence_of(:state)}
+    it { is_expected.to validate_presence_of(:city)}
+    it { is_expected.to validate_presence_of(:zip)}
+  end
 end
