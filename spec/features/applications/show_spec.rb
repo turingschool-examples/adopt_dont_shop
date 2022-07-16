a29ab7c1dbcfa0dfe159e20135b68c8e355cfd39
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-07-16 13:53:18 -0500

    add submission of application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -97,4 +97,34 @@ RSpec.describe 'Applications Show Page' do
       expect(page).to have_content('Clawdia')
     end
   end
+
+  it 'allows submission of the application if it has one or more pets' do 
+    app = Application.create!(name: 'Brigitte Bardot', street_address: '123 Main Street', city: 'Denver', state: 'CO', zip_code: '80111', status: 0)
+
+    shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+    shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    pirate = shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    clawdia = shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    lucy = shelter_3.pets.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    ApplicationPet.create!(application: app, pet: pirate)
+    ApplicationPet.create!(application: app, pet: lucy)
+
+    visit "/applications/#{app.id}"
+    # save_and_open_page
+
+    expect(page).to have_content 'Finalize and Submit Your Application'
+    expect(page).to have_content 'Please tell us why you would make a good owner for the pets.'
+    expect(page).to have_button 'Submit Application' 
+
+    fill_in :description, with: 'I love animals!' 
+    click_button 'Submit Application' 
+    save_and_open_page
+
+    expect(current_path).to eq("/applications/#{app.id}")
+    expect(page).to have_content 'Pending' 
+    expect(page).to have_content 'Mr. Pirate' 
+    expect(page).to have_content 'Lucille Bald' 
+    expect(page).to_not have_content 'Add a Pet to this Application' 
+  end
 end
