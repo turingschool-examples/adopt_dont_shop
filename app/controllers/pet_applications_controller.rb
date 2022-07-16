1c5f2309dc44d8f928bb5e34533250a9d18db396
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-16 18:22:53 -0400

    add new pet applications controller 1. Add action to create pet application that receives params from views/applications/show.html.erb in div_id = pet_found 2. Add private params to receive pet_id and application_id 3. Add redirect to applications after adding

---
+++ app/controllers/pet_applications_controller.rb
@@ -0,0 +1,16 @@
+class PetApplicationsController < ApplicationController
+
+  def create
+    PetApplication.create!(pet_application_params)
+    redirect_to "/applications/#{params[:application_id]}"
+  end
+
+private
+  def pet_application_params
+    params.permit(
+      :pet_id,
+      :application_id
+    )
+  end
+
+end
