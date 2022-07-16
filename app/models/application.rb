853e270f0f69e92b14b8e903ef426df13210076f
Sunny Moore <moore.sunny@gmail.com>
2022-07-16 13:48:23 -0700

    test you can start an application. create application#new and #create

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,9 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications  
 
+  def status
+    # logic that searches all pets on this application, 
+    # And returns a status based on those pets status's
+    return 'In Progress'
+  end
 end
