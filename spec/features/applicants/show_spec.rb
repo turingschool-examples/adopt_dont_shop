53f90fcc693b9e3e8498719c1e5275a1014a9fca
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-07-15 20:47:34 -0600

    commit push

--- spec/features/applicants/show_spec.rb
+++ spec/features/applicants/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'applicant show page' do
     shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-    bob_1 = Applicant.create(name: "Billy Bob", address: "Street address 6093", description: "I'm bob", status: "Pending", zip: 22323, city: "denver", state: "CO")
+    bob_1 = Applicant.create(name: "Billy Bob", address: "Street address 6093", description: "I'm bob", status: "In Progress", zip: 22323, city: "denver", state: "CO")
     pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
     pet_2 = shelter_2.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
     pet_3 = shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
@@ -26,5 +26,9 @@ RSpec.describe 'applicant show page' do
     click_link('Mr. Pirate')
 
     expect(current_path).to eq("/pets/#{pet_1.id}")
+  end
+
+  it 'user story 4' do
+
   end
 end
