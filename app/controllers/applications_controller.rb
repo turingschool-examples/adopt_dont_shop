cc25249c25d099e012775edc4b4ab1dab2e2fe92
Tyler Caudill <teecaud@hotmail.com>
2022-07-15 19:08:33 -0500

    feat us_25

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,28 @@
 class ApplicationsController < ApplicationController
+
+  def index
+    @applications = Application.all
+  end
+
   def show
     @application = Application.find(params[:id])
     @pets = @application.pets
+  end
+
+  def new
+    @application = Application.new
+  end
+
+  def create
+    # binding.pry
+    @application = Application.create(application_params)
+    redirect_to "/applications/#{@application.id}"
+  end
+
+
+  private
+
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description)
   end
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -7,6 +7,4 @@ class Application < ApplicationRecord
   validates :state, length: { maximum: 2 }
   validates :zip_code, numericality: true, length: { maximum: 5 }
   validates :status, inclusion: ["In Progress", "Pending", "Accepted", "Rejected"]
-
-  
 end
