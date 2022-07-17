6e076accb0c1d84bee34e69c2d96f05bea0d1d7f
Eli Sachs <easachs13@gmail.com>
2022-07-17 17:09:59 -0600

    Clean up controllers

--- app/controllers/admin/apps_controller.rb
+++ app/controllers/admin/apps_controller.rb
@@ -2,17 +2,7 @@ class Admin::AppsController < ApplicationController
 
   def show
     @app = App.find(params[:id])
-    if params[:search].present?
-      @pets = Pet.search(params[:search])
-    elsif app_params[:status] == "in progress"
-      @pets = Pet.adoptable
-    elsif app_params[:status] == "pending"
-      @pets = @app.pets
-    end
+    @pets = @app.pets
   end
 
-  private
-  def app_params
-    params.permit(:id, :name, :address, :city, :state, :zip, :description, :status)
-  end
 end
