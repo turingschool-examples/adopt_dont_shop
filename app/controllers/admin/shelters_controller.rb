f36ed062fc79521c6aa94fd7b2fd8404522fa4fd
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-17 16:01:12 -0400

    1. Add tests to spec/features/admin/shelters/index_spec.rb for reverse alphabetical ordering of shelters on admin shelters page 2. Remove reference to Shelter in class method self.order_by_reverse_alphabetical_by_name 3. Reformat index.html in admin/shelter view

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,5 +1,5 @@
 class Admin::SheltersController < ApplicationController
   def index
-    @shelters = Shelter.all
+    @shelters = Shelter.order_by_reverse_alphabetical_by_name
   end
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -9,15 +9,17 @@ class Shelter < ApplicationRecord
     order(created_at: :desc)
   end
 
-  def self.order_by_reverse_alphabetical_by_name
-    select("shelters.*").order("shelters.name DESC")
-  end
-
   def self.order_by_number_of_pets
     select("shelters.*, count(pets.id) AS pets_count")
       .joins("LEFT OUTER JOIN pets ON pets.shelter_id = shelters.id")
       .group("shelters.id")
       .order("pets_count DESC")
+  end
+
+  def self.order_by_reverse_alphabetical_by_name
+    find_by_sql("
+      SELECT * FROM shelters 
+      ORDER BY shelters.name DESC;")
   end
 
   def pet_count
