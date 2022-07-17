02d0195fcce076bb88c39bad4b82c6df1d4ac9aa
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-07-16 20:53:33 -0700

    cleaned up applicant show_spec file

--- spec/features/applicant/show_spec.rb
+++ spec/features/applicant/show_spec.rb
@@ -46,16 +46,6 @@ RSpec.describe 'the applicant show page', type: :feature do
     expect(current_path).to eq("/pets/#{pet.id}")
   end
 
-# As a visitor
-# When I visit an application's show page
-# And that application has not been submitted,
-# Then I see a section on the page to "Add a Pet to this Application"
-# In that section I see an input where I can search for Pets by name
-# When I fill in this field with a Pet's name
-# And I click submit,
-# Then I am taken back to the application show page
-# And under the search bar I see any Pet whose name matches my search
-
   it 'US23: can search for a pet by name to add to an application' do 
     applicant = Applicant.create!(name: 'Oliver Smudger', 
                                   street_address: '1234 N Random Avenue', 
