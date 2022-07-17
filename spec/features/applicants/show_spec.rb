0206710dd21fce53d2c71396fd5e0117747219db
Paker Thomson <pdthomson1239@gmail.com>
2022-07-17 12:49:03 -0400

    Added test and functionality for user story 4 creating a pet search bar

--- spec/features/applicants/show_spec.rb
+++ spec/features/applicants/show_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'the applicant show page' do
-  # As a visitor
-  # When I visit an applications show page
-  # Then I can see the following:
-  # - Name of the Applicant
-  # - Full Address of the Applicant including street address, city, state, and zip code
-  # - Description of why the applicant says they'd be a good home for this pet(s)
-  # - names of all pets that this application is for (all names of pets should be links to their show page)
-  # - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
   it "shows the applicant and the completed applicants form" do
     Applicant.destroy_all
     Pet.destroy_all
@@ -41,9 +33,38 @@ RSpec.describe 'the applicant show page' do
     within "#pet-#{tistermickles.id}" do
       click_on "#{tistermickles.name}"
     end
-    
+
     expect(current_path).to eq("/pets/#{tistermickles.id}")
     expect(page).to have_link("TisterMickles")
     expect(page).to have_content("otter")
+  end
+
+  it "can search a pets name and return the results" do
+    # As a visitor
+    # When I visit an application's show page
+    # And that application has not been submitted,
+    # Then I see a section on the page to "Add a Pet to this Application"
+    # In that section I see an input where I can search for Pets by name
+    # When I fill in this field with a Pet's name
+    # And I click submit,
+    # Then I am taken back to the application show page
+    # And under the search bar I see any Pet whose name matches my search
+    shelter = Shelter.create!(name: "Whacky Waldorf", city: "Waldorf", foster_program: false, rank: 2)
+    heeny = Pet.create!(name: 'Heeny', age: 2, breed:"wyvern", adoptable: true, shelter_id: shelter.id)
+    tistermickles = Pet.create!(name: 'TisterMickles', age:4, breed:'otter', adoptable: true, shelter_id: shelter.id)
+    applicant = Applicant.create!(name: "Beannah Durke", street_address: "129 Frog lane", city: "Cuntish", state: "Colorado", zip_code: "21401", description: "I need to stomp the frogs", application_status: "In Progress")
+    application = PetApplication.create!(applicant: applicant, pet: heeny)
+
+    visit "/applicants/#{applicant.id}"
+    # save_and_open_page
+    expect(page).to have_content("Add a Pet to this application")
+    expect(page).to have_button("Find by pet name")
+    fill_in("Search", with: 'TisterMickles')
+    click_on("Find by pet name")
+    # save_and_open_page
+    # require "pry"; binding.pry
+    expect(current_path).to eq("/applicants/#{applicant.id}")
+    expect(page).to have_content("TisterMickles")
+
   end
 end
