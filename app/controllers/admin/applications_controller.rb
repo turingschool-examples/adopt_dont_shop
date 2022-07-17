a4a3e47d2fafad5076571f3a2bba498def547442
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-17 11:58:44 -0600

    feat: change pet adoptable status to false when application approved

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -8,11 +8,18 @@ class Admin::ApplicationsController < ApplicationController
     pet_app = PetApplication.where(application_id: params[:application_id], pet_id: params[:pet_id])
     pet_app.update({app_status: params[:status]})
     application = Application.find(params[:application_id])
+    pets = application.pets
     pet_app_count = PetApplication.where(application_id: params[:application_id]).count
     pet_app_approved_count = PetApplication.where("application_id = ? AND app_status = ?", params[:application_id], "Approved").count
-    pet_app_approved_count == pet_app_count ? application.update({status: "Approved"}) : nil 
+    pet_app_approved_count == pet_app_count ? application.update({status: "Approved"}) && update_pet_status(pets) : nil 
     pet_app_rejected_count = PetApplication.where("application_id = ? AND app_status = ?", params[:application_id], "Rejected").count
-    pet_app_rejected_count > 0 ? application.update({status: "Rejected"}) : nil 
+    pet_app_rejected_count > 0 ? application.update({status: "Rejected"}) : nil
     redirect_to "/admin/applications/#{params[:application_id]}"
+  end
+
+  def update_pet_status(pets)
+    pets.each do |pet|
+      pet.update({adoptable: false})
+    end
   end
 end
