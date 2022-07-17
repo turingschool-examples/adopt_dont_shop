12f275432458ebaab3e2e011051fe4888c56a73f
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-17 07:40:52 -0600

    feat: admin app show page reject button with test

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -40,4 +40,25 @@ RSpec.describe 'admin application index page' do
       expect(page).to have_content(lobster.name)
     end
   end
+
+  it 'has a reject button for every pet' do
+    aurora = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    application_1 = Application.create!(name: "Bob Bobbicus", street: "123 Main street", city: "Newtown", state: "CO", zipcode: 80009, status:"Pending", description:"I love dogs so much and have lots of food for them")
+    lucille = application_1.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: aurora.id)
+    lobster = application_1.pets.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: aurora.id)
+
+    visit "/admin/applications/#{application_1.id}"
+    
+    within ("#pet-#{lucille.id}") do
+      click_button('Reject Application')
+      expect(page).to have_content(lucille.name)
+      expect(page).to have_no_button('Reject Application')
+      expect(page).to have_content('Rejected')
+    end
+    
+    within ("#pet-#{lobster.id}") do
+      expect(page).to have_button('Reject Application')
+      expect(page).to have_content(lobster.name)
+    end
+  end
 end
