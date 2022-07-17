f17fea94623fd617669e1054e693af4aa4302043
Ben Silverstein <benjsilverstein@comcast.net>
2022-07-16 19:27:30 -0600

    Adjust test conditions

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -83,29 +83,6 @@ RSpec.describe 'application show page' do
     expect(page).to_not have_content('Vida')
   end
   it 'can search for pets for an application' do
-
-    # app_1 = Application.create!(
-    #           name: 'Mike Dao',
-    #           street_address: '245 Maple St',
-    #           city: 'Centennial',
-    #           state: 'Colorado',
-    #           zip_code: '80112',
-    #           applicant_bio: 'My dog needs another to chase up trees.',
-    #           application_status: 'Pending')
-    # app_2 = Application.create!(
-    #           name: 'Chris Simmons',
-    #           street_address: '533 Oak St',
-    #           city: 'Columbus',
-    #           state: 'Ohio',
-    #           zip_code: '43004',
-    #           applicant_bio: 'Because how much more work could a third cat be?',
-    #           application_status: 'In Progress')
-    #
-    # shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    # pet_1 = shelter.pets.create!(adoptable: true, age: 3, breed: 'GSD', name: 'Charlie')
-    # pet_2 = shelter.pets.create!(adoptable: true, age: 1, breed: 'ND', name: 'Mina')
-    # app_pet_1 = ApplicationPet.create!(application: app_1, pet: pet_1)
-    # app_pet_2 = ApplicationPet.create!(application: app_2, pet: pet_2)
     shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     mike = Application.create!(
               name: 'Mike Dao',
