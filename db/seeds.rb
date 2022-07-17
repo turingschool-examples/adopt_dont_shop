3b22549a7ae753f1217fc2286151ea90cef4e78c
Thomas Turner <thomasturner482@yahoo.com>
2022-07-17 15:56:43 -0500

    added pets and shelters to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,14 @@
 
 application = Application.create!(name: "Thomas Turner", address_street: "234 Sands St.", address_city: "Chicago", address_state: "Illinois", address_zip_code: 60007, description: "I love pets and have lots of space in the back yard", status: "pending")
 
-shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
 
-
-pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id, application_id: nil)
-pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id, application_id: nil)
-pet_3 = Pet.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id, application_id: application.id)
-pet_4 = Pet.create(adoptable: false, age: 3, breed: 'Whippet', name: 'Annabelle', shelter_id: shelter.id, application_id: application.id)
-pet_5 = Pet.create(adoptable: true, age: 3, breed: 'GSD', name: 'Charlie', shelter_id: shelter.id, application_id: application.id)
+shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+shelter_2.pets.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster')
+shelter_2.pets.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven')
+shelter_2.pets.create(adoptable: false, age: 3, breed: 'Whippet', name: 'Annabelle')
+shelter_2.pets.create(adoptable: true, age: 3, breed: 'GSD', name: 'Charlie')
