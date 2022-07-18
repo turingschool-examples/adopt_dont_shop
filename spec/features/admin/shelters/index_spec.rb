f0ab6091b09f804ff549f11333176e97308f34f5
Gauri Joshi <gauri728@gmail.com>
2022-07-18 12:57:11 -0700

    feature test for user to see shelters that have pending applications

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -22,6 +22,24 @@ RSpec.describe 'admin shelters index' do
       expect(@shelter_2.name).to appear_before(@shelter_1.name)
       expect(@shelter_3.name).to appear_before(@shelter_2.name)
    end
+
+   it "has a section for Shelter's with Pending Applications" do 
+      @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+      @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+
+      @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+      @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+      @pet_3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+      @pet_4 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
+
+      @applicant_1 = Applicant.create!(name: "Mary", address: "123 Main St", city: "Denver", state: "CO", zip: 22222, description: 'love pets!', application_status: 'Pending')
+      @applicant_pet = ApplicantPet.create!(pet: @pet_2, applicant: @applicant_1)
+      
+      visit '/admin/shelters'
+   
+      expect(page).to have_content("#{@shelter_1.name}")
+   end
 end
 
 
