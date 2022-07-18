0f79a6060ae4baa7b99f4f03372f0e9f574be3bb
Ben Silverstein <benjsilverstein@comcast.net>
2022-07-18 10:07:41 -0600

    Add adopt a pet test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -115,7 +115,7 @@ RSpec.describe 'application show page' do
     vida = Pet.create!(adoptable: true, age: 4, breed: 'Yorkshire', name: 'Vida', shelter_id: shelter.id)
 
    visit "/applications/#{mike.id}"
-   
+
    expect(page).to have_content("Search for pet by name")
 
    fill_in :search, with: "Charlie"
@@ -124,4 +124,96 @@ RSpec.describe 'application show page' do
    expect(current_path).to eq("/applications/#{mike.id}")
    expect(page).to have_content(charlie.name)
  end
+
+ it "Add a pet to an Application" do
+
+   shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+   mike = Application.create!(
+             name: 'Mike Dao',
+             street_address: '245 Maple St',
+             city: 'Centennial',
+             state: 'Colorado',
+             zip_code: '80112',
+             applicant_bio: 'My dog needs another to chase up trees.',
+             application_status: 'In Progress')
+   chris = Application.create!(
+             name: 'Chris Simmons',
+             street_address: '533 Oak St',
+             city: 'Columbus',
+             state: 'Ohio',
+             zip_code: '43004',
+             applicant_bio: 'Because how much more work could a third cat be?',
+             application_status: 'In Progress')
+   dani = Application.create!(
+             name: 'Dani Coleman',
+             street_address: '912 Willow St',
+             city: 'Arvada',
+             state: 'Colorado',
+             zip_code: '80003',
+             applicant_bio: 'Because I am just awesome.',
+             application_status: 'In Progress')
+   charlie = Pet.create!(adoptable: true, age: 3, breed: 'GSD', name: 'Charlie', shelter_id: shelter.id)
+   mina = Pet.create!(adoptable: true, age: 1, breed: 'ND', name: 'Mina', shelter_id: shelter.id)
+   chloe = Pet.create!(adoptable: true, age: 2, breed: 'Tabby', name: 'Chloe', shelter_id: shelter.id)
+   vida = Pet.create!(adoptable: true, age: 4, breed: 'Yorkshire', name: 'Vida', shelter_id: shelter.id)
+
+   visit "/applications/#{mike.id}"
+   fill_in :search, with: "Charlie"
+   click_on "Pet name search"
+   click_on "Adopt #{charlie.name}"
+
+   expect(current_path).to eq("/applications/#{mike.id}")
+   expect(page).to have_content(charlie.name)
+ end
+
+ xit "Submit an Application" do
+
+   shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+   mike = Application.create!(
+             name: 'Mike Dao',
+             street_address: '245 Maple St',
+             city: 'Centennial',
+             state: 'Colorado',
+             zip_code: '80112',
+             applicant_bio: 'My dog needs another to chase up trees.',
+             application_status: 'In Progress')
+   chris = Application.create!(
+             name: 'Chris Simmons',
+             street_address: '533 Oak St',
+             city: 'Columbus',
+             state: 'Ohio',
+             zip_code: '43004',
+             applicant_bio: 'Because how much more work could a third cat be?',
+             application_status: 'In Progress')
+   dani = Application.create!(
+             name: 'Dani Coleman',
+             street_address: '912 Willow St',
+             city: 'Arvada',
+             state: 'Colorado',
+             zip_code: '80003',
+             applicant_bio: 'Because I am just awesome.',
+             application_status: 'In Progress')
+   charlie = Pet.create!(adoptable: true, age: 3, breed: 'GSD', name: 'Charlie', shelter_id: shelter.id)
+   mina = Pet.create!(adoptable: true, age: 1, breed: 'ND', name: 'Mina', shelter_id: shelter.id)
+   chloe = Pet.create!(adoptable: true, age: 2, breed: 'Tabby', name: 'Chloe', shelter_id: shelter.id)
+   vida = Pet.create!(adoptable: true, age: 4, breed: 'Yorkshire', name: 'Vida', shelter_id: shelter.id)
+
+   app_pet_1 = ApplicationPet.create!(application: mike, pet: charlie)
+
+   visit "/applications/#{mike.id}"
+   fill_in :search, with: "Charlie"
+   click_on "Pet name search"
+   click_on "Adopt this pet"
+
+   expect(current_path).to eq("/applications/#{mike.id}")
+   expect(page).to have_content("Why I would be a good owner:")
+
+   fill_in :potential_owner_statement, with: "I like dogs a lot."
+   click_on "Submit Application"
+
+   expect(current_path).to eq("/applications/#{mike.id}")
+   expect(page).to have_content("Application Status: Pending")
+
+ end
+
 end
