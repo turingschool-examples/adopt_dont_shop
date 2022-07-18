b3d4330c3f8ef5d9d8d9f07a940fcc7ca4f07b52
Tyler Caudill <teecaud@hotmail.com>
2022-07-18 13:12:27 -0500

    feat US_21 Submit Application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -19,13 +19,21 @@ class ApplicationsController < ApplicationController
 
   def create
     application = Application.create(application_params)
-
     if application.save
       redirect_to "/applications/#{application.id}"
     else
       redirect_to "/applications/new"
       flash[:alert] = "Error: #{error_message(application.errors)}"
     end
+  end
+
+  def update
+    @application = Application.find(params[:id])
+    if @application.status == "In Progress"
+      @application.status = "Pending"
+      @application.save
+    end
+    redirect_to "/applications/#{@application.id}"
   end
 
   private
