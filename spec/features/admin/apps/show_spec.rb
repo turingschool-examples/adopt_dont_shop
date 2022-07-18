6359036309cc871de7c4acc51024f085d8ad5c18
Eli Sachs <easachs13@gmail.com>
2022-07-18 10:37:24 -0600

    One approval at a time spec

--- spec/features/admin/apps/show_spec.rb
+++ spec/features/admin/apps/show_spec.rb
@@ -86,8 +86,8 @@ RSpec.describe 'admin app show' do
     app_3 = App.create!(name: "Terry", address: "8080 Elm St", city: "Boulder", state: "CO", zip: "80222", description: "LOL", status: "pending")
     shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-    pet_3 = Pet.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
+    # pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+    # pet_3 = Pet.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
 
     PetApp.create!(pet: pet_1, app: app_1)
     PetApp.create!(pet: pet_1, app: app_2)
@@ -266,4 +266,54 @@ RSpec.describe 'admin app show' do
     visit "/pets/#{pet_2.id}"
     expect(page).to have_content("Adoptable: false")
   end
+  
+  it 'only lets pets have one approved application at a time' do
+    app_1 = App.create!(name: "Bob", address: "2020 Maple Lane", city: "Denver", state: "CO", zip: "80202", description: "ABC", status: "pending")
+    app_2 = App.create!(name: "John", address: "22 Dexter St", city: "Denver", state: "CO", zip: "80200", description: "123", status: "pending")
+    shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    
+    PetApp.create!(pet: pet_1, app: app_1)
+    PetApp.create!(pet: pet_1, app: app_2)
+    
+    visit "/admin/apps/#{app_1.id}"
+    expect(page).to have_content('Status: pending')
+    
+    visit "/admin/apps/#{app_2.id}"
+    expect(page).to have_content('Status: pending')
+    
+    within "#pet_#{pet_1.id}" do
+      click_button 'Approve'
+    end
+    
+    expect(current_path).to eq("/admin/apps/#{app_2.id}")
+    expect(page).to have_content('Status: accepted')
+    
+    visit "/admin/apps/#{app_1.id}"
+    
+    expect(current_path).to eq("/admin/apps/#{app_1.id}")
+    expect(page).to have_content('Status: pending')
+    
+    within "#pet_#{pet_1.id}" do
+      expect(page).to have_content("Already approved")
+      expect(page).to_not have_button("Approve")
+      expect(page).to have_button("Reject")
+      click_button 'Reject'
+    end
+    
+    expect(current_path).to eq("/admin/apps/#{app_1.id}")
+    expect(page).to_not have_button("Approve")
+    expect(page).to_not have_button("Reject")
+    expect(page).to have_content("Rejected")
+  end
 end
+
+# Pets can only have one approved application on them at any time
+
+# As a visitor
+# When a pet has an "Approved" application on them
+# And when the pet has a "Pending" application on them
+# And I visit the admin application show page for the pending application
+# Then next to the pet I do not see a button to approve them
+# And instead I see a message that this pet has been approved for adoption
+# And I do see a button to reject them
