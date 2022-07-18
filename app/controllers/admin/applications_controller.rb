6d9679b4516cdf8aa2677907015c9a80e2931313
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-18 15:14:45 -0600

    ref: update applications controller update method and pet application model methods with tests

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -5,21 +5,19 @@ class Admin::ApplicationsController < ApplicationController
   end
 
   def update
-    pet_app = PetApplication.where(application_id: params[:application_id], pet_id: params[:pet_id])
+    pet_app = PetApplication.find_pet_app(params[:pet_id], params[:application_id])
     pet_app.update({app_status: params[:status]})
     application = Application.find(params[:application_id])
     pets = application.pets
-    pet_app_count = PetApplication.where(application_id: params[:application_id]).count
-    pet_app_approved_count = PetApplication.where("application_id = ? AND app_status = ?", params[:application_id], "Approved").count
+    pet_app_count = PetApplication.pet_app_count(params[:application_id])
+    pet_app_approved_count = PetApplication.pet_app_approved_count(params[:application_id])
     pet_app_approved_count == pet_app_count ? application.update({status: "Approved"}) && update_pet_status(pets) : nil 
-    pet_app_rejected_count = PetApplication.where("application_id = ? AND app_status = ?", params[:application_id], "Rejected").count
+    pet_app_rejected_count = PetApplication.pet_app_rejected_count(params[:application_id])
     pet_app_rejected_count > 0 ? application.update({status: "Rejected"}) : nil
     redirect_to "/admin/applications/#{params[:application_id]}"
   end
 
   def update_pet_status(pets)
-    pets.each do |pet|
-      pet.update({adoptable: false})
-    end
+    pets.each { |pet| pet.update({adoptable: false}) }
   end
 end
--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,20 @@
 class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  def self.find_pet_app(pet_id, app_id)
+    where(application_id: app_id, pet_id: pet_id)[0]
+  end 
+
+  def self.pet_app_count(app_id)
+    where(application_id: app_id).count
+  end 
+
+  def self.pet_app_approved_count(app_id)
+    where("application_id = ? AND app_status = ?", app_id, "Approved").count
+  end 
+
+  def self.pet_app_rejected_count(app_id)
+    where("application_id = ? AND app_status = ?", app_id, "Rejected").count
+  end 
 end
