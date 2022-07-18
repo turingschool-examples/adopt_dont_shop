1312e408b51c1dcb018eaabbfa84ee6a36f9b1fc
Gauri Joshi <gauri728@gmail.com>
2022-07-18 16:04:30 -0700

    comments out unused validations again

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -7,7 +7,7 @@ class Applicant < ApplicationRecord
     validates :zip, presence: true, numericality: true
     # validates :description, presence: true
     # validates :names_pets_wanted, presence: true
-    validates :application_status, presence: true
+    # validates :application_status, presence: true
 
     has_many :applicant_pets
     has_many :pets, through: :applicant_pets
