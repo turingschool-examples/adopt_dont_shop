c2be908422cddca17e4db7a33277feb84cbb99b5
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-19 10:16:29 -0400

    add model methods to accept and reject applications based on criteria add to admin applications controller show method a set of conditionals that refer to model methods for the application to change the applications status based on user actions

--- app/models/application.rb
+++ app/models/application.rb
@@ -10,4 +10,23 @@ class Application < ApplicationRecord
   validates :state, presence: true
   validates :zipcode, presence: true
 
+  def pending_pet_applications?
+    pet_applications.where(status: "Pending").any?
+  end
+
+  def any_rejected_pet_applications?
+    pet_applications.where(status: "Rejected").any?
+  end
+
+  def all_accepted_pet_applications?
+    pet_applications.where(status: "Approved").all?
+  end
+
+  def reject_application
+    update!(app_status: "Rejected")
+  end
+
+  def approve_application
+    update!(app_status: "Approved")
+  end
 end
