29ae6730af39024451cfb6da56836a1b8ad4acbb
Michael Koulouvaris <mikekoul@gmail.com>
2022-07-19 16:12:46 -0600

    feat: Add link to review application next to every pet with pending applications

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -50,7 +50,7 @@ RSpec.describe 'admin shelter show page' do
         expect(page).to have_content("Adopted pets: 2")
     end 
 
-    it 'in the stats section, I see the number of pets that have been adopted' do
+    it 'action required section lists all pets with pending applications' do
         mystery = Shelter.create!(name: 'Mystery Building', address: "323 Main St", city: 'Irvine, CA', zip: 76543, foster_program: false, rank: 9)
         application_1 = Application.create!(name: "Bob Bobbicus", street: "123 Main street", city: "Newtown", state: "CO", zipcode: 80009, status:"Pending", description:"I love dogs so much and have lots of food for them")
         application_2 = Application.create!(name: "Cindy Smith", street: "727 Lane road", city: "Rockville", state: "CA", zipcode: 95148, status:"Pending", description:"I have a big yard them to run all day")
@@ -65,5 +65,25 @@ RSpec.describe 'admin shelter show page' do
         expect(page).to have_content("Lucille")
         expect(page).to have_content("Lobster")
         expect(page).to_not have_content("Charlie")
+    end 
+
+    it 'each pet in the action required section has link to the admin application page ' do
+        mystery = Shelter.create!(name: 'Mystery Building', address: "323 Main St", city: 'Irvine, CA', zip: 76543, foster_program: false, rank: 9)
+        application_1 = Application.create!(name: "Bob Bobbicus", street: "123 Main street", city: "Newtown", state: "CO", zipcode: 80009, status:"Pending", description:"I love dogs so much and have lots of food for them")
+        application_2 = Application.create!(name: "Cindy Smith", street: "727 Lane road", city: "Rockville", state: "CA", zipcode: 95148, status:"Pending", description:"I have a big yard them to run all day")
+        application_3 = Application.create!(name: "Ryan Scott", street: "21 Longwalk street", city: "Denver", state: "CO", zipcode: 34687, status:"Approved", description:"I have lots of time to play fetch")
+        lucille = application_1.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: mystery.id)
+        lobster = application_2.pets.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: mystery.id)
+        
+        visit "/admin/shelters/#{mystery.id}"
+
+        within "#pets-#{lucille.id}" do
+            expect(page).to have_link("Review Application")
+            expect(page).to have_content("Lucille")
+
+            click_link("Review Application")
+
+            expect(current_path).to eq("/admin/applications/#{application_1.id}")
+        end
     end 
 end 
