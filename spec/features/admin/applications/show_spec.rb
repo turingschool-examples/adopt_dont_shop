63a5075b31b931ecde8300ac6c0c29874803e884
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-18 21:37:24 -0400

    add test for buttons and adding of pets

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe 'Admin/applications#show' do
     pet_1 = Pet.create!(id: 1, name: "Fido", breed: "Poodle", age: 2, adoptable: true, shelter_id: shelter_1.id)
     pet_2 = Pet.create!(id: 2, name: "Buddy", breed: "Poodle", age: 3, adoptable: true, shelter_id: shelter_2.id)
     pet_3 = Pet.create!(id: 3, name: "Bella", breed: "Labrador", age: 4, adoptable: true, shelter_id: shelter_3.id)
+    pet_4 = Pet.create!(id: 4, name: "Bruce", breed: "Pug", age: 4, adoptable: true, shelter_id: shelter_3.id)
 
     pet_application_1 = PetApplication.create!(application_id: application_1.id, pet_id: pet_1.id)
     pet_application_2 = PetApplication.create!(application_id: application_1.id, pet_id: pet_2.id)
@@ -24,24 +25,26 @@ RSpec.describe 'Admin/applications#show' do
     visit '/admin/applications/1'
 
     expect(page).to have_content("John Doe")
-    expect(page).to have_content("123 Main St")
-    expect(page).to have_content("New York")
-    expect(page).to have_content("NY")
-    expect(page).to have_content("20001")
-    expect(page).to have_content("Fido")
-    expect(page).to have_content("Poodle")
-    expect(page).to have_content("2")
-    expect(page).to have_content("Buddy")
-    expect(page).to have_content("Poodle")
-    expect(page).to have_content("3")
-    expect(page).to have_content("Bella")
-    expect(page).to have_content("Labrador")
-    expect(page).to have_content("4")
+    expect(page).to have_no_content("Jane Doe")
 
-    expect(page).to_not have_content("Jane Doe")
-    expect(page).to_not have_content("456 Main St")
-    expect(page).to_not have_content("Boston")
-    expect(page).to_not have_content("MA")
-    expect(page).to_not have_content("30002")
+    within("#pet-#{pet_application_1.id}") do
+      expect(page).to have_content("Fido")
+      expect(page).to have_button("Approve")
+      expect(page).to have_button("Reject")
+    end
+
+    within("#pet-#{pet_application_2.id}") do
+      expect(page).to have_content("Buddy")
+      expect(page).to have_button("Approve")
+      expect(page).to have_button("Reject")
+    end
+
+    within("#pet-#{pet_application_3.id}") do
+      expect(page).to have_content("Bella")
+      expect(page).to have_button("Approve")
+      expect(page).to have_button("Reject")
+    end
+
+    expect(page).to_not have_content("Bruce")
   end
 end
