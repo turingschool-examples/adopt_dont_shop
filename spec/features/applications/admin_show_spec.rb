4eddf596bc4efa656d2b28daf585bf18663b5a1b
Tyler Caudill <teecaud@hotmail.com>
2022-07-20 16:21:44 -0500

    feat partials

--- spec/features/applications/admin_show_spec.rb
+++ spec/features/applications/admin_show_spec.rb
@@ -40,9 +40,7 @@ RSpec.describe 'admin applications show page' do
     banfield = Shelter.create!(foster_program: true, name: "Banfield Animal Hospital", city: "Orlando", rank: 9, street_address:"923 Oscar Meyer Lane", state: "FL", zip_code: 65425)
     spot = Pet.create!(adoptable: true, age: 2, breed: "Dalmatian", name: "Spot", shelter_id: northshore.id)
     fRENCHToasT = Pet.create!(adoptable: true, age: 4, breed: "French toast", name: "FReNCH ToAsT", shelter_id: banfield.id)
-    # spot_application = PetApplication.create!(application_id: jerry_application.id, pet_id: spot.id, status: jerry_application.status)
     fRENCHToasT_application = PetApplication.create!(application_id: jerry_application.id, pet_id: fRENCHToasT.id, status: jerry_application.status)
-    # spot_application1 = PetApplication.create!(application_id: tony_application.id, pet_id: spot.id, status: tony_application.status)
     fRENCHToasT_application1 = PetApplication.create!(application_id: tony_application.id, pet_id: fRENCHToasT.id, status: tony_application.status)
 
     visit "/admin/applications/#{jerry_application.id}"
@@ -60,6 +58,28 @@ RSpec.describe 'admin applications show page' do
     expect(page).to have_button("Approve")
     click_button("Approve")
     expect(page).to_not have_button("Approve")
+  end
+end
+
+  it 'approves the application when all pets are approved' do
+    application = Application.create!(name: "Jerry Rice", street_address: "123 Main Street", city: "Honolulu", state: "HI", zip_code: 12345, description: "We love doggos!")
+    northshore = Shelter.create!(foster_program: true, name: "North Shore Animal Hospital", city: "Long Island", rank: 3, street_address: "456 Shore Drive", state: "NY", zip_code: 15151)
+    frenchie = Pet.create!(adoptable: true, age: 1, breed: "French Bulldog", name: "Frenchie", shelter_id: northshore.id)
+    spot = Pet.create!(adoptable: true, age: 2, breed: "Dalmatian", name: "Spot", shelter_id: northshore.id)
+    frenchie_application = PetApplication.create!(application_id: application.id, pet_id: frenchie.id, status: application.status)
+    spot_application = PetApplication.create!(application_id: application.id, pet_id: spot.id, status: application.status)
+
+    visit "/admin/applications/#{application.id}"
+
+    within '#pet-0' do
+      expect(page).to have_button("Reject")
+      expect(page).to have_button("Approve")
+      click_button("Approve")
+    end
+    within '#pet-1' do
+      expect(page).to have_button("Reject")
+      expect(page).to have_button("Approve")
+      click_button("Approve")
     end
   end
 end
