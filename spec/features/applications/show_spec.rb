8207a69e2ec941aeb8453541601cef2f6b5d07e5
Carter Ball <carter.k.ball@gmail.com>
2022-09-01 17:43:29 -0400

    Create application show spec and basic view

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'applications show page' do
+RSpec.describe 'the application show' do
+  it "shows the application and all its attributes" do
+    application = Application.create!(name: "Carter Ball", street_address: "123 Easy Street", city: "Atlanta", state: "GA", zip_code: 30307, status: "In Progress")
 
+    # pet = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+
+    visit "/applications/#{application.id}"
+
+    expect(page).to have_content(application.name)
+    expect(page).to have_content(application.street_address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zip_code)
+    expect(page).to have_content(application.status)
+  end
 end
