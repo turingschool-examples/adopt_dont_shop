09ac767d0b391c2b4b29560c01e7547fae6c377b
Thomas Musselman <tmusselman@gmail.com>
2022-09-01 17:14:08 -0600

    fix applicaiton show and new to better meet specs

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,6 +2,7 @@ class ApplicationsController < ApplicationController
 
   def show
       @application = Application.find(params[:id])
+      @pets = @application.pets
   end
   
   def new
@@ -9,15 +10,14 @@ class ApplicationsController < ApplicationController
 
   def create
     new_app = Application.create(application_params)
-    new_app.status = 'In Progress'
-    new_app.save
+    new_app.update(status: "In Progress")
     redirect_to "/applications/#{new_app.id}"
   end
 
   private
 
   def application_params
-    params.permit(:name, :street_address, :city, :state, :zip_code, :description)
+    params.permit(:name, :street_address, :city, :state, :zip_code)
   end
 
 end
