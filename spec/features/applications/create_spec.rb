b5128632b803e878ec8decc6e7a40f7142df9a8f
Carter Ball <carter.k.ball@gmail.com>
2022-09-02 01:09:15 -0400

    Create new application form

---
+++ spec/features/applications/create_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'the application creation' do
+
+  describe 'the pet new' do
+    it 'renders the new form' do
+      visit "/applications/new"
+
+      expect(page).to have_content('New Application')
+      expect(find('form')).to have_content('Name')
+      expect(find('form')).to have_content('Address')
+      expect(find('form')).to have_content('City')
+      expect(find('form')).to have_content('State')
+      expect(find('form')).to have_content('Zipcode')
+
+    end
+  end
+
+
+
+
+end
