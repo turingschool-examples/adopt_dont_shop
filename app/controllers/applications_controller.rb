dde7fc8c684b38bb5ba2b035cadb1e397b843320
Sid Mann <sjmann2@outlook.com>
2022-09-01 21:27:01 -0500

    Add new and create action and routes

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,7 +4,19 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
+  def new
+    @application = Application.new
+  end
 
+  def create
+    application = Application.create!(applications_params)
+    application.status = "In Progress"
+    application.save
+    redirect_to "/applications/#{application.id}"
+  end
 
-
+private
+  def applications_params
+    params.permit(:name, :street_address, :city, :state, :zipcode, :status, :description)
+  end
 end
