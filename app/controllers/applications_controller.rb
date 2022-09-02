8cf5bf271f01b533a1192febc702a3fce38d5018
Jake Kim <imjakekim@gmail.com>
2022-09-02 13:00:29 -0700

    finished pet search for application show page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,6 +3,7 @@ class ApplicationsController < ApplicationController
   def show
       @application = Application.find(params[:id])
       @pets = @application.pets
+      @search_results = Pet.search(params[:search])
   end
   
   def new
