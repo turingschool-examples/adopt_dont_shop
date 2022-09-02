9a93856fc4441e55dbb80a31af785259b2bc7daa
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-01 19:37:43 -0600

    Add tests and passing for link to pets

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-# - names of all pets that this application is for (all names of pets should be links to their show page)
-
 RSpec.describe 'application show page', type: :feature do
   describe 'As a visitor' do
     before :each do
       @shelter = Shelter.create!(name: "Happy Paws Shelter", foster_program: true, city: "Denver", rank: 5)
-      @pet = @shelter.pets.create!(name: "Fluffy", adoptable: true, age: 3, breed: "doberman")
+      @pet1 = @shelter.pets.create!(name: "Fluffy", adoptable: true, age: 3, breed: "doberman")
+      @pet2 = @shelter.pets.create!(name: "Floofy", adoptable: true, age: 7, breed: "mixed breed")
       @application = Application.create!(first_name: "Samantha", last_name: "Smith", street_address: "123 Mulberry Street", city: "Denver", state: "CO", zip_code: 20202, description: "I would like this dog for these reasons.", status: "In Progress")
-      @pet_application1 = PetApplication.create!(pet: @pet, application: @application)
+      @pet_application1 = PetApplication.create!(pet: @pet1, application: @application)
+      @pet_application2 = PetApplication.create!(pet: @pet2, application: @application)
+
     end
     it 'I can see all attributes of the application' do
       visit "/application/#{@application.id}"
 
-      save_and_open_page
       expect(page).to have_content('Samantha')
       expect(page).to have_content('Smith')
       expect(page).to have_content('123 Mulberry Street')
@@ -23,6 +23,24 @@ RSpec.describe 'application show page', type: :feature do
       expect(page).to have_content('I would like this dog for these reasons.')
       expect(page).to have_content('In Progress')
       expect(page).to have_content('Fluffy')
+      expect(page).to have_content('Floofy')
+    end
+
+    it 'I has links for the first pet that leads to a show page' do
+      visit "/application/#{@application.id}"
+
+      expect(page).to have_content('Fluffy')
+      click_link('Fluffy')
+      expect(current_path).to eq("/pets/#{@pet1.id}")
+      #  (all names of pets should be links to their show page)
+    end
+
+    it 'I has links for the second pet that leads to that show page' do
+      visit "/application/#{@application.id}"
+
+      expect(page).to have_content('Floofy')
+      click_link('Floofy')
+      expect(current_path).to eq("/pets/#{@pet2.id}")
     end
   end
 end
