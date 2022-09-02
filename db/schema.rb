46569c07b14c47f00138706b59ee03c28e9a48b9
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-01 20:56:13 -0600

    Created many to many relationship between applicant and pet

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_221318) do
+ActiveRecord::Schema.define(version: 2022_09_02_023916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applicant_pets", force: :cascade do |t|
+    t.bigint "pet_id"
+    t.bigint "applicant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["applicant_id"], name: "index_applicant_pets_on_applicant_id"
+    t.index ["pet_id"], name: "index_applicant_pets_on_pet_id"
+  end
 
   create_table "applicants", force: :cascade do |t|
     t.string "first_name"
@@ -48,5 +57,7 @@ ActiveRecord::Schema.define(version: 2022_09_01_221318) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "applicant_pets", "applicants"
+  add_foreign_key "applicant_pets", "pets"
   add_foreign_key "pets", "shelters"
 end
