c096e389b0678243d9618940132bb61b592b497b
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-01 20:22:57 -0600

    add application attributes to show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/', to: 'application#welcome'
+  get '/', to: 'applications#welcome'
+
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
+
+  get "/applications/:id", to: 'applications#show'
 
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
