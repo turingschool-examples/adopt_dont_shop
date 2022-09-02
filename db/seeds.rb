1244dabd0a651d18722c01e5df79bc3035f503f4
Carter Ball <carter.k.ball@gmail.com>
2022-09-01 21:48:33 -0400

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+app_1 = Application.create!(name: "Carter Ball", street_address: "123 Easy Street", city: "Atlanta", state: "GA", zip_code: 30307, status: "In Progress")
+app_2 = Application.create!(name: "Mary Ballantyne", street_address: "888 EZ Lane", city: "Denver", state: "CO", zip_code: 12345, status: "Pending")
+
+shelter_1 = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+shelter_2 = Shelter.create!(name: 'Humane Society', city: 'Humboldt CA', foster_program: true, rank: 4)
+
+
+
+pet_1 = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
+pet_2 = Pet.create!(name: 'Fran', age: 4, breed: 'French Bulldog', adoptable: false, shelter_id: shelter_1.id)
+pet_3 = Pet.create!(name: 'Gilbert', age: 4, breed: 'Mutt', adoptable: true, shelter_id: shelter_2.id)
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,12 +5,11 @@ RSpec.describe 'the application show' do
   before :each do
     @app_1 = Application.create!(name: "Carter Ball", street_address: "123 Easy Street", city: "Atlanta", state: "GA", zip_code: 30307, status: "In Progress")
     @app_2 = Application.create!(name: "Mary Ballantyne", street_address: "888 EZ Lane", city: "Denver", state: "CO", zip_code: 12345, status: "Pending")
-    @shelter = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
-    @pet = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+    @shelter_1 = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    @pet_1 = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter_1.id)
     visit "/applications/#{@app_1.id}"
   end
   it "shows the application and all its attributes" do
-
     expect(page).to have_content(@app_1.name)
     expect(page).to have_content(@app_1.street_address)
     expect(page).to have_content(@app_1.city)
