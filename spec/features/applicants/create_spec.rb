285b8498f5bfb58fb90f05d72af1d6d8dd8cd215
Lucas Colwell <colwellc37@gmail.com>
2022-09-03 14:05:51 -0600

    Test: update test for story 3

--- spec/features/applicants/create_spec.rb
+++ spec/features/applicants/create_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'application creation' do
       expect(find('form')).to have_content('Street address')
       expect(find('form')).to have_content('City')
       expect(find('form')).to have_content('State')
-      expect(find('form')).to have_content('Zip code')
+      expect(find('form')).to have_content('Zipcode')
       expect(find('form')).to have_content('Description')
       expect(page).to have_button("Create Application")
     end
@@ -22,12 +22,14 @@ RSpec.describe 'application creation' do
       it 'creates the application' do
         visit '/applicants/new'
 
-        fill_in 'First name ', with: 'Linda'
+        fill_in 'First name', with: 'Linda'
         fill_in 'Last name', with: 'Smith'
-        fill_in 'Street address', with: '737 Royal lane'
+        fill_in 'Street address', with: '737 Royal Lane'
+        fill_in 'City', with: 'Columbus'
         select 'OH', :from => 'state'
-        fill_in 'Zip code', with: '39053'
+        fill_in 'Zipcode', with: '39053'
         fill_in 'Description', with: 'I would make a great pet owner'
+        fill_in 'Status', with: 'In Progress'
         click_button 'Create Application'
 
 
@@ -38,7 +40,7 @@ RSpec.describe 'application creation' do
         expect(page).to have_content('Columbus')
         expect(page).to have_content('OH')
         expect(page).to have_content('I would make a great pet owner')
-        expect(page).to have_content('Status: In Progress')
+        expect(page).to have_content('In Progress')
 
         expect(page).not_to have_content('Sherry')
       end
