fa3faff46d5b6d51db914e7ead110ecec3dff1f7
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-03 11:14:28 -0600

    create application form

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,6 +3,18 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
-  private
+  def new  
+  end
 
+  def create
+    application = Application.create(application_params)
+    application.status = "In Progress"
+    application.save
+    redirect_to "/applications/#{application.id}"
+  end
+  
+private
+  def application_params
+    params.permit(:first_name, :last_name, :str_address, :city, :state, :zip_code, :home_description, :status)
+  end
 end
