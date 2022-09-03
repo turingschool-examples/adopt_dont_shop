011dcef42e10a922fae896e74c3181852d4d8449
Sid Mann <sjmann2@outlook.com>
2022-09-03 18:15:00 -0500

    Formatting

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,15 +1,12 @@
 class PetApplicationsController < ApplicationController
-
   def create
-    @petapplication = PetApplication.create(pet_applications_params)
-    redirect_to "/applications/#{@petapplication.application_id}"
+    @pet_application = PetApplication.create(pet_applications_params)
+    redirect_to "/applications/#{@pet_application.application_id}"
   end
-
-
-end
 
 private
 
-def pet_applications_params
-  params.permit(:pet_id, :application_id)
+  def pet_applications_params
+    params.permit(:pet_id, :application_id)
+  end
 end
