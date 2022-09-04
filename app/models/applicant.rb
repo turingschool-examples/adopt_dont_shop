5e61d54d03cd6b958b3093c5a969683f16e443a4
Lucas Colwell <colwellc37@gmail.com>
2022-09-04 12:58:03 -0600

    Add validations

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,4 +1,12 @@
 class Applicant < ApplicationRecord
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :street_address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :description
+  validates_presence_of :status
+
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 end
