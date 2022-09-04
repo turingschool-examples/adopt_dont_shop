440f90a7b8f4fa13474e380947a197de7bc96d0f
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-04 13:38:41 -0700

    feat: submit app form not complete error to apps new

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,7 +1,7 @@
 class App < ApplicationRecord
   has_many :app_pets
   has_many :pets, through: :app_pets
-  
+  validates :name, :address, :city, :zip_code, :description, presence: true
   attribute :status, :string, default: "In Progress"
 end
 
