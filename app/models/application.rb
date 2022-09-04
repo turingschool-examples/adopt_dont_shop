4ce66ab24c3773f16bb2ea6977f960ca27b6318d
Sid Mann <sjmann2@outlook.com>
2022-09-04 09:35:22 -0500

    Edit validations to be true

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,20 +1,12 @@
 class Application < ApplicationRecord
-  validates :name, presence: false
-  validates :street_address, presence: false
-  validates :city, presence: false
-  validates :state, presence: false
-  validates :zipcode, presence: false, numericality: false
-  validates :status, presence: false
-  validates :description, presence: false
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true, numericality: true
+  validates :status, presence: true
+  validates :description, presence: true
   has_many :pet_applications
   has_many :pets, through: :pet_applications
-
-  # def show_matching_pets(search)
-  #   Pets.select{ |pet| pet.name.include?(search) }
-  # end
-
-  # def self.search_pets_by_name(search_param)
-  #
-  # end
 
 end
