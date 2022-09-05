bbbb2113f0690be92cea19b0cd1298e738ec9012
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-05 11:23:26 -0600

    Passing test for showing approved after pet has been approved

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -23,13 +23,13 @@ RSpec.describe 'the admin applications show page' do
 
       visit "/admin/applications/#{samantha_application.id}"
 
-      within("#pet_#{fluffy.id}") do #change this specific ID
+      within("#pet_#{fluffy.id}") do 
         click_button("Approve This Pet")
       end
       expect(current_path).to eq("/admin/applications/#{samantha_application.id}")
     end
 
-    xit 'when I click that button I am taken back to the application show page & I do not see a buton to approve the pet' do
+    it 'when I click that button I am taken back to the application show page & I do not see a buton to approve the pet' do
       happypaws = Shelter.create!(name: "Happy Paws Shelter", foster_program: true, city: "Denver", rank: 5)
       fluffy = happypaws.pets.create!(name: "Fluffy", adoptable: true, age: 3, breed: "doberman")
       samantha_application = Application.create!(first_name: "Samantha", last_name: "Smith", street_address: "123 Mulberry Street", city: "Denver", state: "CO", zip_code: 20202, description: "I would like this dog for these reasons.", status: "Pending")
@@ -38,16 +38,13 @@ RSpec.describe 'the admin applications show page' do
 
       visit "/admin/applications/#{samantha_application.id}"
 
-      within("#pet_#{fluffy.id}") do #change this specific ID
+      within("#pet_#{fluffy.id}") do 
       click_button("Approve This Pet")
       end
 
-      expect(current_path).to be("/admin/applications/#{samantha_application.id}")
+      expect(current_path).to eq("/admin/applications/#{samantha_application.id}")
 
-      within("#pet_#{fluffy.id}") do #change this specific ID
       expect(page).to_not have_content("Approve This Pet")
-      end
-
     end
 
     xit "And instead I see an indicator next to the pet that they have been approved" do
