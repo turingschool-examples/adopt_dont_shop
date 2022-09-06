d22c28282eb2e3edebb870b6dd858fbd0504ab4d
Kevin Ta <kvnta@yahoo.com>
2022-09-06 13:18:08 -0600

    save before pull down from github

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -21,11 +21,23 @@ class ApplicantsController < ApplicationController
   def create
     applicant = Applicant.create(applicant_params)
     if applicant.valid?
+      applicant.update(status: "In Progress")
       redirect_to "/applicants/#{applicant.id}"
     else
-    flash[:notice] = "Unable to create application. You must fill in all fields required."
+     flash[:notice] = "Unable to create application. You must fill in all fields required."
       redirect_to '/applicants/new'
     end 
+  end
+
+    def update
+      applicant = Applicant.find(applicant_params)
+
+      if applicant.update(description: params[:description],status: "Pending")
+        redirect_to("/applicants/#{applicant.id}")
+      else
+        redirect_to("/applicants/#{applicant.id}")
+        flash[:alert] = "Error: #{error_message(applicant.errors)}"
+    end
   end
 
 
