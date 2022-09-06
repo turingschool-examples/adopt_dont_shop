05325ca0b6b143d13d7f6a23c25ef6cc3136a30d
Candace Eckels <candace.eckels@gmail.com>
2022-09-06 11:32:29 -0600

    Can view all shelters with pending applications

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -15,6 +15,32 @@ RSpec.describe Shelter do
         expect(shelter2.name).to_not appear_before(shelter1.name)
       end
     end
+
+    it 'in section for pending apps shows the shelters name' do
+      shelter1 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+      shelter2 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      shelter3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+
+      pet1 = shelter1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+      pet2 = shelter1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+
+      app1 = Applicant.create!(name: "Billy Wahl", street_address: "123 S Street Way",
+                    city: "Denver", state: "CO", zip_code: "80123", description: "I Like Dogs", status: 'Pending')
+      
+      pet_app1 = PetApplicant.create!(pet_id: pet1.id, applicant_id: app1.id)
+      pet_app2 = PetApplicant.create!(pet_id: pet2.id, applicant_id: app1.id)
+
+      visit '/admin/shelters'
+
+      within '.pending_apps' do
+        expect(page).to have_link(shelter1.name)
+        expect(page).to_not have_link(shelter2.name)
+        expect(page).to_not have_link(shelter3.name)
+      end
+
+      #Left off in the spec/models/shelter_spec.rb:52
+    end
+
   end
 end
 
