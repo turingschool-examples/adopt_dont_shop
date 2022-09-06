c7d225a651858b39dc5efd1eb23e39126e16a083
Astrid Hecht <silentaviary@gmail.com>
2022-09-05 22:38:08 -0600

    feat: model testing and methods complete for first two db pt 1 US, need complete app create functionality to fully complete though

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -17,12 +17,19 @@ RSpec.describe Pet, type: :model do
     @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
     @pet_3 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
+    
   end
 
   describe 'class methods' do
     describe '#search' do
       it 'returns partial matches' do
         expect(Pet.search("Claw")).to eq([@pet_2])
+      end
+
+      it 'returns case insensitive matches' do
+        pet_4 = @shelter_1.pets.create(name: "Jean-clawed Van Damme", breed: 'sphynx', age: 2, adoptable: true)
+        expect(Pet.search("Claw").include?(@pet_2)).to eq(true)
+        expect(Pet.search("Claw").include?(pet_4)).to eq(true)
       end
     end
 
