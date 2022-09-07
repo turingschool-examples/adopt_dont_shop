e586c7a182f9c722ba8497f5762e367e968f8fd2
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-06 20:02:08 -0400

    feat: approving an application causes all pets to show adoptable false on their show pages

--- spec/features/admin/apps/admin_show_spec.rb
+++ spec/features/admin/apps/admin_show_spec.rb
@@ -9,15 +9,12 @@ RSpec.describe 'admin show page' do
         @pet_3 = @shelter.pets.create!(adoptable: true, name: "Eggs Sinclair", age: 10, breed: "Tres Marias")
         @pet_4 = @shelter.pets.create!(adoptable: true, name: "Monster Truck Wendy", age: 5, breed: "Vancouver Island")
 
-        @pets = [@pet_1, @pet_2, @pet_3, @pet_4]
-
         @app = @shelter.apps.create!(
         name: "Gob Beldof", 
         address: "152 Animal Ave.", 
         city: "Omaha, NE", 
         zip_code: "19593",
-        status: "In Progress"
-        )
+        status: "In Progress")
 
         @app.pets << @pet_1
         @app.pets << @pet_2
@@ -53,30 +50,39 @@ RSpec.describe 'admin show page' do
             expect(page).to have_content("Approved")
         end
     end
+    describe 'for an app with multiple pets, approving 1 pet' do
+        it 'does not change that pets status on other apps' do
+            app_2 = @shelter.apps.create!(
+                name: "Tarzo the Impaler", 
+                address: "154 Animal Ave.", 
+                city: "Omaha, NE", 
+                zip_code: "19593",
+                status: "In Progress")
+            app_2.pets << @pet_1
+
+            click_button "Approve #{@pet_1.name}"
+            expect(current_path).to eq "/admin/apps/#{@app.id}"
+            within("#pet_#{@pet_1.id}") do
+                expect(page).to_not have_button("Approve #{@pet_1.name}")
+                expect(page).to have_content("Approved")
+            end
+            visit "/admin/apps/#{app_2.id}"
+            within("#pet_#{@pet_1.id}") do
+                expect(page).to have_button("Approve #{@pet_1.name}")
+            end
+        end
+    end
 
     it 'when all pets are approved' do
         click_button "Approve #{@pet_1.name}"
         click_button "Approve #{@pet_2.name}"
         click_button "Approve #{@pet_3.name}"
         click_button "Approve #{@pet_4.name}"
-        
+
         expect(current_path).to eq "/admin/apps/#{@app.id}"
         within("#app_status") do
             expect(page).to have_content("Approved")
         end
-    end
-
-    xit 'when one or more pets are rejected' do
-
-    end
-
-    xit 'application approval changes pets adoptable status' do
-        click_button "Approve #{@pet_1.name}"
-        click_button "Approve #{@pet_2.name}"
-        click_button "Approve #{@pet_3.name}"
-        click_button "Approve #{@pet_4.name}"
-
-        expect()
     end
 
     it 'rejects a pet for the application' do
@@ -88,25 +94,24 @@ RSpec.describe 'admin show page' do
         end
     end
 
-    it 'does not change adoptable status across all apps at once' do
-        app_2 = @shelter.apps.create!(
-            name: "Tarzo the Impaler", 
-            address: "154 Animal Ave.", 
-            city: "Omaha, NE", 
-            zip_code: "19593",
-            status: "In Progress"
-            )
-        app_2.pets << @pet_1
+    it 'application approval changes pets adoptable status' do
+        visit "/pets/#{@pet_1.id}"
+        expect(page).to have_content("Adoptable? true")
+
+        visit "/admin/apps/#{@app.id}"
         click_button "Approve #{@pet_1.name}"
-        expect(current_path).to eq "/admin/apps/#{@app.id}"
-        within("#pet_#{@pet_1.id}") do
-            expect(page).to_not have_button("Approve #{@pet_1.name}")
-            expect(page).to have_content("Approved")
-        end
-        visit "/admin/apps/#{app_2.id}"
-        within("#pet_#{@pet_1.id}") do
-            expect(page).to have_button("Approve #{@pet_1.name}")
-        end
-    end
-  
+        click_button "Approve #{@pet_2.name}"
+
+        visit "/pets/#{@pet_2.id}"
+        expect(page).to have_content("Adoptable? true")
+
+        visit "/admin/apps/#{@app.id}"
+        click_button "Approve #{@pet_3.name}"
+        click_button "Approve #{@pet_4.name}"
+
+        visit "/pets/#{@pet_1.id}"
+        expect(page).to have_content("Adoptable? false")
+        visit "/pets/#{@pet_3.id}"
+        expect(page).to have_content("Adoptable? false")
+    end  
 end
