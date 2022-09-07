024f57447223ae709739078ad26c00d289d38336
Lucas Colwell <colwellc37@gmail.com>
2022-09-06 18:35:36 -0600

    Test: story 12

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -19,29 +19,39 @@ RSpec.describe 'As a visitor' do
     describe "Then I see a section for Shelters with Pending Applications" do
       it "in this section I see the name of every shelter that has a pending application" do
         @pirate = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-        @clawdia = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-        @lucille = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+        @clawdia = @shelter_3.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+        @lucille = @shelter_2.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
             #RGV has no pets currently
         @billy = Applicant.create!(first_name: "Billy",
                                   last_name: "Jameson",
                                   street_address: '123 Royal Lane',
                                   city: "Denver",
                                   state: "CO",
-                                  zipcode: 80205)
+                                  zipcode: 80205,
+                                  status: "Pending")
 
         @johnny = Applicant.create!(first_name: "Johnny",
                                   last_name: "Maybach",
                                   street_address: '456 Froot Loop Way',
                                   city: "Mattoon",
                                   state: "IL",
-                                  zipcode: 54242)
+                                  zipcode: 54242,
+                                  status: "Pending")
+
+        @jimmy = Applicant.create!(first_name: "Jimmy",
+                                  last_name: "Long",
+                                  street_address: '123 George street',
+                                  city: "californian",
+                                  state: "CA",
+                                  zipcode: 90210,
+                                  status: "In Progress")
         @aurora = @shelter1
         @rgv = @shelter2
         @fancy = @shelter3
 
-        ApplicationPet.create!(pet: @pirate, applicant: @billy)
-        ApplicationPet.create!(pet: @clawdia, applicant: @johnny)
-        ApplicationPet.create!(pet: @lucille, applicant: @johnny)
+        PetApplication.create!(pet: @pirate, applicant: @billy)
+        PetApplication.create!(pet: @clawdia, applicant: @johnny)
+        PetApplication.create!(pet: @lucille, applicant: @jimmy)
 
 
         visit '/admin/shelters'
