6ed933c49e149d6da0ec6239d4839cb1797bf85e
Astrid Hecht <silentaviary@gmail.com>
2022-09-07 14:05:33 -0600

    feat: make pets index use tables to make everything clearer

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -37,10 +37,10 @@ RSpec.describe 'the pets index' do
 
     visit '/pets'
 
-    expect(page).to have_content("Edit #{pet_1.name}")
-    expect(page).to have_content("Edit #{pet_2.name}")
+    expect(page).to have_button("Edit #{pet_1.name}")
+    expect(page).to have_button("Edit #{pet_2.name}")
 
-    click_link("Edit #{pet_1.name}")
+    click_button("Edit #{pet_1.name}")
 
     expect(page).to have_current_path("/pets/#{pet_1.id}/edit")
   end
@@ -52,10 +52,10 @@ RSpec.describe 'the pets index' do
 
     visit '/pets'
 
-    expect(page).to have_content("Delete #{pet_1.name}")
-    expect(page).to have_content("Delete #{pet_2.name}")
+    expect(page).to have_button("Delete #{pet_1.name}")
+    expect(page).to have_button("Delete #{pet_2.name}")
 
-    click_link("Delete #{pet_1.name}")
+    click_button("Delete #{pet_1.name}")
 
     expect(page).to have_current_path("/pets")
     expect(page).to_not have_content(pet_1.name)
