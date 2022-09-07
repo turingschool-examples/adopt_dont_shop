bc36a77b373eb38ffa4df81139afc795c80fdf1b
Candace Eckels <candace.eckels@gmail.com>
2022-09-07 15:24:03 -0600

    Added status to pet_applicants table

--- app/controllers/pet_applicants_controller 2.rb
+++
@@ -1,8 +0,0 @@
-class PetApplicantsController < ApplicationController
-
-  def create
-    pet = PetApplicant.create!(pet_id:params[:pet], applicant_id: params[:id])
-    redirect_to "/applicants/#{params[:id]}"
-  end
-
-end
