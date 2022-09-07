67eb5ef8a7fa511f527e41df18f558779a9acd6d
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-06 18:53:54 -0600

    add validation tests to model

--- spec/features/applicants/new_spec.rb
+++ spec/features/applicants/new_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'the applicants new' do
-  
+
   it "I see a link to 'Start an Application'" do
     visit '/pets'
 
     expect(find_link("Start an Application").visible?).to be true
-        
+
     click_link "Start an Application"
     expect(current_path).to eq("/applicants/new")
   end
@@ -27,7 +27,7 @@ RSpec.describe 'the applicants new' do
 
   it "I am taken to the new application's show page I see my information, and description and I see an indicator that this application is 'In Progress'" do
     visit '/applicants/new'
-    
+
     fill_in("first_name", with: "Jimmy")
     fill_in("last_name", with: "Dough")
     fill_in("street_address", with: "567 Fake Street")
@@ -35,23 +35,23 @@ RSpec.describe 'the applicants new' do
     fill_in("state", with: "CO")
     fill_in("zip", with: 80205)
     fill_in("description", with: "I'm more awesome than John")
-    
+
     click_button('Next Step')
 
     expect(current_path).to eq("/applicants/#{Applicant.last.id}")
 
     expect(page).to have_content("In Progress")
   end
-  
+
   it "If I fail to fill in any of the form fields and I click submit, then I am taken back to the new applications page and I see a message that I must fill in those fields." do
     visit '/applicants/new'
-    
+
     fill_in("street_address", with: "567 Fake Street")
-    
+
     click_button('Next Step')
 
-    expect(Applicant.count).to eq(0)    
-    save_and_open_page
+    expect(Applicant.count).to eq(0)
+
     expect(current_path).to eq("/applicants/new")
     expect(page).to have_content("First name can't be blank")
     expect(page).to have_content("Last name can't be blank")
@@ -61,5 +61,5 @@ RSpec.describe 'the applicants new' do
     expect(page).to have_content("Description can't be blank")
 
   end
-  
+
 end
--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Applicant, type: :model do
     it { should have_many(:applicant_pets) }
     it { should have_many(:pets).through (:applicant_pets) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_length_of(:state).is_equal_to(2) }
+    it { should validate_presence_of(:zip) }
+    it { should validate_numericality_of(:zip) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:status) }
+    it do
+      should validate_inclusion_of(:status).
+      in_array(['In Progress', 'Pending', 'Approved', 'Rejected'])
+    end
+  end
 end
