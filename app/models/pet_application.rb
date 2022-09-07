3899a3f02e82ace4e13f3f74861bf420cf540e0a
Andrew Mullins <mullinsand15@gmail.com>
2022-09-07 12:17:54 -0600

    add scopes to PetApplication model

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -3,15 +3,19 @@
   belongs_to :application
   validates :pet_status, inclusion: [nil, "Adoption Approved", "Adoption Rejected"]
 
+  scope :pets_app_search, -> (app_id) {where(application_id: app_id)}
+  scope :pets_status, -> (status = "Adoption Approved") {where(pet_status: status)}
+
+  
   def self.pet_count(app_id)
-    where("application_id = ?", app_id).count
+    pets_app_search(app_id).count
   end
 
   def self.pets_app_rej_count(app_id)
-    where(application_id: app_id, pet_status: "Adoption Approved").count + where(application_id: app_id, pet_status: "Adoption Rejected").count
+    pets_app_search(app_id).pets_status.count + pets_app_search(app_id).pets_status("Adoption Rejected").count
   end
 
   def self.pets_rej_count(app_id)
-    where("pet_status = ?", "Adoption Rejected").distinct.count
+    pets_status("Adoption Rejected").count
   end
 end
