4ef78d1310494b6b474d159d7198427b6af0dc09
Joseph Hilby <joseph.hilby@gmail.com>
2022-10-20 16:18:41 -0500

    Fix RESTfull routes, add new controllers

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,20 +11,20 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
 
-  def new
-    @shelter = Shelter.find(params[:shelter_id])
-  end
+  # def new
+  #   @shelter = Shelter.find(params[:shelter_id])
+  # end
 
-  def create
-    pet = Pet.new(pet_params)
+  # def create
+  #   pet = Pet.new(pet_params)
 
-    if pet.save
-      redirect_to "/shelters/#{pet_params[:shelter_id]}/pets"
-    else
-      redirect_to "/shelters/#{pet_params[:shelter_id]}/pets/new"
-      flash[:alert] = "Error: #{error_message(pet.errors)}"
-    end
-  end
+  #   if pet.save
+  #     redirect_to "/shelters/#{pet_params[:shelter_id]}/pets"
+  #   else
+  #     redirect_to "/shelters/#{pet_params[:shelter_id]}/pets/new"
+  #     flash[:alert] = "Error: #{error_message(pet.errors)}"
+  #   end
+  # end
 
   def edit
     @pet = Pet.find(params[:id])
