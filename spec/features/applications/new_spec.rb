faa067aea2516d893221b5049ccc8aa1b265afd4
Emily Port <emilylport@gmail.com>
2022-10-20 16:40:05 -0600

    create applications new spec file and views file, added new and create methods in applications controller

---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'the new application page' do 
+  describe 'when a visitor visits the new application page' do 
+    it 'is a form to enter applicant details' do 
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+
+      pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      application_1 = Application.create!(name: "Taylor Swift", street_address: "22 Blank Space Ln", city: "Denver", state: "CO", zip_code: 80230, status: "In Progress", description: "I love cats")
+      PetApplication.create(pet: pet_1, application: application_1)
+      PetApplication.create(pet: pet_2, application: application_1)
+      
+      visit 'applications/new'
+      
+      fill_in :name, with: "Britney Spears"
+      fill_in :street_address, with: "1234 Apple Ln"
+      fill_in :city, with: "Aurora"
+      fill_in :state, with: "CO"
+      fill_in :zip_code, with: 80230
+      fill_in :status, with: "In Progress" 
+      fill_in :description, with: "I like dogs"
+      
+      
+      visit "/applications/#{application_1.id}"
+      
+    end 
+  end 
+end 
