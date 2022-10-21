4720a4f36c48abc745d6089b3be2e32f30c5ce9e
Amanda Ross <amikaross@gmail.com>
2022-10-21 16:03:42 -0600

    Feat: Added feature for validating that all fields are filled out in the adopt_app creation

--- app/models/adopt_app.rb
+++ app/models/adopt_app.rb
@@ -1,4 +1,10 @@
 class AdoptApp < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :street_address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip_code
+
   has_many :adopt_app_pets
   has_many :pets, through: :adopt_app_pets
 end
