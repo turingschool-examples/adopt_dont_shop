0b563bae21e220424f5d71297aaac74eb72e2390
Emily Port <emilylport@gmail.com>
2022-10-21 08:04:15 -0600

    form to add a new application takes user to the applications show page after submitting

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -7,12 +7,11 @@ RSpec.describe 'the new application page' do
 
       pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
       pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-      application_1 = Application.create!(name: "Taylor Swift", street_address: "22 Blank Space Ln", city: "Denver", state: "CO", zip_code: 80230, status: "In Progress", description: "I love cats")
-      PetApplication.create(pet: pet_1, application: application_1)
-      PetApplication.create(pet: pet_2, application: application_1)
-      
+      # application_1 = Application.create!(name: "Taylor Swift", street_address: "22 Blank Space Ln", city: "Denver", state: "CO", zip_code: 80230, status: "In Progress", description: "I love cats")
+      # PetApplication.create(pet: pet_1, application: application_1)
+      # PetApplication.create(pet: pet_2, application: application_1)
       visit 'applications/new'
-      
+
       fill_in :name, with: "Britney Spears"
       fill_in :street_address, with: "1234 Apple Ln"
       fill_in :city, with: "Aurora"
@@ -20,10 +19,10 @@ RSpec.describe 'the new application page' do
       fill_in :zip_code, with: 80230
       fill_in :status, with: "In Progress" 
       fill_in :description, with: "I like dogs"
-      
-      
-      visit "/applications/#{application_1.id}"
-      
+      click_button("Submit New Application")
+      new_application_id = Application.last.id 
+      expect(current_path).to eq("/applications/#{new_application_id}")
+      visit "/applications/#{new_application_id}"
     end 
   end 
 end 
