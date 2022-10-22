5cfebc091c858908544ed261a53cc92417e858a4
Darby Smith <newman.darby2@gmail.com>
2022-10-22 12:07:33 -0400

    Add tests for pet search on app show

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -14,4 +14,41 @@ RSpec.describe Application do
     it {should validate_presence_of :zip}
     it {should validate_presence_of :description}
   end
+
+  before(:each) do
+    @application_1 = Application.create!(name: 'Pam Pulzone', street_address: '66225 Wallace Rd', city: 'Bend', state: 'OR', zip: 97702, description: 'Fenced yard, loving home', status: 'Pending')
+    @application_2 = Application.create!(name: 'Dan Boston', street_address: '774 Boone Rd', city: 'Greenville', state: 'SC', zip: 56843, description: 'Experience with difficult dogs', status:  'In Progress')
+    @shelter_1= Shelter.create(name: 'Aurora Shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @shelter_2 = Shelter.create(name: 'HSCO', city: 'Bend, OR', foster_program: true, rank: 4)
+    @pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_1.id)
+    @pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_1.id)
+    @pet_3 = Pet.create(adoptable: false, age: 8, breed: 'mix', name: 'Kumquat', shelter_id: @shelter_2.id)
+  end
+
+  # Test moving the seach for pet (within app_contr show method here)
+
+  # describe 'class methods' do
+  #   describe '#search_pet' do
+  #     it 'searches for a pet based on user input' do
+  #       application_1 = Application.create!(name: 'Pam Pulzone', street_address: '66225 Wallace Rd', city: 'Bend', state: 'OR', zip: 97702, description: 'Fenced yard, loving home', status: 'Pending')
+  #       pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_1.id)
+        
+  #       expect(Application.search_pet('Lo')).to eq(pet_2.name)
+  #     end
+
+  #     it 'returns nothing if there is no user input' do
+  #       application_1 = Application.create!(name: 'Pam Pulzone', street_address: '66225 Wallace Rd', city: 'Bend', state: 'OR', zip: 97702, description: 'Fenced yard, loving home', status: 'Pending')
+  #       pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_1.id)
+
+  #       expect(application_1.search_pet).to eq(nil)
+  #     end
+
+  #     it "returns nothing if there is no matching pet" do
+  #       application_1 = Application.create!(name: 'Pam Pulzone', street_address: '66225 Wallace Rd', city: 'Bend', state: 'OR', zip: 97702, description: 'Fenced yard, loving home', status: 'Pending', search: 'Zoe')
+  #       pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_1.id)
+
+  #       expect(application_1.search_pet()).to eq(nil)
+  #     end
+  #   end
+  # end
 end
