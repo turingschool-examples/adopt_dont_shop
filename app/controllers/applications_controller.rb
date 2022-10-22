fd797882f7c5c06f5224b625be2a371b8cf1807d
Emily Port <emilylport@gmail.com>
2022-10-22 12:22:36 -0600

    fixed in-progress word in applications show page to match our logic so form now works

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationsController < ApplicationController
+  def index 
+    @applications = Application.all 
+  end
   def show 
     @application = Application.find(params[:id])
     if params[:commit] == "Search"
