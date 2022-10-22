9fcf770a8f85bda58e7f3eb0cd9fc4c04b04770d
Darby Smith <newman.darby2@gmail.com>
2022-10-22 16:00:45 -0400

    Refactor route to be update

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get '/applications/new', to: 'applications#new'
   post '/applications', to: 'applications#create'
   get '/applications/:id', to: 'applications#show'
-  post '/applications/:id', to: 'applications#edit'
+  patch '/applications/:id', to: 'applications#update'
 
   get '/veterinary_offices', to: 'veterinary_offices#index'
   get '/veterinary_offices/new', to: 'veterinary_offices#new'
