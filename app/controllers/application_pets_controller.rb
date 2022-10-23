b8b5b0cbbe6263a2a6ca50e3c42a165ddecc63b6
Annie Pulzone <ajpulzone@gmail.com>
2022-10-23 15:45:35 -0700

    Deleted applications/pets controllers and views, did not end up taking that path with how we organized our site

--- app/controllers/application_pets_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationPetsController < ApplicationController
-
-  def show
-    @application = Application.find(params[:id])
-    @app_pets = ApplicationPet.where("application_id = #{@application.id}")
-    # @app_pets = ApplicationPet.find_by_sql(Select * FROM applicationpets where application)
-  end
-end
