fbf8de2e741d6a1426bbf791d30388dfe58efcfe
Ashley <ashuhleyt@gmail.com>
2022-10-23 15:41:22 -0700

    completed US 8 & 9

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,8 @@
 @pet8 = @shelter4.pets.create!(name: "Penny", breed: "Cardigan", age: 8, adoptable: true)
 @pet9 = @shelter5.pets.create!(name: "Leonardo", breed: "Pizza Turtle", age: 23, adoptable: true)
 @pet10 = @shelter5.pets.create!(name: "Raphael", breed: "Sewer Turtle", age: 43, adoptable: true)
+@pet11 = @shelter5.pets.create!(name: "Leonard", breed: " Turtle", age: 47, adoptable: true)
+@pet12 = @shelter5.pets.create!(name: "Ralph", breed: "Lizard", age: 6, adoptable: false)
 
 @app1 = Applicant.create!(name: "Ashley Turner", street_address: "123 Oregon Trail Way", city: "Portland", state: "OR", zip_code: "54321", description: "Pet sitting and want to have my own")
 @app2 = Applicant.create!(name: "Kevin Ta", street_address: "2208 Repeater Dr", city: "Aurora", state: "CO", zip_code: "80011", description: "I love Corgi's, can I haz?")
