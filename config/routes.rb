11b260ac30ef8ae5da9bd6542e189eb8563a3791
Joseph Hilby <joseph.hilby@gmail.com>
2022-10-23 13:40:39 -0500

    Delete unused vet & vet_office files

--- config/routes.rb
+++ config/routes.rb
@@ -26,23 +26,4 @@ Rails.application.routes.draw do
   post '/shelters/:id/pets', to: 'shelter_pets#create'
 
   get '/admin/shelters', to: 'admin_shelters#index'
-
-
-  # get '/veterinarians', to: 'veterinarians#index'
-  # get '/veterinarians/:id', to: 'veterinarians#show'
-  # get '/veterinarians/:id/edit', to: 'veterinarians#edit'
-  # patch '/veterinarians/:id', to: 'veterinarians#update'
-  # delete '/veterinarians/:id', to: 'veterinarians#destroy'
-
-  # get '/veterinary_offices', to: 'veterinary_offices#index'
-  # get '/veterinary_offices/new', to: 'veterinary_offices#new'
-  # get '/veterinary_offices/:id', to: 'veterinary_offices#show'
-  # post '/veterinary_offices', to: 'veterinary_offices#create'
-  # get '/veterinary_offices/:id/edit', to: 'veterinary_offices#edit'
-  # patch '/veterinary_offices/:id', to: 'veterinary_offices#update'
-  # delete '/veterinary_offices/:id', to: 'veterinary_offices#destroy'
-
-  # get '/veterinary_offices/:id/veterinarians', to: 'veterinary_office_veterinarians#index'
-  # get '/veterinary_offices/:id/veterinarians/new', to: 'veterinary_office_veterinarians#new'
-  # post '/veterinary_offices/:id/veterinarians', to: 'veterinary_office_veterinarians#create'
 end
