5af0960593ab03a9da079c1f413ce5f4fe884290
Darby Smith <newman.darby2@gmail.com>
2022-10-24 18:07:18 -0400

    Add route for admin applications

--- config/routes.rb
+++ config/routes.rb
@@ -48,4 +48,6 @@ Rails.application.routes.draw do
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
 
   get '/admin/shelters', to: 'admin_shelters#index'
+
+  get '/admin/applications/:id', to: 'admin_applications#show'
 end
