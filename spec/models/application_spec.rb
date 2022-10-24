9ad2f3c3704044808ae58e31d048e56b9a693f1d
Naomi Yocum <naomiyocum24@gmail.com>
2022-10-23 18:22:01 -0600

    add validation testing

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe Application, type: :model do
     it { should have_many(:pets).through(:application_pets)}
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zipcode) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:status) }
+  end
+
   describe 'instance method' do
     describe '#add_pet' do
       it 'adds an instance of pet to application.pets' do
