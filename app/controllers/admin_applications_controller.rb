66667a510f4c4b5540620b62eeb8d93cd765d602
Mostafa Sakr <msakr3@gmail.com>
2022-10-24 11:57:07 -0600

    Add code for admin application approve button

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,5 +1,5 @@
 class AdminApplicationsController < ApplicationsController
-  def show 
+  # def show 
 
-  end
+  # end
 end
--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,7 +1,6 @@
 class AdminSheltersController < SheltersController
   def index
       @shelters = Shelter.order_alphabatically_in_reverse
-      # binding.pry
       @shelters_with_pending_apps = Shelter.shelters_with_pending_applications
   end
 end
