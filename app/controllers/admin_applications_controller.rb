028616a2fba2ba4afb1cb483d87f3e5a23f7c36f
Darby Smith <newman.darby2@gmail.com>
2022-10-24 20:31:32 -0400

    WIP Feat: approving pet

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,11 +1,17 @@
 class AdminApplicationsController < ApplicationController
   def show
-    require 'pry'; binding.pry
     @application = Application.find(params[:id])
     app_pets = ApplicationPet.where(application_id: @application.id)
     @pets = app_pets.flat_map do |app|
       Pet.where(id: app.pet_id)
     end
+  end
+
+  def update
+    application = Application.find(params[:id])
+    app_pet = ApplicationPet.where("application_id = #{application.id} AND pet_id = #{params[:pet]}")
+    app_pet.update(status: params[:status])
+    redirect_to "/admin/applications/#{application.id}"
   end
 
 end
