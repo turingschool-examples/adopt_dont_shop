1b3d90aba2b7ef44a611206985d3fcf6fc930084
Mostafa Sakr <msakr3@gmail.com>
2022-10-24 22:50:32 -0600

    Move some logic to models for SQL querying.

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -6,39 +6,36 @@ class AdminApplicationsController < ApplicationsController
 
   def pending_pet_applications
     @application.pets.map do |pet|
-      pet.pet_applications.pets_based_on_application(false, false, @application.id)
+      pet.pet_applications.based_on_conditions(false, false, @application.id)
     end
   end
 
   def approved_pet_applications
     @application.pets.map do |pet|
-      pet.pet_applications.pets_based_on_application(false, true, @application.id)
+      pet.pet_applications.based_on_conditions(false, true, @application.id)
     end
   end
 
   def rejected_pet_applications
     @application.pets.map do |pet|
-      pet.pet_applications.pets_based_on_application(true, false, @application.id)
+      pet.pet_applications.based_on_conditions(true, false, @application.id)
     end
   end
 
   def application_decision(parameter)
-    pet = Pet.find(params[:pet_id])
-    pet.pet_applications.each do |application|
-      if application.application_id == @application.id && application.pet_id == pet.id
-        application.update(parameter => params[parameter])
-        application.save
-      end
+    applications = Pet.find(params[:pet_id]).pet_applications.based_on_ids(params[:pet_id], @application.id)
+    applications.each do |application|
+      application.update(parameter => params[parameter])
+      application.save
     end
     redirect_to "/admin/applications/#{@application.id}"
   end
 
   def pet_application_lists
-    @pet_applications_pending = pending_pet_applications3.flatten
+    @pet_applications_pending = pending_pet_applications.flatten
     @pet_applications_approved = approved_pet_applications.flatten
     @pet_applications_rejected = rejected_pet_applications.flatten
   end
-
 
   def view_with_decision
     if params[:approved].present?
--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,8 +2,12 @@ class PetApplication < ApplicationRecord
   belongs_to :application
   belongs_to :pet
 
-  def self.pets_based_on_application(reject, approve, application)
+  def self.based_on_conditions(reject, approve, application)
     where("rejected = ? AND approved = ? AND application_id = ?", reject, approve, application)
+  end
+
+  def self.based_on_ids(pet, application)
+    where("pet_id = ? AND application_id = ?", pet, application)
   end
 end
 
