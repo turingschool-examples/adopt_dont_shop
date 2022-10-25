631c770980f146327b5ff635ebb2e38ce8e05f27
Emily Port <emilylport@gmail.com>
2022-10-25 13:10:30 -0600

    add validate numericality of zip code to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,6 +5,6 @@ class Application < ApplicationRecord
   validates_presence_of :street_address
   validates_presence_of :city
   validates_presence_of :state
-  validates_presence_of :zip_code
+  validates_numericality_of :zip_code
   validates_presence_of :status
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -12,6 +12,6 @@ RSpec.describe Application, type: :model do
     it { should validate_presence_of(:street_address)}
     it { should validate_presence_of(:city)}
     it { should validate_presence_of(:state)}
-    it { should validate_presence_of(:zip_code)}
+    it { should validate_numericality_of(:zip_code)}
   end
 end
