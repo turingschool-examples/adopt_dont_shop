7e213d687e251ae6433b7a0ff20ac5b45a3fdd42
Darby Smith <newman.darby2@gmail.com>
2022-10-25 21:36:26 -0400

    Clean up tests

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -1,13 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "creating a new application" do
-
   it 'the pet index page has a link to start an application' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
-    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
-
     visit "/pets"
 
     expect(page).to have_link("Start an Application")
@@ -22,7 +16,6 @@ RSpec.describe "creating a new application" do
     fill_in :city, with: "Bend"
     select "OR", from: :state
     fill_in :zip, with: 77904
-    # fill_in :description, with: "I love dogs"
 
     click_on "Submit"
 
@@ -34,18 +27,11 @@ RSpec.describe "creating a new application" do
     expect(page).to have_content(application.city)
     expect(page).to have_content(application.state)
     expect(page).to have_content(application.zip)
-    expect(page).to have_content("In Progress") # readdress later after user story 12
-    
-    # expect(page).to_not have_content(application.description)
+    expect(page).to have_content("In Progress")
   end
 
   it 'will not create a new application when fields are left blank and will redirect back 
     to the new application page with a message to fill out incomplete fields' do
-      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-      pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
-      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
-      pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
-
       visit "/applications/new"
       have_selector("form")
 
