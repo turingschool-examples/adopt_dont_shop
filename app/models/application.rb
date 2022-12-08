896e63286b7a7cd3f7fe7adff701c1d1621caaab
Ryan Canton <ryancanton999@gmail.com>
2022-12-08 15:58:00 -0700

    Feat: Add functionality for user story 1 (application show page)

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,11 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
   belongs_to :shelter
+
+  def get_pets
+    pet_apps = PetApplication.where(:application_id == self.id)
+    pets = pet_apps.map do |pet_app|
+      Pet.find(pet_app[:pet_id])
+    end
+  end
 end
