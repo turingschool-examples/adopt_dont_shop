bcc87cd1cd42664ea2e202d56c5031c647d81824
Alex Pitzel <pitzelalex@gmail.com>
2022-12-08 17:46:42 -0500

    feat: Add page content to application show page

--- spec/features/applications/application_show_spec.rb
+++ spec/features/applications/application_show_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'visit application show page' do
     @buster = @shelter.pets.create!(adoptable: true, age: 2, breed: 'Shnauzer - Wheaton mix', name: 'Buster')
     @marlowe = @shelter.pets.create!(adoptable: true, age: 9, breed: 'Pembroke Welsh Corgi', name: 'Marlowe')
 
-    @app1 = Application.create!(name: 'Frank Sinatra', street_address: '69 Sinatra Way', city: 'nashville', state: 'Tennessee', zip_code: '69420', description: "I've always liked dogs")
+    @app1 = Application.create!(name: 'Frank Sinatra', street_address: '69 Sinatra Way', city: 'Nashville', state: 'Tennessee', zip_code: '69420', description: "I've always liked dogs")
   end
 
   it 'displays application details' do
@@ -17,9 +17,9 @@ RSpec.describe 'visit application show page' do
 
     expect(page).to have_content("Applicant Details:")
     expect(page).to have_content("Name: #{@app1.name}")
-    expect(page).to have_content("Address: #{@app1.street_address}\n#{@app1.city}, #{@app1.state} #{@app1.zip_code}")
+    expect(page).to have_content("Address: #{@app1.street_address} #{@app1.city}, #{@app1.state} #{@app1.zip_code}")
     expect(page).to have_content("Application: #{@app1.description}")
     expect(page).to have_content("Application Status: #{@app1.status}")
-    expect(page).to have_content("This is for: #{@buster.name}, #{@marlowe.name}")
+    expect(page).to have_content("This Application is for: #{@buster.name}, #{@marlowe.name}")
   end
 end
