89dbc6a5ace936b0b6ced0ea1e8740bb0817af9b
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-08 14:16:35 -0800

    Add display info to view

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Applications show page' do
   describe 'when I visit /applcations/:id' do
     it 'shows all of the application information' do
-      # application = Application.create!(app_name: 'Billy Bob', address: "54984 Bulldog Rd Salem, OR 97301", description: "Billy Bob wants a buddy", pet_names: "Bobby", status: "Pending")
+      application = Application.create!(app_name: 'Billy Bob', address: "54984 Bulldog Rd Salem, OR 97301", description: "Billy Bob wants a buddy", pet_names: "Bobby", status: "Pending")
       visit "/applications/#{application.id}"
-
+save_and_open_page
       expect(page).to have_content(application.app_name)
       expect(page).to have_content(application.address)
       expect(page).to have_content(application.description)
