7842a4c75c6841aaea0e4a4bce149145672780bd
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-08 14:56:29 -0800

    Working on user story 1

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    @pets = @application.pets
+    
   end
 end
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,12 +5,26 @@ RSpec.describe 'Applications show page' do
     it 'shows all of the application information' do
       application = Application.create!(app_name: 'Billy Bob', address: "54984 Bulldog Rd Salem, OR 97301", description: "Billy Bob wants a buddy", pet_names: "Bobby", status: "Pending")
       visit "/applications/#{application.id}"
-save_and_open_page
+
       expect(page).to have_content(application.app_name)
       expect(page).to have_content(application.address)
       expect(page).to have_content(application.description)
       expect(page).to have_content(application.pet_names)
       expect(page).to have_content(application.status)
     end
+
+    it 'has links to the pets show pages' do 
+      shelter = Shelter.create!(name: "Brightside", city: "Salem", rank: 2, foster_program: true)
+      pet_1 = shelter.pets.create!(name: "Bobby", age: 2, breed: 'Bulldog', adoptable: true)
+      application = pet_1.applications.create!(app_name: 'Billy Bob', address: "54984 Bulldog Rd Salem, OR 97301", description: "Billy Bob wants a buddy", pet_names: "Bobby", status: "Pending")
+     
+      visit "/applications/#{application.id}"
+
+      click_link("#{pet_1.name}")
+
+      expect(current_path).to eq("/pets/#{pet_1.id}")
+      expect(page).to have_content("#{pet_1.name}")
+
+     end
   end
 end
