583653b76ce310cce4aee6ca7675977e09c11409
Leo Banos Garcia <bsleo625@gmail.com>
2022-12-09 14:36:59 -0700

    Adds flash error when the application fields are not all filled out

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,7 +6,13 @@ class ApplicationsController < ApplicationController
 
   end
   def create
-    @application = Application.create!(first: params[:first], last: params[:last], street: params[:street], city: params[:city], state: params[:state], zip: params[:zip], description: params[:description], status: "Pending")
-    redirect_to "/applications/#{@application.id}"
+    @application = Application.new(first: params[:first], last: params[:last], street: params[:street], city: params[:city], state: params[:state], zip: params[:zip], description: params[:description], status: "Pending")
+      if @application.save
+        redirect_to "/applications/#{@application.id}"
+      else
+        redirect_to "/applications/new"
+        flash[:alert] = "Error, fill in all fields"
+      end
+    
   end
 end
