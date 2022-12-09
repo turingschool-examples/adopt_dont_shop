5c2fa9ba0b122a64cfecb48f15b375293457f94d
Alastair Lloyd <dex8056@aol.com>
2022-12-09 15:18:46 -0700

    amend test case to fix user story 1 bug

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -12,9 +12,10 @@ RSpec.describe 'the application show page' do
   it 'shows the application and all its attributes' do
     app1 = Application.create(name: "Max", street_address: "Made up St", city: "Denver", state: "CO", zip_code: "80000", description: "Love mix breeds. Lots of energy to play with a dog", status: "In Progress")
     app2 = Application.create(name: "Alastair", street_address: "Fictional St", city: "Golden", state: "CO", zip_code: "80001", description: "Love big dogs. Great mountain walks on doorstep", status: "Accepted")
-    pet1 = Pet.create(name: 'Noodle', age: 2, breed: 'Border Collie', adoptable: true)
-    pet2 = Pet.create(name: 'Hercules', age: 2, breed: 'American Akita', adoptable: true)
-    
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet1 = app1.pets.create(name: 'Noodle', age: 2, breed: 'Border Collie', adoptable: true, shelter_id: shelter.id)
+    pet2 = app1.pets.create(name: 'Hercules', age: 2, breed: 'American Akita', adoptable: true, shelter_id: shelter.id)
+
     visit "/applications/#{app1.id}"
     
     expect(page).to_not have_content(app2.name)
@@ -25,7 +26,7 @@ RSpec.describe 'the application show page' do
     expect(page).to have_content(app1.zip_code)
     expect(page).to have_content(app1.description)
     expect(page).to have_content(app1.status)
-    # expect(page).to have_link(pet1.name)
-    # expect(page).to have_link(pet2.name)
+    expect(page).to have_link(pet1.name)
+    expect(page).to have_link(pet2.name)
   end
 end
