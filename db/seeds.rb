3f266d2df6b914a3f5dfcc87089b4ccd5370accb
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-09 14:20:05 -0800

    Working through user story 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+shelter = Shelter.create!(name: "Brightside", city: "Salem", rank: 2, foster_program: true)
+pet_1 = shelter.pets.create!(name: "Bobby", age: 2, breed: 'Bulldog', adoptable: true)
+pet_2 = shelter.pets.create!(name: "Sammy", age: 5, breed: 'lab', adoptable: true)
+application_1 = pet_1.applications.create!(name: 'Billy Bob', street_address: "54984 Bulldog Rd", city: "Salem", state: "OR", zipcode: "97301", description: "Billy Bob wants a buddy", status: "In Progress")
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Applications show page' do
       pet_1 = shelter.pets.create!(name: "Bobby", age: 2, breed: 'Bulldog', adoptable: true)
       application = pet_1.applications.create!(name: 'Billy Bob', street_address: "54984 Bulldog Rd", city: "Salem", state: "OR", zipcode: "97301", description: "Billy Bob wants a buddy", status: "Pending")
       visit "/applications/#{application.id}"
-save_and_open_page
+
       expect(page).to have_content(application.name)
       expect(page).to have_content(application.street_address)
       expect(page).to have_content(application.city)
@@ -38,6 +38,24 @@ save_and_open_page
 
       expect(current_path).to eq("/pets/#{pet_2.id}")
       expect(page).to have_content("#{pet_2.name}")
-     end
+    end 
+
+    it 'has a section to add pets to the application by name' do
+      shelter = Shelter.create!(name: "Brightside", city: "Salem", rank: 2, foster_program: true)
+      pet_1 = shelter.pets.create!(name: "Bobby", age: 2, breed: 'Bulldog', adoptable: true)
+      pet_2 = shelter.pets.create!(name: "Sammy", age: 5, breed: 'lab', adoptable: true)
+      application_1 = pet_1.applications.create!(name: 'Billy Bob', street_address: "54984 Bulldog Rd", city: "Salem", state: "OR", zipcode: "97301", description: "Billy Bob wants a buddy", status: "In Progress")
+
+      visit "/applications/#{application_1.id}"
+
+      expect(page).to have_content("Add a Pet to this Application")
+      expect(application_1.status).to eq("In Progress")
+
+      fill_in :search, with: pet_2.name
+      click_button "Search"
+  
+      expect(current_path).to eq("/applications/#{application_1.id}")
+      expect("Search").to appear_before("Sammy")
+    end 
   end
 end
