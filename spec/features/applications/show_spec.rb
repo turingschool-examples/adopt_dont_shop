e4a613e095528b76d83d2e8774062b89ea8ce081
Brandon Gray <brandonjoe88@hotmail.com>
2022-12-09 13:21:14 -0700

    feat: updates search functionality to show page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'the application show' do
   before :each do
-    application = Application.create(
+    @application = Application.create(
       name: 'Joseph Puglia',
       street_address: '5201 Whistle St.',
       city: 'Irvine',
@@ -12,13 +12,13 @@ RSpec.describe 'the application show' do
   end
   it "shows the application and all it's attributes" do
     
-    visit "/applications/#{application.id}"
+    visit "/applications/#{@application.id}"
 
-    expect(page).to have_content(application.name)
-    expect(page).to have_content(application.street_address)
-    expect(page).to have_content(application.city)
-    expect(page).to have_content(application.state)
-    expect(page).to have_content(application.zip_code)
+    expect(page).to have_content(@application.name)
+    expect(page).to have_content(@application.street_address)
+    expect(page).to have_content(@application.city)
+    expect(page).to have_content(@application.state)
+    expect(page).to have_content(@application.zip_code)
     # expect(page).to have_content(application.description)
     # expect(page).to have_content(application.pets)
     # expect(page).to have_content(application.status)
@@ -26,13 +26,13 @@ RSpec.describe 'the application show' do
 
   it 'can shows a form to search for pets by name' do
 
-    visit "/applications/#{application.id}"
+    visit "/applications/#{@application.id}"
 
-    fill_in('name', with: "Spot")
+    fill_in('search', with: "Spot")
 
     click_button 'search'
 
-    expect(current_path).to eq("/applications/#{application.id}")
+    expect(current_path).to eq("/applications/#{@application.id}")
     expect(page).to have_content("Spot")
   end
 end
