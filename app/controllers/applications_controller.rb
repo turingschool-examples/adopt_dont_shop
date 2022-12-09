724fcf835412500a337c4ac8e0c75dd08deb8f6a
William Lampke <gravitybangers@gmail.com>
2022-12-09 16:10:59 -0500

    User story 2 and tests completed

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,11 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+  def new
+
+  end
+  def create
+    @application = Application.create!(first: params[:first], last: params[:last], street: params[:street], city: params[:city], state: params[:state], zip: params[:zip], description: params[:description], status: "Pending")
+    redirect_to "/applications/#{@application.id}"
+  end
 end
