639e4cc0cdd77120e64a2e3bc43a49672494aebf
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-12-08 19:54:36 -0700

    Add application and application pet models and apps show page and tests

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  let!(:application) { Application.create!(
+        human_name: "Brad Pitt",
+        description: "Very rich, multiple homes, ample backyard space.",
+        street_address: "123 Hollywood Blvd",
+        city: "Los Angeles",
+        state: "CA",
+        zip: "90210",
+        status: "Rejected")}
+
+  describe 'relationships' do
+    it { should have_many(:application_pets) }
+  end
+
+  describe 'attributes' do
+    it 'exists' do
+      expect(application).to be_a(Application)  
+      expect(application.human_name).to eq("Brad Pitt")  
+      expect(application.state).to eq("CA")  
+      expect(application.status).to eq("Rejected")  
+    end
+  end
+end
