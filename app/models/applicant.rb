bd5c808ccb759b59d7d1c05b2f2a74469c1fe666
Anhnhi Tran <anhtran811@gmail.com>
2022-12-08 19:51:59 -0700

    Got full address test to pass

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,5 +2,7 @@ class Applicant < ApplicationRecord
   has_many :applicant_pets
   has_many :pets, through: :applicant_pets
 
-  
+  def full_address 
+    "#{street_address}, #{city}, #{state} #{zip_code}"
+  end
 end
