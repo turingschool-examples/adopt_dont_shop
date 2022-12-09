a0d086ab6eb05c5867f40d546b6235718edd8a71
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-12-08 18:32:55 -0800

    Test: create new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,4 +3,14 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @pets = @application.get_pets
   end
+
+  def create
+    application = Application.new(application_params)
+  end
+end
+
+private
+
+def application_params
+  params.permit(:id, :name, :address, :city, :state, :zip_code, :description, :status, :shelter_id)
 end
--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -26,4 +26,21 @@ RSpec.describe 'application creation' do
       expect(find('form')).to have_content('Zip code')
     end
   end
+
+  describe 'the application create' do
+    it 'creates the application and redirects to the application show page' do
+      visit "/applications/new"
+
+      fill_in 'Name', with: 'Jeremy Mitchell'
+      fill_in 'Address', with: '000 Main Street'
+      fill_in 'City', with: 'San Francisco'
+      fill_in 'State', with: 'California'
+      fill_in 'Zip code', with: 94122 
+      click_button 'Submit'
+      new_app_id = Application.last.id
+
+      expect(page).to have_current_path("/applications/#{new_app_id}")
+      expect(page).to have_content('Jeremy')
+    end
+  end
 end
