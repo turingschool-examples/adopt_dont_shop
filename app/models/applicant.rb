b39479a932b280aef209b157dd235ccd2bcd71d5
Anhnhi Tran <anhtran811@gmail.com>
2022-12-09 11:45:50 -0700

    Renamed applicant models and controllers to application

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,8 +1,8 @@
-class Application < ApplicationRecord
-  has_many :application_pets
-  has_many :pets, through: :application_pets
+# class Application < ApplicationRecord
+#   has_many :application_pets
+#   has_many :pets, through: :application_pets
 
-  def full_address 
-    "#{street_address}, #{city}, #{state} #{zip_code}"
-  end
-end
+#   def full_address 
+#     "#{street_address}, #{city}, #{state} #{zip_code}"
+#   end
+# end
