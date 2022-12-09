bccd871e13494e447baf931a204ad50339f99bae
Anhnhi Tran <anhtran811@gmail.com>
2022-12-08 19:48:59 -0700

    Wrote test for full address

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Applicant, type: :model do
     it { should have_many :applicant_pets }
     it { should have_many(:pets).through(:applicant_pets) }
   end
+
+  describe "#full_address" do
+    it 'can create a full address' do
+      applicant_1 = Applicant.create!(
+        name: "Bob",
+        street_address: "123 Leaf Street",
+        city: "Denver",
+        state: "CO",
+        zip_code: 80020,
+        description: "Work from home",
+        status: "In Progress"
+      )
+
+      expect(applicant_1.full_address).to eq("123 Leaf Street, Denver, CO 80020")
+    end
+  end
 end
