357955c68cdce8426f647c9f214f239239f7daaf
Brandon Gray <brandonjoe88@hotmail.com>
2022-12-10 12:13:50 -0700

    test: button for adopting searched pet name

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -10,45 +10,66 @@ RSpec.describe 'the application show' do
     @pet_3       = create(:pet, shelter: @shelter_1)
   end
 
-  it "shows the application and all it's attributes" do
+  describe 'User Story #1' do
+    it "shows the application and all it's attributes" do
 
-    visit "/applications/#{@application.id}"
+      visit "/applications/#{@application.id}"
 
-    expect(page).to have_content(@application.name)
-    expect(page).to have_content(@application.street_address)
-    expect(page).to have_content(@application.city)
-    expect(page).to have_content(@application.state)
-    expect(page).to have_content(@application.zip_code)
-    expect(page).to have_content(@application.description)
-    expect(page).to have_content(@pet_1.name)
-    expect(page).to have_content(@application.status)
+      expect(page).to have_content(@application.name)
+      expect(page).to have_content(@application.street_address)
+      expect(page).to have_content(@application.city)
+      expect(page).to have_content(@application.state)
+      expect(page).to have_content(@application.zip_code)
+      expect(page).to have_content(@application.description)
+      expect(page).to have_content(@pet_1.name)
+      expect(page).to have_content(@application.status)
+    end
   end
 
-  it 'can show a form to search for pets by name' do
+  describe 'User Story #4' do
+    it 'can show a form to search for pets by name' do
 
-    visit "/applications/#{@application.id}"
-    fill_in('search', with: @pet_1.name)
-    click_button 'search'
+      visit "/applications/#{@application.id}"
+      fill_in('search', with: @pet_1.name)
+      click_button 'search'
 
-    expect(current_path).to eq("/applications/#{@application.id}")
-    expect(page).to have_content(@pet_1.name)
+      expect(current_path).to eq("/applications/#{@application.id}")
+      expect(page).to have_content(@pet_1.name)
+    end
   end
-  
-  it 'links to pet objects via search and list' do
 
-    visit "/applications/#{@application.id}"
-    click_link @pet_1.name
+  describe 'User Story #4 (cont.)' do
+    it 'links to pet objects via search and list' do
 
-    expect(current_path).to eq("/applications/pets/#{@pet_1.id}")
+      visit "/applications/#{@application.id}"
+      click_link @pet_1.name
 
-    visit "/applications/#{@application.id}"
-    fill_in('search', with: @pet_1.name)
-    click_button 'search'
+      expect(current_path).to eq("/applications/pets/#{@pet_1.id}")
 
-    expect(current_path).to eq("/applications/#{@application.id}")
+      visit "/applications/#{@application.id}"
+      fill_in('search', with: @pet_1.name)
+      click_button 'search'
 
-    page.all(:link, :text => @pet_1.name)[1].click
+      expect(current_path).to eq("/applications/#{@application.id}")
 
-    expect(current_path).to eq("/applications/pets/#{@pet_1.id}")
+      page.all(:link, :text => @pet_1.name)[1].click
+
+      expect(current_path).to eq("/applications/pets/#{@pet_1.id}")
+    end
+  end
+
+  describe 'User Story #5' do
+    it 'displays an "Adopt Me" button next to search pets results' do
+
+      visit "/applications/#{@application.id}"
+      fill_in('search', with: @pet_1.name)
+      click_button 'search'
+
+      expect(page).to have_button("Adopt Me")
+
+      click_button "Adopt Me"
+
+      expect(page.has_field?("Pets", with: @pet_1.name)).to be true
+    end
   end
 end
