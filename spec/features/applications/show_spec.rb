950f4610c2af3e8669b5239f06298ee6803ce80d
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-09 19:14:27 -0800

    user story 5 complete

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -57,5 +57,23 @@ RSpec.describe 'Applications show page' do
       expect(current_path).to eq("/applications/#{application_1.id}")
       expect("Search").to appear_before("Sammy")
     end 
+
+    it 'can add a pet to the application' do 
+      shelter = Shelter.create!(name: "Brightside", city: "Salem", rank: 2, foster_program: true)
+      pet_1 = shelter.pets.create!(name: "Bobby", age: 2, breed: 'Bulldog', adoptable: true)
+      pet_2 = shelter.pets.create!(name: "Sammy", age: 5, breed: 'lab', adoptable: true)
+      application_1 = Application.create!(name: 'Billy Bob', street_address: "54984 Bulldog Rd", city: "Salem", state: "OR", zipcode: "97301", description: "Billy Bob wants a buddy", status: "In Progress")
+    
+      visit "/applications/#{application_1.id}"
+     
+      fill_in :search, with: pet_2.name
+      click_button "Search"
+
+      expect(current_path).to eq("/applications/#{application_1.id}")
+      
+      click_on "Adopt this Pet"
+      expect(current_path).to eq("/applications/#{application_1.id}")
+      expect(pet_2.name).to appear_before(application_1.status)
+    end 
   end
 end
