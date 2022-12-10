5bc009c0822e7699e76af95b859484fd7c7ca2a0
cemccabe <46434166+cemccabe@users.noreply.github.com>
2022-12-10 15:49:33 -0700

    Create tests for US4; Update Applications controller #show; Pass tests; Complete US4

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/applications', to: 'applications#index'
   post '/applications', to: "applications#create"
   get '/applications/:id', to: 'applications#show'
+  post '/applications/:id', to: 'applications#show'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
