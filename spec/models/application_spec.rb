8488b49e06d9907afeeec7875eb91e25563cb06d
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-10 10:53:42 -0800

    Add model test for status change

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -3,4 +3,13 @@ require 'rails_helper'
 RSpec.describe Application do
   it {should have_many(:pets).through(:application_pets)}
   it { should have_many :application_pets}
+  describe 'Change status' do
+    it 'changes the status to pending' do
+      application = Application.create!(name: 'Billy Bob', street_address: "54984 Bulldog Rd", city: "Salem", state: "OR", zipcode: "97301")
+     
+      application.change_status
+
+      expect(application.status).to eq('Pending')
+    end
+  end
 end
