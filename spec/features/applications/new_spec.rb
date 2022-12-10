a49d58185ece2e52c472b1b6caa4b597e1512314
Bryan Flanagan <bflanagan78@yahoo.com>
2022-12-10 13:59:09 -0700

    Complete user story 3, all tests pass, 100% test coverage

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -1,15 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "new application" do
-  before (:each) do 
-    shelter_1 = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
-    application_1 = Application.create!(name: 'Shaggy', street_address: '123 Mystery Lane', city: 'Denver', state: 'Colorado', zip_code: '80203', description: "I have snacks", status: "Pending")
-    pet_1 = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
-    pet_2 = Pet.create!(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
-    app_pet_1 = ApplicationPet.create!(pet_id: pet_1.id, application_id: application_1.id)
-    app_pet_1 = ApplicationPet.create!(pet_id: pet_2.id, application_id: application_1.id)
-  end
-
   it "application form" do
     visit "/pets"
 
@@ -37,7 +28,6 @@ RSpec.describe "new application" do
     click_button("Submit Application")
 
     new_application = Application.last
-    # save_and_open_page
     expect(current_path).to eq("/applications/#{new_application.id}")
   end
 
