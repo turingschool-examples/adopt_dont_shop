f91b52bbf7b98ead453eb017a27ec7f96a68188a
Max MacGillivray <mary.macgillivray@gmail.com>
2022-12-10 01:15:28 -0700

    Edited the before each to include @ symbols before objects, separated a link for the pets into its own test, edited seed and test objects for readability

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-    app1 = Application.create(name: "Max", street_address: "Made up St", city: "Denver", state: "CO", zip_code: "80000", description: "Love mix breeds. Lots of energy to play with a dog", status: "In Progress")
-    app2 = Application.create(name: "Alastair", street_address: "Fictional St", city: "Golden", state: "CO", zip_code: "80001", description: "Love big dogs. Great mountain walks on doorstep", status: "Accepted")
-    pet1 = Pet.create(name: 'Noodle', age: 2, breed: 'Border Collie', adoptable: true)
-    pet2 = Pet.create(name: 'Atlas', age: 2, breed: 'American Akita', adoptable: true)
+
+Application.destroy_all
+Pet.destroy_all
+
+app1 = Application.create!(name: "Max", 
+                          street_address: "Made up St", 
+                          city: "Denver", 
+                          state: "CO", zip_code: "80000", 
+                          description: "Love mix breeds. Lots of energy to play with a dog", 
+                          status: "In Progress"
+                          )
+app2 = Application.create!(name: "Alastair", 
+                          street_address: "Fictional St", 
+                          city: "Golden", 
+                          state: "CO", 
+                          zip_code: "80001", 
+                          description: "Love big dogs. Great mountain walks on doorstep", 
+                          status: "Accepted"
+                          )
+pet1 = Pet.create!(name: 'Noodle', 
+                  age: 2, 
+                  breed: 'Border Collie', 
+                  adoptable: true
+                  )
+pet2 = Pet.create!(name: 'Hercules', 
+                  age: 2, 
+                  breed: 'American Akita', 
+                  adoptable: true
+                  )
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,32 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe 'the application show page' do
-  # before(:each) do
-  #   app1 = Application.create(name: "Max", street_address: "Made up St", city: "Denver", state: "CO", zip_code: "80000", description: "Love mix breeds. Lots of energy to play with a dog", status: "In Progress")
-  #   app2 = Application.create(name: "Alastair", street_address: "Fictional St", city: "Golden", state: "CO", zip_code: "80001", description: "Love big dogs. Great mountain walks on doorstep", status: "Accepted")
-  #   pet1 = Pet.create(name: 'Noodle', age: 2, breed: 'Border Collie', adoptable: true)
-  #   pet2 = Pet.create(name: 'Hercules', age: 2, breed: 'American Akita', adoptable: true)
-  # end
+  before(:each) do
+    @app1 = Application.create(name: "Max", 
+                                street_address: "Made up St", 
+                                city: "Denver", 
+                                state: "CO", zip_code: "80000", 
+                                description: "Love mix breeds. Lots of energy to play with a dog", 
+                                status: "In Progress"
+                              )
+    @app2 = Application.create(name: "Alastair", 
+                                street_address: "Fictional St", 
+                                city: "Golden", 
+                                state: "CO", 
+                                zip_code: "80001", 
+                                description: "Love big dogs. Great mountain walks on doorstep", 
+                                status: "Accepted"
+                              )
+    @pet1 = Pet.create(name: 'Noodle', 
+                        age: 2, 
+                        breed: 'Border Collie', 
+                        adoptable: true
+                      )
+    @pet2 = Pet.create(name: 'Hercules', 
+                        age: 2, 
+                        breed: 'American Akita', 
+                        adoptable: true
+                      )
+  end
   # not sure why this isnt working, think it will be cleaner to use.
 
   it 'shows the application and all its attributes' do
-    app1 = Application.create(name: "Max", street_address: "Made up St", city: "Denver", state: "CO", zip_code: "80000", description: "Love mix breeds. Lots of energy to play with a dog", status: "In Progress")
-    app2 = Application.create(name: "Alastair", street_address: "Fictional St", city: "Golden", state: "CO", zip_code: "80001", description: "Love big dogs. Great mountain walks on doorstep", status: "Accepted")
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet1 = app1.pets.create(name: 'Noodle', age: 2, breed: 'Border Collie', adoptable: true, shelter_id: shelter.id)
-    pet2 = app1.pets.create(name: 'Hercules', age: 2, breed: 'American Akita', adoptable: true, shelter_id: shelter.id)
-
-    visit "/applications/#{app1.id}"
+    visit "/applications/#{@app1.id}"
     
-    expect(page).to_not have_content(app2.name)
-    expect(page).to have_content(app1.name)
-    expect(page).to have_content(app1.street_address)
-    expect(page).to have_content(app1.city)
-    expect(page).to have_content(app1.state)
-    expect(page).to have_content(app1.zip_code)
-    expect(page).to have_content(app1.description)
-    expect(page).to have_content(app1.status)
-    expect(page).to have_link(pet1.name)
-    expect(page).to have_link(pet2.name)
+    expect(page).to_not have_content(@app2.name)
+    expect(page).to have_content(@app1.name)
+    expect(page).to have_content(@app1.street_address)
+    expect(page).to have_content(@app1.city)
+    expect(page).to have_content(@app1.state)
+    expect(page).to have_content(@app1.zip_code)
+    expect(page).to have_content(@app1.description)
+    expect(page).to have_content(@app1.status)
+    expect(page).to have_content(@pet1.name)
+    expect(page).to have_content(@pet2.name)
+  end
+  
+  it 'has links to pet pages' do
+    visit "/applications/#{@app1.id}"
+    
+    expect(page).to have_link(@pet1.name)
+    expect(page).to have_link(@pet2.name)
   end
 end
