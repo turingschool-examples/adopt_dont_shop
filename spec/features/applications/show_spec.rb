27d6930d6afc1c05c99cee5bc3599fc2d2c74099
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-12-09 22:03:54 -0800

    Feat/test: add application pet search and tests

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -11,6 +11,7 @@
 require 'rails_helper'
 
 RSpec.describe 'The application show page' do
+  # add visit url to before each
   it 'displays all of the attributes of an application, and all the pets the application is for' do
     shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     application = Application.create!(name: "Jimbo Kepler", 
@@ -62,9 +63,28 @@ RSpec.describe 'The application show page' do
                                       zipcode: 00000, 
                                       description: "I love animals and they love me!", 
                                       status: "Pending")
-
+ 
     visit "/applications/#{application.id}"
 
     expect(page).to have_no_content("Add a pet to this application")
+  end
+
+  it 'searches for pets and renders results' do
+    shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    application = Application.create!(name: "Jimbo Kepler", 
+                                      address: "000 Street Name",
+                                      city: "City Name",
+                                      state: "STATE",
+                                      zipcode: 00000, 
+                                      description: "I love animals and they love me!", 
+                                      status: "In Progress")
+    
+    visit "/applications/#{application.id}"
+    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+    fill_in :query, with: 'Lobster'
+    click_button 'Search'
+
+    expect(page).to have_content("Lobster")
   end
 end
