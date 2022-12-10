9639aa0bff5aa0cf29fcc98c91e8df4661af15e1
Ryan Canton <ryancanton999@gmail.com>
2022-12-10 10:55:43 -0700

    Test: Add test for adding pets to applications

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -88,4 +88,38 @@ RSpec.describe 'The application show page' do
     expect(page).to have_content("Lobster")
     expect(current_path).to eq("/applications/#{application.id}")
   end
+
+#   As a visitor
+# When I visit an application's show page
+# And I search for a Pet by name
+# And I see the names Pets that match my search
+# Then next to each Pet's name I see a button to "Adopt this Pet"
+# When I click one of these buttons
+# Then I am taken back to the application show page
+# And I see the Pet I want to adopt listed on this application
+
+  it 'contains a button to add a pet to an application' do
+    shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    application = Application.create!(name: "Jimbo Kepler", 
+                                      address: "000 Street Name",
+                                      city: "City Name",
+                                      state: "STATE",
+                                      zipcode: 00000, 
+                                      description: "I love animals and they love me!", 
+                                      status: "In Progress")
+    
+    visit "/applications/#{application.id}"
+    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+    fill_in :query, with: 'Lobster'
+    click_button 'Search'
+
+    expect(page).to have_button("Adopt this pet")
+
+    click_button("Adopt this pet")
+
+    expect(current_path).to eq("/applications/#{application.id}")
+    expect(page).to have_content("Pets Added to Application:")
+    expect(page).to have_link("Lobster")
+  end
 end
