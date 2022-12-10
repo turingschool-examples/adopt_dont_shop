73f582815543e7090edd14c8a6b75aceaa884782
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-12-10 13:16:00 -0800

    Set up test even further for index

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -2,13 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'admin_shelters index page' do
   before :each do
-    @application = create(:application)
     @shelter_1   = create(:shelter)
+    @shelter_2   = create(:shelter)
+    @shelter_3   = create(:shelter)
+    @shelter_4   = create(:shelter)
+    @shelter_5   = create(:shelter)
   end
+
+  it 'displays the name of all the shelters' do
+    visit "/admin/shelters"
+
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_2.name)
+    expect(page).to have_content(@shelter_3.name)
+    expect(page).to have_content(@shelter_4.name)
+    expect(page).to have_content(@shelter_5.name)
+  end
+
   it 'displays the name of all the shelters in reverse alphabetical order' do
     visit "/admin/shelters"
 
-    expect(page).to have_content()
-    expect(page).to_not have_content()
+    expect(@shelter_1.name).to appear_before(@shelter_2.name)
+    expect(@shelter_2.name).to appear_before(@shelter_3.name)
+    expect(@shelter_3.name).to appear_before(@shelter_4.name)
+    expect(@shelter_4.name).to appear_before(@shelter_5.name)
   end
 end
