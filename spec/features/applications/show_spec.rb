7e54751bf9d8ab5214e6bae5d19e0af0665ec6a3
Joe King <j.king92@gmail.com>
2022-12-09 17:39:24 -0700

    Add tests for searching for a pet on an application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'the application show page' do
   it 'shows name of applicant' do
     shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
     pet = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
-    application1 = pet.applications.create!(name: 'John Doe', street: '123 N Washington Ave.', city: 'Denver', state: 'Colorado', zip: '91234', applicant_argument: 'caring and loving', status: "Pending")
+    application1 = pet.applications.create!(name: 'John Doe', street: '123 N Washington Ave.', city: 'Denver', state: 'Colorado', zip: '91234', applicant_argument: 'caring and loving', app_status: "Pending")
 
     visit "/applications/#{application1.id}"
 
@@ -15,16 +15,33 @@ RSpec.describe 'the application show page' do
     expect(page).to have_content(application1.zip)
     expect(page).to have_content(application1.applicant_argument)
     expect(page).to have_content(application1.pets.first.name)
-    expect(page).to have_content(application1.status)
+    expect(page).to have_content(application1.app_status)
+  end
+
+  it 'has a pet search field for in progress applications' do
+    shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    pet = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    pet1 = Pet.create(name: 'Scrappy', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    application1 = Application.create!(name: 'John Doe', street: '123 N Washington Ave.', city: 'Denver', state: 'Colorado', zip: '91234', applicant_argument: 'caring and loving', app_status: "In Progress")
+
+    visit "/applications/#{ application1.id }"
+
+    expect(page).to have_content("Add a Pet to this Application")
+    
+    fill_in("pet_search", with: "scooby")
+    click_button('Search')
+    expect(current_path).to eq("/applications/#{ application1.id }")
+    expect('Search').to appear_before('Scooby')
   end
 end
 
 
 # As a visitor
-# When I visit an applications show page
-# Then I can see the following:
-# - Name of the Applicant
-# - Full Address of the Applicant including street address, city, state, and zip code
-# - Description of why the applicant says they'd be a good home for this pet(s)
-# - names of all pets that this application is for (all names of pets should be links to their show page)
-# - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
+# When I visit an application's show page
+# And that application has not been submitted,
+# Then I see a section on the page to "Add a Pet to this Application"
+# In that section I see an input where I can search for Pets by name
+# When I fill in this field with a Pet's name
+# And I click submit,
+# Then I am taken back to the application show page
+# And under the search bar I see any Pet whose name matches my search
