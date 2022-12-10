f1a3a948c38c036847d58f6ac1e7052c51df1db1
Kelsie Porter <krporter22@gmail.com>
2022-12-09 18:22:28 -0700

    Create #new and #create

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationsController < ApplicationController
+  def new
+    
+  end
+
   def show
     @application = Application.find(params[:id])
+  end
+
+  def create 
+    application = Application.create(
+      permitted_params.merge({status: "In Progress"})
+    )
+
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+
+  def permitted_params
+    params.permit(
+      :human_name,
+      :street_address,
+      :city,
+      :state,
+      :zip,
+      :description
+    )
   end
 end
