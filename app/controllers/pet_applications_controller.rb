f52e026d1492fb466b9be5ea4b6a4ceeb3df26a0
Brandon Gray <brandonjoe88@hotmail.com>
2022-12-10 07:01:47 -0700

    feat: adds pet_application join table for pets & applications

---
+++ app/controllers/pet_applications_controller.rb
@@ -0,0 +1,3 @@
+class PetApplicationsController < ApplicationController
+  
+end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,6 @@
 class Application < ApplicationRecord
-  enum status: {"in progress": 0, "pending": 1, "Accepted": 2, "Rejected": 3}
+  has_many :pet_applications
+  has_many :pets, through: :pet_applications
 
+  enum status: {"in progress": 0, "pending": 1, "Accepted": 2, "Rejected": 3}
 end
