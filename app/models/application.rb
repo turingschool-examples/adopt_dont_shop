baa4706fe096e608189b2817219601ee7a371dec
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-10 12:06:31 -0800

    complete us 9

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,7 @@ class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
   validates :name, :street_address, :city, :state, :zipcode, presence: true
+  
   def change_status
     self.status = 'Pending'
   end
