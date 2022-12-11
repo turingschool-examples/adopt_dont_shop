055b854954b452d58591b8e35d0a4daa236b6d3e
J Seymour <JustJakeSeymour@gmail.com>
2022-12-11 12:06:22 -0700

    complete factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -14,8 +14,35 @@ FactoryBot.define do
     age { Faker::Number.number(digits: 1) }
     breed { Faker::Creature::Dog.breed }
     adoptable { true }
+    shelter
+  end
+
+  factory :shelter do
+    name { Faker::Educator.campus }
+    city { Faker::Address.city }
+    foster_program { Faker::Boolean.boolean }
+  end
+
+  factory :veterinarian do
+    name { Faker::Name.name }
+    on_call { Faker::Boolean.boolean }
+    review_rating { Faker::Number.between(from: 1, to: 10) }
+    veterinary_office
+  end
+
+  factory :veterinary_office do
+    name { Faker::University.suffix }
+    max_patient_capacity { Faker::Number.between(from: 15, to: 30) }
+    boarding_services { Faker:Boolean.boolean }
+  end
+
+  factory :application_pet do
+    pet
+    application
   end
 end
+
+# Build vs Create (build is destroyed after being used, better for tests?)
 # application = build(:application)
 # pet = build(:pet)
 # shelter = build(:shelter)
