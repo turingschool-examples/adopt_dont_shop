639e3f4b3586d45ad1826d29ee16de024dca4e26
Anhnhi Tran <anhtran811@gmail.com>
2022-12-10 17:16:38 -0700

    User story 11 - wrote test to see section with passing test

--- spec/features/admin/shelters_index_spec.rb
+++ spec/features/admin/shelters_index_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe "Admin Shelters Index" do
       expect("Healthy Paws").to appear_before("AAA Shelter")
     end
   end
+  
+  # As a visitor
+  # When I visit the admin shelter index ('/admin/shelters')
+  # Then I see a section for "Shelter's with Pending Applications"
+  # And in this section I see the name of every shelter that has a pending application
+
+  describe "User Story 11" do
+    it 'has a section for shelters with pending applications' do
+      seed_shelters
+      seed_applications
+
+      visit '/admin/shelters'
+
+      expect(page).to have_content("Shelter's with Pending Applications")
+    end
+  end
 end
