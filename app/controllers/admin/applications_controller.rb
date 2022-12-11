be21fd8300cda0461e8c9b6b21d40a45d862acec
Joe King <j.king92@gmail.com>
2022-12-11 13:32:56 -0700

    Add approved column to Application Pets table and add reject and approve methods to Application Pet model

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,11 +1,18 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    # @application_pets = @application.application_pets
     @pets = @application.pets
     if params[:pet_id]
+      @application_pet = ApplicationPet.find_by_pet_and_app(params[:pet_id], params[:id])
+      @application_pet.approve
+      @status = @application_pet.approved
       pet = Pet.find(params[:pet_id])
       pet.adopt
-      # require 'pry'; binding.pry
+    elsif params[:reject_pet_id]
+      @application_pet = ApplicationPet.find_by_pet_and_app(params[:reject_pet_id], params[:id])
+      @application_pet.reject
+      @status = @application_pet.approved
     end
   end
 end
