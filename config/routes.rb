fc76a849f32591b8aaadc83dfe21b36881962de7
Leo Banos Garcia <bsleo625@gmail.com>
2022-12-11 14:09:46 -0700

    Adds route for application_pet get

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   resources :applications, only: [:show, :new, :create]
 
-
   resources :pets, only: [:index, :destroy, :show, :edit, :update]
 
   resources :shelters, only: [:index, :new, :create, :destroy, :show, :edit, :update]
+
+  resources :application_pets, only: [:create]
 
 end
--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -4,6 +4,5 @@ RSpec.describe ApplicationPet, type: :model do
     describe 'relationships' do
         it {should belong_to :application}
         it {should belong_to :pet}
-
     end
 end
