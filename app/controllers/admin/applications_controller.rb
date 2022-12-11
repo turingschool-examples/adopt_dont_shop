e25b76204ed263b91ae06a39e44f9dbaa3bd8b9a
Braxton Eddings <beddings81@gmail.com>
2022-12-11 16:03:39 -0600

    Make adjustments towards admins apllications show page

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,18 +1,24 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    # @application_pets = @application.application_pets
+    # @application_pets needs to be defined here?
     @pets = @application.pets
+    @application_pets = []
+    @pets.each do |pet|
+      require 'pry'; binding.pry
+      @application_pets << ApplicationPet.find_by_pet_and_app(pet.id, params[:id])
+    end
     if params[:pet_id]
       @application_pet = ApplicationPet.find_by_pet_and_app(params[:pet_id], params[:id])
       @application_pet.approve
-      @status = @application_pet.approved
       pet = Pet.find(params[:pet_id])
       pet.adopt
     elsif params[:reject_pet_id]
       @application_pet = ApplicationPet.find_by_pet_and_app(params[:reject_pet_id], params[:id])
       @application_pet.reject
-      @status = @application_pet.approved
+    # else
+    #   @application_pet = ApplicationPet.find_by_pet_and_app(params[:reject_pet_id], params[:id])
+    #   @status = @application_pet.approved
     end
   end
 end
--- spec/features/admins/shelters/index_spec.rb
+++ spec/features/admins/shelters/index_spec.rb
@@ -1,19 +1,7 @@
 require 'rails_helper' 
 
 RSpec.describe 'admin shelter index' do
-  # before(:each) do
-  #   @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-  #   @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-  #   @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
 
-  #   @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
-  #   @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-  #   @pet_3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
-  #   @pet_4 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
-
-  #   @application = @pet_1.applications.create!(name: 'John Doe', street: '123 N Washington Ave.', city: 'Denver', state: 'Colorado', zip: '91234', applicant_argument: 'caring and loving', app_status: "Pending")
-  # end
-  
   it 'lists all shelters in reverse alphabetical order' do
     shelter1 = Shelter.create!(foster_program: true, name: "Doggie Dog World", city: "Denver", rank: 3)
     shelter2 = Shelter.create!(foster_program: true, name: "Endless Pawsabilities", city: "Durango", rank: 1)
