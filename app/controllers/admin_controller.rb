40da627dce05720e8f26abe8de5c15d96b9e7c13
Anhnhi Tran <anhtran811@gmail.com>
2022-12-11 08:43:49 -0700

    Used helper method pending application in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,10 @@
 class AdminController < ApplicationController
   def shelters_index
+   
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name desc")
+    pending_applications = Application.pending_application
+    require 'pry'; binding.pry
+
   end
+
 end
