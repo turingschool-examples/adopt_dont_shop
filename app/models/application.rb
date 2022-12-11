0ecc42c003d1caf31f851c51b35cc917f734755f
Samuel Cox <samc1253@gmail.com>
2022-12-11 15:54:11 -0700

    Add feature that means if all pets are approved on an application, that application is approved

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,19 @@ class Application < ApplicationRecord
   validates :name, :street_address, :city, :state, :zip_code, presence: true
   validates :zip_code, length: { is: 5 }
   validates :zip_code, :numericality => { :greater_than_or_equal_to => 0}
+
+  def approved?
+    application_pets = pets.map do |pet|
+      ApplicationPet.find_application_pet(id, pet.id)
+    end
+
+    if application_pets.all? { |application_pet| application_pet.pet_status == 'Approved' }
+      update(status: "Approved")
+      save
+      pets.each { |pet| pet.update(adoptable: false) pet.save }
+    elsif application_pets.any? { |application_pet| application_pet.pet_status == 'Rejected' }
+      update(status: "Rejected")
+      save
+    end
+  end
 end
