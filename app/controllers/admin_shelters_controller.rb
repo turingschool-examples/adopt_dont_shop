038c3e2e19c3d0241d6b747bcfa7042e403ac1a4
Anhnhi Tran <anhtran811@gmail.com>
2022-12-11 10:38:41 -0700

    Fixed naming of admin to adminshelter in all specs, views, renamed directories and files

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,8 +1,9 @@
-class AdminController < ApplicationController
-  def shelters_index
+class AdminSheltersController < ApplicationController
+  def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name desc")
     pending_applications = Application.pending_application
     @shelters_pending_apps = Shelter.find_shelters_from_application(pending_applications)
   end
 
 end
+
