07a3651100483e783c742b37a8fd8b707ef8b902
J Seymour <JustJakeSeymour@gmail.com>
2022-12-11 14:50:02 -0700

    admin application show behaving per user story 12

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,5 +1,6 @@
 class AdminApplicationsController < ApplicationController
   def show
+    @application_pets
     @application = Application.find(params[:application_id])
     @pets = @application.pets
   end
