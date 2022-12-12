3018a3ac93b742adf310ac81703a409921e87d24
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-12-12 13:28:43 -0800

    set up partial for approved pets

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'application#welcome'
 
   get '/admin/shelters', to: 'admin_shelters#index'
+  get '/admin/applications/:id', to: 'applications#show'
 
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'application' do
   it 'displays a link to all pets' do
     visit '/'
-    expect(page).to have_content("Adopt, don't shop!")
+    expect(page).to have_content("Adopt, Don't Shop!")
     expect(page).to have_link("Pets")
     click_link("Pets")
     expect(page).to have_current_path('/pets')
