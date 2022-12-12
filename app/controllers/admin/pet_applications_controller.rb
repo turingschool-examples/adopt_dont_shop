d0f75f8b156aa2656ed2b9f25388e47955719423
Alex Pitzel <pitzelalex@gmail.com>
2022-12-12 18:04:10 -0500

    fix: Move private params methods correctly into the pet_applications_controller class block

--- app/controllers/admin/pet_applications_controller.rb
+++ app/controllers/admin/pet_applications_controller.rb
@@ -4,14 +4,15 @@ class Admin::PetApplicationsController < ApplicationController
     pa.update(update_params)
     redirect_to "/admin/applications/#{pa.application_id}"
   end
+  
+  private
+  
+  def pa_params
+    { pet_id: params[:pet_id], application_id: params[:id] }
+  end
+  
+  def update_params
+    { status: params[:approve] }
+  end
 end
 
-private
-
-def pa_params
-  { pet_id: params[:pet_id], application_id: params[:id] }
-end
-
-def update_params
-  { status: params[:approve] }
-end
