9e312d164c5c1d7c93a4d720029016793f79a2a2
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-12-12 07:47:45 -0800

    add a little to admin application show

---
+++ spec/features/admin/applications/show_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'admin applications show page' do
+# As a visitor
+# When I visit an admin application show page ('/admin/applications/:id')
+# For every pet that the application is for, I see a button to approve the application for that specific pet
+# When I click that button
+# Then I'm taken back to the admin application show page
+# And next to the pet that I approved, I do not see a button to approve this pet
+# And instead I see an indicator next to the pet that they have been approved
+
+end
