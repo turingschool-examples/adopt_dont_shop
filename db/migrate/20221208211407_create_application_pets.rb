9f5c2f8ac57d19d9bf97d1461bec761115f202f2
Anhnhi Tran <anhtran811@gmail.com>
2022-12-12 16:08:52 -0700

    Add default value for adopted to be false

--- db/migrate/20221208211407_create_application_pets.rb
+++ db/migrate/20221208211407_create_application_pets.rb
@@ -1,6 +1,7 @@
 class CreateApplicationPets < ActiveRecord::Migration[5.2]
   def change
     create_table :application_pets do |t|
+      t.boolean :adopted
       t.references :pet, foreign_key: true
       t.references :application, foreign_key: true
 
