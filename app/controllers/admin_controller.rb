6a5766c103bae8c0c77626ef7f19add3de34904d
Anhnhi Tran <anhtran811@gmail.com>
2022-12-12 12:58:08 -0700

    Removed helper method in admin controller, fixed view, test passing

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,6 @@
 class AdminController < ApplicationController
   def shelters_index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name desc")
-    pending_applications = Application.pending_application
-    @shelters_pending_apps = Shelter.find_shelters_from_application(pending_applications)
+    @shelters_pending_apps = Shelter.find_shelters_from_application
   end
 end 
