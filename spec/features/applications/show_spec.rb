73a447c581c66db9a168790e49356206beb65789
Lacey Weaver <jlweave89@gmail.com>
2022-12-12 10:29:12 -0700

    Add finished user story nine

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -125,4 +125,22 @@ RSpec.describe 'the application show' do
       end
     end
   end
+  describe 'User Story #9' do
+    describe 'Visit application show page And I search for pets by name' do
+      it 'Then I see any pet by name and search is not case sensitve' do
+        shelter_1 = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+        application_1 = Application.create!(name: 'Shaggy', street_address: '123 Mystery Lane', city: 'Denver', state: 'Colorado', zip_code: '80203', description: "I have snacks", status: "Pending")
+        pet_1 = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
+        pet_2 = Pet.create!(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
+        app_pet_1 = ApplicationPet.create!(pet_id: pet_1.id, application_id: application_1.id)
+
+        visit "/applications/#{application_1.id}"
+        
+        fill_in "search_for_pet", with: "sCoobY"
+        click_on "Submit"
+
+        expect(page).to have_content("Scooby")
+      end
+    end
+  end
 end
