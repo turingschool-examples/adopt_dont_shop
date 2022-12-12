72fbdafc625cca2ceeda2d75cddd02fcb2f3fa9b
Max MacGillivray <mary.macgillivray@gmail.com>
2022-12-12 11:31:22 -0700

    Updated tests for app submit, fixed errors in show page, complete user story 6

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
   post'/applications/new', to: 'applications#create'
+  
+  patch '/applications/:id', to: 'applications#update'
+  # post '/applications/:id', to: 'applications#show'
+  
 
   post '/applications/applicationpets/create', to: 'application_pets#create'
 
