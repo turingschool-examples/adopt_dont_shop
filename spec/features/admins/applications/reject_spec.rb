5455edaa9e4a84d5210713e7619df0818c75dad5
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-12 16:54:12 -0800

    add and pass us 14

--- spec/features/admins/applications/reject_spec.rb
+++ spec/features/admins/applications/reject_spec.rb
@@ -18,5 +18,23 @@ RSpec.describe 'rejecting application' do
       expect(page).to_not have_content("Reject #{pet_1.name}")
       expect(page).to_not have_content("Approve #{pet_1.name}")
     end 
+
+    it 'will not effect another application if approved or rejected' do 
+      shelter = Shelter.create!(name: "Brightside", city: "Salem", rank: 2, foster_program: true)
+      pet_1 = shelter.pets.create!(name: "Bobby", age: 2, breed: 'Bulldog', adoptable: true)
+      pet_2 = shelter.pets.create!(name: "Sammy", age: 5, breed: 'lab', adoptable: true)
+      application_1 = pet_1.applications.create!(name: 'Billy Bob', street_address: "54984 Bulldog Rd", city: "Salem", state: "OR", zipcode: "97301", description: "Billy Bob wants a buddy", status: "Pending")
+      ApplicationPet.create!(pet: pet_2, application: application_1)
+      
+      application_2 = pet_1.applications.create!(name: 'Billy Bob', street_address: "54984 Bulldog Rd", city: "Salem", state: "OR", zipcode: "97301", description: "Billy Bob wants a buddy", status: "Pending")
+
+      visit "/admin/applications/#{application_1.id}"
+     
+      click_on("Reject #{pet_1.name}")
+
+      visit "/admin/applications/#{application_2.id}"
+      
+      expect(page).to_not have_content("Rejected")
+    end 
   end 
 end 
