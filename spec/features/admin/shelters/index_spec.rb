e7d9312a43cd455ace7876d27f6b2efa3e243e91
Lacey Weaver <jlweave89@gmail.com>
2022-12-13 14:04:59 -0700

    Add test for user story 11

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -5,9 +5,20 @@ RSpec.describe 'admin shelters index' do
     @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-    @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @shelter_3.pets.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    
+    @shelter_1.pets.create!(name: 'Scooby', breed: 'Great Dane', age: 2, adoptable: true)
+    @shelter_1.pets.create!(name: 'Scrappy', breed: 'Great Dane', age: 1, adoptable: true)
+    @shelter_3.pets.create!(name: 'Spot', breed: 'Dalmation', age: 3, adoptable: true)
+    # @shelter_4.pets.create!(name: 'Garfield', breed: 'Tabby', age: 7, adoptable: true)
+    
+    @pet_1 = @application_1.pets.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
+    @pet_2 = @application_1.pets.create!(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
+    @pet_3 = @application_2.pets.create!(name: 'Spot', age: 3, breed: 'Dalmation', adoptable: true, shelter_id: shelter_3.id)
+    # @pet_4 = @application_3.pets.create!(name: 'Garfield', age: 7, breed: 'Tabby', adoptable: true, shelter_id: shelter_2.id)
+    
+    @application_1 = Application.create!(name: 'Shaggy', street_address: '123 Mystery Lane', city: 'Denver', state: 'Colorado', zip_code: '80203')
+    @application_2 = Application.create!(name: 'Cruela De Ville', street_address: '666 Dalmation Way', city: 'Highland Ranch', state: 'Colorado', zip_code: '80234')
+    @application_3 = Application.create!(name: 'Sarah McGlachlan', street_address: '444 Arms of an Angel', city: 'Aspen', state: 'Colorado', zip_code: '80444')
   end
 
   describe 'When I visit the admin shelter index (/admin/shelters)' do
@@ -21,7 +32,9 @@ RSpec.describe 'admin shelters index' do
       visit "/admin/shelters"
 
       expect(page).to have_content("Shelters with Pending Applications")
-      # expect()
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_3.name)
+      expect(page).to_not have_content(@shelter_2.name)
     end
   end
 
