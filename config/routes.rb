e8ae2421d578886d2918a897aac0f498cf5200ff
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-12-13 07:32:43 -0800

    Add partialand rute for admin app show

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/admin/shelters', to: 'admin_shelters#index'
   get '/admin/applications/:id', to: 'applications#show'
+  post '/admin/applications/:id', to: 'applications#show'
 
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
