3d35b30426d1a0df2b778d9ac831820ae3afeae9
Braxton Eddings <beddings81@gmail.com>
2022-12-13 15:58:19 -0600

    Completes user story 21

--- spec/features/admins/shelters/index_spec.rb
+++ spec/features/admins/shelters/index_spec.rb
@@ -30,4 +30,27 @@ RSpec.describe 'admin shelter index' do
       expect(page).to_not have_content('Fancy pets of Colorado')
     end
   end
+
+  it 'has a link to every shelters show page' do
+    shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+    shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+    pet_2 = shelter_2.pets.create(name: 'Mr. Krabs', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+    application1 = pet_1.applications.create!(name: 'John Doe', street: '123 N Washington Ave.', city: 'Denver', state: 'Colorado', zip: '91234', applicant_argument: 'caring and loving', app_status: "Pending")
+    application2 = pet_2.applications.create!(name: 'Jimmy Law', street: '123 N Washington Ave.', city: 'Denver', state: 'Colorado', zip: '91234', applicant_argument: 'caring and loving', app_status: "Pending")
+
+    visit "/admin/shelters"
+
+    within("div#all") do
+      expect(page).to have_link('Aurora shelter', href: "/admin/shelters/#{shelter_1.id}")
+      expect(page).to have_link('RGV animal shelter', href: "/admin/shelters/#{shelter_2.id}")
+      expect(page).to have_link('Fancy pets of Colorado', href: "/admin/shelters/#{shelter_3.id}")
+    end
+    
+    within("div#pending") do
+      expect(page).to have_link('Aurora shelter', href: "/admin/shelters/#{shelter_1.id}")
+      expect(page).to have_link('RGV animal shelter', href: "/admin/shelters/#{shelter_2.id}")
+    end
+  end
 end
