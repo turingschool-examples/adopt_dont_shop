c2a3b27cd8e51651d66dc8bd009d723e3ac2913e
Matisse Mallette <matissemallette@gmail.com>
2022-12-12 17:54:20 -0700

    complete user_story_20

--- spec/features/admin_shelters/index_spec.rb
+++ spec/features/admin_shelters/index_spec.rb
@@ -45,7 +45,6 @@ RSpec.describe 'the shelters index' do
     within('#pending') do 
       expect(@shelter_1.name).to appear_before(@shelter_3.name)
       expect(@shelter_3.name).to appear_before(@shelter_2.name)
-      save_and_open_page
     end
   end
 end
