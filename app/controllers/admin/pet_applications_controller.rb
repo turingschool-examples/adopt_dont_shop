a82f791a49094e4d20267988aed712770742c2a0
Alex Pitzel <pitzelalex@gmail.com>
2022-12-13 17:18:20 -0500

    refactor: Move application status update trigger out of the admins controller and move it to the pet_applications controller

--- app/controllers/admin/pet_applications_controller.rb
+++ app/controllers/admin/pet_applications_controller.rb
@@ -2,6 +2,14 @@ class Admin::PetApplicationsController < ApplicationController
   def update
     pa = PetApplication.where(pa_params).first
     pa.update(update_params)
+    @application = Application.find(params[:id])
+    @pets = @application.pets
+    if approved
+      @application.update!(status: "Approved")
+      adopt_pets
+    end
+    @application.update!(status: "Rejected") if rejected
+
     redirect_to "/admin/applications/#{pa.application_id}"
   end
   
@@ -14,5 +22,22 @@ class Admin::PetApplicationsController < ApplicationController
   def update_params
     { status: params[:approve] }
   end
-end
 
+  def check_approved
+    @application.pet_applications.pluck(:status)
+  end
+
+  def approved
+    check_approved.all? { |status| status == 'true' }
+  end
+
+  def rejected
+    check_approved.include?('false') && check_approved.include?(nil) == false
+  end
+
+  def adopt_pets
+    @pets.each do |pet|
+      pet.update!(adoptable: false)
+    end
+  end
+end
--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,34 +4,9 @@ class AdminsController < ApplicationController
     @pending_app_shelters = Shelter.pending_app_shelters
   end
 
-  def show 
+  def show
     @application = Application.find(params[:id])
     @pets = @application.pets
-    if approved
-      @application.update!(status: "Approved")
-      adopt_pets
-    end
-    @application.update!(status: "Rejected") if rejected
     @pet_applications = @application.pet_applications
-  end
-
-  private
-
-  def check_approved
-    @application.pet_applications.pluck(:status)
-  end
-
-  def approved
-    check_approved.all? { |status| status == 'true' }
-  end
-
-  def rejected
-    check_approved.include?('false') && check_approved.include?(nil) == false
-  end
-
-  def adopt_pets
-    @pets.each do |pet|
-      pet.update!(adoptable: false)
-    end
   end
 end
