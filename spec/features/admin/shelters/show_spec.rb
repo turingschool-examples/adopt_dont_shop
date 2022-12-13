aea276a3495418c5bd18b815fdb57cdefd4e0c3b
J Seymour <JustJakeSeymour@gmail.com>
2022-12-13 16:55:45 -0700

    write test for admin shelters show per story 19

---
+++ spec/features/admin/shelters/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Shelters Show" do
+
+  let!(:shelter) {build_stubbed(:shelter)}
+  let!(:another_shelter) {build_stubbed(:shelter)}
+  
+  describe "User Story 19" do
+    it 'shows name and full address on show page' do
+      # Admin Shelters Show Page
+      visit "/admin/shelters/#{shelter.id}"
+      # As a visitor
+      # When I visit an admin shelter show page
+      # Then I see that shelter's name and full address
+      expect(page).to have_content(shelter.name)
+      expect(page).to have_content(shelter.city)
+      expect(page).to_not have_content(another_shelter.name)
+      expect(page).to_not have_content(another_shelter.city)
+      # NOTE: Your query should only return the necessary data to complete the story
+    end
+  end    
+end
