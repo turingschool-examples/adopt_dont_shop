90605ec2c01c264cadbd9b0f4a4c5670fbbf62ad
Max MacGillivray <mary.macgillivray@gmail.com>
2022-12-13 01:18:52 -0700

    Started in on the reject button

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -70,16 +70,38 @@ RSpec.describe 'the admin application show page' do
     visit "admin/applications/#{@app1.id}"
     click_on "Approve #{@pet1.name}'s Adoption"
     
-    expect(current_path).to eq ("/applications/#{@app1.id}")
+    expect(current_path).to eq ("/applications/#{@app1.id}") #Not the path outlined in the US, that's the next line
+    
     # expect(current_path).to eq ("/admin/applications/#{@app1.id}")
     # expect(@app1.status).to eq('Approved')
+    # √ When I visit an admin application show page ('/admin/applications/:id')
+    # √ For every pet that the application is for, I see a button to approve the application for that specific pet
+    # √ When I click that button
+    
+    # Then I'm taken back to the admin application show page
+    
+    # √ And next to the pet that I approved, I do not see a button to approve this pet
+    # √ And instead I see an indicator next to the pet that they have been approved
   end
-  # √ When I visit an admin application show page ('/admin/applications/:id')
-  # √ For every pet that the application is for, I see a button to approve the application for that specific pet
-  # √ When I click that button
   
-  # Then I'm taken back to the admin application show page
+  it 'every pet listed has a reject button' do
+    visit "admin/applications/#{@app1.id}"
+
+    expect(page).to have_button("Reject #{@pet1.name}'s Adoption")
+    expect(page).to have_button("Reject #{@pet2.name}'s Adoption")
+  end
   
-  # √ And next to the pet that I approved, I do not see a button to approve this pet
-  # √ And instead I see an indicator next to the pet that they have been approved
+  xit 'clicking the reject button changes the application status' do
+    visit "admin/applications/#{@app1.id}"
+    click_on "Reject #{@pet1.name}'s Adoption"
+    
+    expect(current_path).to eq ("/applications/#{@app1.id}") #Not the path outlined in the US, that's the next line
+
+    # √ When I visit an admin application show page ('/admin/applications/:id')
+    # √ For every pet that the application is for, I see a button to reject the application for that specific pet
+    # √ When I click that button
+    # Then I'm taken back to the admin application show page
+    # And next to the pet that I rejected, I do not see a button to approve or reject this pet
+    # And instead I see an indicator next to the pet that they have been rejected
+  end
 end
