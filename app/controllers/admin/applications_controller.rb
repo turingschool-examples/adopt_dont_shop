1e7bd8357db15508176d6637c08a6e45a54e8172
Alastair Lloyd <dex8056@aol.com>
2022-12-14 14:54:19 -0700

    remove unused method which moved to correct controller

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -2,10 +2,4 @@ class Admin::ApplicationsController < ApplicationController
   def show 
     @application = Application.find(params[:id])
   end
-  
-  def update
-    application = ApplicationPet.find(params[:id])
-    application.update(status: "Approved")
-      redirect_to "admin/applications/#{application.id}"
-  end
 end
