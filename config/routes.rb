6245dedda8bf597a65ce0161733dd8830ebcc685
Harrison Ryan <hwryan12@gmail.com>
2023-02-09 16:43:23 -0600

    feature: Create application show page and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'application#welcome'
 
+  get '/applications/:id', to: 'applications#show'
+
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
