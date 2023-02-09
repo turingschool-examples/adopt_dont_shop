c6c03600974e7934475034be6cd9694e09559e50
Isaac Alter <i_alter@yahoo.com>
2023-02-09 15:08:10 -0700

    Add feat: User Story 2 check for link test in pet index spec

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -81,4 +81,12 @@ RSpec.describe 'the pets index' do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
+
+  it 'has a link that lead to a ''new application'' page' do
+    expect(page).to have_content("Start an Application")
+
+    click_link("Start an Application")
+
+    expect(page).to have_current_path("/applications/new")
+  end
 end
