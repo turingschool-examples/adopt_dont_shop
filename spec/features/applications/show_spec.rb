fd6a530e0d8170d35f61afd64f8ec41f2ce53db9
Tori Enyart <torienyart@gmail.com>
2023-02-09 15:39:17 -0700

    Set up Application and Pets many to many relationship

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Application Show Page' do 
+  describe 'When I visit an applications show page' do
+    it 'shows name of the applicant' do
+      visit 
+    end
+
+    it 'shows Full Address of the Applicant including street address, city, state, and zip code' 
+
+    it "shows Description of why the applicant says they'd be a good home for this pet(s)"
+
+    it "shows names of all pets that this application is for"
+
+    it "all names of pets should be links to their show page"
+
+    it "The Application's status, either 'In Progress', 'Pending', 'Accepted', or 'Rejected'"
+
+  end
+end
