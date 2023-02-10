58980424583e309cb4137b7ce0f35a2acd5eb031
KaraJH <karas.jones@yahoo.com>
2023-02-10 16:08:45 -0700

    Adds actions and views to submit an application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationsController < ApplicationController 
 
+  def show 
+
+  end
+
+  def new 
+    @new_app = Application.new
+  end
+
+  def create 
+    @new_app = Application.create(application_attributes)
+      # if @new_app.status.nil? 
+      #   @new_app.status = "In Progress"
+      #   @new_app.save
+      # end # otherwise, it saved without a status..
+    redirect_to "/applications/#{@new_app.id}"
+    
+  end
+
+  def application_attributes 
+    params.permit(:name, :street_address, :city, :state, :zipcode, :description, :status)
+  end
+
 end
