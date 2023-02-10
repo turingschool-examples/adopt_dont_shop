459e437d90e086c89ba48849ba3cff973bbdf332
Elle Majors <hellolovely@ellemajors.com>
2023-02-10 11:43:08 -0700

    add: user story 1 describe block

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -9,26 +9,28 @@ describe 'applications show page', type: :feature do
 
   let!(:shelter) {Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)}
 
-  it 'shows applicant name, address, description, application pets, and application status' do
-    visit "applications/#{application.id}"
+  describe 'user story 1' do
+    it 'shows applicant name, address, description, application pets, and application status' do
+      visit "applications/#{application.id}"
 
-    expect(page).to have_content(application.name)
-    expect(page).to_not have_content(application2.name)
-    expect(page).to have_content("#{application.street_address} #{application.city}, #{application.state} #{application.zip_code}")
-    expect(page).to have_content(application.description)
-    expect(page).to have_content(pet1.name)
-    expect(page).to have_content(pet2.name)
-    expect(page).to have_content(application.status)
-  end
+      expect(page).to have_content(application.name)
+      expect(page).to_not have_content(application2.name)
+      expect(page).to have_content("#{application.street_address} #{application.city}, #{application.state} #{application.zip_code}")
+      expect(page).to have_content(application.description)
+      expect(page).to have_content(pet1.name)
+      expect(page).to have_content(pet2.name)
+      expect(page).to have_content(application.status)
+    end
 
-  it 'has a link to the show page of each pet listed in the application' do
-    visit "applications/#{application.id}"
-    
-    expect(page).to have_link(pet1.name)
-    expect(page).to have_link(pet2.name)
+    it 'has a link to the show page of each pet listed in the application' do
+      visit "applications/#{application.id}"
+      
+      expect(page).to have_link(pet1.name)
+      expect(page).to have_link(pet2.name)
 
-    click_link "#{pet1.name}"
+      click_link "#{pet1.name}"
 
-    expect(current_path).to eq("/pets/#{pet1.id}")
-  end
+      expect(current_path).to eq("/pets/#{pet1.id}")
+    end
+  end  
 end
