76e0ef438ee15d1730d9c67b2a0012565e5f7313
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-02-10 16:25:22 -0700

    User story 4

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'visit application show page' do
     let!(:pet_1) { shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true) }
     let!(:pet_2) { shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true) }
     let!(:pet_3) { shelter_3.pets.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true) }
+    let!(:pet_4) { shelter_1.pets.create!(name: 'Ms. Pirate', breed: 'tuxedo shorthair', age: 8, adoptable: true) }
 
     let!(:app_1) { Application.create!(name: 'Steve', 
                                 street: '152 Steve St.', 
@@ -17,7 +18,7 @@ RSpec.describe 'visit application show page' do
                                 desc: "I'm nice.", 
                                 status: 'Pending') }
     
-    let!(:pet_app_1) {PetApplication.create!(pet_id: pet_1.id, application_id: app_1.id)}
+    # let!(:pet_app_1) {PetApplication.create!(pet_id: pet_1.id, application_id: app_1.id)}
 
   describe 'application show' do
     it 'it shows the application attributes' do
@@ -28,9 +29,36 @@ RSpec.describe 'visit application show page' do
       expect(page).to have_content(app_1.city)
       expect(page).to have_content(app_1.state)
       expect(page).to have_content(app_1.zip)
-      expect(page).to have_content(pet_1.name)
       expect(page).to have_content(app_1.desc)
       expect(page).to have_content(app_1.status)
     end
   end
+
+	describe 'application in progress, add pets' do
+		it 'has an add pets field and button' do
+			visit "/applications/#{app_1.id}"
+
+			expect(page).to have_field('Add a Pet to this Application')
+			expect(page).to have_button('Search Pets')
+		end
+
+		it 'when I fill out field and click Search Pet, I am taken back to show page' do
+			visit "/applications/#{app_1.id}"
+
+			fill_in 'pet_name', with: 'Mr. Pirate'
+			click_button
+
+			expect(current_path).to eq("/applications/#{app_1.id}")
+		end
+
+		it 'shows all pets with search value under search field' do
+			visit "/applications/#{app_1.id}"
+
+			fill_in 'pet_name', with: 'Pirate'
+			click_button
+
+			expect(page).to have_content(pet_1.name)
+			expect(page).to have_content(pet_4.name)
+		end
+	end
 end
