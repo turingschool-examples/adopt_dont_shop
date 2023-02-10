21badaad3a332c7923649e4d1b0e1c1828c15979
Elle Majors <hellolovely@ellemajors.com>
2023-02-09 19:31:11 -0700

    add: applications schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_001717) do
+ActiveRecord::Schema.define(version: 2023_02_10_021734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.string "name"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.string "description"
+    t.string "status"
+
+    t.timestamp
+  end
 
   create_table "pets", force: :cascade do |t|
     t.boolean "adoptable"
