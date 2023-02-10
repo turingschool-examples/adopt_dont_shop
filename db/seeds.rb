9ff6d77522a40ef1f7d366010c212515de3122a4
Caleb Thomas <cjthomas00@comcast.net>
2023-02-09 20:19:34 -0700

    add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+shelter_1 = Shelter.create!(foster_program: false, name: "The Farm", city: "Denver", rank: 2)
+pet_1 = Pet.create!(adoptable: true, age: 1, breed: "Great Dane", shelter_id: 1, name: 'Scooby', )
+app_1 = Application.create!(name: "John Smith", street_address: "123 Main St.", city: "Denver", state: "CO", zip_code: 80202, description:"I want a pet.", status: "Accepted")
