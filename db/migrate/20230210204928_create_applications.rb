bf6e88cdf8323756b98a94b5ebd944f555cef29e
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-02-10 14:50:09 -0700

    Add: relationship tests for application and application_pet models

---
+++ db/migrate/20230210204928_create_applications.rb
@@ -0,0 +1,6 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+    end
+  end
+end
---
+++ spec/models/application_pet_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationPet, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:application) }
+    it { should belong_to(:pet) }
+  end
+end
