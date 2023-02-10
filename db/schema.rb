3875034eabf9618293fe0f2dafad662717a8dd24
Elle Majors <hellolovely@ellemajors.com>
2023-02-09 19:44:14 -0700

    add: application pet schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_021734) do
+ActiveRecord::Schema.define(version: 2023_02_10_024147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "application_pets", force: :cascade do |t|
+    t.bigint "application_id"
+    t.bigint "pet_id"
+    t.index ["application_id"], name: "index_application_pets_on_application_id"
+    t.index ["pet_id"], name: "index_application_pets_on_pet_id"
+  end
 
   create_table "applications", force: :cascade do |t|
     t.string "name"
@@ -23,8 +30,6 @@ ActiveRecord::Schema.define(version: 2023_02_10_021734) do
     t.string "zip_code"
     t.string "description"
     t.string "status"
-
-    t.timestamp
   end
 
   create_table "pets", force: :cascade do |t|
@@ -65,6 +70,8 @@ ActiveRecord::Schema.define(version: 2023_02_10_021734) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "application_pets", "applications"
+  add_foreign_key "application_pets", "pets"
   add_foreign_key "pets", "shelters"
   add_foreign_key "veterinarians", "veterinary_offices"
 end
