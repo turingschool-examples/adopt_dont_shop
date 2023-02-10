0f4a982b34015ccede712d320b38eb9a6c7ab54b
David Marino <davidjmarino8@gmail.com>
2023-02-10 17:24:55 -0500

    User Story 3

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,7 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+  validates :name, :street, :city, :state, :zip, :desc, presence: true
 
   def pet_names
     self.pets.pluck(:name)
