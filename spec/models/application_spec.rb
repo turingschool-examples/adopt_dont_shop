f817b3e9d4389e9e8d2ce7f68cf45263e5e7d52f
Jade Stewart <jadekstewart3@gmail.com>
2023-02-10 16:42:10 -0700

    Create validation tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Application, type: :model do
     it {should have_many :application_pets}
     it {should have_many(:pets).through(:application_pets)}
   end
+
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :street_address}
+    it {should validate_presence_of :city} 
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip} 
+    it {should validate_presence_of :description}
+  end
 end
