23601077e89813e351dd5e2b1a8fb2ef2495351f
Jade Stewart <jadekstewart3@gmail.com>
2023-02-10 14:07:26 -0700

    Add create action for application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,4 +6,19 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    new_app = Application.create(app_params)
+    redirect_to"/applications/#{new_app.id}"
+  end
+
+  private
+  
+  def app_params
+    params.permit(:name, :street_address, :city, :state, :zip, :description, :status)
+  end
 end
