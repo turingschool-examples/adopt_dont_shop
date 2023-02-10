32db2dc5e4ac1e11434085b4a562e06bd2b25d2f
Jade Stewart <jadekstewart3@gmail.com>
2023-02-10 16:45:19 -0700

    Add validations for application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+  validates :name, :street_address, :city, :state, :zip, :description, presence: true
 end
