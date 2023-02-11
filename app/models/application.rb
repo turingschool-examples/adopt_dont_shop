08d8fc8143e2746e1d848c9fa663c72f5d3be7b2
Matt Enyeart <menyeart1@gmail.com>
2023-02-11 12:29:01 -0700

    Add dependent detroy association to has many pets relationship

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,6 @@
 class Application < ApplicationRecord 
   has_many :pet_applications
-  has_many :pets, through: :pet_applications
+  has_many :pets, through: :pet_applications, dependent: :destroy
   validates :name, :street_address, :city, :state, :zipcode, :description, presence: true
   def search_for_pet(pet_name)
     if pet_name 
