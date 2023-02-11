2875613488e77d2d876b5c311f3b26b40a2f71ef
Chris Crane <chrisjcrane@gmail.com>
2023-02-10 19:39:42 -0700

    add new application page

---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+# Then I am taken to the new application page where I see a form
+# When I fill in this form with my:
+
+# Name
+# Street Address
+# City
+# State
+# Zip Code
+# And I click submit
+# Then I am taken to the new application's show page
+# And I see my Name, address information, and description of why I would make a good home
+# And I see an indicator that this application is "In Progress"
+
+RSpec.describe 'new application page' do
+  describe 'fields' do
+    it 'has correct fields' do
+      visit '/applications/new'
+      expect(page).to have_field('Name')
+      expect(page).to have_field('Street Address')
+      expect(page).to have_field('City')
+      expect(page).to have_field('State')
+      expect(page).to have_field('Zip Code')
+    end
+
+    xit 'saves the information' do
+      visit '/applications/new'
+      fill_in 'Name', with: 'My Application'
+      fill_in 'Street Address', with: '123 Main Street'
+      fill_in 'City', with: 'San Francisco'
+      fill_in 'State', with: 'CA'
+      fill_in 'Zip Code', with: '94107'
+
+      click_on 'Submit'
+
+      expect_current_path('/applications/')
+    end
+  end
+end
