26495a300d5ea88e68985e1fd001b5edce2bfe37
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-10 18:15:38 -0700

    Test: add tests for form not complete validation

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -3,8 +3,16 @@ require "rails_helper"
 RSpec.describe "Applications New Page" do
   describe "As a visitor" do
     describe "When I visit new application page" do
-      it 'has a form you can fill out, hit submit, and be taken to new applications show page' do
+      before(:each) do 
         visit '/applications/new'
+
+        fill_in :name, with: "Brian"
+        fill_in :street_address, with: "123 Road lane"
+        fill_in :city, with: "Boulder"
+        fill_in :state, with: "Colorado"
+        fill_in :zip_code, with: "12345"
+      end
+      it 'has a form you can fill out, hit submit, and be taken to new applications show page' do
         expect(page).to have_field(:name)
         expect(page).to have_field(:street_address)
         expect(page).to have_field(:city)
@@ -12,11 +20,6 @@ RSpec.describe "Applications New Page" do
         expect(page).to have_field(:zip_code)
         expect(page).to have_field(:description)
 
-        fill_in :name, with: "Brian"
-        fill_in :street_address, with: "123 Road lane"
-        fill_in :city, with: "Boulder"
-        fill_in :state, with: "Colorado"
-        fill_in :zip_code, with: "12345"
         fill_in :description, with: "I have a large back yard"
 
         click_button "Create Application"
@@ -24,6 +27,14 @@ RSpec.describe "Applications New Page" do
         expect(Application.all.size).to eq(1)
         expect(current_path).to eq("/applications/#{Application.first.id}")
         expect(page).to have_content("Brian")
+      end
+      
+      it "will return to the new application page with an error message when all fields are not filled out" do 
+        click_button "Create Application"
+        expect(Application.all.size).to eq(0)
+        expect(current_path).to eq("/applications")
+        expect(page).to have_content("Application not created: Required information missing.")
+       
       end
     end
   end
