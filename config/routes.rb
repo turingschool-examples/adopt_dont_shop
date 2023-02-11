003a8bb2a452d2f111505e5e1ac9cd1ded232d37
Conner Van Loan <cdvanloan@gmail.com>
2023-02-11 14:50:02 -0700

    Fixes un-restfull routes for US6
    Co-authored-by: Huy Phan <huyphan520@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
   post '/applications/:id', to: 'applications#create'
-  post '/applications/:id/pets/:pet_id', to: 'applications#add_pet'
   patch '/applications/:id', to: 'applications#update'
+
+  post '/applications/:id/application_pets', to: 'application_pets#create'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
