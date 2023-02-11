ac5b11d357bf1f2652af9da361f96b683895ff6e
Jade Stewart <jadekstewart3@gmail.com>
2023-02-10 17:09:52 -0700

    Fix smol routing issue

--- spec/models/application_pets_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationPet, type: :model do
-  describe "relationships" do
-    it {should belong_to :application}
-    it {should belong_to :pet}
-  end
-end
