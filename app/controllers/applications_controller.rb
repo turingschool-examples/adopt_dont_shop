fcf72a403ba892fb92a5cd6db54702b4c121b73f
Adam Bailey <ab67319@gmail.com>
2023-02-11 11:54:32 -0700

    feat: create functionality to search for a pet on application show page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,6 +2,7 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
+    @pet = Pet.find_by(name: params[:pet_name])
   end
 
   def new
