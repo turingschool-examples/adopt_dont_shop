e81e291916c4b8030ea902552d454b883905c440
KaraJH <karas.jones@yahoo.com>
2023-02-11 17:35:20 -0700

    test: Partial name search

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -19,10 +19,22 @@ RSpec.describe Application do
       shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
 
-      expect(applicant_1.search_for_pet("Clawdia")).to eq(pet_2)
-      expect(applicant_1.search_for_pet("clawdia")).to eq(pet_2)
+      expect(applicant_1.search_for_pet("Clawdia")).to eq([pet_2])
+      expect(applicant_1.search_for_pet("clawdia")).to eq([pet_2])
       expect(applicant_1.search_for_pet("Geoff Dawg")).to eq("Pet name not in system")
 
+    end
+
+    it 'can serch for partial name matches' do 
+      applicant_1 = Application.create!(name: "Joe", street_address: "123 House", city: "Denver", state: "CO", zipcode: 80220, description: "Dog dad", status: "In Progress")
+      shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_3 = shelter_1.pets.create(name: 'Cornelius', breed: 'shorthair', age: 3, adoptable: true)
+      pet_4 = shelter_1.pets.create(name: 'Lil Turtle', breed: 'shorthair', age: 10, adoptable: true)
+      pet_5 = shelter_1.pets.create(name: 'Lil Possum', breed: 'shorthair', age: 2, adoptable: true)
+      expect(applicant_1.search_for_pet("Corn")).to eq([pet_3])
+      expect(applicant_1.search_for_pet("Nel")).to eq([pet_3])
+      expect(applicant_1.search_for_pet("Lil")).to eq([pet_4, pet_5])
+      expect(applicant_1.search_for_pet("Geoff Dawg")).to eq("Pet name not in system")
     end
   end
 end
