58a4f053fa85fa44770566db6edb8acc5da3541a
KaraJH <karas.jones@yahoo.com>
2023-02-12 11:16:47 -0700

    feat: Fixed the view to display pets correctly

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,24 +3,12 @@ class Application < ApplicationRecord
   has_many :pets, through: :pet_applications, dependent: :destroy
   validates :name, :street_address, :city, :state, :zipcode, :description, presence: true
 
-  # def search_for_pet(pet_name)
-  #   if pet_name 
-  #     # name_present = Pet.find_by('name ILIKE ?', pet_name)
-  #     name_present = Pet.where("name ILIKE ?", "%#{pet_name}%")
-  #     if name_present
-  #       name_present
-  #     else
-  #       "Pet name not in system"
-  #     end
-  #   end
-  # end
-
   def search_for_pet(pet_name)
     name_present = Pet.where("name ILIKE ?", "%#{pet_name}%")
     if name_present.count == 0
       "Pet name not in system"
     else
-      name_present.distinct
+      name_present
     end
   end
 end
