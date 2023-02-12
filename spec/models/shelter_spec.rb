6968ac85eb570990f1467262d7426c2dc3ffd6f6
KaraJH <karas.jones@yahoo.com>
2023-02-12 12:14:09 -0700

    feat/test: Adds admins shelter index test

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -70,5 +70,11 @@ RSpec.describe Shelter, type: :model do
         expect(@shelter_1.pet_count).to eq(3)
       end
     end
+
+    describe '::order_by_reverse_alphabetical' do 
+      it 'orders shelters in reverse alphabetical order' do 
+        expect(Shelter.order_by_reverse_alphabetical).to eq([@shelter_2, @shelter_3, @shelter_1])
+      end
+    end
   end
 end
