295f6ea9e5fdb5a742c0a767b27e2bef07bde387
Isaac Alter <i_alter@yahoo.com>
2023-02-12 13:10:50 -0700

    Add feat: User Story 10 implement test logic in shelter spec for class method #order_by_reverse_alphabet

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -41,6 +41,12 @@ RSpec.describe Shelter, type: :model do
         expect(Shelter.order_by_number_of_pets).to eq([@shelter_1, @shelter_3, @shelter_2])
       end
     end
+
+    describe '#order_by_reverse_alphabet' do
+      it 'orders the shelters by number of pets they have, descending' do
+        expect(Shelter.order_by_reverse_alphabet).to eq([@shelter_2, @shelter_3, @shelter_1])
+      end
+    end
   end
 
   describe 'instance methods' do
