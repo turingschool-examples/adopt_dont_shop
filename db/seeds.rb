9ad330d80efb1f91f362804617305445885511ea
Isaac Alter <i_alter@yahoo.com>
2023-02-12 13:24:01 -0700

    Add feat: User Story 10 simple refactor of seeds to provide prrof that when admin/shelters is looked at via server it will show the shelters in alphabetical order along with getting rid of extraneous search function in admin/shelters as it was not asked for in user stories

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@
 # @petition2 = Petition.create!(name: 'Nhoj', street_address: '2 Sesame St', city: 'Renved',
 #                     state: 'OC', zip_code: 54321, description: 'Dogs like I', status: 'Pending')
 @shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+@shelter1 = Shelter.create(name: 'Ystery Building', city: 'Irvine CA', foster_program: false, rank: 4)
 @pet1 = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
 # @pet2 = Pet.create!(name: 'Dooby', age: 3, breed: 'Greater Dane', adoptable: true, shelter_id: @shelter.id, petition_id: @petition.id)
 # @pet3 = Pet.create!(name: 'Roo', age: 5, breed: 'Greatest Dane', adoptable: true, shelter_id: @shelter.id, petition_id: @petition2.id)
