7a01ac4e65796749ce45d08294beb842795dd7aa
Tori Enyart <torienyart@gmail.com>
2023-02-12 13:02:37 -0700

    fix: Change button to say submit instead of Submit Applciation

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -9,9 +9,8 @@ RSpec.describe 'Starting a new application' do
     fill_in 'City:', with: 'Golden'
     fill_in 'State:', with: 'Colorado'
     fill_in 'Zip Code:', with: 80401
-    fill_in 'Application Justification', with: "Pets love me!"
 
-    click_on 'Submit Application'
+    click_on 'Submit'
 
     expect(current_path).to eq("/applications/#{Application.last.id}")
   end
@@ -22,11 +21,10 @@ RSpec.describe 'Starting a new application' do
     fill_in 'Name:', with: 'Amy Nelson'
     fill_in 'Street Address:', with: '123 Rainbow rd'
     fill_in 'City:', with: 'Golden'
-    fill_in 'State:', with: 'Colorado'
 
-    click_on 'Submit Application'
+    click_on 'Submit'
 
     expect(current_path).to eq("/applications/new")
-    expect(page).to have_content("Description can't be blank")
+    expect(page).to have_content("State can't be blank")
   end
 end
