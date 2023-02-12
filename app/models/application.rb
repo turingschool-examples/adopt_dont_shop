8ca94571bffaebb656cd5c5ac0989ab65764d699
Tori Enyart <torienyart@gmail.com>
2023-02-12 13:01:33 -0700

    Add methods for updating application status

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,13 +1,24 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
-  validates :name, :street_address, :city, :state, :zip, :description, presence: true
+  validates :name, :street_address, :city, :state, :zip, presence: true
 
   def in_progress?
-    if pets == []
-      "In Progress"
-    else
-      "Pending"
+   status == 'In Progress'
+  end
+
+  def pets?
+    if pets != []
+      true
     end
+  end
+
+  def update_status
+    if pets? && description?
+      self.update(status: "Pending")
+    else       
+      self.update(status: "In Progress")
+    end
+    self
   end
 end
