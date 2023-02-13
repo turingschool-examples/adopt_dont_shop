41088b98aa69a6b73aba50d620a93e4aba9b3d3b
Chris Crane <chrisjcrane@gmail.com>
2023-02-13 15:44:36 -0700

    add #pet_application_by_pet with passing test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -40,4 +40,33 @@ RSpec.describe Application, type: :model do
       expect(app.pets_list).to eq("#{fido.name}, #{bruno.name}")
     end
   end
+
+  describe '#pet_application_by_pet' do
+    it 'returns the pet application for a given pet' do
+      app = Application.create!(
+        name: 'John Smith',
+        address: '123 Fake Street',
+        city: 'Springfield',
+        state: 'IL',
+        zipcode: 12_345,
+        description: 'I like dogs.',
+        status: 'In Progress'
+      )
+      shelter = Shelter.create!(
+        foster_program: true,
+        name: 'Dog house',
+        city: 'Springfield',
+        rank: 1
+      )
+      fido = shelter.pets.create!(
+        adoptable: true,
+        age: 1,
+        breed: 'weiner',
+        name: 'Fido'
+      )
+      petapp1 = PetApplication.create!(application_id: app.id, pet_id: fido.id)
+
+      expect(app.pet_application_by_pet(fido)).to eq(petapp1)
+    end
+  end
 end
