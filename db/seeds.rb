c9486e952dfe2ccd7b4ca88d61a5b6b0eb1381a3
Conner Van Loan <cdvanloan@gmail.com>
2023-02-13 16:39:19 -0700

    Adds tests for US14 CO-Authored-by: Huy Phan <huyphan520@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ Shelter.destroy_all
 @buster = @humaine.pets.create!(adoptable: true, age: 7, breed: "Doberman", name: 'Buster')
 @meko = @humaine.pets.create!(adoptable: true, age: 4, breed: "Pit Bull", name: 'Meko')
 
-@huy = Application.create!(applicant_name: "Huy Phan", app_street: "123 Park Ave", app_city: "Denver", app_state: "CO", app_zip_code: "80205", description: "Im super cool and stuffy" )
+@huy = Application.create!(applicant_name: "Huy Phan", app_street: "123 Park Ave", app_city: "Denver", app_state: "CO", app_zip_code: "80205", description: "Im super cool and stuff" )
 @tori = Application.create!(applicant_name: "Tori", app_street: "345 Main St", app_city: "Denver", app_state: "CO", app_zip_code: "80206", description: "I lyke pets and treat them nice")
 @khoa = Application.create!(applicant_name: "Khoa", app_street: "Somewhere St", app_city: "Littleton", app_state: "CO", app_zip_code: "80121", description: "I am the best, duh" )
 @axel = Application.create!(applicant_name: "Axel", app_street: "959 Broadway", app_city: "Louisville", app_state: "CO", app_zip_code: "80423", description: "I can grow plants, why not raise pets" )
--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'admin_shelter show page' do
       @snugglez = @pound.pets.create!(adoptable: true, age: 2, breed: "Tabby Cat", name: 'Snugglez')
       @khoa = Application.create!(applicant_name: "Khoa", app_street: "Somewhere St", app_city: "Littleton", app_state: "CO", app_zip_code: "80121", description: "I am the best, duh", status: "Pending")
       @khoa.add_pet(@snugglez)
+      @tori = Application.create!(applicant_name: "Tori", app_street: "345 Main St", app_city: "Denver", app_state: "CO", app_zip_code: "80206", description: "I lyke pets and treat them nice", status: "Pending")
+      @tori.add_pet(@snugglez)
     end
 
     it 'I should see' do
@@ -30,5 +32,22 @@ RSpec.describe 'admin_shelter show page' do
       expect(page).to have_content("Pets Applied For\n#{@snugglez.name} Application Status: Rejected")
       expect(page).to_not have_button("Reject Application")
     end
+
+    it 'I should see' do
+      visit "/admin/applications/#{@khoa.id}"
+      expect(page).to have_content("Pets Applied For\n#{@snugglez.name} Application Status: Pending") 
+
+      visit "/admin/applications/#{@tori.id}"
+      expect(page).to have_content("Pets Applied For\n#{@snugglez.name} Application Status: Pending") 
+
+      click_button "Approve Application"
+
+      expect(current_path).to eq("/admin/applications/#{@tori.id}")
+      expect(page).to have_content("Pets Applied For\n#{@snugglez.name} Application Status: Accepted")
+
+      visit "/admin/applications/#{@khoa.id}"
+      expect(page).to have_content("Pets Applied For\n#{@snugglez.name} Application Status: Pending") 
+    end
+
   end
 end
