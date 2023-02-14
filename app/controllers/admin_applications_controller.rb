fd2eaf56bcef0812977346384a4c152e8ed43b02
KaraJH <karas.jones@yahoo.com>
2023-02-14 13:22:51 -0700

    feat: User story 12 and 13, update method to update pet apps

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -2,25 +2,23 @@ class AdminApplicationsController < ApplicationController
   def show 
     
     @application = Application.find(params[:id])
-    # @pets = @application.pets
     @pet_applications = @application.pet_applications
   end
 
   def update 
-    
-    pet_application = PetApplication.find(params[:id]) #get specific pet_app with pet_1 and app_1
-    pet_application.update!(status: params[:status]) #update specific app_1, pet_1 id
-    # require 'pry'; binding.pry
-    application = pet_application.application #specific applicant app_1
-    pet_applications = application.pet_applications #all of app_1's pet_applications
-  # require 'pry'; binding.pry
-    
-    if application.application_status_count("Rejected") > 0 
-      application.update!(status: "Rejected")
-    elsif application.application_status_count("Accepted") == pet_applications.count
-      application.update!(status: "Accepted")
+    @pet_application = PetApplication.find(params[:id]) 
+    @pet_application.update!(status: params[:status]) 
+
+    @application = @pet_application.application 
+    @pet_applications = @application.pet_applications 
+
+    # Later user story, dosn't do anything right now
+    if @application.application_status_count("Rejected") > 0 #.any?
+      @application.update!(status: "Rejected")
+    elsif @application.application_status_count("Accepted") == @pet_applications.count 
+      @application.update!(status: "Accepted")
     end
-  
-    redirect_to "/admin/applications/#{application.id}"
+    
+    redirect_to "/admin/applications/#{@pet_application.application_id}"
   end
 end
