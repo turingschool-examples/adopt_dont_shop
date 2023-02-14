a7ceb17071eb85cb82aca1b87d6a8658cd2707d8
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-14 11:45:07 -0700

    Test: add test to check and see if other applications are not affected by the status of another application with the same pet

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe "Admin Application Show Page" do
       before(:each) do 
         @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
         @application_1 = Application.create!(name: "Brian", street_address: "853 West Linden st", city: "Louisville", state: "colorado", zip_code: "80027", description: "I like animals", status: 1)
+        @application_2 = Application.create!(name: "John", street_address: "853 West Linden st", city: "Louisville", state: "colorado", zip_code: "80027", description: "I like animals", status: 1)
         @rylo = @application_1.pets.create!(adoptable: true, age: 1, breed: "Lab", name: "Rylo", shelter: @shelter_1)
+        PetApplication.create!(application_id: @application_2.id, pet_id: @rylo.id)
+
         visit "/admin/applications/#{@application_1.id}"
       end
 
@@ -57,6 +60,22 @@ RSpec.describe "Admin Application Show Page" do
           expect(page).to_not have_button("Reject")
           expect(page).to have_content("Status: Rejected")
         }
+      end
+
+      context "When there are two applications in they system for the same pet" do 
+        it 'when I approve or reject a pet on one application, the other application is not affected' do 
+          click_on "Approve"
+
+          expect(current_path).to eq("/admin/applications/#{@application_1.id}")
+
+          visit "/admin/applications/#{@application_2.id}"
+
+          within(".pet") {
+            expect(page).to have_button("Approve")
+            expect(page).to have_button("Reject")
+            expect(page).to have_content("Status: In Progress")
+          }
+        end
       end
     end
   end 
