00b539cd635ee9802cc9400b97753ae25be0dfb1
Weston Sandfort <sandfortw@gmail.com>
2023-02-15 15:58:46 -0700

    edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Pet.destroy_all
+Application.destroy_all
+Shelter.destroy_all
+ApplicationPet.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
