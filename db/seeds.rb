18e150373311e2bc64f40d8d17a1f9dc44a7f04d
Sam Walker <sgwalker327@gmail.com>
2023-02-14 17:26:10 -0700

    Complete US 11

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,12 @@ Pet.destroy_all
 Shelter.destroy_all
 Application.destroy_all
 
-shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-pet_1 = Pet.create!(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
-pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
-pet_3 = Pet.create!(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+shelter_2 = Shelter.create!(name: 'Denver shelter', city: 'Denver, CO', foster_program: false, rank: 8)
+shelter_3 = Shelter.create!(name: 'Vail shelter', city: 'Vail, CO', foster_program: false, rank: 7)
+pet_1 = Pet.create!(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter_1.id)
+pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter_2.id)
+pet_3 = Pet.create!(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter_1.id)
 applicant = Application.create!(name: 'Dawson', 
 street_address: '1234 example ave.', 
 city: 'Denver', 
