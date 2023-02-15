44b2ee7dcc37a494cfb5cab70253a2dd7ac1a87d
Chris Crane <chrisjcrane@gmail.com>
2023-02-15 11:35:35 -0700

    dry up applications controller with private methods

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -7,20 +7,29 @@ class Admin::ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @petapplication = PetApplication.find(params[:pet_application_id])
     @petapplication.update(application_id: params[:id], pet_id: params[:petid], status: params[:status])
-    status_array = @application.pets.map do |pet|
-      @application.pet_application_by_pet(pet).status
-    end
+    
     unless status_array.include?('Pending')
       if status_array.include?('Rejected')
         @application.update(status: 'Rejected')
       else
-        @application.update(status: 'Approved')
-        @application.pets.each do |pet|
-          pet.update(adoptable: false)
-          pet.reload
-        end
+        approve_application
       end
     end
     redirect_to "/admin/applications/#{params[:id]}"
+  end
+end
+
+private
+
+def status_array
+  @application.pets.map do |pet|
+    @application.pet_application_by_pet(pet).status
+  end
+end
+
+def approve_application
+  @application.update(status: 'Approved')
+  @application.pets.each do |pet|
+    pet.update(adoptable: false)
   end
 end
