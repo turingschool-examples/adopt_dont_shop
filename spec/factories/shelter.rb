266b0e00f23cc77077037689699e1ce194014aec
Weston Sandfort <sandfortw@gmail.com>
2023-02-15 12:29:33 -0700

    replace existing shelter test objects with faker objects

--- spec/factories/shelter.rb
+++ spec/factories/shelter.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :shelter do
     name { Faker::Name.name }
+    city { Faker::Address.city }
+    rank { Faker::Number.between(from: 1, to: 15) }
+    foster_program { Faker::Boolean.boolean }
   end
 end
