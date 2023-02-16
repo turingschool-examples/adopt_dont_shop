e18f8b88574c73f39fddb33e3a90329164b5b0ca
Sam Walker <sgwalker327@gmail.com>
2023-02-16 08:29:57 -0700

    Fix repeating pending shelters bug

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,6 +1,6 @@
 class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT name FROM shelters ORDER BY name DESC")
-    @pending_app_shelters = Shelter.joins(pets: :application_pets).joins(:applications).where(applications: {status: "Pending"})
+    @pending_app_shelters = Shelter.joins(pets: :application_pets).joins(:applications).where(applications: {status: "Pending"}).distinct
   end
 end
