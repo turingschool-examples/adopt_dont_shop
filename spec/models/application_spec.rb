2fe4ab4019cad37115fd41e74dc472abad399131
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-03-30 17:35:03 -0600

    create application and pet_application classes with corresonding tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -8,7 +8,13 @@ RSpec.describe Application, type: :model do
   end
   
   describe "validations" do
-    
+    it {should validate_presence_of(:applicant_name)}
+    it {should validate_presence_of(:street_address)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zip_code)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:status)}
   end
   
   before(:each) do
@@ -23,3 +29,4 @@ RSpec.describe Application, type: :model do
 
   end
 end
+
