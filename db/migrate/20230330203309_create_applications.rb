79d3d31d6b38f2460600595aa6d779a4c27bcbef
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-03-30 17:55:16 -0400

    migrations and update schema

---
+++ db/migrate/20230330203309_create_applications.rb
@@ -0,0 +1,9 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :address
+      t.string :reason
+    end
+  end
+end
