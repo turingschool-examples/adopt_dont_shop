df4a514eb16fd6e4f14580c50ba0d54ea914ccdb
Young Koh <youngheon6@gmail.com>
2023-03-30 19:07:49 -0600

    feat: Fix user story 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_30_215823) do
+ActiveRecord::Schema.define(version: 2023_03_31_005712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
