5f2912eabb35a9f0ffbb97e8df16926fc0b808ff
Isaac Thill <ithill22@turing.edu>
2023-03-30 18:55:45 -0600

    add applications db and test for show_spec

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,23 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'applications/:id', type: :feature do
-  let(:applicant) { Applicant.create!(name: "John Doe", address: "123 Main St, Anytown USA 12345", description: "I love dogs and have a big yard for them to run in.", status: "Pending") }
-  let(:pet1) { Pet.create!(name: "Fido", age: 2, breed: "Lab", adoptable: true) }
-  let(:pet2) { Pet.create!(name: "Spot", age: 3, breed: "Pitbull", adoptable: true) }
-  applicant.pets << pet1
-  applicant.pets << pet2
+RSpec.describe "/applications/:id" do
+  let!(:application_1) { Application.create!(name: "John Doe", address: "123 Main St", city: "Denver", state: "CO", zip: "80202", description: "I love dogs", status: "In Progress") }
+  let!(:pet_1) { application_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false) }
+  let!(:pet_2) { application_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true) }
 
-  describe 'As a visitor, when I visit an applications show page' do
-    it 'I see an applicant and their attributes' do
-      visit "/applications/#{applicant.id}"
+  describe "As a visitor, when I visit an application's show page" do
+    it "I can see the name of an applicant, their address, reason they would make a good home for this pet, and the names of all pets that this application is for" do
+      vist "/applications/#{application_1.id}"
 
-      expect(page).to have_content(applicant.name)
-      expect(page).to have_content(applicant.address)
-      expect(page).to have_content(applicant.description)
-      expect(page).to have_content(applicant.status)
-
-      expect(page).to have_link(pet1.name, href: "/pets/#{pet1.id}")
-      expect(page).to have_link(pet2.name, href: "/pets/#{pet2.id}")
+      expect(page).to have_content(application_1.name)
+      expect(page).to have_content(application_1.street_address)
+      expect(page).to have_content(application_1.description)
+      expect(page).to have_link(pet_1.name)
+      expect(page).to have_content(application_1.status)
     end
   end
 end
