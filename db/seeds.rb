45366bfc73689b6393c8fc815a2519a04bf10235
Nick Sacco <nicksacco10@gmail.com>
2023-03-30 18:00:20 -0700

    modify us1 pet show links

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Application.destroy_all
+Shelter.destroy_all
+Pet.destroy_all
+Veterinarian.destroy_all
+VeterinaryOffice.destroy_all
 
 @application_1 = Application.create!(name: 'Bob', street_address: '1234 Southeast St',
   city: 'San Francisco', state: 'CA', zip_code: 12345,
@@ -22,3 +26,19 @@ Application.destroy_all
 @application_4 = Application.create!(name: 'Jane', street_address: '74026 Deep South St',
   city: 'San Diego', state: 'CA', zip_code: 92056,
   description: 'Doesnt like animals', names_of_pets: 'Mittens', application_status: 'Rejected')
+
+@shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+@shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+@shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+
+@pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter: @shelter_1)
+@pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter: @shelter_2)
+@pet_3 = Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter: @shelter_3)
+
+@vet_office_1 = VeterinaryOffice.create!(name: 'Special Friends', boarding_services: true, max_patient_capacity: 100)
+@vet_office_2 = VeterinaryOffice.create!(name: 'Pet Emergency Room', boarding_services: true, max_patient_capacity: 50)
+@vet_office_3 = VeterinaryOffice.create!(name: 'The Country Vet', boarding_services: true, max_patient_capacity: 200)
+
+@vet_1 = Veterinarian.create!(name: 'Taylor', review_rating: 10, on_call: true, veterinary_office: @vet_office_1)
+@vet_2 = Veterinarian.create!(name: 'Jim', review_rating: 8, on_call: true, veterinary_office: @vet_office_2)
+@vet_3 = Veterinarian.create!(name: 'Heather', review_rating: 9, on_call: true, veterinary_office: @vet_office_3)
