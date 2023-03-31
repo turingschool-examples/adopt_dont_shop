f8d487c475f27ab18a3a3a88729e16a74f952890
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-03-31 15:37:26 -0400

    add tests to start application

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -81,4 +81,50 @@ RSpec.describe 'the pets index' do
     expect(page).to have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
   end
+
+  it 'has a link to start an application for that pet' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+
+    visit "/pets"
+
+    expect(page).to have_link("Start an Application for #{pet_1.name}")
+    expect(page).to have_link("Start an Application for #{pet_2.name}")
+    expect(page).to have_link("Start an Application for #{pet_3.name}")
+  end
+
+  it 'takes you to a new application page' do
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
+
+    visit "/pets"
+
+    click_link("Start an Application for #{pet_1.name}")
+
+    expect(page).to have_current_path("/applications/new")
+
+    expect(page).to have_field("Name")
+    fill_in "Name", with: "Bob"
+    expect(page).to have_field("Address")
+    fill_in "Address", with: "1234 Bob St."
+    expect(page).to have_field("City")
+    fill_in "City", with: "Bobville"
+    expect(page).to have_field("State")
+    fill_in "State", with: "FL"
+    expect(page).to have_field("Zip")
+    fill_in "Zip", with: "12345"
+    expect(page).to have_field("Why I would make a good home")
+    fill_in "Why I would make a good home", with: "I love cats"
+
+    expect(page).to have_button("Submit Application")
+
+    click_button("Submit Application")
+
+    expect(page).to have_current_path("/applications/#{Application.last.id}")
+    expect(page).to have_content("In Progress")
+  end
 end
