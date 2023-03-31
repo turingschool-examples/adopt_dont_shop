438e8e68a8117d820550771b7ac7ecf219da1711
Isaac Thill <ithill22@turing.edu>
2023-03-31 17:57:35 -0600

    feat: add logic to controller and add route for user story 5

--- config/routes.rb
+++ config/routes.rb
@@ -41,4 +41,6 @@ Rails.application.routes.draw do
   get 'applications/new', to: 'applications#new'
   post "/applications", to: 'applications#create'
   get '/applications/:id', to: 'applications#show'
+
+  post '/application_pets/:id/new', to: 'application_pets#new'
 end
