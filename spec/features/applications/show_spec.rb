459a6f4902527a88f89bbb62c55f3b46e021d4fc
Angel Byun <abyun22@turing.edu>
2023-03-31 15:06:36 -0600

    Modify show_spec

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,19 +3,15 @@ require 'rails_helper'
 RSpec.describe 'application show page' do
   let!(:application_1) { Application.create!(name: 'Bob', street_address: '1234 Southeast St',
                                             city: 'San Francisco', state: 'CA', zip_code: 12345,
-                                            description: 'Wants a dog', names_of_pets: 'Fluffy', application_status: 'Pending') }
+                                            description: 'Wants a dog', name_of_pet: 'Lobster', application_status: 'Pending') }
 
   let!(:application_2) { Application.create!(name: 'Sally', street_address: '4321 Bridge Way',
                                             city: 'San Francisco', state: 'CA', zip_code: 54321,
-                                            description: 'Would like a siamese cat', names_of_pets: 'Coz', application_status: 'In Progress') }
+                                            description: 'Would like a siamese cat', name_of_pet: 'Lucille Bald', application_status: 'In Progress') }
 
   let!(:application_3) { Application.create!(name: 'Fred', street_address: '376 Monroe St',
                                             city: 'Los Angeles', state: 'CA', zip_code: 67890,
-                                            description: 'Interested in sibling pets', names_of_pets: 'Jack, Jill', application_status: 'Accepted') }
-
-  let!(:application_4) { Application.create!(name: 'Jane', street_address: '74026 Deep South St',
-                                            city: 'San Diego', state: 'CA', zip_code: 92056,
-                                            description: 'Doesnt like animals', names_of_pets: 'Mittens', application_status: 'Rejected') }
+                                            description: 'Interested in sibling pets', name_of_pet: 'Beethoven', application_status: 'Accepted') }
 
 
     it 'displays application information' do
@@ -27,7 +23,7 @@ RSpec.describe 'application show page' do
       expect(page).to have_content(application_1.state)
       expect(page).to have_content(application_1.zip_code)
       expect(page).to have_content(application_1.description)
-      expect(page).to have_content(application_1.names_of_pets)
+      expect(page).to have_content(application_1.name_of_pet)
       expect(page).to have_content(application_1.application_status)
     end
 end
