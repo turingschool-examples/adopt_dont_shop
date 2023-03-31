30ce1cb042425568d95872346c60734ee0b38dcb
Amy Spears <amspears007@gmail.com>
2023-03-30 22:03:04 -0600

    Test: Updated data in applications show spec with pet, shelter and pet_application objects and added link to test and html page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Application Show', type: :feature do
   let!(:applicant1) { Application.create!(name: "Jamison Ordway", street_address: '123 Cat St', city: 'Denver', state: 'CO', zip_code: 80238, description:"I love cats so I want to adopt them", status: 'In Progress') }
+  let!(:shelter_3) {Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)}
+  let!(:pet1) {Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'George Hairlesson', shelter_id: shelter_3.id)}
+  let!(:pet1_application) {PetApplication.create!(application_id: applicant1.id, pet_id: pet1.id)}
+
 
   it 'I see applicants name, full address, descripition, name of all pets applying for and status'  do
     visit "/applications/#{applicant1.id}" 
@@ -11,5 +15,6 @@ RSpec.describe 'Application Show', type: :feature do
     expect(page).to have_content('123 Cat St') 
     expect(page).to have_content("I love cats so I want to adopt them") 
     expect(page).to have_content('In Progress') 
+    expect(page).to have_link(pet1.name)
   end
 end
