493fb7281ef4970b223b5842aecafc22d357db2b
Amy Spears <amspears007@gmail.com>
2023-03-31 13:59:50 -0600

    Test: Add additional pet in applications show_spec data and link test for it

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe 'Application Show', type: :feature do
   let!(:applicant1) { Application.create!(name: "Jamison Ordway", street_address: '123 Cat St', city: 'Denver', state: 'CO', zip_code: 80238, description:"I love cats so I want to adopt them", status: 'In Progress') }
   let!(:shelter_3) {Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)}
   let!(:pet1) {Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'George Hairlesson', shelter_id: shelter_3.id)}
+  let!(:pet2) {Pet.create!(adoptable: false, age: 2, breed: 'american shorthair', name: 'Furry', shelter_id: shelter_3.id)}
+
   let!(:pet1_application) {PetApplication.create!(application_id: applicant1.id, pet_id: pet1.id)}
+  let!(:pet2_application) {PetApplication.create!(application_id: applicant1.id, pet_id: pet2.id)}
+  
 
 
   it 'I see applicants name, full address, descripition, name of all pets applying for and status'  do
@@ -16,5 +20,6 @@ RSpec.describe 'Application Show', type: :feature do
     expect(page).to have_content("I love cats so I want to adopt them") 
     expect(page).to have_content('In Progress') 
     expect(page).to have_link(pet1.name)
+    expect(page).to have_link(pet2.name)
   end
 end
