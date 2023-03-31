f89938684201b7a8de0dd589bfa7bdc9a094eb65
Logan Wilson <loganwilson777@gmail.com>
2023-03-31 16:45:08 -0600

    Feat:add validation for any applications fields

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ApplicationRecord
+  validates :name, :address, :city, :state, :zip, :description, presence: true
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
