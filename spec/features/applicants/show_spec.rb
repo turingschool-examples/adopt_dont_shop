52a9e683aa87babf7edfc609d153b038ebff47b6
margdaly <margaret.daly@gmail.com>
2023-03-31 17:53:20 -0600

    fix: Update tests/show page with new applicant params

--- spec/features/applicants/show_spec.rb
+++ spec/features/applicants/show_spec.rb
@@ -3,38 +3,47 @@ require 'rails_helper'
 RSpec.describe "Applicant Show" do
   before(:each)do 
     @olivia = Applicant.create!(name: "Olivia Valentin",
-                                address: "1234 Main St, Denver, CO 80203",
-                                reasons_for_adoption: "I love animals",
-                                status: "In Progress")
+                                street: "1234 Main St",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80203",
+                                good_home: "I have vetenarian expierence")
+
     @shelter = Shelter.create!(name: 'Aurora shelter', 
                                city: 'Aurora, CO', 
                                foster_program: false, 
                                rank: 9)
+
     @pet_1 = Pet.create(adoptable: true,
                         age: 1, 
                         breed: 'sphynx', 
                         name: 'Lucille Bald', 
                         shelter_id: @shelter.id)
+
     @pet_2 = Pet.create(adoptable: true, 
                         age: 3, 
                         breed: 'doberman', 
                         name: 'Lobster', 
                         shelter_id: @shelter.id)
+
     PetApplicant.create!(applicant: @olivia,
                          pet: @pet_1)
+
     PetApplicant.create!(applicant: @olivia,
                          pet: @pet_2)
   end
   
   it "shows applicant's attributes" do
     visit "/applicants/#{@olivia.id}"
-    save_and_open_page
-    expect(page).to have_content("Name: #{@olivia.name}")
-    expect(page).to have_content("Address: #{@olivia.address}")
-    expect(page).to have_content("Reasons for Adoption: #{@olivia.reasons_for_adoption}")
-    expect(page).to have_content("Status: #{@olivia.status}")
-    expect(page).to have_content("Currently applying for: #{@pet_1.name}")
-    expect(page).to have_content("Currently applying for: #{@pet_2.name}")
+
+    expect(page).to have_content("#{@olivia.name}")
+    expect(page).to have_content("#{@olivia.street}")
+    expect(page).to have_content("#{@olivia.city}")
+    expect(page).to have_content("#{@olivia.state}")
+    expect(page).to have_content("#{@olivia.good_home}")
+    expect(page).to have_content("#{@olivia.status}")
+    expect(page).to have_content("#{@pet_1.name}")
+    expect(page).to have_content("#{@pet_2.name}")
   end
   
   it "has link to each pet's show page" do
