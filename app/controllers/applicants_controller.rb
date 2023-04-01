a23a901cc8b9f58165b76c3fa0cac5600fb14d6a
Reilly Miller <reillyjmiller31@gmail.com>
2023-03-31 20:23:39 -0600

    feat: form not able to submit unless completed

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -10,12 +10,18 @@ class ApplicantsController < ApplicationController
 
   def create
     applicant = Applicant.new(applicant_params)
-    applicant.save
-    redirect_to "/applicants/#{applicant.id}"
+    # require 'pry'; binding.pry
+     if applicant.save
+      redirect_to "/applicants/#{applicant.id}"
+     else 
+      redirect_to "/applicants/new"
+      flash[:alert] = "Error: #{error_message(applicant.errors)}"
+     end
   end
 
   private
   def applicant_params
+    
     params.permit(:id, :name, :street, :city, :state, :zip, :good_home)  
   end
 end
--- spec/features/applicants/new_spec.rb
+++ spec/features/applicants/new_spec.rb
@@ -29,4 +29,23 @@ RSpec.describe 'the new applicant page' do
       expect(current_path).to eq("/applicants/#{Applicant.last.id}")
     end
   end
+  describe "fail to fill any form fields" do
+    it "If I don't fill out any form field and press submit, I am
+        taken back to 'applicants/new' where I see messages to fill
+        those fields" do
+      visit "/applicants/new"
+
+      fill_in :name, with: "Maura McGonagle"
+      fill_in :street, with: "124 Hill Rd"
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "80210"
+
+      click_on "Submit"
+
+      expect(current_path).to eq("/applicants/new")
+
+      
+    end
+  end
 end
