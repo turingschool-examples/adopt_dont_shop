30e9f3e156ba74957030e3e3ab1bf0f118ccdf98
Thomas Hawley <thawley22@turing.edu>
2023-04-01 12:52:41 -0600

    feat: Add pets_search to the show action

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationsController < ApplicationController
+
   def show
     @application = Application.find(params[:id])
     @pets = @application.pets
+    @pets_search = Pet.where(name: params[:search])
   end
 end
