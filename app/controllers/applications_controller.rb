3bd646172f45e82ab2076d25a85c3e76ac40a67b
Sean Cowans <zedesia@yahoo.com>
2023-04-01 14:01:51 -0500

    feat: Complete user story 3

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -10,12 +10,12 @@ class ApplicationsController < ApplicationController
   def create
     application = Application.new(application_params)
 
-    # if application.save
-    #   redirect_to "/applications/#{application.id}"
-    # else
-    #   redirect_to "/pets/#{application_params[:shelter_id]}/applications/new"
-    #   flash[:alert] = "Error: #{error_message(application.errors)}"
-    # end
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error: #{error_message(application.errors)}"
+    end
   end
   
   def application_params
