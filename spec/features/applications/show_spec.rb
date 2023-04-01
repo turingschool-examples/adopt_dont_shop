f4ae77b5fcfe914ce423fef295ce803371c9ea2b
Crow Rising <miscbyrd@gmail.com>
2023-04-01 12:08:49 -0600

    test: Add start of user story 4 test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -20,4 +20,17 @@ RSpec.describe 'application show page' do
     expect(page).to have_content(@app_1.status)
     end
   end
+
+  describe 'does not have submitted application' do
+    before(:each) do
+      @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+      @app_1 = Application.create!(name: 'JoJo', address: '23 Tanby', city: 'Denver', state: 'CO', zip: '90345', description: 'I have dog food and a roof', status: "In Progress")
+    end
+    it 'display a place to add a pet to application'
+      visit "/applications/#{@app_1.id}"
+
+       expect(page).to have_content ("Add a Pet to this Application")
+    end
+  end
 end
