8b6e4b3bf9276bd142c44e40c7ef8f600fcd884e
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-01 11:05:10 -0600

    Fix: Add actions to controller methods

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,4 +7,17 @@ class ApplicationsController < ApplicationController
   def new
     
   end
+
+  def create
+    new_app = Application.create(app_params)
+    redirect_to "/applications/#{new_app.id}"
+  end
+
+  private
+
+  def app_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :reason, :status)
+  end
 end
+
+
