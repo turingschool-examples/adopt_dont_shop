e933df9b883e96cbdb26cbc6d6a71d793ca59b54
Logan Wilson <loganwilson777@gmail.com>
2023-04-01 13:08:37 -0600

    Test:Add test for application flash message

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -16,6 +16,21 @@ RSpec.describe 'applications/new', type: :feature do
 
       expect(current_path).to eq("/applications/#{Application.last.id}")
     end
+
+    it 'I can see a flash message if I do not fill out all fields' do
+      visit '/applications/new'
+
+      fill_in 'Name', with: 'Taylor'
+      fill_in 'Address', with: '123 Side St'
+      fill_in 'City', with: 'Denver'
+      fill_in 'State', with: 'CO'
+      fill_in 'Zip', with: '80202'
+
+      click_button 'Submit'
+
+      expect(current_path).to eq("/applications/new")
+      expect(page).to have_content("Please fill in all required fields.")
+    end
   end
 
 end
