bb0657ab2437cb69d3d7e84eaff69ef2c6c306ff
Logan Wilson <loganwilson777@gmail.com>
2023-04-01 12:48:28 -0600

    Feat:Add new application page, and adds to applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,4 +3,22 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @pets = @application.pets
   end
+
+  def application_params
+    params.permit(:name, :address, :city, :state, :zip, :description)
+  end
+
+  def new
+    @application = Application.new
+  end
+
+  def create
+    @application = Application.new(application_params)
+    if @application.save
+      redirect_to "/applications/#{@application.id}"
+    else
+      render :new
+    end
+  end
+  
 end
