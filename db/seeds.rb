ffc6664b7a62b2e50ce38d5f01c6a2a0384f6a39
Sean Cowans <zedesia@yahoo.com>
2023-04-02 15:35:12 -0600

    fix: Correct typo in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-PetsApplication.destroy_all
+PetApplication.destroy_all
 Pet.destroy_all
 Application.destroy_all
 Shelter.destroy_all
