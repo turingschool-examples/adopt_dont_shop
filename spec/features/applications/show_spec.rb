63b0b33cb462578a8446329a016536e2ad3d1446
Nick Sacco <nicksacco10@gmail.com>
2023-04-01 17:43:43 -0700

    Modify us1 now complete

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -12,7 +12,20 @@ RSpec.describe 'application show page' do
   let!(:application_3) { Application.create!(name: 'Fred', street_address: '376 Monroe St',
                                             city: 'Los Angeles', state: 'CA', zip_code: 67890,
                                             description: 'Interested in sibling pets', name_of_pet: 'Beethoven', application_status: "In Progress") }
+  
+  let!(:shelter_1) { Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9) }
+  let!(:shelter_2) { Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5) }
+  let!(:shelter_3) { Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10) }
 
+  let!(:pet_1) { Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter: shelter_1) }
+  let!(:pet_2) { Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter: shelter_2) }
+  let!(:pet_3) { Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter: shelter_3) }
+
+  before do
+    PetsApplication.create!(application: application_1, pet: pet_2)
+    PetsApplication.create!(application: application_2, pet: pet_1)
+    PetsApplication.create!(application: application_3, pet: pet_3)
+  end
 
   it 'displays application information' do
     visit "/applications/#{application_1.id}"
@@ -22,20 +35,18 @@ RSpec.describe 'application show page' do
     expect(page).to have_content(application_1.state)
     expect(page).to have_content(application_1.zip_code)
     expect(page).to have_content(application_1.description)
-    expect(page).to have_content(application_1.name_of_pet)
     expect(page).to have_content(application_1.application_status)
   end
 
+  it 'has a link to each pets show page' do
+    visit "/applications/#{application_1.id}"
+
+    click_link("#{pet_2.name}")
+        
+    expect(current_path).to eq("/pets/#{pet_2.id}")
+  end
+
   it 'lists partial matches as search results' do
-    shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-    shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-
-    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter: shelter_1)
-    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter: shelter_2)
-    pet_3 = Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter: shelter_3)
-
-
     visit "/applications/#{application_1.id}"
 
     fill_in(:search_pet, with: 'Lobster')
@@ -48,13 +59,6 @@ RSpec.describe 'application show page' do
   end
     
   it 'can add a pet to an application' do
-    shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-    shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-    
-    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter: shelter_1)
-    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter: shelter_2)
-    pet_3 = Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter: shelter_3)
     
     visit "/applications/#{application_1.id}"
     
@@ -65,7 +69,7 @@ RSpec.describe 'application show page' do
     expect(current_path).to eq("/applications/#{application_1.id}")
     
     click_button('Adopt This Pet')
-    
+
     expect(page).to have_content(pet_2.name)
     expect(current_path).to eq("/applications/#{application_1.id}")
   end
