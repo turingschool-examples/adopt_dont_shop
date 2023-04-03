5bd3d7e885d2c2dcd04fbc0d3141654504a55667
Isaac Thill <ithill22@turing.edu>
2023-04-03 15:13:44 -0600

    complete user story 17

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe 'the shelter show' do
     expect(page).to have_current_path('/pets')
     expect(page).to_not have_content(pet.name)
   end
+
+  it 'when an application is approved for a pet, the pet is no longer adoptable' do
+    shelter = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    pet = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    application = Application.create!(name: 'John Doe', street_address: '123 Main St', city: 'Denver', state: 'CO', zip_code: '80202', description: 'I love dogs', status: 'In Progress')
+    application_pet = ApplicationPet.create!(application_id: application.id, pet_id: pet.id)
+
+    visit "/admin/applications/#{application.id}"
+
+    expect(pet.adoptable).to eq(true)
+
+    click_button "Approve All Pets"
+
+    expect(page).to have_content("Application Status: Approved")
+    expect(pet.reload.adoptable).to eq(false)
+  end
 end
