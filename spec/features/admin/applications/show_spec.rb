5bd152422a01cb6dded24b95f3be10914892ee3b
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-03 13:06:28 -0400

    finish user story 14

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe 'admin application show', type: :feature do
   let!(:pet_1) { shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false) }
   let!(:pet_2) { shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true) }
   let!(:application_1) { Application.create!(name: "John Doe", street_address: "123 Main St", city: "Denver", state: "CO", zip_code: "80202", description: "I love dogs", status: "In Progress") }
+  let!(:application_2) { Application.create!(name: "Billy Bob", street_address: "123 Main St", city: "Denver", state: "CO", zip_code: "80202", description: "I love cats", status: "In Progress") }
   let!(:application_pets_1) { ApplicationPet.create!(application_id: application_1.id, pet_id: pet_1.id) }
+  let!(:application_pets_2) { ApplicationPet.create!(application_id: application_2.id, pet_id: pet_1.id) }
 
   describe 'As a visitor, when I visit an admin applications show page' do
     it 'For every pet that the application is for, I see a button to approve the application for that specific pet' do
@@ -23,6 +25,15 @@ RSpec.describe 'admin application show', type: :feature do
       click_button('Reject Pet')
       expect(current_path).to eq("/admin/applications/#{application_1.id}")
       expect(page).to have_content('Rejected')
+    end
+
+    it 'When I approve an application for the same pet another application show page still has its own status' do
+      visit "/admin/applications/#{application_1.id}"
+      click_button('Approve Pet')
+
+      visit "/admin/applications/#{application_2.id}"
+      expect(page).to have_button('Reject Pet')
+      expect(page).to have_button('Approve Pet')
     end
   end
 end
