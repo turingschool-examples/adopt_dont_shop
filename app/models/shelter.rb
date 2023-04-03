665029fd5fde66cd94ce6cf7b6b336de15984da5
margdaly <margaret.daly@gmail.com>
2023-04-02 21:51:22 -0600

    Add sql query to shelters name reverse

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -17,7 +17,7 @@ class Shelter < ApplicationRecord
   end
 
   def self.order_by_name_reverse
-    # order(name: :desc)
+    find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
   end
 
   def pet_count
