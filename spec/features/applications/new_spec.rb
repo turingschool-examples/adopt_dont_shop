4caaa362c45308146ad97288dd671149ce1dbba0
Logan Cole <emtlogancole@gmail.com>
2023-04-03 15:43:42 -0500

    test: new appliation validation and submission

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Filling out forms" do
   
-  it 'fails to fill out form' do
-    visit '/applications/new'
-    
-    click_button "Create Application"
-    
-    expect(page).to have_current_path('/applications/new')
-    #not sure why this test doesn't work, lol
-  end
+ it 'fills out form' do
+  visit "/applications/new"
+
+  fill_in "name", with: "Jimbo"
+  fill_in "street_address", with: "269 North"
+  fill_in "city", with: "Provo"
+  fill_in "state", with: "UT"
+  fill_in "zip", with: "84606"
+  fill_in "description", with: "pass the test please"
+  
+  click_button "Create Application"
+ end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,5 +4,11 @@ RSpec.describe Application, type: :model do
   describe "relationship" do
     it {should have_many(:application_pets)}
     it {should have_many(:pets).through(:application_pets)}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :street_address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip}
+    it {should validate_presence_of :description}
   end
 end
