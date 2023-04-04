d341f0de5ccf77e81473e89c131bb76851c44232
Thomas Hawley <thawley22@turing.edu>
2023-04-04 16:20:15 -0600

    feat: Add feature to update action to update an application based on the status of pet applications

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -9,11 +9,22 @@ class PetApplicationsController < ApplicationController
 
   def update
     pet_app = PetApplication.find(params[:id])
+    application = Application.find(pet_app.application.id)
     if params[:approved] == "true"
       pet_app.update(approved: true)
     else
       pet_app.update(approved: false)
     end
+    update_application(application)
     redirect_to "/admin/applications/#{pet_app.application.id}"
+  end
+
+  def update_application(application)
+    if application.has_pending_pets?
+    elsif application.has_rejected_pets?
+      application.update(status: 3)
+    else
+      application.update(status: 2)
+    end
   end
 end
