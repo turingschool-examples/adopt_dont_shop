c8ce6fc006409a5efce00fb501210079e193a74f
Thomas Hawley <thawley22@turing.edu>
2023-04-04 16:21:59 -0600

    feat: Add #has_pending_pets? and #has_rejected_pets? methods

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,12 @@ class Application < ApplicationRecord
   has_many :pets, through: :pet_applications
 
   enum status: {"In Progress": 0, "Pending": 1, "Accepted": 2, "Rejected": 3}
+
+  def has_pending_pets?
+    PetApplication.where(approved: nil, application_id: self.id).present?
+  end
+
+  def has_rejected_pets?
+    PetApplication.where(approved: false, application_id: self.id).present?
+  end
 end
